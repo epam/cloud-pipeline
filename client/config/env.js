7aad7149adda811041e8a7c6a53cfa2e8bc60e4c
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2019-04-16 23:07:28 +0300

    UI build improvements: 'VERSION' env variable is now available depending on the build mode (production / development)

--- client/config/env.js
+++ client/config/env.js
@@ -91,8 +91,7 @@ function getClientEnvironment (publicUrl) {
       // This should only be used as an escape hatch. Normally you would put
       // images into the `src` and `import` them in code to get their paths.
       API_PATH: '/restapi',
-      PUBLIC_URL: publicUrl,
-      VERSION: process.env.VERSION
+      PUBLIC_URL: publicUrl
     });
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
