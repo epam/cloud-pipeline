b6bd92b09e26be999414ae75b5a0f4b92d8d63cc
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2019-04-22 19:53:55 +0300

    Debian 8 - expired registry handling

--- scripts/pipeline-launch/launch.sh
+++ scripts/pipeline-launch/launch.sh
@@ -280,7 +280,9 @@ function configure_package_manager {
             echo "deb [check-valid-until=no] http://cdn-fastly.deb.debian.org/debian jessie main" > /etc/apt/sources.list.d/jessie.list
             echo "deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list
             sed -i '/deb http:\/\/deb.debian.org\/debian jessie-updates main/d' /etc/apt/sources.list
-            apt-get -o Acquire::Check-Valid-Until=false update
+            mkdir -p /etc/apt/apt.conf.d/
+            echo "Acquire::Check-Valid-Until false;" > /etc/apt/apt.conf.d/10-nocheckvalid
+            apt-get update
       fi
 }
 
