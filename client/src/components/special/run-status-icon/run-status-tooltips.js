61b2869a24ce9afb06cd02cf7d241a42dcd63964
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2019-04-24 15:35:20 +0300

    Issue #204 : tooltips descriptions added

--- client/src/components/special/run-status-icon/run-status-tooltips.js
+++ client/src/components/special/run-status-icon/run-status-tooltips.js
@@ -19,49 +19,58 @@ import Statuses from './run-statuses';
 
 export default {
   [Statuses.failure]: {
-    description: 'Run was failed',
+    description: 'Run execution failure may occur for a lot of reasons: ' +
+                 'Compute node faile to initialize, environment cannot be prepared, job errored itself, etc. ' +
+                 'Review the logs of the job tasks, which are marked with the red exclamation icon',
     title: <b>Failed</b>
   },
   [Statuses.paused]: {
-    description: 'Run is paused',
-    next: <b>Resuming</b>,
+    description: 'Run is paused. At this moment compute node is already stopped but keeps it\'s state. ' +
+                 'Such a job may be resumed - this will restore the environment and all the changes, that were made before pausing',
     title: <b>Paused</b>
   },
   [Statuses.pausing]: {
-    description: 'Run is pausing',
-    next: <b>Paused</b>,
+    description: 'Run is being paused. At this moment compute node will be stopped (but persisted) and the docker image state will be kept as well. ' +
+                 'Further restart of the paused job will restore the environment and all the changes, that were made before pausing',
     title: <b>Pausing</b>
   },
   [Statuses.pulling]: {
-    description: 'Run is pulling a docker image',
-    next: <b>Running</b>,
+    description: 'Compute node is available and a job is assigned. ' +
+                 'System is now pulling the docker image to setup an environment. ' +
+                 'ETA for this phase varies depending on the image size, but in average it shall take about one minute',
     title: <b>Pulling</b>
   },
   [Statuses.queued]: {
-    description: 'Run is queued',
-    next: <b>Scheduled</b>,
+    description: 'Job is waiting in the queue for the available compute node. ' +
+                 'Typically a job will sit in the queue for a couple of second and proceed to the initialization phase. ' +
+                 'But if this state lasts for a long time - it may mean that a cluster capacity is reached',
     title: <b>Queued</b>
   },
   [Statuses.resuming]: {
-    description: 'Run is resuming',
-    next: <b>Running</b>,
+    description: 'A paused run is being resumed. At this moment compute node is starting back from the stopped state. ' +
+                 'Docker image and the local filesystem will contain all the modification made previously',
     title: <b>Resuming</b>
   },
   [Statuses.running]: {
-    next: <b>Success</b>,
+    description: 'Compute environment is pulled to the compute node. ' +
+                 'The system is preparing the requested assets and runs a job. ' +
+                 'During this stage - input data will be copies to the compute node, data storage mounted, cluster configuration set, etc. ' +
+                 'Such steps can be visible via corresponding tasks logs in the run details page. ' +
+                 'Once all preparations are done - job entrypoint script will be executed',
     title: <b>Running</b>
   },
   [Statuses.scheduled]: {
-    description: 'Run is scheduled',
-    next: <span><b>Pulling</b> a docker image</span>,
-    title: <b>Scheduled</b>
+    description: 'Run is being initialized, at this stage a new compute node will be created ' +
+                 'or an existing node will be reused, if available in the cluster. ' +
+                 'This stage takes 3 min in average to complete',
+    title: <b>Initializing</b>
   },
   [Statuses.stopped]: {
-    description: 'Run was stopped by user',
+    description: 'Run is stopped manually, no errors occured. Somebody requested this job to stop',
     title: <b>Stopped</b>
   },
   [Statuses.success]: {
-    description: 'Run was successfully completed',
+    description: 'Job is completed successfuly, no errors occured',
     title: <b>Success</b>
   }
 };
