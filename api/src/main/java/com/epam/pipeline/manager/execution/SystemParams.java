805c8ed098b5e5a17cfb4031424c34d1daf86f98
mariia_zueva <mariia_zueva@epam.com>
2019-04-30 15:39:33 +0300

    Do not save account creds to pipeline run envvars
    (cherry picked from commit c5ae711)

--- api/src/main/java/com/epam/pipeline/manager/execution/SystemParams.java
+++ api/src/main/java/com/epam/pipeline/manager/execution/SystemParams.java
@@ -16,6 +16,9 @@
 
 package com.epam.pipeline.manager.execution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum SystemParams {
 
     API("api", "API"),
@@ -63,6 +66,13 @@ public enum SystemParams {
     public static final String CLOUD_ACCOUNT_KEY_PREFIX = "CP_ACCOUNT_KEY_";
     public static final String CLOUD_PROVIDER_PREFIX = "CP_CLOUD_PROVIDER_";
     public static final String CLOUD_CREDENTIALS_FILE_CONTENT_PREFIX = "CP_CREDENTIALS_FILE_CONTENT_";
+
+    public static final Set<String> SECURED_PREFIXES = new HashSet<>();
+
+    static {
+        SECURED_PREFIXES.add(CLOUD_ACCOUNT_PREFIX);
+        SECURED_PREFIXES.add(CLOUD_ACCOUNT_KEY_PREFIX);
+    }
 
     private String optionName;
     private String envName;
