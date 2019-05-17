5ea02edf128786e3a08ce53f09ca05d2869b7f55
Yulia_Kamyshova <yulia_kamyshova@epam.com>
2019-05-17 21:02:55 +0300

    UI_tests_fix: improve ui tests from 17.05.19 (#304)
    * UI_Tests: improve ui tests from 17.05.19

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/GlobalSearchTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/GlobalSearchTest.java
@@ -54,7 +54,6 @@ public class GlobalSearchTest extends AbstractBfxPipelineTest implements Navigat
                                 .setValue(DISK, customDisk)
                                 .selectValue(INSTANCE_TYPE, defaultInstanceType)
                                 .setValue(NAME, customConfigurationProfile)
-                                .click(SAVE)
                                 .selectDockerImage(dockerImage ->
                                         dockerImage
                                                 .selectRegistry(defaultRegistry)
@@ -110,7 +109,7 @@ public class GlobalSearchTest extends AbstractBfxPipelineTest implements Navigat
                 .showIssues()
                 .clickNewIssue()
                 .addNewIssue(title, description);
-        home().sleep(5, MINUTES);
+        home().sleep(6, MINUTES);
         search()
                 .search(title)
                 .enter()
