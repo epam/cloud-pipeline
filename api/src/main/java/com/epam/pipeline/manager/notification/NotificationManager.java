5a9e87cb48a7458f8fc49d51f5e1f9aa51225173
mariia_zueva <mariia_zueva@epam.com>
2019-05-21 20:13:48 +0300

    PMD fix

--- api/src/main/java/com/epam/pipeline/manager/notification/NotificationManager.java
+++ api/src/main/java/com/epam/pipeline/manager/notification/NotificationManager.java
@@ -21,7 +21,6 @@ import static com.epam.pipeline.entity.notification.NotificationSettings.Notific
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
