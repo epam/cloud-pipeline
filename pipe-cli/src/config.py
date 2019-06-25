f4dda840ee56f195145fb9ffbc1a1947e7ca2c54
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2019-06-25 18:36:08 +0300

    Pipe command line shall warn user on the JWT expiration - done (fix for #436)

--- pipe-cli/src/config.py
+++ pipe-cli/src/config.py
@@ -12,10 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import click
+import base64
+from functools import update_wrapper
 import json
 import os
 import pytz
 import tzlocal
+from datetime import datetime, timedelta
 from pypac import api as PacAPI
 from pypac.resolver import ProxyResolver as PacProxyResolver
 
@@ -23,11 +27,108 @@ from .utilities import time_zone_param_type
 
 PROXY_TYPE_PAC = "pac"
 PROXY_PAC_DEFAULT_URL = "https://google.com"
+ALL_ERRORS = Exception
+
 
 class ConfigNotFoundError(Exception):
     def __init__(self):
         super(ConfigNotFoundError, self).__init__('Unable to locate configuration or it is incomplete. '
                                                   'You can configure pipe by running "pipe configure"')
+
+
+def check_token(token, timezone, print_info=False):
+    if not token:
+        click.echo(click.style('No access token is provided', fg='red'), err=True)
+        return
+    try:
+        (header_decoded, payload_decoded, signature) = token.split('.')
+        # payload_decoded_correct_padding - is the same string with additional '=' symbols to make string length
+        # be dividable by 4:
+        payload_decoded_correct_padding = payload_decoded + b'=' * (4 - len(payload_decoded) % 4)
+        payload = json.loads(base64.b64decode(payload_decoded_correct_padding))
+        subject = None
+        issued_at = None
+        not_before = None
+        expiration = None
+        tz = tzlocal.get_localzone()
+        if timezone == 'utc':
+            tz = pytz.utc
+        if 'sub' in payload:
+            subject = payload['sub']
+        if 'iat' in payload:
+            issued_at = datetime.utcfromtimestamp(payload['iat'])
+        if 'nbf' in payload:
+            not_before = datetime.utcfromtimestamp(payload['nbf'])
+        if 'exp' in payload:
+            expiration = datetime.utcfromtimestamp(payload['exp'])
+        now = datetime.utcnow()
+
+        def print_date_time(naive_time):
+            return pytz.utc.localize(naive_time, is_dst=None).astimezone(tz).strftime('%Y-%m-%d %H:%M')
+
+        if print_info:
+            click.echo('Access token info:')
+            if subject is not None:
+                click.echo('Issued to: {}'.format(subject))
+            if issued_at is not None:
+                click.echo('Issued at: {}'.format(print_date_time(issued_at)))
+            if not_before is not None:
+                click.echo('Valid not before: {}'.format(print_date_time(not_before)))
+            if expiration is not None:
+                click.echo('Expires at: {}'.format(print_date_time(expiration)))
+        if not_before is not None and not_before > now:
+            click.echo(
+                click.style(
+                    'Access token is not valid yet: not before {}'.format(print_date_time(not_before)),
+                    fg='red'
+                ),
+                err=True
+            )
+        if expiration is not None:
+            last_week = expiration - timedelta(days=7)
+            if expiration < now:
+                click.echo(
+                    click.style(
+                        'Access token is expired: {}'.format(print_date_time(expiration)),
+                        fg='red'
+                    ),
+                    err=True
+                )
+            elif last_week < now:
+                delta = expiration - now
+                days = delta.days
+                hours = delta.seconds // 60
+                minutes = (delta.seconds // 3600) // 60
+
+                def plural(count, word):
+                    return '{} {}{}'.format(count, word, 's' if count != 1 else '')
+                expiration_period = plural(minutes, 'minute')
+                if days > 0:
+                    expiration_period = plural(days, 'day')
+                elif hours > 0:
+                    expiration_period = plural(hours, 'hour')
+                click.echo(
+                    click.style(
+                        'Access token will expire in {}: {}'.format(
+                            expiration_period,
+                            print_date_time(expiration)
+                        ),
+                        fg='yellow'
+                    ),
+                    err=True
+                )
+    except ALL_ERRORS as parse_error:
+        click.echo(click.style('Access token parse error: {}'.format(str(parse_error)), fg='red'), err=True)
+
+
+def silent_print_config_info():
+    try:
+        config = Config.instance()
+        if config is not None:
+            click.echo()
+            config.validate(print_info=True)
+    except Exception:
+        pass
 
 
 class Config(object):
@@ -56,6 +157,29 @@ class Config(object):
         else:
             raise ConfigNotFoundError()
 
+    def validate(self, print_info=False):
+        check_token(self.access_key, self.tz, print_info=print_info)
+
+    @classmethod
+    def validate_access_token(cls, _func=None, quiet_flag_property_name=None):
+        def decorator(f):
+            @click.pass_context
+            def validate_access_token_wrapper(ctx, *args, **kwargs):
+                skip_validation = False
+                if quiet_flag_property_name is not None and quiet_flag_property_name in ctx.params:
+                    skip_validation = bool(ctx.params[quiet_flag_property_name])
+                if not skip_validation:
+                    try:
+                        Config.instance().validate()
+                    except Exception:
+                        pass
+                return ctx.invoke(f, *args, **kwargs)
+            return update_wrapper(validate_access_token_wrapper, f)
+        if _func is None:
+            return decorator
+        else:
+            return decorator(_func)
+
     def resolve_proxy(self, target_url=None):
         if not self.proxy:
             return None
@@ -64,7 +188,7 @@ class Config(object):
             if not pac_file:
                 return None
             proxy_resolver = PacProxyResolver(pac_file)
-            
+
             url_to_resolve = target_url
             if not url_to_resolve and self.api:
                 url_to_resolve = self.api
@@ -79,6 +203,7 @@ class Config(object):
 
     @classmethod
     def store(cls, access_key, api, timezone, proxy):
+        check_token(access_key, timezone)
         config = {'api': api, 'access_key': access_key, 'tz': timezone, 'proxy': proxy}
         config_file = cls.config_path()
 
