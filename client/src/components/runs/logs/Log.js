1adb4ec4d2a2e2e126d6a283f02cd18b92177572
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2019-07-01 19:02:18 +0300

    GUI Run log form: scroll styles fixed for task lists; log requests auto-update fixed

--- client/src/components/runs/logs/Log.js
+++ client/src/components/runs/logs/Log.js
@@ -742,7 +742,7 @@ class Logs extends localization.LocalizedReactComponent {
             backgroundClip: 'padding',
             zIndex: 1
           }}>
-          <div style={{display: 'flex', flex: 1, height: '100%'}}>
+          <div style={{display: 'flex', flex: 1, height: '100%', overflowY: 'auto'}}>
             <Menu
               selectedKeys={selectedTask ? [selectedTask] : []}
               mode="inline"
