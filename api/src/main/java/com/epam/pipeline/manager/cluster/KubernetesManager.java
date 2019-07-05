4c38eff7661c8260ff81a0d2918b244c546cb3e7
Pavel Silin <pavel_silin@epam.com>
2019-07-05 16:31:19 +0300

    Issue 468 load pod logs partially (#480)
    * wip, add number of bytes for pod logs as system parameter
    
    * tests
    
    * new System Pref type: MemorySizePreference
    
    * refactor and style fixes
    
    * load only tail of the log
    
    * Issue #468. Use truncate sign if log is too big to be fully loaded
    
    * Issue #468. correction on review

--- api/src/main/java/com/epam/pipeline/manager/cluster/KubernetesManager.java
+++ api/src/main/java/com/epam/pipeline/manager/cluster/KubernetesManager.java
@@ -75,6 +75,7 @@ public class KubernetesManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesManager.class);
     private static final String DEFAULT_SVC_SCHEME = "http";
     private static final int NODE_PULL_TIMEOUT = 200;
+    private static final String NEW_LINE = "\n";
 
     private ObjectMapper mapper = new JsonMapper();
 
@@ -177,13 +178,22 @@ public class KubernetesManager {
         }
     }
 
-    public String getPodLogs(String podId) {
-        try (KubernetesClient client = getKubernetesClient()) {
-            return client.pods().inNamespace(kubeNamespace).withName(podId).getLog();
+    public String getPodLogs(final String podId, final int limit) {
+        try (final KubernetesClient client = getKubernetesClient()) {
+            final String tail = client.pods().inNamespace(kubeNamespace)
+                    .withName(podId)
+                    .tailingLines(limit + 1).getLog();
+            return isLogTruncated(tail, limit)
+                    ? messageHelper.getMessage(MessageConstants.LOG_WAS_TRUNCATED) + NEW_LINE + tail
+                    : tail;
         } catch (KubernetesClientException e) {
             LOGGER.error(e.getMessage(), e);
             return null;
         }
+    }
+
+    private boolean isLogTruncated(final String tail, final int limit) {
+        return tail.split(NEW_LINE).length > limit;
     }
 
     /**
