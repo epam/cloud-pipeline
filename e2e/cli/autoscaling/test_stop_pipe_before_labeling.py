2f7080b782377901b041219213e21269f995ac4b
ekaterina_kazachkova <ekaterina_kazachkova@epam.com>
2019-08-07 19:52:51 +0300

    increment waiting time for getting node state

--- e2e/cli/autoscaling/test_stop_pipe_before_labeling.py
+++ e2e/cli/autoscaling/test_stop_pipe_before_labeling.py
@@ -50,7 +50,7 @@ class TestStopPipelineBeforeLabeling(object):
             instance = wait_for_instance_creation(run_id, MAX_REP_COUNT)
             private_ip = get_private_ip(instance)
             logging.info("Started to find node {}.".format(private_ip))
-            node_state = wait_for_node_up_without_id(MAX_REP_COUNT, private_ip)
+            node_state = wait_for_node_up_without_id(MAX_REP_COUNT * 5, private_ip)
             cls.node_name = get_node_name_from_cluster_state(node_state)
             logging.info("Node {} in use.".format(private_ip))
             stop_pipe(run_id)
