c4aab00b2352fb16aeeeb50923b174009e20e83a
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-13 16:16:35 +0300

    simple-web-application upgraded. sub-task 2757-2759 completed

--- tes-adapter/src/main/java/com/epam/pipeline/adaptor/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/adaptor/TesAdapterApplication.java
@@ -1,4 +1,11 @@
 package com.epam.pipeline.adaptor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TesAdapterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TesAdapterApplication.class, args);
+    }
 }
--- tes-adapter/src/main/java/com/epam/pipeline/adaptor/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/adaptor/controller/TesAdapterController.java
@@ -1,4 +1,13 @@
 package com.epam.pipeline.adaptor.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TesAdapterController {
+
+    @GetMapping("/v1/tasks/service-info")
+    public String getSimpleResponse(){
+        return "OK";
+    }
 }
