d8be81093cd54ea9e41b99ef07016f57585fc32e
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-15 19:15:15 +0300

    simple swagger implementation fixed. returned @GetMapping annotation. Configuring swagger paths to controller package and also tasks

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -2,13 +2,12 @@ package com.epam.pipeline.tesadapter.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesAdapterController {
-    @RequestMapping(method = RequestMethod.GET, value = "/v1/tasks/service-info")
+    @GetMapping("/v1/tasks/service-info")
     public ResponseEntity<String> serviceInfo() {
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
