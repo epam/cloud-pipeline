98497a8f92f6bf017aa99da303093562519340f4
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-15 14:03:30 +0300

    simple-web-application upgraded. fixed last pull-request mistakes, added WebConfiguration class and simple ResponseEntity<> implementation

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
@@ -1,9 +1,12 @@
-package com.epam.pipeline.adaptor;
+package com.epam.pipeline.tesadapter;
 
+import com.epam.pipeline.tesadapter.configuration.TesAdapterWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({TesAdapterWebConfiguration.class})
 public class TesAdapterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TesAdapterApplication.class, args);
