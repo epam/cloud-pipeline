1e280d76441d5671dd14257428894e9adb03dc0b
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-16 19:45:05 +0300

    simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -1,5 +1,7 @@
 package com.epam.pipeline.tesadapter.controller;
 
+import com.epam.pipeline.tesadapter.service.TesTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesAdapterController {
+    @Autowired
+    private TesTaskService tesTaskService;
+
     @GetMapping("/v1/tasks/service-info")
     public ResponseEntity<String> serviceInfo() {
         return new ResponseEntity<>("OK", HttpStatus.OK);
