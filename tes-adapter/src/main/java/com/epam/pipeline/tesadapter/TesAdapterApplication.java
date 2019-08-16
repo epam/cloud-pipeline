2ac73ebb365143c4f146c8e4084ea7a051788a41
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-16 19:45:05 +0300

    simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
@@ -3,10 +3,12 @@ package com.epam.pipeline.tesadapter;
 import com.epam.pipeline.tesadapter.configuration.TesSwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({TesSwaggerConfig.class})
+@ComponentScan({"com.epam.pipeline.tesadapter.controller", "com.epam.pipeline.tesadapter.service"})
 public class TesAdapterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TesAdapterApplication.class, args);
