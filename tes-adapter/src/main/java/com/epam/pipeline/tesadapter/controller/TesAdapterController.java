4fa52f5c667f0b8f546ba410332f6ab2ab6706ef
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-21 15:56:17 +0300

    update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,14 +39,15 @@ public class TesAdapterController {
 
     @GetMapping("/v1/tasks/{id}")
     @ResponseBody
-    ResponseEntity<TesTask> getTesTask(@PathVariable String id) {
+    ResponseEntity<TesTask> getTesTask(@RequestParam String id, @RequestParam(required = false,
+            defaultValue = "MINIMAL") String view) {
         tesTaskService.stub();
         return new ResponseEntity<TesTask>(new TesTask(), HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping("/v1/tasks/{id}:cancel")
     @ResponseBody
-    ResponseEntity<TesCancelTaskResponse> cancelTesTask(@PathVariable String id) {
+    ResponseEntity<TesCancelTaskResponse> cancelTesTask(@RequestParam String id) {
         tesTaskService.stub();
         return new ResponseEntity<TesCancelTaskResponse>(new TesCancelTaskResponse(), HttpStatus.NOT_IMPLEMENTED);
     }
