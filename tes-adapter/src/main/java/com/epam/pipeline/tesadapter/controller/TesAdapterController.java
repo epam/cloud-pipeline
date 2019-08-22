f2757235cee91bcd3def208471c6cd030be905c2
Pavel Silin <pavel_silin@epam.com>
2019-08-22 17:56:04 +0300

    Added submit/get/cancel-task methods (#9)
    * simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan
    
    * implemented task entities with swagger-codegen and task_execution.swagger.json
    
    * added stubbed methods submit/cancel/get-task. Reformatting entities imports. added TES-API interface.
    
    * update TesAdapterController. changed import wildcard "*" to single-class imports. Changed settings to not use wildcard import.
    
    * update TesAdapterController. removed optional value for @PathVariable
    
    * update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -1,13 +1,19 @@
 package com.epam.pipeline.tesadapter.controller;
 
 import com.epam.pipeline.tesadapter.entity.TaskView;
+import com.epam.pipeline.tesadapter.entity.TesCancelTaskResponse;
+import com.epam.pipeline.tesadapter.entity.TesCreateTaskResponse;
 import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
+import com.epam.pipeline.tesadapter.entity.TesTask;
 import com.epam.pipeline.tesadapter.service.TesTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,5 +39,27 @@ public class TesAdapterController {
             @RequestParam(name = "page_token", required = false) String pageToken,
             @RequestParam(name = "view", required = false, defaultValue = "MINIMAL") TaskView view) {
         return ResponseEntity.status(HttpStatus.OK).body(tesTaskService.listTesTask());
+    }
+
+    @PostMapping("/v1/tasks")
+    @ResponseBody
+    ResponseEntity<TesCreateTaskResponse> submitTesTask(@RequestBody TesTask body) {
+        tesTaskService.stub();
+        return new ResponseEntity<TesCreateTaskResponse>(new TesCreateTaskResponse(), HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/v1/tasks/{id}")
+    @ResponseBody
+    ResponseEntity<TesTask> getTesTask(@RequestParam String id, @RequestParam(required = false,
+            defaultValue = "MINIMAL") TaskView view) {
+        tesTaskService.stub();
+        return new ResponseEntity<TesTask>(new TesTask(), HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PostMapping("/v1/tasks/{id}:cancel")
+    @ResponseBody
+    ResponseEntity<TesCancelTaskResponse> cancelTesTask(@RequestParam String id) {
+        tesTaskService.stub();
+        return new ResponseEntity<TesCancelTaskResponse>(new TesCancelTaskResponse(), HttpStatus.NOT_IMPLEMENTED);
     }
 }
