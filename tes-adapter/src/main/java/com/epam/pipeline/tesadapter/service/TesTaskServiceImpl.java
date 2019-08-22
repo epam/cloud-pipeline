533cfe77bcdbdd0d85d44f60b20ba5d348dd9248
Pavel Silin <pavel_silin@epam.com>
2019-08-22 20:46:37 +0300

    add cloud-pipeline-common module in dependencies (#16)
    implemented CloudPipelineAPIClient

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskServiceImpl.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskServiceImpl.java
@@ -1,10 +1,18 @@
 package com.epam.pipeline.tesadapter.service;
 
 import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TesTaskServiceImpl implements TesTaskService {
+    private final CloudPipelineAPIClient cloudPipelineAPIClient;
+
+    @Autowired
+    public TesTaskServiceImpl(CloudPipelineAPIClient cloudPipelineAPIClient) {
+        this.cloudPipelineAPIClient = cloudPipelineAPIClient;
+    }
+
     @Override
     public TesListTasksResponse listTesTask() {
         return new TesListTasksResponse();
