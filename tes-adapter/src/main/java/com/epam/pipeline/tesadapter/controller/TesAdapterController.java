85bbe18b94ff85c24b587a7c0b54ace897137255
Evgenii <48137680+evgeniimv@users.noreply.github.com>
2019-08-22 17:54:53 +0300

    TES exception handler (#14)
    * simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan
    
    * implemented task entities with swagger-codegen and task_execution.swagger.json
    
    * added stubbed methods submit/cancel/get-task. Reformatting entities imports. added TES-API interface.
    
    * update TesAdapterController. changed import wildcard "*" to single-class imports. Changed settings to not use wildcard import.
    
    * update TesAdapterController. removed optional value for @PathVariable
    
    * update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.
    
    * Added TesExceptionHandler. injected Slf4j logger, HttpStatus stubbed as INTERNAL_SERVER_ERROR.
    
    * add EPMCMBIBPC-2764 Task (#13)
    
    * add EPMCMBIBPC-2764 Task
    
    * add RequestParam into listTesTasks method
    
    * add taskView Enum and refactoring TesAdapterControll
    
    * refactoring TesAdapterControll
    
    * PMD & checkstyle fix
    
    * Merging with current state of tes-support remote branch. Add needed changes to Controller.

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -1,7 +1,7 @@
 package com.epam.pipeline.tesadapter.controller;
 
-import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
 import com.epam.pipeline.tesadapter.entity.TaskView;
+import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
 import com.epam.pipeline.tesadapter.service.TesTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
