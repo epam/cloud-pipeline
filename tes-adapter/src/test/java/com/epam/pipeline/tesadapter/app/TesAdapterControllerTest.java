bae7135aff1f89f71f74ae41a180135479133796
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-23 18:43:27 +0300

    updated test implementation. Not finished

---
+++ tes-adapter/src/test/java/com/epam/pipeline/tesadapter/app/TesAdapterControllerTest.java
@@ -0,0 +1,35 @@
+package com.epam.pipeline.tesadapter.app;
+
+
+import com.epam.pipeline.tesadapter.controller.TesAdapterController;
+import com.epam.pipeline.tesadapter.service.TesTaskServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TesAdapterController.class)
+public class TesAdapterControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TesTaskServiceImpl tesTaskService;
+
+
+    @Test
+    public void cancelTesTaskWhenRequestingIdReturnCanceledTask() throws Exception {
+        when(tesTaskService.cancelTesTask("5")).thenReturn(null);
+        this.mockMvc.perform(get("/v1/tasks/{id}:cancel")).andDo(print());
+    }
+
+}
