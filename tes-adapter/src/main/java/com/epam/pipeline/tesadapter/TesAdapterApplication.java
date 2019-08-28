74b32c32893a4fc5dd00c2ab53c1a38b0c4ef9ee
Pavel Silin <pavel_silin@epam.com>
2019-08-28 14:07:44 +0300

    f tes mapping cancel (#17)
    * simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan
    
    * implemented task entities with swagger-codegen and task_execution.swagger.json
    
    * added stubbed methods submit/cancel/get-task. Reformatting entities imports. added TES-API interface.
    
    * update TesAdapterController. changed import wildcard "*" to single-class imports. Changed settings to not use wildcard import.
    
    * update TesAdapterController. removed optional value for @PathVariable
    
    * update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.
    
    * added stubbed implementation for cancel-id mapping
    
    * finished mapping for cancel-Id method.
    
    * finished mapping for cancel-Id method. Changed mathod-name "checkRunId" to "parseRunId"
    
    * Added $API and $API_TOKEN to application.properties. Changed application config in build.gradle and in main TesAdapterApplication class.
    Reformatting cancel-Id method.
    
    * refactored parseRunId() method in TesTaskServiceImpl. Removed "null" as a possible response(swapped to IllegalArgumentException)
    
    * removed local variable in parseRunId method in TesTaskServiceImpl class.
    
    * Update TesAdapterController.java
    
    Change `@RequestParam` to `@PathVariable`
    
    * Update TesAdapterController.java

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
@@ -1,7 +1,11 @@
 package com.epam.pipeline.tesadapter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -10,6 +14,9 @@ import org.springframework.context.annotation.ComponentScan;
         "com.epam.pipeline.tesadapter.controller",
         "com.epam.pipeline.tesadapter.service"})
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 public class TesAdapterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TesAdapterApplication.class, args);
