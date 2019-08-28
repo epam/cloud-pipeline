564b05fae4bbc99ab30b57a8e971bcbfaebf2e39
Evgenii <48137680+evgeniimv@users.noreply.github.com>
2019-08-28 14:07:44 +0300

    f tes mapping cancel (#17)
    * simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan
    
    * implemented task entities with swagger-codegen and task_execution.swagger.json
    
    * added stubbed methods submit/cancel/get-task. Reformatting entities imports. added TES-API interface.
    
    * update TesAdapterController. changed import wildcard "*" to single-class imports. Changed settings to not use wildcard import.
    
    * update TesAdapterController. removed optional value for @PathVariable
    
    * update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.
    
    * added stubbed implementation for cancel-id mapping
    
    * finished mapping for cancel-Id method.
    
    * finished mapping for cancel-Id method. Changed mathod-name "checkRunId" to "parseRunId"
    
    * Added $API and $API_TOKEN to application.properties. Changed application config in build.gradle and in main TesAdapterApplication class.
    Reformatting cancel-Id method.
    
    * refactored parseRunId() method in TesTaskServiceImpl. Removed "null" as a possible response(swapped to IllegalArgumentException)
    
    * removed local variable in parseRunId method in TesTaskServiceImpl class.
    
    * Update TesAdapterController.java
    
    Change `@RequestParam` to `@PathVariable`
    
    * Update TesAdapterController.java

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/CloudPipelineAPIClient.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/CloudPipelineAPIClient.java
@@ -4,6 +4,7 @@ import com.epam.pipeline.client.pipeline.CloudPipelineAPI;
 import com.epam.pipeline.client.pipeline.CloudPipelineApiBuilder;
 import com.epam.pipeline.entity.pipeline.PipelineRun;
 import com.epam.pipeline.utils.QueryUtils;
+import com.epam.pipeline.vo.RunStatusVO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,10 @@ public class CloudPipelineAPIClient {
 
     public PipelineRun loadPipelineRun(final Long pipelineRunId) {
         return QueryUtils.execute(cloudPipelineAPI.loadPipelineRun(pipelineRunId));
+    }
+
+    public PipelineRun updateRunStatus(final Long pipelineRunId, RunStatusVO statusUpdate) {
+        return QueryUtils.execute(cloudPipelineAPI.updateRunStatus(pipelineRunId, statusUpdate));
     }
 
 
--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskService.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskService.java
@@ -1,9 +1,12 @@
 package com.epam.pipeline.tesadapter.service;
 
+import com.epam.pipeline.tesadapter.entity.TesCancelTaskResponse;
 import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
 
 public interface TesTaskService {
 
     TesListTasksResponse listTesTask();
     void stub();
+
+    TesCancelTaskResponse cancelTesTask(String id);
 }
