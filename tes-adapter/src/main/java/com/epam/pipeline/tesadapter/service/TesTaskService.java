17e9238b11c1121b2eb8f67c9f0cdaed56ef5399
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-29 17:19:41 +0300

    first implementation of submitTesTask() method. fully stubbed

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskService.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TesTaskService.java
@@ -2,10 +2,15 @@ package com.epam.pipeline.tesadapter.service;
 
 import com.epam.pipeline.tesadapter.entity.TesCancelTaskResponse;
 import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
+import com.epam.pipeline.tesadapter.entity.TesTask;
+import com.epam.pipeline.tesadapter.entity.TesCreateTaskResponse;
 
 public interface TesTaskService {
 
+    TesCreateTaskResponse submitTesTask(TesTask body);
+
     TesListTasksResponse listTesTask();
+
     void stub();
 
     TesCancelTaskResponse cancelTesTask(String id);
