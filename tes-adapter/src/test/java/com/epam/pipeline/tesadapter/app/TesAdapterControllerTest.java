c970209a5f8ed51a24359f739447b5fde3a2066b
Pavel Silin <pavel_silin@epam.com>
2019-08-30 15:10:35 +0300

    simple integration test for WebMvc (#19)
    * simple test tasks service implemented. injected to TesAdapterController and also in ComponentScan
    
    * implemented task entities with swagger-codegen and task_execution.swagger.json
    
    * added stubbed methods submit/cancel/get-task. Reformatting entities imports. added TES-API interface.
    
    * update TesAdapterController. changed import wildcard "*" to single-class imports. Changed settings to not use wildcard import.
    
    * update TesAdapterController. removed optional value for @PathVariable
    
    * update TesAdapterController. changed annotation @PathVariable to @RequestParam. Add appropriate modifications to the last one.
    
    * added stubbed implementation for cancel-id mapping
    
    * finished mapping for cancel-Id method.
    
    * finished mapping for cancel-Id method. Changed mathod-name "checkRunId" to "parseRunId"
    
    * updated test implementation. Not finished
    
    * Added $API and $API_TOKEN to application.properties. Changed application config in build.gradle and in main TesAdapterApplication class.
    Reformatting cancel-Id method.
    
    * refactored build.gradle file. added needed dependencies for junit5 and integration tests.
    
    * added simple webMvcTest for cancel-Id method. fixed same method in Controller class, changed annotation @RequestParam to @PathVariable
    
    * added webMvcTest for listTesTask method as a part of integration test for TesAdapterController
    
    * moved "magic-numbers" to appropriate constants.
    
    * changed constants view to upper-case

---
+++ tes-adapter/src/test/java/com/epam/pipeline/tesadapter/app/TesAdapterControllerTest.java
@@ -0,0 +1,52 @@
+package com.epam.pipeline.tesadapter.app;
+
+
+import com.epam.pipeline.tesadapter.controller.TesAdapterController;
+import com.epam.pipeline.tesadapter.entity.TesCancelTaskResponse;
+import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
+import com.epam.pipeline.tesadapter.service.CloudPipelineAPIClient;
+import com.epam.pipeline.tesadapter.service.TesTaskServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TesAdapterController.class)
+public class TesAdapterControllerTest {
+    private static final String STUBBED_TASK_ID = "5";
+    private static final Long STUBBED_PAGE_SIZE = 55L;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TesTaskServiceImpl tesTaskService;
+
+    @MockBean
+    private CloudPipelineAPIClient cloudPipelineAPIClient;
+
+    @Test
+    public void cancelTesTaskWhenRequestingIdReturnCanceledTask() throws Exception {
+        when(tesTaskService.cancelTesTask(STUBBED_TASK_ID)).thenReturn(new TesCancelTaskResponse());
+        this.mockMvc.perform(post("/v1/tasks/{id}:cancel", STUBBED_TASK_ID)).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listTesTaskWhenRequestingReturnTesListTasksResponse() throws Exception {
+        when(tesTaskService.listTesTask()).thenReturn(new TesListTasksResponse());
+        this.mockMvc.perform(get("/v1/tasks?page_size={size}", STUBBED_PAGE_SIZE)).andDo(print())
+                .andExpect(status().isOk());
+    }
+}
