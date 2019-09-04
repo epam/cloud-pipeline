a3f488e9b187be4d1da4a5884d121b3cfb207e8c
Ilya Ugryumov <ilya.ugryumov@mail.ru>
2019-09-04 20:55:27 +0300

    i merged branch tes-support to f_2803. and did refactoring parseStringToLong with MessageHelper

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/controller/TesAdapterController.java
@@ -1,14 +1,18 @@
 package com.epam.pipeline.tesadapter.controller;
 
+import com.epam.pipeline.tesadapter.common.MessageConstants;
+import com.epam.pipeline.tesadapter.common.MessageHelper;
 import com.epam.pipeline.tesadapter.entity.TaskView;
 import com.epam.pipeline.tesadapter.entity.TesCancelTaskResponse;
 import com.epam.pipeline.tesadapter.entity.TesCreateTaskResponse;
 import com.epam.pipeline.tesadapter.entity.TesListTasksResponse;
 import com.epam.pipeline.tesadapter.entity.TesTask;
 import com.epam.pipeline.tesadapter.service.TesTaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +20,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TesAdapterController {
     private TesTaskService tesTaskService;
+    private MessageHelper messageHelper;
 
     @Autowired
-    public TesAdapterController(TesTaskService tesTaskService) {
+    public TesAdapterController(TesTaskService tesTaskService, MessageHelper messageHelper) {
+        this.messageHelper = messageHelper;
         this.tesTaskService = tesTaskService;
     }
 
@@ -58,9 +65,12 @@ public class TesAdapterController {
     }
 
     private Long parseStringIdToLongId(String id){
+        Assert.hasText(id, messageHelper.getMessage(MessageConstants.ERROR_PARAMETER_NON_SCALAR_TYPE));
         try {
             return Long.parseLong(id);
         } catch (NumberFormatException e) {
+            log.error(messageHelper.getMessage(MessageConstants.ERROR_PARAMETER_INCOMPATIBLE_CONTENT,
+                    "ID", id));
             throw new IllegalArgumentException(e);
         }
     }
