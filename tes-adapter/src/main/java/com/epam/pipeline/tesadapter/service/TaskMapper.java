e04ea9bb9dfe9f1b21b9e22c4c3e141df3c09076
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-09-05 16:56:56 +0300

    updated TaskMapper, refactored, optimized ramGb + vCPU mapper implementation

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TaskMapper.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/service/TaskMapper.java
@@ -1,8 +1,10 @@
 package com.epam.pipeline.tesadapter.service;
 
 import com.epam.pipeline.entity.cluster.AllowedInstanceAndPriceTypes;
+import com.epam.pipeline.entity.cluster.InstanceType;
 import com.epam.pipeline.entity.configuration.ExecutionEnvironment;
 import com.epam.pipeline.entity.configuration.PipeConfValueVO;
+import com.epam.pipeline.entity.pipeline.Tool;
 import com.epam.pipeline.entity.pipeline.run.PipelineStart;
 import com.epam.pipeline.tesadapter.common.MessageConstants;
 import com.epam.pipeline.tesadapter.common.MessageHelper;
@@ -16,18 +18,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class TaskMapper {
     private final Integer defaultHddSize;
-    private final Long defaultToolId;
-    private final Long defaultRegionId;
     private MessageHelper messageHelper;
     private final CloudPipelineAPIClient cloudPipelineAPIClient;
 
@@ -35,31 +35,36 @@ public class TaskMapper {
     private static final String INPUT_TYPE = "input";
     private static final String OUTPUT_TYPE = "output";
     private static final String DEFAULT_TYPE = "string";
+    private static final String IMAGE = "image";
+    private static final String EXECUTORS = "executors";
+    private static final String ZONES = "zones";
     private static final Integer FIRST = 0;
+    private static final Integer ONLY_ONE = 1;
+    private static final Double GiB_TO_GiB = 1.0;
+    private static final Double MiB_TO_GiB = 0.0009765625;
+
 
     @Autowired
     public TaskMapper(@Value("${cloud.pipeline.hddSize}") Integer hddSize,
-                      @Value("${cloud.pipeline.instanceType.toolId}") Long defaultToolId,
-                      @Value("${cloud.pipeline.instanceType.regionId}") Long defaultRegionId,
                       CloudPipelineAPIClient cloudPipelineAPIClient, MessageHelper messageHelper) {
         this.defaultHddSize = hddSize;
-        this.defaultToolId = defaultToolId;
-        this.defaultRegionId = defaultRegionId;
         this.cloudPipelineAPIClient = cloudPipelineAPIClient;
         this.messageHelper = messageHelper;
-
     }
 
     public PipelineStart mapToPipelineStart(TesTask tesTask) {
         PipelineStart pipelineStart = new PipelineStart();
         Map<String, PipeConfValueVO> params = new HashMap<>();
         TesExecutor tesExecutor = getExecutorFromTesExecutorsList(tesTask.getExecutors());
+        Assert.notNull(tesExecutor.getImage(), messageHelper.getMessage(
+                MessageConstants.ERROR_PARAMETER_NULL_OR_EMPTY, IMAGE));
+        Tool pipelineTool = loadToolByTesImage(tesExecutor.getImage());
 
+        pipelineStart.setInstanceType(getProperInstanceType(tesTask, pipelineTool));
         pipelineStart.setCmdTemplate(String.join(SEPARATOR, tesExecutor.getCommand()));
         pipelineStart.setDockerImage(tesExecutor.getImage());
         pipelineStart.setExecutionEnvironment(ExecutionEnvironment.CLOUD_PLATFORM);
         pipelineStart.setHddSize(defaultHddSize);
-        pipelineStart.setInstanceType(getProperInstanceType(tesTask));
         pipelineStart.setIsSpot(tesTask.getResources().getPreemptible());
         pipelineStart.setForce(false);
         pipelineStart.setNonPause(true);
@@ -74,46 +79,53 @@ public class TaskMapper {
     }
 
     private TesExecutor getExecutorFromTesExecutorsList(List<TesExecutor> tesExecutors) {
-        Assert.notEmpty(tesExecutors, messageHelper.getMessage(MessageConstants.ERROR_PARAMETER_NULL_OR_EMPTY,
-                "executors"));
+        Assert.isTrue(tesExecutors.size() == ONLY_ONE, messageHelper.getMessage(
+                MessageConstants.ERROR_PARAMETER_INCOMPATIBLE_CONTENT, EXECUTORS, tesExecutors));
         return tesExecutors.get(FIRST);
     }
 
-    private String getProperInstanceType(TesTask tesTask) {
+    private String getProperInstanceType(TesTask tesTask, Tool pipelineTool) {
         Double ramGb = tesTask.getResources().getRamGb();
         Long cpuCores = tesTask.getResources().getCpuCores();
-        //TODO Need clear mappping to loadAllowedInstanceAndPriceTypes() method
-        //TODO toolId, regionId and spot
-        Long toolId = 4L;
-        Long regionId = Long.valueOf(String.join("", tesTask.getResources().getZones()));
-        HashMap<String, Map> initialTypesMap =
-                loadAllowedInstanceAndPriceTypes(toolId, regionId,
-                        tesTask.getResources().getPreemptible());
-        return evaluateInstanceTypeFromMapOfTypes(ramGb, cpuCores, initialTypesMap);
+        Long toolId = pipelineTool.getId();
+        Long regionId = getProperRegionIdInCloudRegionsByTesZone(tesTask.getResources().getZones());
+        Boolean spot = tesTask.getResources().getPreemptible();
+
+        AllowedInstanceAndPriceTypes allowedInstanceAndPriceTypes = cloudPipelineAPIClient
+                .loadAllowedInstanceAndPriceTypes(toolId, regionId, spot);
+
+        return evaluateMostProperInstanceType(allowedInstanceAndPriceTypes, ramGb, cpuCores);
     }
 
-    public HashMap<String, Map> loadAllowedInstanceAndPriceTypes(Long toolId,
-                                                                 Long regionId, Boolean spot) {
-        HashMap<String, Map> allowedInstanceTypesMap = new HashMap<>();
-        AllowedInstanceAndPriceTypes allowedInstanceAndPriceTypes =
-                cloudPipelineAPIClient.loadAllowedInstanceAndPriceTypes(toolId, regionId, spot);
-        allowedInstanceAndPriceTypes.getAllowedInstanceTypes().forEach(instanceType ->
-                allowedInstanceTypesMap.put(instanceType.getName(), Collections.singletonMap(instanceType.getMemory(),
-                        instanceType.getVCPU())));
-        return allowedInstanceTypesMap;
+    private Tool loadToolByTesImage(String image) {
+        return cloudPipelineAPIClient.loadTool(image);
     }
 
-    private String evaluateInstanceTypeFromMapOfTypes(Double ramGb, Long cpuCores,
-                                                      HashMap<String, Map> initialTypesMap) {
-        Map<String, Double> weightedTypesMap = new HashMap<>();
-        initialTypesMap.forEach((instanceName, instanceParams) ->
-                weightedTypesMap.put(instanceName, calculateInstanceWeight(instanceParams, ramGb, cpuCores)));
-        return weightedTypesMap.entrySet()
-                .stream().min(Comparator.comparing(Map.Entry::getValue)).get().getKey();
+    private Long getProperRegionIdInCloudRegionsByTesZone(List<String> zones) {
+        Assert.isTrue(zones.size() == ONLY_ONE, messageHelper.getMessage(
+                MessageConstants.ERROR_PARAMETER_INCOMPATIBLE_CONTENT, ZONES, zones));
+        return cloudPipelineAPIClient.loadAllRegions().stream().filter(
+                region -> region.getName().equalsIgnoreCase(zones.get(FIRST)))
+                .collect(Collectors.toList()).get(FIRST).getId();
     }
 
-    private Double calculateInstanceWeight(Map<Double, Long> instanceParamsMap, Double ramGb, Long cpuCores) {
-        Map.Entry<Double, Long> doubleLongEntry = instanceParamsMap.entrySet().iterator().next();
-        return Math.abs((doubleLongEntry.getKey() / ramGb + doubleLongEntry.getValue() / cpuCores) / 2 - 1);
+    public String evaluateMostProperInstanceType(AllowedInstanceAndPriceTypes allowedInstanceAndPriceTypes,
+                                                 Double ramGb, Long cpuCores) {
+        return allowedInstanceAndPriceTypes.getAllowedInstanceTypes().stream()
+                .collect(Collectors.toMap(InstanceType::getName, instancaType ->
+                        calculateInstanceCoef(instancaType, ramGb, cpuCores))).entrySet().stream()
+                .min(Comparator.comparing(Map.Entry::getValue)).orElseThrow(NullPointerException::new).getKey();
+    }
+
+    private Double calculateInstanceCoef(InstanceType instanceType, Double ramGb, Long cpuCores) {
+        return Math.abs((instanceType.getMemory() * parseInstanceMemoryUnit(instanceType.getMemoryUnit())
+                / ramGb + instanceType.getVCPU() / cpuCores) / 2 - 1);
+    }
+
+    private Double parseInstanceMemoryUnit(String memoryUnit) {
+        if (memoryUnit != null && memoryUnit.equalsIgnoreCase("MiB")) {
+            return MiB_TO_GiB;
+        }
+        return GiB_TO_GiB;
     }
 }
