6ffcf34302707ca83dd55d3261939d085f2facbd
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-09-06 17:01:36 +0300

    refactoring in TaskMapper. added defaultValue for "preemptible" parameter< added empty TaskMapperTest class

---
+++ tes-adapter/src/test/java/com/epam/pipeline/tesadapter/service/TaskMapperTest.java
@@ -0,0 +1,12 @@
+package com.epam.pipeline.tesadapter.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskMapperTest {
+    @Test
+    void mapToPipelineStartTest() {
+
+    }
+}
