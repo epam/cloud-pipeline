c9f97d10be1f307cc321d1728d3b867381c9266a
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2019-09-13 21:00:00 +0300

    Build fsbrowser with travis and install it via launch.sh (#223)

--- api/src/main/java/com/epam/pipeline/app/SAMLSecurityConfiguration.java
+++ api/src/main/java/com/epam/pipeline/app/SAMLSecurityConfiguration.java
@@ -158,9 +158,9 @@ public class SAMLSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     public String[] getUnsecuredResources() {
         return new String[] {
-            "/saml/web/**", "/launch.sh", "/PipelineCLI.tar.gz", "/PipelineQSUB.tar.gz",
+            "/saml/web/**", "/launch.sh", "/PipelineCLI.tar.gz",
             "/pipe-common.tar.gz", "/commit-run-scripts/**", "/restapi/**", "/pipe",
-            "/pipe-mount", "/error", "/error/**"
+            "/pipe-mount", "/fsbrowser.tar.gz", "/error", "/error/**"
         };
     }
 
