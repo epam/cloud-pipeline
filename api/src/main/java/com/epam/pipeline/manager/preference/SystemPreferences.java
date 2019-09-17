3db15de53e18364d57a0d14340b589653b6a2a10
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-09-17 17:52:54 +0300

    Fix pmd issues

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -67,7 +67,6 @@ import java.util.stream.Collectors;
 
 import static com.epam.pipeline.manager.preference.PreferenceValidators.isGreaterThan;
 import static com.epam.pipeline.manager.preference.PreferenceValidators.isGreaterThanOrEquals;
-import static com.epam.pipeline.manager.preference.PreferenceValidators.isNotBlank;
 import static com.epam.pipeline.manager.preference.PreferenceValidators.isNullOrValidJson;
 import static com.epam.pipeline.manager.preference.PreferenceValidators.pass;
 
@@ -452,7 +451,8 @@ public class SystemPreferences {
     public static final IntPreference MISC_MAX_TOOL_ICON_SIZE_KB = new IntPreference("misc.max.tool.icon.size.kb", 50,
                                                                                      MISC_GROUP, isGreaterThan(0));
     public static final StringPreference MISC_SYSTEM_EVENTS_CONFIRMATION_METADATA_KEY = new StringPreference(
-            "system.events.confirmation.metadata.key", "confirmed_notifications", MISC_GROUP, isNotBlank);
+            "system.events.confirmation.metadata.key", "confirmed_notifications", MISC_GROUP,
+            PreferenceValidators.isNotBlank);
 
     // Search
     public static final StringPreference SEARCH_ELASTIC_SCHEME = new StringPreference("search.elastic.scheme",
