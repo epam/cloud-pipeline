c3d06b045f498825fb343d66e8dc53e36f521a45
ekaterina_kazachkova <ekaterina_kazachkova@epam.com>
2019-09-24 14:26:20 +0300

    Issue #589: pipe CLI shall allow to update the executable automatically - Windows: check for permissions added, log added, print result at the and of update operation

--- pipe-cli/src/utilities/update_cli_version.py
+++ pipe-cli/src/utilities/update_cli_version.py
@@ -21,6 +21,8 @@ import platform
 import zipfile
 import subprocess
 import uuid
+from datetime import datetime
+
 
 from src.config import Config
 
@@ -45,8 +47,10 @@ class UpdateCLIVersionManager(object):
     def get_updater():
         if platform.system() == 'Windows':
             return WindowsUpdater()
-        else:
+        elif platform.system() == 'Linux':
             return LinuxUpdater()
+        else:
+            raise RuntimeError("Update operation available for Windows or Linux platforms only")
 
     @staticmethod
     def is_downloadable(url):
@@ -99,32 +103,82 @@ class LinuxUpdater(CLIVersionUpdater):
 class WindowsUpdater(CLIVersionUpdater):
     WINDOWS_SRC_ZIP = 'pipe.zip'
     ATTEMPTS_COUNT = 10
+    LOG_FILE = 'update.log'
 
     def get_download_suffix(self):
         return self.WINDOWS_SRC_ZIP
 
     def update_version(self, path):
-        path_to_src_dir = os.path.dirname(sys.executable)
         random_prefix = str(uuid.uuid4()).replace("-", "")
-        tmp_src_dir = self.download_new_src(path, random_prefix)
+
         tmp_folder = self.get_tmp_folder()
+        self.check_write_permissions(tmp_folder)
+
+        path_to_src_dir = os.path.dirname(sys.executable)
+        self.check_write_permissions(path_to_src_dir)
+
+        tmp_src_dir = self.download_new_src(path, random_prefix)
         path_to_bat = os.path.join(tmp_folder, "pipe-update-%s.bat" % random_prefix)
-        with open(path_to_bat, 'a') as bat_file:
-            bat_file.write('@echo off\n')
-            bat_file.write('for /L %%a in (1,1,{}) do (\n'.format(self.ATTEMPTS_COUNT))
-            bat_file.write('\ttasklist | find /i "{}" > nul\n'.format(os.getpid()))
-            bat_file.write('\tif errorlevel 1 (\n')
-            bat_file.write('\t\trd /s /q "{src_dir}" '  # remove old src folder
-                           '& xcopy "{tmp_dir}/pipe" "{src_dir}" /s /i > nul '  # copy new src to old path
-                           '& rd /s /q "{tmp_dir}"\n'  # remove tmp src folder)
-                           .format(src_dir=path_to_src_dir, tmp_dir=tmp_src_dir))
-            bat_file.write('\t\tgoto end\n')
-            bat_file.write('\t) else (\n')
-            bat_file.write('\t\ttimeout /T 2 > nul\n')
-            bat_file.write('\t)\n')
-            bat_file.write(')\n')
-            bat_file.write(':end\n')
-            bat_file.write('(goto) 2>nul & del "%~f0"\n')
+
+        log_file_path = os.path.join(tmp_folder, self.LOG_FILE)
+        with open(log_file_path, 'a') as log_file:
+            log_file.write('[%s] Starting a new update operation %s' % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                                                                        random_prefix))
+
+        bat_file_content = """@echo off
+        for /L %%a in (1,1,{attempts_count}) do (
+            tasklist | find /i "{pipe_pid}" >> "{log_file}"
+            if errorlevel 1 (
+                for /d %%b in ("{src_dir}\\*") do (
+                    rd /q /s "%%b" || (
+                        echo . >> "{log_file}"
+                        echo Failed to delete "%%b" file >> "{log_file}"
+                        goto fail
+                    )
+                )
+                echo Source subfolders folder was deleted >> "{log_file}"
+                del /q "{src_dir}" >> "{log_file}" || (
+                    echo . >> "{log_file}"
+                    echo Failed to delete src files >> "{log_file}"
+                    goto fail
+                )
+                xcopy "{tmp_dir}/pipe" "{src_dir}" /y /s /i > nul || (
+                    echo . >> "{log_file}"
+                    echo Failed to copy files >> "{log_file}"
+                    goto fail
+                )
+                rd /s /q "{tmp_dir}" || (
+                    echo . >> "{log_file}"
+                    echo Failed to delete tmp directory '{tmp_dir}' >> "{log_file}"
+                    goto fail
+                )
+                goto success
+            ) else (
+                echo . >> "{log_file}"
+                echo Pipeline CLI process '{pipe_pid}' still alive >> "{log_file}"
+                timeout /T 2 > nul
+            ) 
+        )
+        goto fail
+        
+        :success
+        echo Pipeline CLI successfully updated
+        goto end
+
+        :fail
+        echo Fail to update Pipeline CLI
+        goto end
+
+        :end
+        (goto) 2>nul & del "%~f0
+        """.format(attempts_count=self.ATTEMPTS_COUNT,
+                   log_file=log_file_path,
+                   pipe_pid=os.getpid(),
+                   src_dir=path_to_src_dir,
+                   tmp_dir=tmp_src_dir)
+        with open(path_to_bat, 'w') as bat_file:
+            bat_file.write(bat_file_content)
+
         subprocess.Popen("{}".format(path_to_bat), shell=True)
 
     def download_new_src(self, path, prefix):
@@ -145,3 +199,8 @@ class WindowsUpdater(CLIVersionUpdater):
         if not os.path.exists(tmp_folder):
             os.makedirs(tmp_folder)
         return tmp_folder
+
+    @staticmethod
+    def check_write_permissions(path):
+        if not os.access(path, os.X_OK & os.W_OK):
+            raise RuntimeError("Access denied: the user has no permissions to modify folder '%s'" % path)
