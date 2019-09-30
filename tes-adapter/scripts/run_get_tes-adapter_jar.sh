5dad79156722313940d118049a5c7661ca17e71f
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-09-30 17:46:32 +0300

    updated bash script. Added exit-status check for necessary points

--- tes-adapter/scripts/run_get_tes-adapter_jar.sh
+++ tes-adapter/scripts/run_get_tes-adapter_jar.sh
@@ -10,14 +10,26 @@ fi
 
 mkdir tmp;
 cd tmp;
-
 tmpdir=$(pwd);
 
-git clone $gitRepoUrl $tmpdir;
+#Step 1
+echo "Cloning git-repo from $gitRepoUrl"
+git clone $gitRepoUrl $tmpdir || { echo "Cloning failed!" >&2; exit 1; };
 
-./gradlew bootJar;
+#Step 2
+echo "Building jar-file..."
+./gradlew bootJar &> /dev/null || { echo "Building jar-file failed!" >&2; exit 1; };
+
+#Step 3
 echo "Copying jar-file from tes-adapter to $workdir"
-cp -i tes-adapter/build/libs/tes-adapter-1.0-SNAPSHOT.jar ..;
+cp -i tes-adapter/build/libs/tes-adapter-1.0-SNAPSHOT.jar .. || { echo "Copying jar-file failed!" >&2; exit 1; };
 
-rm -r -f $tmpdir;
-echo "tmp directory removed"
+#Check exit status and remove tmp files:
+echo "Removing tmp directory..."
+if [ $? -ne 0 ]; then
+	echo "The program failed! See errorlog file"
+else
+	 rm -r -f $tmpdir; echo "Success!"
+fi
+
+
