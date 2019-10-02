a3cbc9403733da024c1bc210076511bf88a36caa
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-10-02 13:52:44 +0300

    refactored bash-script, added cleanup function, added trap cleanup for EXIT state, deleted Log-file

--- tes-adapter/scripts/get_jar-file.sh
+++ tes-adapter/scripts/get_jar-file.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-LOGFILE=script.log
 # exit when any command fails
 set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?." | tee -a $LOGFILE' ERR
+trap 'echo "\"${last_command}\" command filed with exit code $?." cleanup' ERR
+
+# deletes the temp directory
+function cleanup {
+  rm -rf $tmpdir
+  echo "Deleted temp directory $tmpdir"
+}
+
+#cleanup before exiting
+trap cleanup EXIT
 
 workdir=$(pwd)
 
@@ -17,11 +25,10 @@ else
 fi
 
 echo "Creating tmp directory..."
-mkdir tmp
-cd tmp
-tmpdir=$(pwd)
+
+tmpdir=$(mktemp -d -t ci-XXXXXXXXXX)
 TES_PATH=$tmpdir/tes-adapter
-cd ..
+TES_JAR_PATH=$TES_PATH/build/libs/tes-adapter-1.0-SNAPSHOT.jar
 
 # Step 1.
 # Cloning cloud-pipeline(CP) API from git repository,
@@ -34,18 +41,14 @@ git clone $gitRepoUrl $tmpdir;
 # Build jar file from tes-adapter module(TES-PATH)
 echo "Building jar-file..."
 if [ -d "$TES_PATH" ]; then
-    cd tmp
-    ./gradlew tes-adapter:bootJar > /dev/null 2>&1
+    cd $tmpdir
+    ./gradlew tes-adapter:bootJar >/dev/null 2>&1
 else
    echo "The $TES_PATH directory doesn't exist!" 2>&1; exit 1
 fi
 
 # Step 3
 echo "Copying jar-file from tes-adapter to $workdir"
-cp -rf tes-adapter/build/libs/tes-adapter-1.0-SNAPSHOT.jar ..
-
-# Remove tmp files:
-echo "Removing tmp directory..."
-rm -r -f $tmpdir
+cp -rf $TES_JAR_PATH $workdir
 
 echo "Success! You can find the corresponding jar-file in the folder $workdir"
