fd6ab592a889bbc92760a00c78bc04f4c6451512
Yulia Kamyshova <yulia_kamyshova@epam.com>
2019-10-02 17:24:06 +0300

    ui_tests: the wait between character insertion was added

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/utils/Utils.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/utils/Utils.java
@@ -145,10 +145,10 @@ public class Utils {
         for (final char character : charArray) {
             while (true) {
                 field.sendKeys(String.valueOf(character));
-                sleep(500, MILLISECONDS);
+                sleep(1, SECONDS);
                 final String enteredText = field.getAttribute("value");
-                sleep(500, MILLISECONDS);
-                if (enteredText.charAt(charNumber) == character) {
+                sleep(1, SECONDS);
+                if (enteredText.length() == charArray.length) {
                     break;
                 }
             }
