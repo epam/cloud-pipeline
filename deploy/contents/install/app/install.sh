e886cc4b357616d21aa75bfc40ed8e1504d488dc
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-10-08 22:35:08 +0300

    Added service specs for k&s deployment, also added couple corresponded modifications to install.sh script

--- deploy/contents/install/app/install.sh
+++ deploy/contents/install/app/install.sh
@@ -238,6 +238,11 @@ CP_VM_MONITOR_KUBE_NODE_NAME=${CP_VM_MONITOR_KUBE_NODE_NAME:-$KUBE_MASTER_NODE_N
 print_info "-> Assigning cloud-pipeline/cp-share-srv to $CP_VM_MONITOR_KUBE_NODE_NAME"
 kubectl label nodes "$CP_VM_MONITOR_KUBE_NODE_NAME" cloud-pipeline/cp-share-srv="true" --overwrite
 
+# Allow to schedule TES service to the master
+CP_TES_SRV_KUBE_NODE_NAME=${CP_TES_SRV_KUBE_NODE_NAME:-$KUBE_MASTER_NODE_NAME}
+print_info "-> Assigning cloud-pipeline/cp-tes-srv to $CP_TES_SRV_KUBE_NODE_NAME"
+kubectl label nodes "$CP_TES_SRV_KUBE_NODE_NAME" cloud-pipeline/cp-tes-srv="true" --overwrite
+
 echo
 
 ##########
@@ -1024,9 +1029,14 @@ print_ok "[Starting TES Service deployment]"
     if is_install_requested; then
         print_info "-> Deploying TES service"
         create_kube_resource $K8S_SPECS_HOME/cp-tes-srv/cp-tes-srv-dpl.yaml
+        create_kube_resource $K8S_SPECS_HOME/cp-tes-srv/cp-tes-srv-svc.yaml --svc
 
         print_info "-> Waiting for TES service to initialize"
         wait_for_deployment "cp-tes-srv"
+
+        expose_cluster_port "cp-tes-srv" \
+                            "31086" \
+                            "8080"
 
         CP_INSTALL_SUMMARY="$CP_INSTALL_SUMMARY\ncp-tes-srv: deployed"
     fi
