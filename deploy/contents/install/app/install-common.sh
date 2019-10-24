657eb071a0ce7633a9e69885cdb250987501891d
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2019-10-24 19:25:45 +0300

    Use separate git configs/credentials for the admin and owner (#774)

--- deploy/contents/install/app/install-common.sh
+++ deploy/contents/install/app/install-common.sh
@@ -29,6 +29,6 @@ git config --global http.sslVerify "false"
 
 if ! check_installed "jq"; then
     print_info "Installing jq"
-    wget -q "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -O /usr/bin/jq
+    wget -q "https://cloud-pipeline-oss-builds.s3.amazonaws.com/tools/jq/jq-1.6/jq-linux64" -O /usr/bin/jq
     chmod +x /usr/bin/jq
 fi
