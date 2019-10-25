0d52289d7d98cb46ddff50aa570a0a06b6d5856c
Andrey Georgitsa <andrey_georgitsa@epam.com>
2019-10-25 17:38:10 +0300

    Issue #757 PKI certificates expiration monitoring (#773)
    * Issue #757 PKI certificates expiration monitoring
    
    * Issue #757 Test for CertificateMonitor
    
    * Issue #757 PR review fixes

--- vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/impl/VMNotificationServiceImpl.java
+++ vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/impl/VMNotificationServiceImpl.java
@@ -17,12 +17,14 @@
 package com.epam.pipeline.vmmonitor.service.impl;
 
 import com.epam.pipeline.entity.cluster.NodeInstance;
+import com.epam.pipeline.vmmonitor.model.cert.PkiCertificate;
 import com.epam.pipeline.vmmonitor.model.vm.VirtualMachine;
 import com.epam.pipeline.vmmonitor.service.NotificationSender;
 import com.epam.pipeline.vmmonitor.service.VMNotificationService;
 import com.epam.pipeline.vo.notification.NotificationMessageVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -33,6 +35,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.security.cert.CertificateParsingException;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -46,8 +50,10 @@ public class VMNotificationServiceImpl implements VMNotificationService {
     private final NotificationSender notificationSender;
     private final String missingNodeSubject;
     private final String missingLabelsSubject;
+    private final String certificateExpirationSubject;
     private final String missingNodeTemplatePath;
     private final String missingLabelsTemplatePath;
+    private final String certificateExpirationTemplatePath;
     private final String toUser;
     private final List<String> copyUsers;
 
@@ -57,6 +63,8 @@ public class VMNotificationServiceImpl implements VMNotificationService {
             @Value("${notification.missing-node.template}") final String missingNodeTemplatePath,
             @Value("${notification.missing-labels.subject}") final String missingLabelsSubject,
             @Value("${notification.missing-labels.template}") final String missingLabelsTemplatePath,
+            @Value("${notification.missing-labels.subject}") final String certificateExpirationSubject,
+            @Value("${notification.missing-labels.template}") final String certificateExpirationTemplatePath,
             @Value("${notification.to-user}") final String toUser,
             @Value("${notification.copy-users}") final String copyUsers) {
         this.notificationSender = notificationSender;
@@ -64,6 +72,8 @@ public class VMNotificationServiceImpl implements VMNotificationService {
         this.missingLabelsSubject = missingLabelsSubject;
         this.missingNodeTemplatePath = missingNodeTemplatePath;
         this.missingLabelsTemplatePath = missingLabelsTemplatePath;
+        this.certificateExpirationSubject = certificateExpirationSubject;
+        this.certificateExpirationTemplatePath = certificateExpirationTemplatePath;
         this.toUser = toUser;
         this.copyUsers = Arrays.asList(copyUsers.split(","));
     }
@@ -89,6 +99,13 @@ public class VMNotificationServiceImpl implements VMNotificationService {
         sendMessage(parameters, missingLabelsSubject, missingLabelsTemplatePath);
     }
 
+    @Override
+    public void notifyExpiringCertificate(final PkiCertificate certificate) {
+        final Map<String, Object> parameters = createCertParameters(certificate);
+        log.debug("Sending {} certificate expiration notification", certificate.getX509Certificate().getSubjectDN());
+        sendMessage(parameters, certificateExpirationSubject, certificateExpirationTemplatePath);
+    }
+
     private void addNodeParameters(final NodeInstance node, final Map<String, Object> parameters) {
         parameters.put("nodeName", node.getName());
     }
@@ -98,6 +115,23 @@ public class VMNotificationServiceImpl implements VMNotificationService {
         parameters.put("privateIp", vm.getPrivateIp());
         parameters.put("provider", vm.getCloudProvider().name());
         parameters.put("instanceName", vm.getInstanceName());
+    }
+
+    private Map<String, Object> createCertParameters(final PkiCertificate certificate) {
+        final Map<String, Object> parameters = new HashMap<>();
+        final X509Certificate x509Cert = certificate.getX509Certificate();
+        try {
+            parameters.put("san", x509Cert.getSubjectAlternativeNames());
+        } catch (CertificateParsingException e) {
+            log.warn("Can't obtain SAN info from certificate!", e);
+            parameters.put("san", StringUtils.EMPTY);
+        }
+        parameters.put("serialNumber", x509Cert.getSerialNumber());
+        parameters.put("dn", x509Cert.getSubjectDN());
+        parameters.put("issuer", x509Cert.getIssuerDN());
+        parameters.put("notAfter", x509Cert.getNotAfter());
+        parameters.put("filePath", certificate.getPathToCertFile());
+        return parameters;
     }
 
     private void sendMessage(final Map<String, Object> parameters, final String subject, final String template) {
