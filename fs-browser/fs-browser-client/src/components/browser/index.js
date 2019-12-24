27750cb1a323636cc5f0728719d26ccf7a3172fd
Timofey Alyakin <Timofey_Alyakin@epam.com>
2019-12-24 16:55:15 +0300

    Hide 'Upload' button in FSBrowser's header if there're errors during directory load (#877)

--- fs-browser/fs-browser-client/src/components/browser/index.js
+++ fs-browser/fs-browser-client/src/components/browser/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {inject, observer} from 'mobx-react';
+import {Route} from 'react-router-dom';
 import classNames from 'classnames';
+import Header from '../header';
 import Icon from '../shared/icon';
 import displaySize from '../../utilities/display-size';
 import {parse} from '../../utilities/query-parameters';
@@ -208,21 +210,34 @@ class Browser extends React.Component {
   };
 
   render() {
-    const {path, taskManager} = this.props;
+    const {directory, path, taskManager} = this.props;
+    const hasError = !!(directory.error || (!directory.pending && !directory.loaded));
     return (
-      <Upload
-        className={styles.uploadContainer}
-        path={path || ''}
-      >
-        <div
-          className={styles.container}
+      <>
+        <Route
+          path="/"
+          render={props => (
+            <Header
+              {...props}
+              hasError={hasError}
+            />
+          )}
+        />
+        <Upload
+          showUploadArea={!hasError}
+          className={styles.uploadContainer}
+          path={path || ''}
         >
-          {this.renderDirectory()}
-          <TaskQueue
-            activeTasksCount={(taskManager.items || []).length}
-          />
-        </div>
-      </Upload>
+          <div
+            className={styles.container}
+          >
+            {this.renderDirectory()}
+            <TaskQueue
+              activeTasksCount={(taskManager.items || []).length}
+            />
+          </div>
+        </Upload>
+      </>
     );
   }
 }
