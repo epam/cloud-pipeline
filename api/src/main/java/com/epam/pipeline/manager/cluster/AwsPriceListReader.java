0c0f70edac136422c8ac69850a32c5f49e706ac6
Mariia_Zueva <mariia_zueva@epam.com>
2020-02-04 19:12:28 +0300

    Fix parsing of AWS price list for bare metal instances

--- api/src/main/java/com/epam/pipeline/manager/cluster/AwsPriceListReader.java
+++ api/src/main/java/com/epam/pipeline/manager/cluster/AwsPriceListReader.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017-2019 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@
 package com.epam.pipeline.manager.cluster;
 
 import com.epam.pipeline.entity.cluster.InstanceOffer;
+import com.epam.pipeline.manager.cloud.CloudInstancePriceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
@@ -29,13 +30,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Slf4j
 @RequiredArgsConstructor
 public class AwsPriceListReader {
+
+    private static final Set<String> COMPUTE_FAMILY = new HashSet<>();
+    static {
+        COMPUTE_FAMILY.add(CloudInstancePriceService.INSTANCE_PRODUCT_FAMILY);
+        COMPUTE_FAMILY.add("Compute Instance (bare metal)");
+    }
 
     private final Long regionId;
 
@@ -66,7 +75,7 @@ public class AwsPriceListReader {
         offer.setInstanceType(record.get("instance type"));
         offer.setTenancy(record.get("tenancy"));
         offer.setOperatingSystem(record.get("operating system"));
-        offer.setProductFamily(record.get("product family"));
+        offer.setProductFamily(parseProductFamily(record.get("product family")));
         offer.setVolumeType(record.get("volume type"));
         offer.setVCPU(parseInteger(record.get("vcpu")));
         offer.setGpu(parseInteger(record.get("gpu")));
@@ -74,6 +83,13 @@ public class AwsPriceListReader {
         offer.setRegionId(regionId);
         parseMemoryValue(offer, record.get("memory"));
         return offer;
+    }
+
+    private String parseProductFamily(final String productFamily) {
+        if (COMPUTE_FAMILY.contains(productFamily)) {
+            return CloudInstancePriceService.INSTANCE_PRODUCT_FAMILY;
+        }
+        return productFamily;
     }
 
     private void parseMemoryValue(InstanceOffer offer, String memoryValue) {
