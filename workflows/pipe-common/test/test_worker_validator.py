82dda657ebdcc272e044b74ab6ad38c76b212448
Andrei Tcibin <andrei_tcibin@epam.com>
2020-02-11 18:25:07 +0300

    Handle dead additional workers in GE autoscaler (#947)
    * Fix api error handling
    
    * Slightly improve ge autoscaler errors formatting
    
    * Remove worker from ge before reducing pe slots
    
    * Consider unheard, errored and disabled hosts as invalid
    
    * Consider not running worker instances as invalid
    
    * Write warn log messages rather than error log messages
    
    * Make several warning messages crucial
    
    * Remove host from a list of administrative hosts on scaling down
    
    * Resolve minor log message formatting issue

--- workflows/pipe-common/test/test_worker_validator.py
+++ workflows/pipe-common/test/test_worker_validator.py
@@ -17,9 +17,12 @@ from mock import Mock, MagicMock
 from scripts.autoscale_sge import GridEngineWorkerValidator, MemoryHostStorage, GridEngineJob
 from utils import assert_first_argument_contained, assert_first_argument_not_contained
 
-HOST1 = 'HOST1'
-HOST2 = 'HOST2'
-HOST3 = 'HOST3'
+HOST1 = 'HOST-1'
+HOST2 = 'HOST-2'
+HOST3 = 'HOST-3'
+HOST1_RUN_ID = '1'
+HOST2_RUN_ID = '2'
+HOST3_RUN_ID = '3'
 
 executor = Mock()
 host_storage = MemoryHostStorage()
@@ -34,11 +37,11 @@ def setup_function():
     host_storage.clear()
     for host in [HOST1, HOST2, HOST3]:
         host_storage.add_host(host)
-    executor.execute = MagicMock()
+    executor.execute = MagicMock(return_value='RUNNING')
     grid_engine.is_valid = MagicMock(side_effect=[True, False, True])
     grid_engine.get_jobs = MagicMock(return_value=[])
     grid_engine.kill_jobs = MagicMock()
-    scale_down_handler._get_run_id_from_host = MagicMock(side_effect=lambda x: x)
+    scale_down_handler._get_run_id_from_host = MagicMock(side_effect=[HOST1_RUN_ID, HOST2_RUN_ID, HOST3_RUN_ID])
 
 
 def test_stopping_hosts_that_are_invalid_in_grid_engine():
@@ -50,10 +53,9 @@ def test_stopping_hosts_that_are_invalid_in_grid_engine():
 def test_stopping_invalid_worker_pipeline():
     worker_validator.validate_hosts()
 
-    assert_first_argument_contained(executor.execute, 'pipe stop ')
-    assert_first_argument_contained(executor.execute, HOST2)
-    assert_first_argument_not_contained(executor.execute, HOST1)
-    assert_first_argument_not_contained(executor.execute, HOST3)
+    assert_first_argument_contained(executor.execute, 'pipe stop --yes ' + HOST2_RUN_ID)
+    assert_first_argument_not_contained(executor.execute, 'pipe stop --yes ' + HOST1_RUN_ID)
+    assert_first_argument_not_contained(executor.execute, 'pipe stop --yes ' + HOST3_RUN_ID)
 
 
 def test_force_killing_invalid_host_jobs():
@@ -63,3 +65,11 @@ def test_force_killing_invalid_host_jobs():
     worker_validator.validate_hosts()
 
     grid_engine.kill_jobs.assert_called_with(jobs, force=True)
+
+
+def test_stopping_dead_worker_hosts():
+    executor.execute = MagicMock(side_effect=['STOPPED', 'RUNNING', 'FAILURE'])
+    grid_engine.is_valid = MagicMock(return_value=True)
+    worker_validator.validate_hosts()
+
+    assert [HOST2] == host_storage.load_hosts()
