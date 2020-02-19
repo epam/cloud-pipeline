9b33c218b1d3277aaf914691f3d95c32e9769477
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2020-02-19 16:48:25 +0300

    Introduced kube/system reservation for the kubelet (develop/7de1dd1bf7ee1fc45aac89c762c9f026a1c76bc0)

--- scripts/autoscaling/init_multicloud.sh
+++ scripts/autoscaling/init_multicloud.sh
@@ -282,10 +282,16 @@ _KUBE_LOG_ARGS="--logtostderr=false --log-dir=$_KUBELET_LOG_PATH"
 
 _KUBELET_INITD_DROPIN_PATH="/etc/systemd/system/kubelet.service.d/20-kubelet-labels.conf"
 rm -f $_KUBELET_INITD_DROPIN_PATH
-## Append node-labels string to the systemd config
+
+## Configure kubelet reservations
+## FIXME: shall be moved to the preferences
+_KUBE_RESERVED_ARGS="--kube-reserved cpu=500m,memory=500Mi,storage=1Gi"
+_KUBE_SYS_RESERVED_ARGS="--system-reserved cpu=500m,memory=500Mi,storage=1Gi"
+
+## Append extra kube-config to the systemd config
 cat > $_KUBELET_INITD_DROPIN_PATH <<EOF
 [Service]
-Environment="KUBELET_EXTRA_ARGS=$_KUBE_NODE_INSTANCE_LABELS $_KUBE_LOG_ARGS"
+Environment="KUBELET_EXTRA_ARGS=$_KUBE_NODE_INSTANCE_LABELS $_KUBE_LOG_ARGS $_KUBE_RESERVED_ARGS $_KUBE_SYS_RESERVED_ARGS"
 EOF
 chmod +x $_KUBELET_INITD_DROPIN_PATH
 
