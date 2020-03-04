c1b07afb750ef734b8eb47c2e50edefbce454283
Aleksandr Gorodetskii <gorod2004@gmail.com>
2020-03-04 12:05:19 +0300

    GUI Billing reports: <GroupedBarChart> changed to class-based component

--- client/src/components/billing/reports/charts/grouped-bar-chart.js
+++ client/src/components/billing/reports/charts/grouped-bar-chart.js
@@ -15,54 +15,72 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import {observer} from 'mobx-react';
 import BarChart from './bar-chart';
 import styles from './charts.css';
 import Export from '../export';
 
-function GroupedBarChart (
-  {
-    request,
-    onSelect,
-    title,
-    height
-  }
-) {
-  const data = request.loaded ? (request.value || {}) : {};
-  const groups = Object.keys(data || {});
-  const itemsCount = groups.map(group => Object.keys(data[group] || {}).length);
-  const total = itemsCount.reduce((r, c) => r + c, 0);
-  const charts = groups.map((group, index) => (
-    <BarChart
-      key={group}
-      request={request}
-      data={data[group]}
-      title={group}
-      subChart
-      style={Object.assign({
-        width: total > 0 ? `${100.0 * itemsCount[index] / total}%` : `${100 / itemsCount.length}%`,
-        display: 'inline-block',
-        height
-      })}
-      onSelect={onSelect ? ({key} = {}) => onSelect({group, key}) : undefined}
-      onScaleSelect={onSelect ? () => onSelect({group}) : undefined}
-      axisPosition={index === 0 ? 'left' : 'right'}
-      useImageConsumer={false}
-    />
-  ));
-  return (
-    <Export.ImageConsumer
-      style={{position: 'relative'}}
-      order={2}
-    >
-      {title && <div className={styles.title}>{title}</div>}
-      <div style={{position: 'relative', display: 'block', height}}>
-        {
-          charts.length > 0 ? charts : '\u00A0'
-        }
-      </div>
-    </Export.ImageConsumer>
-  );
-}
+class GroupedBarChart extends React.Component {
+  static propTypes = {
+    request: PropTypes.object,
+    onSelect: PropTypes.func,
+    title: PropTypes.string,
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  };
+
+  static defaultProps = {
+    height: '500px'
+  };
+
+  get billingData () {
+    const {request = {}} = this.props;
+    const data = request.loaded ? (request.value || {}) : {};
+    const groups = Object.keys(data || {});
+    const itemsCount = groups.map(group => Object.keys(data[group] || {}).length);
+    const total = itemsCount.reduce((r, c) => r + c, 0);
+    return {
+      data,
+      groups,
+      itemsCount,
+      total
+    }
+  };
+
+  render () {
+    const {title, height, request, onSelect} = this.props;
+    const {data, itemsCount, groups, total} = this.billingData;
+    return (
+      <Export.ImageConsumer
+        style={{position: 'relative'}}
+        order={2}
+      >
+        {title && <div className={styles.title}>{title}</div>}
+        <div style={{position: 'relative', display: 'block', height}}>
+          {
+            groups.length > 0 ? groups.map((group, index) => (
+              <BarChart
+                key={group}
+                request={request}
+                data={data[group]}
+                title={group}
+                subChart
+                style={Object.assign({
+                  width: total > 0 ? `${100.0 * itemsCount[index] / total}%` : `${100 / itemsCount.length}%`,
+                  display: 'inline-block',
+                  height
+                })}
+                onSelect={onSelect ? ({key} = {}) => onSelect({group, key}) : undefined}
+                onScaleSelect={onSelect ? () => onSelect({group}) : undefined}
+                axisPosition={index === 0 ? 'left' : 'right'}
+                useImageConsumer={false}
+              />
+          )) : '\u00A0'
+          }
+        </div>
+      </Export.ImageConsumer>
+    );
+  };
+};
 
 export default observer(GroupedBarChart);
