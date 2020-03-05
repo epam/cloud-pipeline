4bce919d58da5ab495cf6b424b432b87787ecf37
Andrey Georgitsa <andrey_georgitsa@epam.com>
2020-03-05 20:54:38 +0300

    Issue #761 Parse AWS prices into POJO instead of working with JsonNode

---
+++ billing-report-agent/src/main/java/com/epam/pipeline/billingreportagent/model/pricing/AwsPricingCard.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.pipeline.billingreportagent.model.pricing;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AwsPricingCard {
+
+    @JsonProperty(value = "product")
+    private AwsProduct product;
+
+    @JsonProperty(value = "serviceCode")
+    private String serviceCode;
+
+    @JsonProperty(value = "version")
+    private String version;
+
+    @JsonProperty(value = "publicationDate")
+    private String publicationDate;
+
+    private List<AwsPriceRate> priceDimensions;
+}
