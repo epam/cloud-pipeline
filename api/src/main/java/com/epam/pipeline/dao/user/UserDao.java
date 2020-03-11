d1668b4f40a2b1ab96d0edd707c332185fcc55bf
Andrey Georgitsa <andrey_georgitsa@epam.com>
2020-03-11 14:10:02 +0300

    Issue #907 Change user update propagation to assign default storage

--- api/src/main/java/com/epam/pipeline/dao/user/UserDao.java
+++ api/src/main/java/com/epam/pipeline/dao/user/UserDao.java
@@ -147,7 +147,7 @@ public class UserDao extends NamedParameterJdbcDaoSupport {
         return getJdbcTemplate().query(loadUsersByGroupQuery, UserParameters.getUserExtractor(), group);
     }
 
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Transactional(propagation = Propagation.REQUIRED)
     public PipelineUser updateUser(PipelineUser user) {
         getNamedParameterJdbcTemplate().update(updateUserQuery, UserParameters.getParameters(user, getConnection()));
         return user;
