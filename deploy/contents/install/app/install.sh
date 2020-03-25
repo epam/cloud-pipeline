6cc6cdd90db9bf5dd896f88a1298e8e5fbc98d28
pavel_silin <pavel_silin@epam.com>
2020-03-25 20:34:51 +0300

    (issue #989) deploy infrastructure changes for log storage deploy

--- deploy/contents/install/app/install.sh
+++ deploy/contents/install/app/install.sh
@@ -959,7 +959,11 @@ if is_service_requested cp-search; then
 
     print_info "-> Deleting existing instance of Search ELK service"
     delete_deployment_and_service   "cp-search-elk" \
-                                    "/opt/search-elk"    
+                                    "/opt/search-elk"
+
+     print_info "-> Deleting existing instance of Search KIBANA service"
+    delete_deployment_and_service   "cp-search-kibana" \
+                                    "/opt/search-kibana"
 
     if is_install_requested; then
         print_info "-> Deploying Search ELK service"
@@ -969,9 +973,16 @@ if is_service_requested cp-search; then
         print_info "-> Waiting for Search ELK service to initialize"
         wait_for_deployment "cp-search-elk"
 
+        print_info "-> Deploying Search KIBANA service"
+        create_kube_resource $K8S_SPECS_HOME/cp-search/cp-search-elk-dpl.yaml
+        create_kube_resource $K8S_SPECS_HOME/cp-search/cp-search-elk-svc.yaml
+
+        print_info "-> Waiting for Search KIBANA service to initialize"
+        wait_for_deployment "cp-search-kibana"
+
         CP_INSTALL_SUMMARY="$CP_INSTALL_SUMMARY\ncp-search-elk:"
         CP_INSTALL_SUMMARY="$CP_INSTALL_SUMMARY\nElastic:   http://$CP_SEARCH_ELK_INTERNAL_HOST:$CP_SEARCH_ELK_ELASTIC_INTERNAL_PORT"
-        CP_INSTALL_SUMMARY="$CP_INSTALL_SUMMARY\nKibana:    http://$CP_SEARCH_ELK_INTERNAL_HOST:$CP_SEARCH_ELK_KIBANA_INTERNAL_PORT"
+        CP_INSTALL_SUMMARY="$CP_INSTALL_SUMMARY\nKibana:    http://$CP_SEARCH_KIBANA_INTERNAL_HOST:$CP_SEARCH_KIBANA_INTERNAL_PORT"
 
         print_info "-> Deploying Search service"
         create_kube_resource $K8S_SPECS_HOME/cp-search/cp-search-srv-dpl.yaml
