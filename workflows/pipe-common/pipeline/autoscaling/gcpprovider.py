56dab65e4c746848429268fee007ecbe8d5f5f9b
Mariia Zueva <mariia_zueva@epam.com>
2020-03-27 19:34:26 +0300

    Issue #1017 Allow to specify region specific tags for GCP instances (#1023)
    * Issue #1017: Allow to specify region specific tags for GCP instances
    
    * Issue #1017: Allow to specify region specific tags for GCP instances - minor fix

--- workflows/pipe-common/pipeline/autoscaling/gcpprovider.py
+++ workflows/pipe-common/pipeline/autoscaling/gcpprovider.py
@@ -72,7 +72,7 @@ class GCPInstanceProvider(AbstractInstanceProvider):
             'canIpForward': True,
             'disks': self.__get_disk_devices(ins_img, OS_DISK_SIZE, ins_hdd, swap_size),
             'networkInterfaces': network_interfaces,
-            'labels': GCPInstanceProvider.get_tags(run_id),
+            'labels': GCPInstanceProvider.get_tags(run_id, self.cloud_region),
             "metadata": {
                 "items": [
                     {
@@ -236,7 +236,7 @@ class GCPInstanceProvider(AbstractInstanceProvider):
         if len(project_and_family) != 2:
             # TODO: missing exception?
             print("node_image parameter doesn't match to Google image name convention: <project>/<imageFamily>")
-        image = self.client.images().get(project=project_and_family[0], image=project_and_family[1])
+        image = self.client.images().get(project=project_and_family[0], image=project_and_family[1]).execute()
         if image is None or 'diskSizeGb' not in image:
             utils.pipe_log('Failed to get image disk size info. Falling back to default size %d ' % disk_size)
             image_disk_size = disk_size
@@ -340,10 +340,15 @@ class GCPInstanceProvider(AbstractInstanceProvider):
         }
 
     @staticmethod
-    def get_tags(run_id):
+    def get_tags(run_id, cloud_region):
         tags = GCPInstanceProvider.run_id_tag(run_id)
-        res_tags = GCPInstanceProvider.resource_tags()
-        for key in res_tags:
-            tags[key.lower()] = res_tags[key].lower()
+        GCPInstanceProvider.append_tags(tags, GCPInstanceProvider.resource_tags())
+        GCPInstanceProvider.append_tags(tags, utils.get_region_tags(cloud_region))
         return tags
 
+    @staticmethod
+    def append_tags(tags, tags_to_add):
+        if tags_to_add is None:
+            return
+        for key in tags_to_add:
+            tags[key.lower()] = tags_to_add[key].lower()
