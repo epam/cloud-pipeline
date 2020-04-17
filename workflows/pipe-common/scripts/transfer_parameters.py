be88f76d435c8d986296c58d3c0417412152dbc9
Mariia_Zueva <mariia_zueva@epam.com>
2020-04-17 21:10:08 +0300

    Issue #1063: Parallel data transfer - unify approach for autoscaled and static clusters

--- workflows/pipe-common/scripts/transfer_parameters.py
+++ workflows/pipe-common/scripts/transfer_parameters.py
@@ -81,6 +81,7 @@ class Cluster:
         if int(os.getenv('node_count', 0)) == 0:
             Logger.info('No child nodes requested. Using only master node for data transfer.', task_name=task_name)
             return local_cluster
+
         distributed_cluster = cls.build_distributed_cluster(slots_per_node, api, task_name)
         if distributed_cluster is None or len(distributed_cluster.nodes) == 0:
             Logger.info('Failed to find child nodes. Using only master node for data transfer.', task_name=task_name)
@@ -89,38 +90,26 @@ class Cluster:
 
     @classmethod
     def build_distributed_cluster(cls, slots_per_node, api, task_name):
-        if cls.is_autoscaled_cluster():
-            Logger.info('Using autoscaled cluster for data transfer.', task_name=task_name)
-            return cls.build_autoscaled_cluster(slots_per_node, api, task_name)
-
-        if 'DEFAULT_HOSTFILE' not in os.environ:
+        if not cls.is_distributed_cluster():
             return None
-        hostfile = os.environ['DEFAULT_HOSTFILE']
-        if not os.path.isfile(hostfile):
-            return None
-        with open(hostfile) as f:
-            Logger.info('Using static cluster for data transfer.', task_name=task_name)
-            hosts = f.readlines()
-            master_host = socket.gethostname()
-            nodes = [Node('localhost' if x.strip() == master_host else x.strip()) for x in hosts]
-            return Cluster(nodes, slots_per_node)
-
-    @classmethod
-    def is_autoscaled_cluster(cls):
-        flag_values = ['yes', 'true']
-        sge_enabled = os.getenv('CP_CAP_SGE', None) in flag_values
-        autoscale_enabled = os.getenv('CP_CAP_AUTOSCALE', None) in flag_values
-        return sge_enabled and autoscale_enabled
-
-    @classmethod
-    def build_autoscaled_cluster(cls, slots_per_node, api, task_name):
+        Logger.info('Using distributed cluster for data transfer. Master node will be used.', task_name=task_name)
         child_runs = api.load_child_pipelines(os.getenv('RUN_ID'))
         nodes = [Node('localhost')]
         static_runs = filter(lambda run: cls.is_static_run(run), child_runs)
         if len(static_runs) > 0:
-            Logger.info('Found %s running static for data transfer ' % len(static_runs), task_name=task_name)
-            nodes.extend([Node(static_run['podId']) for static_run in static_runs])
+            child_hosts = [static_run['podId'] for static_run in static_runs]
+            Logger.info('Found %d running child nodes for data transfer: %s.' % (len(static_runs), ','.join(child_hosts)),
+                        task_name=task_name)
+            nodes.extend([Node(host) for host in child_hosts])
         return Cluster(nodes, slots_per_node)
+
+    @classmethod
+    def is_distributed_cluster(cls):
+        flag_values = ['yes', 'true']
+        sge_enabled = os.getenv('CP_CAP_SGE', None) in flag_values
+        autoscale_enabled = os.getenv('CP_CAP_AUTOSCALE', None) in flag_values
+        hostfile_configured = os.path.isfile(os.getenv('DEFAULT_HOSTFILE', None))
+        return (sge_enabled and autoscale_enabled) or hostfile_configured
 
     @classmethod
     def build_local_cluster(cls, slots_per_node):
