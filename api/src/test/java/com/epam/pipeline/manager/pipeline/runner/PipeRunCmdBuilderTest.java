a0c1998b28f14bac42b3181283ec4a13ae52a8e1
Andrey Georgitsa <andrey_georgitsa@epam.com>
2020-05-07 13:55:09 +0300

    Issue #1090 Wrap launch command parameter values (#1092)
    * Issue #1090 Put quotes on the value of parameters with multiple value selection in launch command generation
    
    * Issue #1090 Move map sorting to test

--- api/src/test/java/com/epam/pipeline/manager/pipeline/runner/PipeRunCmdBuilderTest.java
+++ api/src/test/java/com/epam/pipeline/manager/pipeline/runner/PipeRunCmdBuilderTest.java
@@ -25,19 +25,29 @@ import org.junit.Test;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PipeRunCmdBuilderTest {
 
     private static final String TEST_VERSION = "draft";
     private static final String TEST_PARAM_NAME_1 = "--param1";
     private static final String TEST_PARAM_NAME_2 = "--param2";
+    private static final String TEST_PARAM_NAME_3 = "--param3";
+    private static final String TEST_PARAM_NAME_4 = "--param4";
+    private static final String TEST_PARAM_NAME_5 = "--param5";
+    private static final String TEST_PARAM_NAME_6 = "--param6";
+    private static final String TEST_PARAM_NAME_7 = "--param7";
     private static final String TEST_PARAM_VALUE_1 = "string value";
     private static final String TEST_PARAM_VALUE_2 = "1";
+    private static final String TEST_PARAM_VALUE_MULTIPLE_PATHS = "/bucket1/, /bucket2/";
     private static final String INSTANCE_TYPE = "type";
     private static final String DOCKER_IMAGE = "image";
     private static final String CMD_TEMPLATE = "sleep 100";
     private static final String CMD_TEMPLATE_WITH_DOUBLE_QUOTES = "do \"command\"";
+    private static final String QUOTED_PARAMETER = "%s '%s' ";
+    private static final String NON_QUOTED_PARAMETER = "%s %s ";
 
     @Test
     public void shouldGenerateLaunchCommand() {
@@ -46,15 +56,42 @@ public class PipeRunCmdBuilderTest {
         runParameters.put(TEST_PARAM_NAME_1, pipeConfValue1);
         final PipeConfValueVO pipeConfValue2 = new PipeConfValueVO(TEST_PARAM_VALUE_2, "int");
         runParameters.put(TEST_PARAM_NAME_2, pipeConfValue2);
-
-        final PipeRunCmdStartVO pipeRunCmdStartVO = getPipeRunCmdStartVO(runParameters);
+        final PipeConfValueVO pipeConfValue3 = new PipeConfValueVO(TEST_PARAM_VALUE_MULTIPLE_PATHS, "input");
+        runParameters.put(TEST_PARAM_NAME_3, pipeConfValue3);
+        final PipeConfValueVO pipeConfValue4 = new PipeConfValueVO(TEST_PARAM_VALUE_MULTIPLE_PATHS, "output");
+        runParameters.put(TEST_PARAM_NAME_4, pipeConfValue4);
+        final PipeConfValueVO pipeConfValue5 = new PipeConfValueVO(TEST_PARAM_VALUE_MULTIPLE_PATHS, "path");
+        runParameters.put(TEST_PARAM_NAME_5, pipeConfValue5);
+        final PipeConfValueVO pipeConfValue6 = new PipeConfValueVO(TEST_PARAM_VALUE_MULTIPLE_PATHS, "common");
+        runParameters.put(TEST_PARAM_NAME_6, pipeConfValue6);
+        final PipeConfValueVO pipeConfValue7 = new PipeConfValueVO(Boolean.TRUE.toString(), "boolean");
+        runParameters.put(TEST_PARAM_NAME_7, pipeConfValue7);
+        final Map<String, PipeConfValueVO> sortedParameters = runParameters.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+        final PipeRunCmdStartVO pipeRunCmdStartVO = getPipeRunCmdStartVO(sortedParameters);
 
         final PipeRunCmdBuilder pipeRunCmdBuilder = new PipeRunCmdBuilder(pipeRunCmdStartVO);
         final String actualResult = buildCmd(pipeRunCmdBuilder);
-        final String expectedResult = String.format("pipe run -n 1@%s %s '%s' %s %s parent-id 1 -p -y -id 10 " +
-                        "-it type -di image -cmd '%s' -t 10 -q -ic 5 -s -pt spot -r 1 -pn 1",
-                TEST_VERSION, TEST_PARAM_NAME_1, TEST_PARAM_VALUE_1, TEST_PARAM_NAME_2, TEST_PARAM_VALUE_2,
-                CMD_TEMPLATE);
+        final String expectedResult = String.format("pipe run -n 1@%s "
+                                                    + QUOTED_PARAMETER
+                                                    + NON_QUOTED_PARAMETER
+                                                    + QUOTED_PARAMETER
+                                                    + QUOTED_PARAMETER
+                                                    + QUOTED_PARAMETER
+                                                    + QUOTED_PARAMETER
+                                                    + NON_QUOTED_PARAMETER
+                                                    + "parent-id 1 -p -y -id 10 -it type -di image -cmd '%s' "
+                                                    + "-t 10 -q -ic 5 -s -pt spot -r 1 -pn 1",
+                                                    TEST_VERSION,
+                                                    TEST_PARAM_NAME_1, TEST_PARAM_VALUE_1,
+                                                    TEST_PARAM_NAME_2, TEST_PARAM_VALUE_2,
+                                                    TEST_PARAM_NAME_3, TEST_PARAM_VALUE_MULTIPLE_PATHS,
+                                                    TEST_PARAM_NAME_4, TEST_PARAM_VALUE_MULTIPLE_PATHS,
+                                                    TEST_PARAM_NAME_5, TEST_PARAM_VALUE_MULTIPLE_PATHS,
+                                                    TEST_PARAM_NAME_6, TEST_PARAM_VALUE_MULTIPLE_PATHS,
+                                                    TEST_PARAM_NAME_7, true,
+                                                    CMD_TEMPLATE);
         Assert.assertEquals(expectedResult, actualResult);
     }
 
