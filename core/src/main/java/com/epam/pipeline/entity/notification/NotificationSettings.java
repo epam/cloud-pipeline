49332319513672ef5d881bb267134f8026cedc01
Mariia_Zueva <mariia_zueva@epam.com>
2020-05-15 23:29:45 +0300

    Notifications for long pause/resume operations (#1087) (PMD fix)

--- core/src/main/java/com/epam/pipeline/entity/notification/NotificationSettings.java
+++ core/src/main/java/com/epam/pipeline/entity/notification/NotificationSettings.java
@@ -22,7 +22,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
