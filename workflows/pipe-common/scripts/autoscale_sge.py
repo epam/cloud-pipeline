2f50e14a4c5d398137a4203bd9f0a8e770959c73
Mariia_Zueva <mariia_zueva@epam.com>
2020-05-15 20:09:33 +0300

    Disable endpoints for autoscale workers

--- workflows/pipe-common/scripts/autoscale_sge.py
+++ workflows/pipe-common/scripts/autoscale_sge.py
@@ -490,6 +490,7 @@ class GridEngineScaleUpHandler:
                            'CP_CAP_SGE false ' \
                            'CP_CAP_AUTOSCALE false ' \
                            'CP_CAP_AUTOSCALE_WORKERS 0' \
+                           'CP_DISABLE_RUN_ENDPOINTS true' \
                            % (self.instance_disk, self.instance_type, self.instance_image, self.parent_run_id,
                               self._pipe_cli_price_type(self.price_type), self.region_id)
         run_id = int(self.executor.execute_to_lines(pipe_run_command)[0])
