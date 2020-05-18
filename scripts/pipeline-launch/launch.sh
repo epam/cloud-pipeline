701819520d24e0ec82e6a363e4f2cc318fe831f3
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2020-05-18 12:44:12 +0300

    Apply custom fixes at a whole platform, when we don't want to fix some issue on a docker-per-docker basis (#1104)

--- scripts/pipeline-launch/launch.sh
+++ scripts/pipeline-launch/launch.sh
@@ -1318,6 +1318,10 @@ fi
 cd $ANALYSIS_DIR
 echo "CWD is now at $ANALYSIS_DIR"
 
+# Apply the "custom fixes" script, which contains very specific modifications to fix the docker images
+# This is used, when we don't want to fix some issue on a docker-per-docker basis
+custom_fixes
+
 # Tell the environment that initilization phase is finished and a source script is going to be executed
 pipe_log SUCCESS "Environment initialization finished" "InitializeEnvironment"
 
