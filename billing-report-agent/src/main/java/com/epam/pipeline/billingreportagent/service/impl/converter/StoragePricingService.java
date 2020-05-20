2d80fe818b73e341af95c041507de0a2ecd55e1d
Mariia_Zueva <mariia_zueva@epam.com>
2020-05-20 19:53:44 +0300

    Billing agent - pmd fix

--- billing-report-agent/src/main/java/com/epam/pipeline/billingreportagent/service/impl/converter/StoragePricingService.java
+++ billing-report-agent/src/main/java/com/epam/pipeline/billingreportagent/service/impl/converter/StoragePricingService.java
@@ -42,6 +42,7 @@ public class StoragePricingService {
         this.defaultPriceGb = calculateDefaultPriceGb();
     }
 
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
     public void updatePrices() {
         try {
             if (priceListLoader != null) {
