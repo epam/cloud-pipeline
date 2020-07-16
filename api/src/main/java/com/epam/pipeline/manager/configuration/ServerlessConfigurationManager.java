ba0cafd86038eac71f5ea92c6d6fdafb2499ccc6
ekaterina_kazachkova <ekaterina_kazachkova@epam.com>
2020-07-16 15:25:46 +0300

    Issue #1038: "Serverless" Cloud Pipeline API - checkstyle fix

--- api/src/main/java/com/epam/pipeline/manager/configuration/ServerlessConfigurationManager.java
+++ api/src/main/java/com/epam/pipeline/manager/configuration/ServerlessConfigurationManager.java
@@ -71,7 +71,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
