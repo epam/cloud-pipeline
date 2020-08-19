2c66a5c8c10bc6c53396083a92b1ce1664b42b87
Mariia_Zueva <mariia_zueva@epam.com>
2020-08-19 18:21:42 +0300

    Add more logging

--- pipe-cli/src/utilities/storage/gs.py
+++ pipe-cli/src/utilities/storage/gs.py
@@ -20,8 +20,10 @@ from datetime import datetime, timedelta
 
 from requests import RequestException
 from requests.adapters import HTTPAdapter
+from requests.exceptions import InvalidProxyURL
+from requests.utils import select_proxy, prepend_scheme_if_needed
 from urllib3 import PoolManager
-from urllib3.util import ssl_
+from urllib3.util import ssl_, parse_url
 import logging
 import ssl
 
@@ -673,6 +675,33 @@ class TlsAdapter(HTTPAdapter):
 
     def __init__(self,  **kwargs):
         super(TlsAdapter, self).__init__(**kwargs)
+
+    def get_connection(self, url, proxies=None):
+        """Returns a urllib3 connection for the given URL. This should not be
+        called from user code, and is only exposed for use when subclassing the
+        :class:`HTTPAdapter <requests.adapters.HTTPAdapter>`.
+
+        :param url: The URL to connect to.
+        :param proxies: (optional) A Requests-style dictionary of proxies used on this request.
+        :rtype: urllib3.ConnectionPool
+        """
+        proxy = select_proxy(url, proxies)
+        click.echo("Proxy: %s" % str(proxy))
+        if proxy:
+            proxy = prepend_scheme_if_needed(proxy, 'http')
+            proxy_url = parse_url(proxy)
+            if not proxy_url.host:
+                raise InvalidProxyURL("Please check proxy URL. It is malformed"
+                                      " and could be missing the host.")
+            proxy_manager = self.proxy_manager_for(proxy)
+            conn = proxy_manager.connection_from_url(url)
+        else:
+            # Only scheme should be lower case
+            parsed = urlparse(url)
+            url = parsed.geturl()
+            conn = self.poolmanager.connection_from_url(url)
+
+        return conn
 
     def init_poolmanager(self, *pool_args, **pool_kwargs):
         ctx = ssl_.create_urllib3_context(ssl.PROTOCOL_TLS)
