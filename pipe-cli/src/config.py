6ed0e6273134ca5d772103c9297fe17a0b700cd8
ekaterina_kazachkova <ekaterina_kazachkova@epam.com>
2020-08-21 14:35:56 +0300

    revert additional logging for config.py

--- pipe-cli/src/config.py
+++ pipe-cli/src/config.py
@@ -128,12 +128,10 @@ class Config(object):
 
     def resolve_proxy(self, target_url=None):
         if not self.proxy:
-            click.echo("no proxy")
             return None
         elif self.proxy == PROXY_TYPE_PAC:
-            pac_file = PacAPI.get_pac(url='http://127.0.0.1:9000/localproxy.pac')
+            pac_file = PacAPI.get_pac()
             if not pac_file:
-                click.echo("no pac file")
                 return None
             proxy_resolver = PacProxyResolver(pac_file)
             url_to_resolve = target_url
@@ -142,9 +140,7 @@ class Config(object):
             if not url_to_resolve:
                 url_to_resolve = PROXY_PAC_DEFAULT_URL
 
-            result = proxy_resolver.get_proxy_for_requests(url_to_resolve)
-            click.echo(result)
-            return result
+            return proxy_resolver.get_proxy_for_requests(url_to_resolve)
         elif self.proxy_ntlm:
             ntlm_proxy = network_utilities.NTLMProxy.get_proxy(self.build_ntlm_module_path(),
                                                                self.proxy_ntlm_domain,
@@ -152,17 +148,13 @@ class Config(object):
                                                                self.proxy_ntlm_pass,
                                                                self.proxy)
             ntlm_aps_proxy_url = ntlm_proxy.get_ntlm_aps_local_url()
-            result = {'http': ntlm_aps_proxy_url,
-                      'https': ntlm_aps_proxy_url,
-                      'ftp': ntlm_aps_proxy_url}
-            click.echo(result)
-            return result
+            return {'http': ntlm_aps_proxy_url,
+                    'https': ntlm_aps_proxy_url,
+                    'ftp': ntlm_aps_proxy_url}
         else:
-            result = {'http': self.proxy,
-                      'https': self.proxy,
-                      'ftp': self.proxy}
-            click.echo(result)
-            return result
+            return {'http': self.proxy,
+                    'https': self.proxy,
+                    'ftp': self.proxy}
 
     @classmethod
     def get_base_source_dir(cls):
