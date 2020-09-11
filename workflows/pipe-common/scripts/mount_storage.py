a0d5049866681b2ce2e45b3235989b10025a2e79
Andrei Tcibin <andrei_tcibin@epam.com>
2020-09-11 18:22:47 +0300

    Add missing fuse type parameter to GCPMounter.
    Relates to #1371.

--- workflows/pipe-common/scripts/mount_storage.py
+++ workflows/pipe-common/scripts/mount_storage.py
@@ -472,6 +472,7 @@ class GCPMounter(StorageMounter):
                 'path': self.get_path(),
                 'mask': mask,
                 'permissions': permissions,
+                'fuse_type': self.fuse_type,
                 'tmp_dir': self.fuse_tmp,
                 'credentials': creds_named_pipe_path,
                 'mount_timeout': mount_timeout
