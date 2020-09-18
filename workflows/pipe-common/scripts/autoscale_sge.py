540f4fb0da0f39d0b68281f6a1451d5449c757a6
Mariia_Zueva <mariia_zueva@epam.com>
2020-09-18 21:44:20 +0300

    Issue #1399 Storage for runs Shared FS - fix FS type for autoscaled workers

--- workflows/pipe-common/scripts/autoscale_sge.py
+++ workflows/pipe-common/scripts/autoscale_sge.py
@@ -537,7 +537,7 @@ class GridEngineScaleUpHandler:
 
     def __init__(self, cmd_executor, pipe, grid_engine, host_storage, instance_helper, parent_run_id, default_hostfile, instance_disk,
                  instance_image, price_type, region_id, polling_timeout=_POLL_TIMEOUT, polling_delay=_POLL_DELAY,
-                 instance_family=None):
+                 instance_family=None, shared_fs_type='lfs'):
         """
         Grid engine scale up implementation. It handles additional nodes launching and hosts configuration (/etc/hosts
         and self.default_hostfile).
@@ -557,6 +557,7 @@ class GridEngineScaleUpHandler:
         :param polling_delay: Polling delay - in seconds.
         :param instance_family: Instance family for launching additional instance,
                 e.g. c5 means that you can run instances like c5.large, c5.xlarge etc.
+        :param shared_fs_type: Type of shared fs to initialize
         """
         self.executor = cmd_executor
         self.pipe = pipe
@@ -572,6 +573,7 @@ class GridEngineScaleUpHandler:
         self.polling_timeout = polling_timeout
         self.polling_delay = polling_delay
         self.instance_family = instance_family
+        self.shared_fs_type = shared_fs_type
 
     def scale_up(self, resource):
         """
@@ -621,8 +623,9 @@ class GridEngineScaleUpHandler:
                            'CP_CAP_AUTOSCALE false ' \
                            'CP_CAP_AUTOSCALE_WORKERS 0 ' \
                            'CP_DISABLE_RUN_ENDPOINTS true ' \
+                           'CP_CAP_SHARE_FS_TYPE %s ' \
                            % (self.instance_disk, instance, self.instance_image, self.parent_run_id,
-                              self._pipe_cli_price_type(self.price_type), self.region_id)
+                              self._pipe_cli_price_type(self.price_type), self.region_id, self.shared_fs_type)
         run_id = int(self.executor.execute_to_lines(pipe_run_command)[0])
         Logger.info('Additional worker run id is %s.' % run_id)
         return run_id
@@ -1325,6 +1328,7 @@ if __name__ == '__main__':
     hybrid_instance_cores = int(os.getenv('CP_CAP_AUTOSCALE_HYBRID_MAX_CORE_PER_NODE', sys.maxint))
     instance_family = os.getenv('CP_CAP_AUTOSCALE_HYBRID_FAMILY',
                                 CloudPipelineInstanceHelper.get_family_from_type(cloud_provider, instance_type))
+    shared_fs_type = os.getenv('CP_CAP_SHARE_FS_TYPE', 'lfs')
 
     # TODO: Replace all the usages of PipelineAPI raw client with an actual CloudPipelineAPI client
     pipe = PipelineAPI(api_url=pipeline_api, log_dir=os.path.join(shared_work_dir, '.pipe.log'))
@@ -1362,7 +1366,8 @@ if __name__ == '__main__':
                                                 instance_disk=instance_disk, instance_image=instance_image,
                                                 price_type=price_type, region_id=region_id,
                                                 polling_timeout=scale_up_polling_timeout,
-                                                instance_family=instance_family)
+                                                instance_family=instance_family,
+                                                shared_fs_type=shared_fs_type)
     scale_down_handler = GridEngineScaleDownHandler(cmd_executor=cmd_executor, grid_engine=grid_engine,
                                                     default_hostfile=default_hostfile)
     worker_validator = GridEngineWorkerValidator(cmd_executor=cmd_executor, api=api, host_storage=host_storage,
