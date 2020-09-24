9085071fcdf0216955c750f0af10d907bd9d081d
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2020-09-24 17:30:43 +0300

    Use the env vars to configure timeouts for the Python scripts to API calls

--- workflows/pipe-common/pipeline/api/api.py
+++ workflows/pipe-common/pipeline/api/api.py
@@ -203,7 +203,9 @@ class PipelineAPI:
     RESPONSE_STATUS_OK = 'OK'
     MAX_PAGE_SIZE = 400
 
-    def __init__(self, api_url, log_dir, attempts=3, timeout=5, connection_timeout=10):
+    def __init__(self, api_url, log_dir,
+                attempts=os.getenv('CP_PY_API_TRY_COUNT', 3), timeout=os.getenv('CP_PY_API_TRY_TIMEOUT', 5),
+                connection_timeout=os.getenv('CP_PY_API_CONN_TIMEOUT', 10)):
         urllib3.disable_warnings()
         token = os.environ.get('API_TOKEN')
         self.api_url = api_url
