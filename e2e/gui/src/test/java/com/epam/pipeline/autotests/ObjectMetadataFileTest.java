68a698a2930a3b89c9edb366b28835ec577377e6
Yulia Kamyshova <yulia_kamyshova@epam.com>
2020-10-20 20:02:09 +0300

    e2e: update run ids

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/ObjectMetadataFileTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/ObjectMetadataFileTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,8 @@ public class ObjectMetadataFileTest extends AbstractBfxPipelineTest implements A
                 .library()
                 .createStorage(bucket)
                 .selectStorage(bucket)
-                .uploadFile(getFile(fileLessThan10kb));
+                .uploadFile(getFile(fileLessThan10kb))
+                .sleep(30, SECONDS);
     }
 
     @AfterClass
