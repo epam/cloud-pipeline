824070081ffb08d3aa15dbe28444f94b275c2d10
Andrey Georgitsa <andrey_georgitsa@epam.com>
2020-10-27 20:06:53 +0300

    Issue #1519 Configure Quartz to work in HA environments

--- api/src/main/java/com/epam/pipeline/app/AppConfiguration.java
+++ api/src/main/java/com/epam/pipeline/app/AppConfiguration.java
@@ -16,12 +16,16 @@
 
 package com.epam.pipeline.app;
 
+import static org.quartz.impl.StdSchedulerFactory.PROP_JOB_STORE_PREFIX;
+
 import com.epam.pipeline.common.MessageHelper;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import com.epam.pipeline.manager.scheduling.AutowiringSpringBeanJobFactory;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -38,6 +42,7 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.security.concurrent.DelegatingSecurityContextExecutor;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import javax.sql.DataSource;
@@ -54,6 +59,10 @@ import javax.sql.DataSource;
 public class AppConfiguration implements SchedulingConfigurer {
 
     private static final int MAX_LOG_PAYLOAD_LENGTH = 1000;
+    private static final String JOB_STORE_IS_CLUSTERED = ".isClustered";
+    private static final String JOB_STORE_CLUSTER_CHECKIN_INTERVAL = ".clusterCheckinInterval";
+    private static final String JOB_STORE_CLUSTER_MISFIRE_THRESHOLD = ".misfireThreshold";
+    private static final String JOB_STORE_DRIVER_DELEGATE_CLASS = ".driverDelegateClass";
 
     @Value("${scheduled.pool.size:5}")
     private int scheduledPoolSize;
@@ -86,11 +95,38 @@ public class AppConfiguration implements SchedulingConfigurer {
     }
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(final ApplicationContext applicationContext) {
+    @ConditionalOnProperty(name = "ha.deploy.enabled", havingValue = "false", matchIfMissing = true)
+    public SchedulerFactoryBean schedulerFactoryBeanInMemory(final ApplicationContext applicationContext) {
         final SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
         final AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
         jobFactory.setApplicationContext(applicationContext);
         schedulerFactory.setJobFactory(jobFactory);
+        return schedulerFactory;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "ha.deploy.enabled", havingValue = "true")
+    public SchedulerFactoryBean schedulerFactoryBeanPersistent(final ApplicationContext applicationContext,
+                                                               final DataSource dataSource,
+                                                               @Value("${scheduled.ha.checkin.interval:25000}")
+                                                                   final String checkInInterval,
+                                                               @Value("${scheduled.ha.misfire.threshold:60000}")
+                                                                   final String misfireThreshold,
+                                                               @Value("${scheduled.ha.db.driverDelegateClass}")
+                                                                   final String driverDelegateClass) {
+        final SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        final AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        schedulerFactory.setJobFactory(jobFactory);
+        schedulerFactory.setDataSource(dataSource);
+        final Properties quartzProperties = new Properties();
+        quartzProperties.setProperty(PROP_JOB_STORE_PREFIX + JOB_STORE_IS_CLUSTERED, "true");
+        quartzProperties.setProperty(PROP_JOB_STORE_PREFIX + JOB_STORE_CLUSTER_CHECKIN_INTERVAL, checkInInterval);
+        quartzProperties.setProperty(PROP_JOB_STORE_PREFIX + JOB_STORE_DRIVER_DELEGATE_CLASS, driverDelegateClass);
+        quartzProperties.setProperty(PROP_JOB_STORE_PREFIX + JOB_STORE_CLUSTER_MISFIRE_THRESHOLD, misfireThreshold);
+        quartzProperties.setProperty(StdSchedulerFactory.PROP_SCHED_INSTANCE_ID,
+                                     StdSchedulerFactory.AUTO_GENERATE_INSTANCE_ID);
+        schedulerFactory.setQuartzProperties(quartzProperties);
         return schedulerFactory;
     }
 
