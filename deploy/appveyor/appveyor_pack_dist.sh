d66382a4e08eb193d90eb93e12bf3032a55cc0bd
Aleksandr Sidoruk <Aleksandr_Sidoruk@epam.com>
2020-11-06 17:46:01 +0300

    AppVeyor shall use a single build command

---
+++ deploy/appveyor/appveyor_pack_dist.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+API_STATIC_PATH=api/src/main/resources/static
+rm -rf ${API_STATIC_PATH}/*
+rm -rf build/install/dist/*
+
+
+_BUILD_DOCKER_IMAGE="lifescience/cloud-pipeline:python2.7-centos6" ./gradlew -PbuildNumber=${APPVEYOR_BUILD_NUMBER}.${APPVEYOR_REPO_COMMIT} -Pprofile=release pipe-cli:buildLinux --no-daemon -x :pipe-cli:test
+mv pipe-cli/dist/dist-file/pipe ${API_STATIC_PATH}/pipe-el6
+mv pipe-cli/dist/dist-folder/pipe.tar.gz ${API_STATIC_PATH}/pipe-el6.tar.gz
+
+./gradlew distTar   -PbuildNumber=${APPVEYOR_BUILD_NUMBER}.${APPVEYOR_REPO_COMMIT} \
+                    -Pprofile=release \
+                    -x test \
+                    -Pfast \
+                    --no-daemon
+
+if [ "$APPVEYOR_REPO_NAME" == "epam/cloud-pipeline" ]; then
+    DIST_TGZ_NAME=$(echo build/install/dist/cloud-pipeline*)
+
+    # Publish repackaged distribution tgz to S3 into builds/ prefix
+    # Only if it is one of the allowed branches and it is a push (not PR)
+    if [ "$APPVEYOR_REPO_BRANCH" == "develop" ] || [ "$APPVEYOR_REPO_BRANCH" == "master" ] || [[ "$APPVEYOR_REPO_BRANCH" == "release/"* ]] || [[ "$APPVEYOR_REPO_BRANCH" == "stage/"* ]]; then
+            aws s3 cp $DIST_TGZ_NAME s3://cloud-pipeline-oss-builds/builds/${APPVEYOR_REPO_BRANCH}/
+    fi
+fi
--- deploy/travis/travis_publish_docs.sh
+++ deploy/travis/travis_publish_docs.sh
@@ -17,26 +17,27 @@
 set -e 
 
 # Publish docs only if it is one of the allowed branches and it is a push (not PR)
-if [ "$APPVEYOR_REPO_NAME" != "epam/cloud-pipeline" ] || \
-    [ "$APPVEYOR_REPO_BRANCH" != "develop" ]; then
+if [ "$TRAVIS_REPO_SLUG" != "epam/cloud-pipeline" ] || \
+    [ "$TRAVIS_BRANCH" != "develop" ] || \
+    [ "$TRAVIS_EVENT_TYPE" != "push" ]; then
     echo "Skipping docs publishing, as a build is not triggered from the correct repo/branch via push"
     exit 0
 fi
 
 # Build docs - it will produce a tar.gz file in ./dist/
 echo "Building docs"
-./gradlew -PbuildNumber=${APPVEYOR_BUILD_NUMBER}.${APPVEYOR_REPO_COMMIT} -Pprofile=release buildDoc --no-daemon
+./gradlew -PbuildNumber=${TRAVIS_BUILD_NUMBER}.${TRAVIS_COMMIT} -Pprofile=release buildDoc --no-daemon
 mkdir -p $HOME/cloud-pipeline-docs
 tar -zxf dist/cloud-pipeline-docs.tar.gz -C $HOME/cloud-pipeline-docs
 
 # Setup git with "travis" committer name
 cd $HOME
-git config --global user.email "ci@epam.com"
-git config --global user.name "ci"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
 
 # Clone gh-pages and replace with new docs
 echo "Preparing gh-pages branch"
-git clone --quiet --branch=gh-pages https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/epam/cloud-pipeline.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/epam/cloud-pipeline.git gh-pages > /dev/null
 cd gh-pages
 find . -maxdepth 1 ! -name '.git' ! -name '.' ! -name '..' ! -name 'CNAME' -exec rm -rf {} +
 cp -Rf $HOME/cloud-pipeline-docs/* ./
@@ -44,7 +45,7 @@ cp -Rf $HOME/cloud-pipeline-docs/* ./
 # Add files, commit and force push
 echo "Pushing gh-pages branch"
 git add -f .
-git commit -m "Docs update via CI Build $APPVEYOR_BUILD_NUMBER"
+git commit -m "Docs update via Travis Build $TRAVIS_BUILD_NUMBER"
 git push -fq origin gh-pages > /dev/null
 
 echo "Done publishing docs"
