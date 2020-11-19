a569d89261aeb46d1ee307eba221028e515119d2
Mariia_Zueva <mariia_zueva@epam.com>
2020-11-19 21:36:21 +0300

    VMMonitor PMD fix

--- vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/notification/VMNotificationServiceImpl.java
+++ vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/notification/VMNotificationServiceImpl.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
