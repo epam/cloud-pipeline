dec5a75db7261c12360c9d34414f2af187c8f4f1
Mariia_Zueva <mariia_zueva@epam.com>
2020-11-25 16:30:06 +0300

    Fix duplicated constants in tests

--- api/src/test/java/com/epam/pipeline/test/creator/CommonCreatorConstants.java
+++ api/src/test/java/com/epam/pipeline/test/creator/CommonCreatorConstants.java
@@ -26,15 +26,13 @@ import java.util.Set;
 
 public final class CommonCreatorConstants {
 
-    public static final TypeReference<Result<String>> STRING_INSTANCE_TYPE =
-            new TypeReference<Result<String>>() {};
-    public static final TypeReference<Result<Boolean>> BOOLEAN_INSTANCE_TYPE =
-            new TypeReference<Result<Boolean>>() {};
-    public static final TypeReference<Result<Long>> LONG_INSTANCE_TYPE =
-            new TypeReference<Result<Long>>() {};
+    public static final TypeReference<Result<Integer>> INTEGER_TYPE = new TypeReference<Result<Integer>>() {};
+    public static final TypeReference<Result<Object>> OBJECT_TYPE = new TypeReference<Result<Object>>() {};
+    public static final TypeReference<Result<String>> STRING_INSTANCE_TYPE = new TypeReference<Result<String>>() {};
+    public static final TypeReference<Result<Boolean>> BOOLEAN_INSTANCE_TYPE = new TypeReference<Result<Boolean>>() {};
+    public static final TypeReference<Result<Long>> LONG_INSTANCE_TYPE = new TypeReference<Result<Long>>() {};
     public static final TypeReference<Result<Map<String, String>>> STRING_MAP_INSTANCE_TYPE =
             new TypeReference<Result<Map<String, String>>>() {};
-
     public static final long ID = 1L;
     public static final long ID_2 = 2L;
     public static final int TEST_INT = 4;
@@ -46,12 +44,6 @@ public final class CommonCreatorConstants {
     public static final byte[] TEST_ARRAY = {1, 1, 1};
     public static final Map<String, String> TEST_STRING_MAP = Collections.singletonMap(TEST_STRING, TEST_STRING);
     public static final Set<String> TEST_STRING_SET = Collections.singleton(TEST_STRING);
-
-    public static final TypeReference<Result<Integer>> INTEGER_TYPE = new TypeReference<Result<Integer>>() {};
-    public static final TypeReference<Result<Object>> OBJECT_TYPE = new TypeReference<Result<Object>>() {};
-    public static final TypeReference<Result<String>> STRING_TYPE = new TypeReference<Result<String>>() {};
-    public static final TypeReference<Result<Map<String, String>>> STRING_STRING_MAP_TYPE =
-            new TypeReference<Result<Map<String, String>>>() {};
 
     private CommonCreatorConstants() {
 
