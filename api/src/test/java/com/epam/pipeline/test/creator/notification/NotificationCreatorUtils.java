da434e33fa4744c876a94d310078489f1e7c1fb3
Mikhail_Iniakin <mikhail_iniakin@epam.com>
2020-11-25 18:31:10 +0300

    Issue #1405: Implemented tests for SystemNotificationController

--- api/src/test/java/com/epam/pipeline/test/creator/notification/NotificationCreatorUtils.java
+++ api/src/test/java/com/epam/pipeline/test/creator/notification/NotificationCreatorUtils.java
@@ -17,14 +17,21 @@
 package com.epam.pipeline.test.creator.notification;
 
 import com.epam.pipeline.controller.Result;
+import com.epam.pipeline.controller.vo.SystemNotificationFilterVO;
 import com.epam.pipeline.controller.vo.notification.NotificationMessageVO;
 import com.epam.pipeline.entity.notification.NotificationMessage;
 import com.epam.pipeline.entity.notification.NotificationSettings;
 import com.epam.pipeline.entity.notification.NotificationTemplate;
+import com.epam.pipeline.entity.notification.SystemNotification;
+import com.epam.pipeline.entity.notification.SystemNotificationConfirmation;
+import com.epam.pipeline.entity.notification.SystemNotificationConfirmationRequest;
+import com.epam.pipeline.entity.notification.SystemNotificationSeverity;
+import com.epam.pipeline.entity.notification.SystemNotificationState;
 import com.epam.pipeline.entity.pipeline.TaskStatus;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import static com.epam.pipeline.test.creator.CommonCreatorConstants.ID;
@@ -42,6 +49,12 @@ public final class NotificationCreatorUtils {
             new TypeReference<Result<NotificationTemplate>>() { };
     public static final TypeReference<Result<List<NotificationTemplate>>> NOTIFICATION_TEMPLATE_LIST_TYPE =
             new TypeReference<Result<List<NotificationTemplate>>>() { };
+    public static final TypeReference<Result<SystemNotification>> SYSTEM_NOTIFICATION_TYPE =
+            new TypeReference<Result<SystemNotification>>() { };
+    public static final TypeReference<Result<List<SystemNotification>>> SYSTEM_NOTIFICATION_LIST_TYPE =
+            new TypeReference<Result<List<SystemNotification>>>() { };
+    public static final TypeReference<Result<SystemNotificationConfirmation>> CONFIRMATION_TYPE =
+            new TypeReference<Result<SystemNotificationConfirmation>>() { };
     private static final List<Long> LONG_LIST = Collections.singletonList(ID);
 
     private NotificationCreatorUtils() {
@@ -79,5 +92,33 @@ public final class NotificationCreatorUtils {
         notificationTemplate.setName(TEST_STRING);
         notificationTemplate.setSubject(TEST_STRING);
         return notificationTemplate;
+    }
+
+    public static SystemNotification getSystemNotification() {
+        final SystemNotification systemNotification = new SystemNotification();
+        systemNotification.setBlocking(true);
+        systemNotification.setBody(TEST_STRING);
+        systemNotification.setCreatedDate(new Date());
+        systemNotification.setNotificationId(ID);
+        systemNotification.setSeverity(SystemNotificationSeverity.INFO);
+        systemNotification.setState(SystemNotificationState.ACTIVE);
+        systemNotification.setTitle(TEST_STRING);
+        return systemNotification;
+    }
+
+    public static SystemNotificationFilterVO getSystemNotificationFilterVO() {
+        final SystemNotificationFilterVO systemNotificationFilterVO = new SystemNotificationFilterVO();
+        systemNotificationFilterVO.setCreatedDateAfter(new Date());
+        systemNotificationFilterVO.setSeverityList(Collections.singletonList(SystemNotificationSeverity.CRITICAL));
+        systemNotificationFilterVO.setStateList(Collections.singletonList(SystemNotificationState.ACTIVE));
+        return systemNotificationFilterVO;
+    }
+
+    public static SystemNotificationConfirmation getSystemNotificationConfirmation() {
+        return new SystemNotificationConfirmation(ID, TEST_STRING, TEST_STRING, TEST_STRING, new Date());
+    }
+
+    public static SystemNotificationConfirmationRequest getSystemNotificationConfirmationRequest() {
+        return new SystemNotificationConfirmationRequest();
     }
 }
