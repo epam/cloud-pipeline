0531209ba38e986bc176947a1be07168e24c4483
Mikhail_Iniakin <mikhail_iniakin@epam.com>
2020-12-18 14:32:38 +0300

    Issue #1652: Pool entity added to common module

---
+++ cloud-pipeline-common/model/src/main/java/com/epam/pipeline/entity/cluster/pool/NodePool.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.pipeline.entity.cluster.pool;
+
+import com.epam.pipeline.entity.cluster.PriceType;
+import com.epam.pipeline.entity.cluster.pool.filter.PoolFilter;
+import com.epam.pipeline.entity.pipeline.RunInstance;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
+
+@Data
+public class NodePool {
+
+    private Long id;
+    private String name;
+    private LocalDateTime created;
+    private Long regionId;
+    private String instanceType;
+    private int instanceDisk;
+    private PriceType priceType;
+    private Set<String> dockerImages;
+    private String instanceImage;
+    private int count;
+    private NodeSchedule schedule;
+    private PoolFilter filter;
+
+    public boolean isActive(final LocalDateTime timestamp) {
+        if (count == 0) {
+            return false;
+        }
+        return Optional.ofNullable(schedule)
+                .map(s -> s.isActive(timestamp))
+                .orElse(false);
+    }
+
+    @Override
+    public String toString() {
+        return "NodePool{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", regionId=" + regionId +
+                ", instanceType='" + instanceType + '\'' +
+                ", instanceDisk=" + instanceDisk +
+                ", priceType=" + priceType +
+                ", dockerImages=" + dockerImages +
+                ", instanceImage='" + instanceImage + '\'' +
+                ", count=" + count +
+                '}';
+    }
+
+    public RunningInstance toRunningInstance() {
+        final RunningInstance runningInstance = new RunningInstance();
+        runningInstance.setInstance(toRunInstance());
+        runningInstance.setPrePulledImages(dockerImages);
+        runningInstance.setPool(this);
+        return runningInstance;
+    }
+
+    public RunInstance toRunInstance() {
+        final RunInstance runInstance = new RunInstance();
+        runInstance.setNodeType(instanceType);
+        runInstance.setCloudRegionId(regionId);
+        runInstance.setNodeDisk(instanceDisk);
+        runInstance.setEffectiveNodeDisk(instanceDisk);
+        runInstance.setSpot(PriceType.SPOT.equals(priceType));
+        runInstance.setNodeImage(instanceImage);
+        runInstance.setPrePulledDockerImages(dockerImages);
+        return runInstance;
+    }
+}
---
+++ cloud-pipeline-common/model/src/main/java/com/epam/pipeline/entity/cluster/pool/filter/value/MapValueMatcher.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.pipeline.entity.cluster.pool.filter.value;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Map;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Getter
+public class MapValueMatcher implements ValueMatcher<Map<String, String>> {
+
+    private final Map<String, String> value;
+
+    @Override
+    public boolean matches(final Map<String, String> anotherValue) {
+        final Map<String, String> currentValue = getValue();
+        if (Objects.isNull(currentValue) || Objects.isNull(anotherValue)) {
+            return false;
+        }
+        return currentValue.entrySet()
+                .stream()
+                .allMatch(entry -> {
+                    final String value = anotherValue.get(entry.getKey());
+                    if (Objects.isNull(value)) {
+                        return false;
+                    }
+                    return Objects.equals(value, entry.getValue());
+                });
+    }
+
+    /**
+     * @param anotherValue
+     * @return {@code true} if current map does not contain any value for
+     * any of {@param anotherValue} keys or does not contain key at all
+     */
+    @Override
+    public boolean empty(final Map<String, String> anotherValue) {
+        final Map<String, String> currentValue = getValue();
+        if (Objects.isNull(currentValue)) {
+            return Objects.nonNull(anotherValue);
+        }
+        return MapUtils.emptyIfNull(currentValue)
+                .keySet()
+                .stream()
+                .allMatch(key -> {
+                    final String value = anotherValue.get(key);
+                    return StringUtils.isBlank(value);
+                });
+    }
+}
