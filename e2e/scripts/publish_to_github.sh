6e9542e31f62301d727aa39de1839a292a2d2483
Andrei Tcibin <andrei_tcibin@epam.com>
2021-01-15 13:44:59 +0300

    Fix environment variables in publish script

--- e2e/scripts/publish_to_github.sh
+++ e2e/scripts/publish_to_github.sh
@@ -92,7 +92,7 @@ if [[ -z "$GITHUB_EXPORT_PATH" ]]; then echo "Option --destination is missing"; 
 GITHUB_EXPORT_DESTINATION="${GITHUB_EXPORT_TMPDIR}/${GITHUB_EXPORT_PATH}"
 
 rm -rf "${GITHUB_EXPORT_TMPDIR}"
-git clone "https://$CP_AUTO_USERNAME:$CP_AUTO_PASSWORD@github.com/epam/cloud-pipeline.git" -b "${GITHUB_EXPORT_BRANCH}" "${GITHUB_EXPORT_TMPDIR}"
+git clone "https://$GITHUB_EXPORT_USERNAME:$GITHUB_EXPORT_PASSWORD@github.com/epam/cloud-pipeline.git" -b "${GITHUB_EXPORT_BRANCH}" "${GITHUB_EXPORT_TMPDIR}"
 if [[ ! -d "${GITHUB_EXPORT_TMPDIR}" ]]; then echo "Cloning has failed"; exit 1; fi
 rm -rf "${GITHUB_EXPORT_DESTINATION}"
 mkdir -p "$(dirname "${GITHUB_EXPORT_DESTINATION}")"
