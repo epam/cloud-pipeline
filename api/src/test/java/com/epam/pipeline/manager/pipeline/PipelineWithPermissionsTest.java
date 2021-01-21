df4d2b982d460f8b72f3943e5cdbd7e958b05abf
Mariia_Zueva <mariia_zueva@epam.com>
2021-01-21 14:57:48 +0300

    Issue #1736 Draft test implemented

--- api/src/test/java/com/epam/pipeline/manager/pipeline/PipelineWithPermissionsTest.java
+++ api/src/test/java/com/epam/pipeline/manager/pipeline/PipelineWithPermissionsTest.java
@@ -16,144 +16,158 @@
 
 package com.epam.pipeline.manager.pipeline;
 
-import com.epam.pipeline.app.TestApplicationWithAclSecurity;
 import com.epam.pipeline.controller.vo.PermissionGrantVO;
-import com.epam.pipeline.dao.pipeline.FolderDao;
-import com.epam.pipeline.dao.pipeline.PipelineDao;
-import com.epam.pipeline.dao.util.AclTestDao;
+import com.epam.pipeline.entity.AbstractSecuredEntity;
 import com.epam.pipeline.entity.pipeline.Folder;
 import com.epam.pipeline.entity.pipeline.Pipeline;
 import com.epam.pipeline.entity.pipeline.PipelineWithPermissions;
 import com.epam.pipeline.entity.security.acl.AclClass;
 import com.epam.pipeline.entity.security.acl.AclPermissionEntry;
-import com.epam.pipeline.manager.AbstractManagerTest;
+import com.epam.pipeline.manager.EntityManager;
 import com.epam.pipeline.manager.security.GrantPermissionManager;
+import com.epam.pipeline.mapper.PipelineWithPermissionsMapper;
 import com.epam.pipeline.security.acl.AclPermission;
+import com.epam.pipeline.test.acl.AbstractAclTest;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static com.epam.pipeline.manager.ObjectCreatorUtils.constructPipeline;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
 
-@DirtiesContext
-@ContextConfiguration(classes = TestApplicationWithAclSecurity.class)
-public class PipelineWithPermissionsTest extends AbstractManagerTest {
+public class PipelineWithPermissionsTest extends AbstractAclTest {
     private static final String TEST_FOLDER1 = "testFolder1";
     private static final String TEST_FOLDER2 = "testFolder2";
-    private static final String TEST_OWNER1 = "testUser1";
-    private static final String TEST_OWNER2 = "testUser2";
+    private static final String TEST_OWNER1 = "owner1";
+    private static final String TEST_OWNER2 = "owner2";
     private static final String TEST_PIPELINE1 = "Pipeline1";
     private static final String TEST_PIPELINE2 = "Pipeline2";
     private static final String TEST_PIPELINE_REPO = "///";
     private static final String TEST_PIPELINE_REPO_SSH = "git@test";
+    private static final String TEST_USER_1 = "user1";
+    private static final String TEST_USER_2 = "user2";
 
     @Autowired
-    private AclTestDao aclTestDao;
-
-    @Autowired
-    private FolderDao folderDao;
-
-    @Autowired
-    private PipelineDao pipelineDao;
+    private EntityManager mockEntityManager;
 
     @Autowired
     private GrantPermissionManager permissionManager;
+
+    @Autowired
+    private PipelineWithPermissionsMapper mockPipelineWithPermissionsMapper;
 
     private Folder folder1;
     private Folder folder2;
     private Pipeline pipeline1;
     private Pipeline pipeline2;
-    private AclTestDao.AclSid testUserSid1;
-    private AclTestDao.AclSid testUserSid2;
 
     @Before
     public void setUp() {
-        AclTestDao.AclClass folderAclClass = new AclTestDao.AclClass(Folder.class.getCanonicalName());
-        aclTestDao.createAclClassIfNotPresent(folderAclClass);
-
-        AclTestDao.AclClass pipelineAclClass = new AclTestDao.AclClass(Pipeline.class.getCanonicalName());
-        aclTestDao.createAclClassIfNotPresent(pipelineAclClass);
-
-        testUserSid1 = new AclTestDao.AclSid(true, TEST_OWNER1);
-        aclTestDao.createAclSid(testUserSid1);
-
-        testUserSid2 = new AclTestDao.AclSid(true, TEST_OWNER2);
-        aclTestDao.createAclSid(testUserSid2);
-
         folder1 = getFolder(TEST_FOLDER1, TEST_OWNER1);
-        folderDao.createFolder(folder1);
+        folder1.setId(1L);
 
         folder2 = getFolder(TEST_FOLDER2, TEST_OWNER2);
-        folder2.setParentId(folder1.getId());
-        folderDao.createFolder(folder2);
+        folder2.setId(2L);
 
         pipeline1 = constructPipeline(TEST_PIPELINE1, TEST_PIPELINE_REPO, TEST_PIPELINE_REPO_SSH, folder2.getId());
+        pipeline1.setId(1L);
         pipeline1.setOwner(TEST_OWNER1);
-        pipelineDao.createPipeline(pipeline1);
 
         pipeline2 = constructPipeline(TEST_PIPELINE2, TEST_PIPELINE_REPO, TEST_PIPELINE_REPO_SSH, folder1.getId());
+        pipeline2.setId(2L);
         pipeline2.setOwner(TEST_OWNER1);
-        pipelineDao.createPipeline(pipeline2);
     }
 
     @Test
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @WithMockUser(username = TEST_OWNER1)
-    public void testMergePermissions() {
+    public void shouldReturnPermissionsWhenGrantedToPipeline() {
         // key - pipelineId, value - <sid, mask>
-        Map<Long, Map<String, Integer>> expectedMap = new HashMap<>();
-        HashMap<String, Integer> permissionsForPipeline1 = new HashMap<>();
-        HashMap<String, Integer> permissionsForPipeline2 = new HashMap<>();
+        final Map<Long, Map<String, Integer>> expectedMap = new HashMap<>();
+        final Map<String, Integer> permissionsForPipeline1 = new HashMap<>();
+        final Map<String, Integer> permissionsForPipeline2 = new HashMap<>();
 
-        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid1.getSid(), folder1.getId());
-        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid1.getSid(), folder2.getId());
-        grantPermissionsToPipeline(AclPermission.WRITE.getMask(), testUserSid1.getSid(), pipeline1.getId());
-        grantPermissionsToPipeline(AclPermission.READ.getMask(), testUserSid1.getSid(), pipeline2.getId());
 
-        permissionsForPipeline1.put(TEST_OWNER1.toUpperCase(), 5);
-        permissionsForPipeline2.put(TEST_OWNER1.toUpperCase(), 1);
+        final List<Pair<AbstractSecuredEntity, List<AbstractGrantPermission>>> pipelines = Arrays.asList(
+                new ImmutablePair<>(pipeline1, Collections.singletonList(new UserPermission(TEST_USER_1, AclPermission.WRITE.getMask()))),
+                new ImmutablePair<>(pipeline2, Collections.singletonList(new UserPermission(TEST_USER_1, AclPermission.READ.getMask()))));
+
+        initAclEntity(pipelines);
+
+        doReturn(Arrays.asList(pipeline1, pipeline2))
+                .when(mockEntityManager)
+                .loadAllWithParents(eq(AclClass.PIPELINE), any(), any());
+
+        final PipelineWithPermissions pipelineWithPermissions1 = new PipelineWithPermissions();
+        pipelineWithPermissions1.setId(pipeline1.getId());
+        pipelineWithPermissions1.setOwner(pipeline1.getOwner());
+        doReturn(pipelineWithPermissions1).when(mockPipelineWithPermissionsMapper).toPipelineWithPermissions(eq(pipeline1));
+
+        final PipelineWithPermissions pipelineWithPermissions2 = new PipelineWithPermissions();
+        pipelineWithPermissions2.setId(pipeline2.getId());
+        pipelineWithPermissions2.setOwner(pipeline2.getOwner());
+        doReturn(pipelineWithPermissions2).when(mockPipelineWithPermissionsMapper).toPipelineWithPermissions(eq(pipeline2));
+
+        doReturn(2).when(mockEntityManager).loadTotalCount(eq(AclClass.PIPELINE));
+
+        permissionsForPipeline1.put(TEST_USER_1.toUpperCase(), 4);
+        permissionsForPipeline2.put(TEST_USER_1.toUpperCase(), 1);
         expectedMap.put(pipeline1.getId(), permissionsForPipeline1);
         expectedMap.put(pipeline2.getId(), permissionsForPipeline2);
+
         assertPipelineWithPermissions(expectedMap);
 
-        grantPermissionsToPipeline(AclPermission.WRITE.getMask(), testUserSid2.getSid(), pipeline1.getId());
-        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 4);
-        assertPipelineWithPermissions(expectedMap);
+//        case0
+//        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid1.getSid(), folder1.getId());
+//        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid1.getSid(), folder2.getId());
+//        grantPermissionsToPipeline(AclPermission.WRITE.getMask(), testUserSid1.getSid(), pipeline1.getId());
+//        grantPermissionsToPipeline(AclPermission.READ.getMask(), testUserSid1.getSid(), pipeline2.getId());
 
-        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid2.getSid(), folder1.getId());
-        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 5);
-        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 1);
-        assertPipelineWithPermissions(expectedMap);
 
-        grantPermissionToFolder(AclPermission.NO_READ.getMask(), testUserSid2.getSid(), folder1.getId());
-        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid2.getSid(), folder2.getId());
-        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 2);
-        assertPipelineWithPermissions(expectedMap);
+//        case1
+//        grantPermissionsToPipeline(AclPermission.WRITE.getMask(), testUserSid2.getSid(), pipeline1.getId());
+//        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 4);
+//        assertPipelineWithPermissions(expectedMap);
 
-        grantPermissionToFolder(AclPermission.WRITE.getMask(), testUserSid2.getSid(), folder1.getId());
-        grantPermissionsToPipeline(AclPermission.NO_WRITE.getMask(), testUserSid2.getSid(), pipeline1.getId());
-        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 9);
-        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 4);
-        assertPipelineWithPermissions(expectedMap);
+//        case3
+//        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid2.getSid(), folder1.getId());
+//        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 5);
+//        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 1);
+//        assertPipelineWithPermissions(expectedMap);
+
+//        case4
+//        grantPermissionToFolder(AclPermission.NO_READ.getMask(), testUserSid2.getSid(), folder1.getId());
+//        grantPermissionToFolder(AclPermission.READ.getMask(), testUserSid2.getSid(), folder2.getId());
+//        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 2);
+//        assertPipelineWithPermissions(expectedMap);
+
+//        case5
+//        grantPermissionToFolder(AclPermission.WRITE.getMask(), testUserSid2.getSid(), folder1.getId());
+//        grantPermissionsToPipeline(AclPermission.NO_WRITE.getMask(), testUserSid2.getSid(), pipeline1.getId());
+//        permissionsForPipeline1.put(TEST_OWNER2.toUpperCase(), 9);
+//        permissionsForPipeline2.put(TEST_OWNER2.toUpperCase(), 4);
+//        assertPipelineWithPermissions(expectedMap);
     }
 
-    private void assertPipelineWithPermissions(Map<Long, Map<String, Integer>> expectedMap) {
-        Set<PipelineWithPermissions> loaded = permissionManager.loadAllPipelinesWithPermissions(null, null)
+    private void assertPipelineWithPermissions(final Map<Long, Map<String, Integer>> expectedMap) {
+        final Set<PipelineWithPermissions> loaded = permissionManager.loadAllPipelinesWithPermissions(null, null)
                 .getPipelines();
         loaded.forEach(pipelineWithPermissions -> {
-            Map<String, Integer> expectedPermissions = expectedMap.get(pipelineWithPermissions.getId());
+            final Map<String, Integer> expectedPermissions = expectedMap.get(pipelineWithPermissions.getId());
             Set<AclPermissionEntry> actualPermissions = pipelineWithPermissions.getPermissions();
             assertEquals(expectedPermissions.size(), actualPermissions.size());
             actualPermissions.forEach(permission -> {
-                String sid = permission.getSid().getName().toUpperCase();
+                final String sid = permission.getSid().getName().toUpperCase();
                 assertEquals(expectedPermissions.get(sid), permission.getMask());
             });
         });
@@ -177,8 +191,8 @@ public class PipelineWithPermissionsTest extends AbstractManagerTest {
         permissionManager.setPermissions(grantVO);
     }
 
-    private Folder getFolder(String name, String owner) {
-        Folder folder = new Folder();
+    private Folder getFolder(final String name, final String owner) {
+        final Folder folder = new Folder();
         folder.setName(name);
         folder.setOwner(owner);
         return folder;
