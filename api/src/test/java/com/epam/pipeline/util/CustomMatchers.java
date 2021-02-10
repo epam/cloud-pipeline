352fcb7b5dbe55818003675bab488f8587e810ce
Ilya Sergeev <ilbras@mail.ru>
2021-02-10 10:46:50 +0300

    Issue-1754: resolving merge conflict

--- api/src/test/java/com/epam/pipeline/util/CustomMatchers.java
+++ api/src/test/java/com/epam/pipeline/util/CustomMatchers.java
@@ -23,6 +23,7 @@ import org.hamcrest.Matcher;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Matchers.anyMapOf;
