epam/cloud-pipeline

commit 7b4caa0e27e7c7cf711fe4bf77dae95accd5ebfa

Improve data storage object tags management (#1873)
* Remove extra file version requests from pipe cli for aws

* Add support for data storage tags bulk operations size overriding

* Extract all data storage tags management to a separate manager

pipe-cli/src/utilities/storage/s3.py
@@ -32,6 +32,7 @@ from botocore.credentials import RefreshableCredentials
 from botocore.exceptions import ClientError
 from botocore.session import get_session
 from s3transfer.manager import TransferManager
+from s3transfer import upload, tasks, copies
 
 from src.api.data_storage import DataStorage
 from src.model.data_storage_item_model import DataStorageItemModel, DataStorageItemLabelModel
@@ -40,6 +41,54 @@ from src.utilities.progress_bar import ProgressPercentage
 from src.utilities.storage.common import StorageOperations, AbstractListingManager, AbstractDeleteManager, \
     AbstractRestoreManager, AbstractTransferManager, TransferResult, UploadResult
 from src.config import Config
+
+
+class UploadedObjectsContainer:
+
+    def __init__(self):
+        self._objects = {}
+
+    def add(self, bucket, key, data):
+        self._objects[bucket + '/' + key] = data
+
+    def pop(self, bucket, key):
+        data = self._objects[bucket + '/' + key]
+        del self._objects[bucket + '/' + key]
+        return data
+
+
+uploaded_objects_container = UploadedObjectsContainer()
+
+
+def _put_object_task_main(self, client, fileobj, bucket, key, extra_args):
+    with fileobj as body:
+        output = client.put_object(Bucket=bucket, Key=key, Body=body, **extra_args)
+        uploaded_objects_container.add(bucket, key, output['VersionId'])
+
+
+def _complete_multipart_upload_task_main(self, client, bucket, key, upload_id, parts, extra_args):
+    output = client.complete_multipart_upload(
+        Bucket=bucket, Key=key, UploadId=upload_id,
+        MultipartUpload={'Parts': parts},
+        **extra_args)
+    uploaded_objects_container.add(bucket, key, output['VersionId'])
+
+
+def _copy_object_task_main(self, client, copy_source, bucket, key, extra_args, callbacks, size):
+    output = client.copy_object(
+        CopySource=copy_source, Bucket=bucket, Key=key, **extra_args)
+    for callback in callbacks:
+        callback(bytes_transferred=size)
+    uploaded_objects_container.add(bucket, key, output['VersionId'])
+
+
+# By default boto library doesn't aggregate uploaded object versions
+# which have to be downloaded via an extra request for each individual object.
+# This monkey patching allows to aggregate uploaded object versions
+# and use them later on without any extra requests being performed.
+upload.PutObjectTask._main = _put_object_task_main
+tasks.CompleteMultipartUploadTask._main = _complete_multipart_upload_task_main
+copies.CopyObjectTask._main = _copy_object_task_main
 
 
 class StorageItemManager(object):
@@ -97,6 +146,9 @@ class StorageItemManager(object):
             return item.get('VersionId')
         except ClientError:
             return None
+
+    def get_uploaded_s3_file_version(self, bucket, key):
+        return uploaded_objects_container.pop(bucket, key)
 
     @staticmethod
     def get_local_file_size(path):
@@ -168,7 +220,7 @@ class UploadManager(StorageItemManager, AbstractTransferManager):
         if clean:
             source_wrapper.delete_item(source_key)
         tags = StorageOperations.parse_tags(tags)
-        version = self.get_s3_file_version(destination_wrapper.bucket.path, destination_key)
+        version = self.get_uploaded_s3_file_version(destination_wrapper.bucket.path, destination_key)
         return UploadResult(source_key=source_key, destination_key=destination_key, destination_version=version,
                             tags=tags)
 
@@ -212,7 +264,7 @@ class TransferFromHttpOrFtpToS3Manager(StorageItemManager, AbstractTransferManag
         else:
             self.bucket.upload_fileobj(file_stream, destination_key, ExtraArgs=extra_args)
         tags = StorageOperations.parse_tags(tags)
-        version = self.get_s3_file_version(destination_wrapper.bucket.path, destination_key)
+        version = self.get_uploaded_s3_file_version(destination_wrapper.bucket.path, destination_key)
         return UploadResult(source_key=source_key, destination_key=destination_key, destination_version=version,
                             tags=tags)
 
@@ -253,7 +305,7 @@ class TransferBetweenBucketsManager(StorageItemManager, AbstractTransferManager)
             self.bucket.copy(copy_source, destination_key, ExtraArgs=extra_args, SourceClient=source_client)
         if clean:
             source_wrapper.delete_item(path)
-        version = self.get_s3_file_version(destination_wrapper.bucket.path, destination_key)
+        version = self.get_uploaded_s3_file_version(destination_wrapper.bucket.path, destination_key)
         return TransferResult(source_key=path, destination_key=destination_key, destination_version=version,
                               tags=StorageOperations.parse_tags(tags))
 

api/src/main/java/com/epam/pipeline/manager/datastorage/DataStorageManager.java
@@ -47,10 +47,6 @@ import com.epam.pipeline.entity.datastorage.StorageUsage;
 import com.epam.pipeline.entity.datastorage.aws.S3bucketDataStorage;
 import com.epam.pipeline.entity.datastorage.azure.AzureBlobStorage;
 import com.epam.pipeline.entity.datastorage.gcp.GSBucketStorage;
-import com.epam.pipeline.entity.datastorage.tag.DataStorageObject;
-import com.epam.pipeline.entity.datastorage.tag.DataStorageTag;
-import com.epam.pipeline.entity.datastorage.tag.DataStorageTagCopyBatchRequest;
-import com.epam.pipeline.entity.datastorage.tag.DataStorageTagCopyRequest;
 import com.epam.pipeline.entity.metadata.PipeConfValue;
 import com.epam.pipeline.entity.pipeline.Folder;
 import com.epam.pipeline.entity.pipeline.PipelineRun;
@@ -61,8 +57,7 @@ import com.epam.pipeline.entity.templates.DataStorageTemplate;
 import com.epam.pipeline.entity.user.PipelineUser;
 import com.epam.pipeline.entity.user.StorageContainer;
 import com.epam.pipeline.manager.datastorage.providers.ProviderUtils;
-import com.epam.pipeline.manager.datastorage.tag.DataStorageTagBatchManager;
-import com.epam.pipeline.manager.datastorage.tag.DataStorageTagManager;
+import com.epam.pipeline.manager.datastorage.tag.DataStorageTagProviderManager;
 import com.epam.pipeline.manager.metadata.MetadataManager;
 import com.epam.pipeline.manager.pipeline.FolderManager;
 import com.epam.pipeline.manager.pipeline.FolderTemplateManager;
@@ -100,7 +95,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -112,10 +106,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 @Service
@@ -173,10 +165,7 @@ public class DataStorageManager implements SecuredEntityManager {
     private GrantPermissionManager permissionManager;
 
     @Autowired
-    private DataStorageTagManager tagManager;
-
-    @Autowired
-    private DataStorageTagBatchManager tagBatchManager;
+    private DataStorageTagProviderManager tagProviderManager;
 
     private AbstractDataStorageFactory dataStorageFactory =
             AbstractDataStorageFactory.getDefaultDataStorageFactory();
@@ -444,7 +433,7 @@ public class DataStorageManager implements SecuredEntityManager {
             } catch (DataStorageException e) {
                 LOGGER.error(e.getMessage(), e);
             }
-            tagManager.deleteAllInFolder(dataStorage.getRootId(), dataStorage.resolveRootPath(""));
+            tagProviderManager.deleteStorageTags(dataStorage);
         }
         dataStorageDao.deleteDataStorage(id);
         return dataStorage;
@@ -480,19 +469,8 @@ public class DataStorageManager implements SecuredEntityManager {
             throw new DataStorageException(messageHelper.getMessage(
                     MessageConstants.ERROR_DATASTORAGE_VERSIONING_REQUIRED, dataStorage.getName()));
         }
-        final String relativePath = dataStorage.resolveRootPath(path);
         storageProviderManager.restoreFileVersion(dataStorage, path, version);
-        storageProviderManager.findFile(dataStorage, path)
-                .map(DataStorageFile::getVersion)
-                .ifPresent(latestVersion -> {
-                    tagManager.copy(dataStorage.getRootId(),
-                            new DataStorageObject(relativePath, version), 
-                            new DataStorageObject(relativePath));
-                    tagManager.copy(dataStorage.getRootId(),
-                            new DataStorageObject(relativePath, version), 
-                            new DataStorageObject(relativePath, latestVersion));
-                });
-        tagManager.delete(dataStorage.getRootId(), new DataStorageObject(relativePath, version));
+        tagProviderManager.restoreFileTags(dataStorage, path, version);
     }
 
     public DataStorageDownloadFileUrl generateDataStorageItemUrl(final Long dataStorageId,
@@ -677,9 +655,7 @@ public class DataStorageManager implements SecuredEntityManager {
     public Map<String, String> loadDataStorageObjectTags(Long id, String path, String version) {
         final AbstractDataStorage dataStorage = load(id);
         checkDataStorageVersioning(dataStorage, version);
-        final String relativePath = dataStorage.resolveRootPath(path);
-        final DataStorageObject object = new DataStorageObject(relativePath, version);
-        return mapFrom(tagManager.load(dataStorage.getRootId(), object));
+        return tagProviderManager.loadFileTags(dataStorage, path, version);
     }
 
     @Transactional
@@ -687,26 +663,8 @@ public class DataStorageManager implements SecuredEntityManager {
                                                            Set<String> tags) {
         final AbstractDataStorage dataStorage = load(id);
         checkDataStorageVersioning(dataStorage, version);
-        final String relativePath = dataStorage.resolveRootPath(path);
-        final DataStorageObject object = new DataStorageObject(relativePath, version);
-        final List<DataStorageTag> existingTags = tagManager.load(dataStorage.getRootId(), object);
-        tags.forEach(tag -> Assert.isTrue(existingTags.stream().anyMatch(it -> it.getKey().equals(tag)),
-                messageHelper.getMessage(MessageConstants.ERROR_DATASTORAGE_FILE_TAG_NOT_EXIST, tag)));
-        if (dataStorage.isVersioningEnabled()) {
-            storageProviderManager.findFile(dataStorage, path)
-                    .map(DataStorageFile::getVersion)
-                    .map(latestVersion -> {
-                        if (StringUtils.isBlank(version)) {
-                            return new DataStorageObject(relativePath, latestVersion);
-                        }
-                        return latestVersion.equals(version) ? new DataStorageObject(relativePath) : null;
-                    })
-                    .ifPresent(obj -> {
-                        tagManager.delete(dataStorage.getRootId(), obj, tags);
-                    });
-        }
-        tagManager.delete(dataStorage.getRootId(), object, tags);
-        return mapFrom(tagManager.load(dataStorage.getRootId(), object));
+        tagProviderManager.deleteFileTags(dataStorage, path, version, tags);
+        return tagProviderManager.loadFileTags(dataStorage, path, version);
     }
 
     @Transactional
@@ -720,15 +678,13 @@ public class DataStorageManager implements SecuredEntityManager {
         }
         final DataStorageFile dataStorageFile = (DataStorageFile) dataStorageItems.get(0);
         final AbstractDataStorage dataStorage = load(dataStorageId);
-        final String relativePath = dataStorage.resolveRootPath(path);
         if (MapUtils.isEmpty(dataStorageFile.getVersions())) {
-            dataStorageFile.setTags(mapFrom(
-                    tagManager.load(dataStorage.getRootId(), new DataStorageObject(relativePath))));
+            dataStorageFile.setTags(tagProviderManager.loadFileTags(dataStorage, path, null));
         } else {
             dataStorageFile
                     .getVersions()
-                    .forEach((version, item) -> item.setTags(mapFrom(
-                            tagManager.load(dataStorage.getRootId(), new DataStorageObject(relativePath, version)))));
+                    .forEach((version, item) ->
+                            item.setTags(tagProviderManager.loadFileTags(dataStorage, path, version)));
         }
         return dataStorageFile;
     }
@@ -739,26 +695,7 @@ public class DataStorageManager implements SecuredEntityManager {
                                                            Boolean rewrite) {
         final AbstractDataStorage dataStorage = load(id);
         checkDataStorageVersioning(dataStorage, version);
-        final String relativePath = dataStorage.resolveRootPath(path);
-        final Function<DataStorageObject, List<DataStorageTag>> updateTags = rewrite
-                ? object -> tagManager.insert(dataStorage.getRootId(), object, tagsToAdd)
-                : object -> tagManager.upsert(dataStorage.getRootId(), object, tagsToAdd);
-        if (dataStorage.isVersioningEnabled()) {
-            storageProviderManager.findFile(dataStorage, path)
-                    .map(DataStorageFile::getVersion)
-                    .map(latestVersion -> {
-                        if (StringUtils.isBlank(version)) {
-                            return new DataStorageObject(relativePath, latestVersion);
-                        }
-                        return latestVersion.equals(version) ? new DataStorageObject(relativePath) : null;
-                    })
-                    .ifPresent(updateTags::apply);
-        }
-        return mapFrom(updateTags.apply(new DataStorageObject(relativePath, version)));
-    }
-
-    private Map<String, String> mapFrom(final List<DataStorageTag> tags) {
-        return tags.stream().collect(Collectors.toMap(DataStorageTag::getKey, DataStorageTag::getValue));
+        return tagProviderManager.updateFileTags(dataStorage, path, version, tagsToAdd, rewrite);
     }
 
     public DataStorageStreamingContent getStreamingContent(long dataStorageId, String path, String version) {
@@ -906,80 +843,23 @@ public class DataStorageManager implements SecuredEntityManager {
                                                     final String oldPath,
                                                     final String newPath) throws DataStorageException {
         final DataStorageFolder folder = storageProviderManager.moveFolder(dataStorage, oldPath, newPath);
-        moveDataStorageFolderTags(dataStorage, oldPath, newPath);
+        tagProviderManager.moveFolderTags(dataStorage, oldPath, newPath);
         return folder;
-    }
-
-    private void moveDataStorageFolderTags(final AbstractDataStorage dataStorage,
-                                           final String oldPath,
-                                           final String newPath) {
-        final String oldRelativePath = dataStorage.resolveRootPath(oldPath);
-        final String newRelativePath = dataStorage.resolveRootPath(newPath);
-        tagManager.copyFolder(dataStorage.getRootId(), oldRelativePath, newRelativePath);
-        if (dataStorage.isVersioningEnabled()) {
-            processInChunks(storageProviderManager.listFiles(dataStorage, newPath + dataStorage.getDelimiter()),
-                chunk -> tagBatchManager.copy(dataStorage.getId(), new DataStorageTagCopyBatchRequest(
-                        chunk.stream()
-                                .map(file -> new DataStorageTagCopyRequest(
-                                        DataStorageTagCopyRequest.object(file.getPath(), null),
-                                        DataStorageTagCopyRequest.object(file.getPath(), file.getVersion())))
-                                .collect(Collectors.toList()))));
-        } else {
-            tagManager.deleteAllInFolder(dataStorage.getRootId(), oldRelativePath);
-        }
-    }
-
-    private <T> void processInChunks(final Stream<T> stream, final Consumer<List<T>> consumer) {
-        final int chunkSize = 100;
-        final Iterator<T> iterator = stream.iterator();
-        final List<T> chunk = new ArrayList<>(chunkSize);
-        while (iterator.hasNext()) {
-            while (iterator.hasNext() && chunk.size() < chunkSize) {
-                chunk.add(iterator.next());
-            }
-            consumer.accept(chunk);
-            chunk.clear();
-        }
     }
 
     private DataStorageFile moveDataStorageFile(final AbstractDataStorage dataStorage,
                                                 final String oldPath,
                                                 final String newPath) throws DataStorageException {
         final DataStorageFile file = storageProviderManager.moveFile(dataStorage, oldPath, newPath);
-        moveDataStorageFileTags(dataStorage, oldPath, newPath, file);
+        tagProviderManager.moveFileTags(dataStorage, oldPath, newPath, file.getVersion());
         return file;
-    }
-
-    private void moveDataStorageFileTags(final AbstractDataStorage dataStorage,
-                                         final String oldPath,
-                                         final String newPath,
-                                         final DataStorageFile file) {
-        final String oldRelativePath = dataStorage.resolveRootPath(oldPath);
-        final String newRelativePath = dataStorage.resolveRootPath(newPath);
-        final Map<String, String> tagMap = mapFrom(tagManager.load(dataStorage.getRootId(), 
-                new DataStorageObject(oldRelativePath)));
-        tagManager.upsert(dataStorage.getRootId(), new DataStorageObject(newRelativePath), tagMap);
-        if (dataStorage.isVersioningEnabled()) {
-            tagManager.upsert(dataStorage.getRootId(), new DataStorageObject(newRelativePath, file.getVersion()), 
-                    tagMap);
-        } else {
-            tagManager.delete(dataStorage.getRootId(), new DataStorageObject(oldRelativePath));
-        }
     }
 
     private void deleteDataStorageFolder(final AbstractDataStorage dataStorage,
                                          final String path,
                                          final Boolean totally) throws DataStorageException {
         storageProviderManager.deleteFolder(dataStorage, path, totally);
-        deleteDataStorageFolderTags(dataStorage, path, totally);
-    }
-
-    private void deleteDataStorageFolderTags(final AbstractDataStorage dataStorage,
-                                             final String path,
-                                             final Boolean totally) {
-        if (!dataStorage.isVersioningEnabled() || totally) {
-            tagManager.deleteAllInFolder(dataStorage.getRootId(), dataStorage.resolveRootPath(path));
-        }
+        tagProviderManager.deleteFolderTags(dataStorage, path, totally);
     }
 
     private void deleteDataStorageFile(final AbstractDataStorage dataStorage,
@@ -987,33 +867,7 @@ public class DataStorageManager implements SecuredEntityManager {
                                        final String version,
                                        final Boolean totally) throws DataStorageException {
         storageProviderManager.deleteFile(dataStorage, path, version, totally);
-        deleteDataStorageFileTags(dataStorage, path, version, totally);
-    }
-
-    private void deleteDataStorageFileTags(final AbstractDataStorage dataStorage,
-                                           final String path,
-                                           final String version,
-                                           final Boolean totally) {
-        final String relativePath = dataStorage.resolveRootPath(path);
-        if (dataStorage.isVersioningEnabled()) {
-            if (version != null) {
-                final Optional<String> latestVersion = storageProviderManager.findFile(dataStorage, path)
-                        .map(DataStorageFile::getVersion);
-                if (latestVersion.isPresent()) {
-                    tagManager.copy(dataStorage.getRootId(), 
-                            new DataStorageObject(relativePath, latestVersion.get()),
-                            new DataStorageObject(relativePath));
-                    tagManager.delete(dataStorage.getRootId(), new DataStorageObject(relativePath, version));
-                } else {
-                    tagManager.delete(dataStorage.getRootId(), new DataStorageObject(relativePath, version));
-                    tagManager.delete(dataStorage.getRootId(), new DataStorageObject(relativePath));
-                }
-            } else if (totally) {
-                tagManager.deleteAll(dataStorage.getRootId(), relativePath);
-            }
-        } else {
-            tagManager.deleteAll(dataStorage.getRootId(), relativePath);
-        }
+        tagProviderManager.deleteFileTags(dataStorage, path, version, totally);
     }
 
     private void deleteDataStorageItem(final AbstractDataStorage dataStorage, UpdateDataStorageItemVO item,
@@ -1110,7 +964,7 @@ public class DataStorageManager implements SecuredEntityManager {
                                                   final String path,
                                                   final byte[] contents) throws DataStorageException {
         final DataStorageFile file = storageProviderManager.createFile(storage, path, contents);
-        createDataStorageFileTags(storage, path, file.getVersion());
+        tagProviderManager.createFileTags(storage, path, file.getVersion());
         return file;
     }
 
@@ -1118,20 +972,8 @@ public class DataStorageManager implements SecuredEntityManager {
                                                   final String path, 
                                                   final InputStream contentStream) {
         final DataStorageFile file = storageProviderManager.createFile(storage, path, contentStream);
-        createDataStorageFileTags(storage, path, file.getVersion());
+        tagProviderManager.createFileTags(storage, path, file.getVersion());
         return file;
-    }
-
-    private void createDataStorageFileTags(final AbstractDataStorage storage,
-                                           final String path,
-                                           final String version) {
-        final String authorizedUser = authManager.getAuthorizedUser();
-        final String relativePath = storage.resolveRootPath(path);
-        final Map<String, String> defaultTags = Collections.singletonMap(ProviderUtils.OWNER_TAG_KEY, authorizedUser);
-        tagManager.insert(storage.getRootId(), new DataStorageObject(relativePath, null), defaultTags);
-        if (storage.isVersioningEnabled()) {
-            tagManager.insert(storage.getRootId(), new DataStorageObject(relativePath, version), defaultTags);
-        }
     }
 
     private AbstractDataStorageItem updateDataStorageItem(final AbstractDataStorage storage,

commit bf6f91e634c810e7f36905519149051b52703f35

Issue #1879: Fixes a key error introduced in #1873.
Relates to #1717.

pipe-cli/src/utilities/storage/s3.py
@@ -63,7 +63,7 @@ uploaded_objects_container = UploadedObjectsContainer()
 def _put_object_task_main(self, client, fileobj, bucket, key, extra_args):
     with fileobj as body:
         output = client.put_object(Bucket=bucket, Key=key, Body=body, **extra_args)
-        uploaded_objects_container.add(bucket, key, output['VersionId'])
+        uploaded_objects_container.add(bucket, key, output.get('VersionId'))
 
 
 def _complete_multipart_upload_task_main(self, client, bucket, key, upload_id, parts, extra_args):
@@ -71,7 +71,7 @@ def _complete_multipart_upload_task_main(self, client, bucket, key, upload_id, p
         Bucket=bucket, Key=key, UploadId=upload_id,
         MultipartUpload={'Parts': parts},
         **extra_args)
-    uploaded_objects_container.add(bucket, key, output['VersionId'])
+    uploaded_objects_container.add(bucket, key, output.get('VersionId'))
 
 
 def _copy_object_task_main(self, client, copy_source, bucket, key, extra_args, callbacks, size):
@@ -79,7 +79,7 @@ def _copy_object_task_main(self, client, copy_source, bucket, key, extra_args, c
         CopySource=copy_source, Bucket=bucket, Key=key, **extra_args)
     for callback in callbacks:
         callback(bytes_transferred=size)
-    uploaded_objects_container.add(bucket, key, output['VersionId'])
+    uploaded_objects_container.add(bucket, key, output.get('VersionId'))
 
 
 # By default boto library doesn't aggregate uploaded object versions