244333e6574f6a3629bd643e374ecf77b251a4e5
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2021-04-07 14:56:40 +0300

    Issue #1865: Allow to skip node reassignment for a run (#1910)

---
+++ api/src/test/java/com/epam/pipeline/manager/cluster/autoscale/ReassignHandlerTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.pipeline.manager.cluster.autoscale;
+
+import com.epam.pipeline.entity.pipeline.PipelineRun;
+import com.epam.pipeline.entity.pipeline.run.parameter.PipelineRunParameter;
+import com.epam.pipeline.manager.cloud.CloudFacade;
+import com.epam.pipeline.manager.pipeline.PipelineRunManager;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
+import static com.epam.pipeline.test.creator.CommonCreatorConstants.ID;
+import static com.epam.pipeline.test.creator.pipeline.PipelineCreatorUtils.getPipelineRun;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class ReassignHandlerTest {
+    private final AutoscalerService autoscalerService = mock(AutoscalerService.class);
+    private final CloudFacade cloudFacade = mock(CloudFacade.class);
+    private final PipelineRunManager pipelineRunManager = mock(PipelineRunManager.class);
+
+    private final ReassignHandler reassignHandler = new ReassignHandler(
+            autoscalerService,
+            cloudFacade,
+            pipelineRunManager,
+            new ArrayList<>());
+
+    @Test
+    public void shouldNotReassignWithCreateNewNodeParameter() {
+        doReturn(Optional.of(pipelineRunWithCreateNewNodeParameter())).when(pipelineRunManager).findRun(ID);
+
+        final boolean result = reassignHandler.tryReassignNode(null, null, null,
+                String.valueOf(ID), ID, null, null);
+        assertThat(result).isFalse();
+    }
+
+    private PipelineRun pipelineRunWithCreateNewNodeParameter() {
+        final PipelineRunParameter pipelineRunParameter =
+                new PipelineRunParameter("CP_CREATE_NEW_NODE", "true");
+        final PipelineRun pipelineRun = getPipelineRun(ID);
+        pipelineRun.setPipelineRunParameters(Collections.singletonList(pipelineRunParameter));
+        return pipelineRun;
+    }
+}
