3876974ada081da8418d5f964c4ca8627ac29803
Aleksandr Gorodetskii <gorod2004@gmail.com>
2021-04-29 16:26:17 +0300

    GUI: Versioned Storages (#1805) - versioned storage browser - header (WIP)

--- client/src/components/pipelines/browser/versioned-storage/index.js
+++ client/src/components/pipelines/browser/versioned-storage/index.js
@@ -15,13 +15,26 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import {inject, observer} from 'mobx-react';
 import {
-  Alert
+  Alert,
+  message
 } from 'antd';
+import VersionedStorageHeader from './header';
+import {
+  ContentIssuesMetadataPanel,
+  METADATA_PANEL_KEY,
+  ISSUES_PANEL_KEY
+} from '../../../special/splitPanel';
+import Issues from '../../../special/issues/Issues';
+import Metadata from '../../../special/metadata/Metadata';
+import roleModel from '../../../../utils/roleModel';
 import localization from '../../../../utils/localization';
 import HiddenObjects from '../../../../utils/hidden-objects';
 import LoadingView from '../../../special/LoadingView';
+import UpdatePipeline from '../../../../models/pipelines/UpdatePipeline';
+import styles from './versioned-storage.css';
 
 @localization.localizedComponent
 @HiddenObjects.checkPipelines(p => (p.params ? p.params.id : p.id))
@@ -39,8 +52,100 @@ import LoadingView from '../../../special/LoadingView';
 })
 @observer
 class VersionedStorage extends localization.LocalizedReactComponent {
+  state = {
+    showIssuesPanel: false,
+    showMetadataPanel: false
+  };
+
+  get actions () {
+    return {
+      openIssuesPanel: this.openIssuesPanel,
+      closeIssuesPanel: this.closeIssuesPanel,
+      openMetadataPanel: this.openMetadataPanel,
+      closeMetadataPanel: this.closeMetadataPanel,
+      openEditPipelineDialog: this.openEditPipelineDialog,
+      openClonePipelineDialog: this.openClonePipelineDialog
+    };
+  }
+
+  updateVSRequest = new UpdatePipeline();
+
+  openIssuesPanel = () => {
+    this.setState({showIssuesPanel: true});
+  };
+
+  closeIssuesPanel = () => {
+    this.setState({showIssuesPanel: false});
+  };
+
+  openMetadataPanel = () => {
+    this.setState({showMetadataPanel: true});
+  };
+
+  closeMetadataPanel = () => {
+    this.setState({showMetadataPanel: false});
+  };
+
+  openEditPipelineDialog = () => {
+    this.setState({editPipelineDialog: true});
+  };
+
+  openClonePipelineDialog = () => {
+    this.setState({clonePipelineDialog: true});
+  };
+
+  onSplitPanelClose = (key) => {
+    switch (key) {
+      case METADATA_PANEL_KEY:
+        this.closeMetadataPanel();
+        break;
+      case ISSUES_PANEL_KEY:
+        this.closeIssuesPanel();
+        break;
+    }
+  };
+
+  renameVersionedStorage = async (name) => {
+    const {pipeline, folders, pipelinesLibrary, onReloadTree} = this.props;
+    if (!pipeline || !pipeline.value) {
+      return;
+    }
+    const hide = message.loading(`Renaming versioned-storage ${name}...`, -1);
+    await this.updateVSRequest.send({
+      id: pipeline.value.id,
+      name: name,
+      description: pipeline.value.description,
+      parentFolderId: pipeline.value.parentFolderId
+    });
+    if (this.updateVSRequest.error) {
+      hide();
+      message.error(this.updateVSRequest.error, 5);
+    } else {
+      hide();
+      const parentFolderId = pipeline.value.parentFolderId;
+      if (parentFolderId) {
+        folders.invalidateFolder(parentFolderId);
+      } else {
+        pipelinesLibrary.invalidateCache();
+      }
+      await pipeline.fetch();
+      if (onReloadTree) {
+        onReloadTree(!pipeline.value.parentFolderId);
+      }
+    }
+  };
+
   render () {
-    const {pipeline} = this.props;
+    const {
+      pipeline,
+      pipelineId,
+      readOnly,
+      listingMode
+    } = this.props;
+    const {
+      showIssuesPanel,
+      showMetadataPanel
+    } = this.state;
     if (!pipeline.loaded && pipeline.pending) {
       return (
         <LoadingView />
@@ -52,13 +157,56 @@ class VersionedStorage extends localization.LocalizedReactComponent {
       );
     }
     return (
-      <div
-        style={{display: 'flex', flexDirection: 'column', height: '100%'}}
-      >
-        <Alert type="info" message="Under construction" />
+      <div className={styles.vsContainer}>
+        <VersionedStorageHeader
+          pipeline={pipeline}
+          pipelineId={pipelineId}
+          readOnly={readOnly}
+          onRenameStorage={this.renameVersionedStorage}
+          actions={this.actions}
+          listingMode={listingMode}
+          issuesPanelOpen={showIssuesPanel}
+          metadataPanelOpen={showMetadataPanel}
+        />
+        <ContentIssuesMetadataPanel
+          style={{flex: 1, overflow: 'auto'}}
+          onPanelClose={this.onSplitPanelClose}
+        >
+          {showIssuesPanel &&
+            <Issues
+              key={ISSUES_PANEL_KEY}
+              onCloseIssuePanel={this.closeIssuesPanel}
+              entityId={pipelineId}
+              entityClass="PIPELINE"
+              entity={pipeline.value}
+            />
+          }
+          {showMetadataPanel &&
+            <Metadata
+              key={METADATA_PANEL_KEY}
+              readOnly={!roleModel.isOwner(pipeline.value)}
+              entityName={pipeline.value.name}
+              entityId={pipelineId}
+              entityClass="PIPELINE"
+            />
+          }
+        </ContentIssuesMetadataPanel>
       </div>
     );
   }
 }
+
+VersionedStorage.propTypes = {
+  pipeline: PropTypes.object,
+  pipelineId: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  listingMode: PropTypes.bool,
+  readOnly: PropTypes.bool,
+  onReloadTree: PropTypes.func,
+  folders: PropTypes.object,
+  pipelinesLibrary: PropTypes.object
+};
 
 export default VersionedStorage;
