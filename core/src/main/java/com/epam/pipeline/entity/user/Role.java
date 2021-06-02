2d08f1917572efada7b5af01608f41907dbb5b23
Mariia_Zueva <mariia_zueva@epam.com>
2021-06-02 12:12:07 +0300

    PMD fix

--- core/src/main/java/com/epam/pipeline/entity/user/Role.java
+++ core/src/main/java/com/epam/pipeline/entity/user/Role.java
@@ -32,8 +32,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 import java.io.Serializable;
 import java.util.List;
 
