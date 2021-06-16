0e2efc8700ec71b4641c784641400865f5c1b7fc
Andrey Georgitsa <andrey_georgitsa@epam.com>
2021-06-16 16:57:09 +0300

    Issue #1832 Allow running unscanned Windows-based tool versions (#2009)
    * Issue #1832 Allow running unscanned Windows-based tool versions
    
    * Issue #1832 Cloud-Data and drive mapping refactoring to work with user seamless logon (#2010)
    
    * Issue #1832 Cloud-Data installation: refactoring to support seamless logon
    
    * Issue #1832 Drive mapping: refactoring to support seamless logon
    
    * Issue #1832 Create Cloud-Data shortcut using Python
    
    * Issue #1832 Configure environment for drive mapping using Python
    
    * Issue #1832 Configure EDGE root certificate installation using Python

--- workflows/pipe-common/scripts/configure_cloud_data_win.py
+++ workflows/pipe-common/scripts/configure_cloud_data_win.py
@@ -16,26 +16,22 @@ import json
 import pathlib
 import os
 import platform
-import subprocess
+from win32com.client import Dispatch
 
-_CREATE_SHORTCUT_PSHELL_SCRIPT_TEMPLATE = '''
-$s=(New-Object -COM WScript.Shell).CreateShortcut(\'{}\\Desktop\\Cloud-Data.lnk\');
-$s.TargetPath=\'{}\\cloud-data.exe\';
-$s.Save()
-'''
+_PUBLIC_CLOUD_DATA_SHORTCUT_PATH = 'C:\\Users\\Public\\Desktop\\Cloud-Data.lnk'
 
 
-def _run_powershell(cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=False)
-    return completed
-
-
-def _create_cloud_data_shortcut(node_user_home, cloud_data_parent_dir):
+def _create_cloud_data_shortcut(cloud_data_parent_dir):
+    shell = Dispatch('WScript.Shell')
+    cloud_data_shortcut_path = os.path.join(_PUBLIC_CLOUD_DATA_SHORTCUT_PATH, )
+    shortcut = shell.CreateShortCut(cloud_data_shortcut_path)
     cloud_data_dir = os.path.join(cloud_data_parent_dir, 'cloud-data-app')
-    _run_powershell(_CREATE_SHORTCUT_PSHELL_SCRIPT_TEMPLATE.format(node_user_home, cloud_data_dir))
+    cloud_data_executable = os.path.join(cloud_data_dir, 'cloud-data.exe')
+    shortcut.Targetpath = cloud_data_executable
+    shortcut.save()
 
 
-def _create_cloud_data_config(node_user_home, edge_url, username, token):
+def _create_cloud_data_config(cloud_data_parent_dir, edge_url, username, token):
     webdav_config = {
         'ignoreCertificateErrors': True,
         'username': username,
@@ -43,18 +39,17 @@ def _create_cloud_data_config(node_user_home, edge_url, username, token):
         'server': "{}/webdav/{}".format(edge_url, username),
         'version': ''
     }
-    cloud_data_config_folder = os.path.join(node_user_home, '.pipe-webdav-client')
+    cloud_data_config_folder = os.path.join(cloud_data_parent_dir, '.pipe-webdav-client')
     pathlib.Path(cloud_data_config_folder).mkdir(parents=True, exist_ok=True)
     with open(os.path.join(cloud_data_config_folder, 'webdav.config'), 'w') as outfile:
         json.dump(webdav_config, outfile)
 
 
-def configure_cloud_data_win(cloud_data_parent_dir, edge_url, node_user, dav_user, token):
+def configure_cloud_data_win(cloud_data_parent_dir, edge_url, dav_user, token):
     current_platform = platform.system()
     if current_platform == 'Windows':
-        user_home_dir = '{}\\{}\\{}'.format(os.getenv('SystemDrive', 'c:'), 'Users', node_user)
-        _create_cloud_data_config(user_home_dir, edge_url, dav_user, token)
-        _create_cloud_data_shortcut(user_home_dir, cloud_data_parent_dir)
+        _create_cloud_data_config(cloud_data_parent_dir, edge_url, dav_user, token)
+        _create_cloud_data_shortcut(cloud_data_parent_dir)
     else:
         raise RuntimeError('Cloud-Data installation is not supported on {platform} platform.'
                            .format(platform=current_platform))
