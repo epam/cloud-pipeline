ebba5766af3ee5029c06691629e57495ed72618e
Mariia Zueva <mariia_zueva@epam.com>
2021-06-16 16:28:03 +0300

    Issue #2023 Handle long Elasticsearch startup, allow to configure ulimit (#2033)

--- deploy/docker/cp-search-elk/init.sh
+++ deploy/docker/cp-search-elk/init.sh
@@ -24,7 +24,7 @@ elif [ "$CP_CLOUD_PLATFORM" == 'az' ]; then
 fi
 
 
-ulimit -n 65536 && sysctl -w vm.max_map_count=262144 && /usr/local/bin/docker-entrypoint.sh &
+ulimit -n ${CP_SEARCH_ELK_ULIMIT:-65536} && sysctl -w vm.max_map_count=262144 && /usr/local/bin/docker-entrypoint.sh &
 
 not_initialized=true
 try_count=0
@@ -36,10 +36,17 @@ while [ $not_initialized ] && [ $try_count -lt 60 ]; do
     else
       echo "...Success."
     fi
-    try_count=$(( $try_count + 1 ))
+    # increment attempts only if java is not running
+    if [ ! "$(ps -A | grep 'java')" ]; then
+      try_count=$(( $try_count + 1 ))
+    fi
     sleep 1
 done
 
+if [ $not_initialized ]; then
+    echo "Failed to start up Elasticsearch server. Exiting..."
+    exit 1
+fi
 
 ILM_POLICY="{
   \"policy\": {
