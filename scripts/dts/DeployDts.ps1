579d3df2712db717839ae5221d46158aa8a660e4
Andrey Tsibin <andrei_tcibin@epam.com>
2021-07-14 17:12:21 +0300

    Issue #2044: Use embedded pipe in data transfer service launcher

--- scripts/dts/DeployDts.ps1
+++ scripts/dts/DeployDts.ps1
@@ -74,6 +74,7 @@ if ($Install) {
 `$env:CP_API_JWT_KEY_PUBLIC = "$env:API_PUBLIC_KEY"
 `$env:DTS_LOCAL_NAME = "$env:DTS_NAME"
 `$env:DTS_IMPERSONATION_ENABLED = "false"
+`$env:DTS_PIPE_EXECUTABLE = "$env:PIPE_DIR\pipe\pipe.exe"
 "@ | Out-File -FilePath Environment.ps1 -Encoding ascii -Force -ErrorAction Stop
 
     Log "Loading environment..."
