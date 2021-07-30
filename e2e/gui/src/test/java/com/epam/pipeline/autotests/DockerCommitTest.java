e5cecb94c3c7e34b04726378335f46ead6bcb925
Mariia_Tolstaia <Mariia_Tolstaia@epam.com>
2021-07-30 15:39:11 +0300

    update locators

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/DockerCommitTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/DockerCommitTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -131,7 +131,7 @@ public class DockerCommitTest
     @TestCase({"EPMCMBIBPC-1350"})
     public void listOfGroupInRegistry() {
         logAO().commit(commit ->
-                commit.hover(GROUP).ensure(byText(group), visible)
+                commit.click(GROUP).ensure(byText(group), visible)
                         .cancel()
         );
     }
