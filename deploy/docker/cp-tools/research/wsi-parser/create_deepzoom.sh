bef8e740696f12280310e43d6725644de897135f
Andrey Georgitsa <andrey_georgitsa@epam.com>
2021-09-20 19:24:19 +0300

    Change processing steps: - Use the biggest image in the target series - Localize data for processing - Allow performing results finalization with AWS CLI

--- deploy/docker/cp-tools/research/wsi-parser/create_deepzoom.sh
+++ deploy/docker/cp-tools/research/wsi-parser/create_deepzoom.sh
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 _FILE_PATH="$1"
-_XML_DETAILS_PATH="$2"
-_SERIES_NUM="$3"
+_SERIES_NUM="$2"
+_TILES_SIZE="$3"
 _TILES_PARENT_DIR="$4"
-_PARSER_TMP_DIR="$5"
+_PARSER_LOCAL_TMP_DIR="$5"
 
 # 2^32 / 8^2 / 2
 BFTOOL_CONVERTER_LIMIT=${WSI_PARSING_CONVERSION_LIMIT:-33554432}
@@ -44,120 +44,80 @@ function get_file_basename() {
 
 function get_cloud_path() {
     _path="$1"
+    _cloud_bucket_scheme="$2"
+
+    if [[ -z "$_cloud_bucket_scheme" ]]; then
+        _cloud_bucket_scheme="cp"
+    fi
     cloud_path=$(echo $_path | sed -rn 's/^(\/cloud-data\/)(.*)/\2/p')
-    echo "cp://$cloud_path"
+    echo "$_cloud_bucket_scheme://$cloud_path"
 }
 
-function convert_to_jpeg() {
-    _path="$1"
-    _dest_file="$(get_file_without_extention "$_path").jpeg"
-    log_info "Converting '$_path' to '$_dest_file'"
-    convert "$_path" "$_dest_file"
-    rm -f "$_path"
-}
-
-function build_jpeg() {
+function build_tiff() {
     _file_path="$1"
-    _file_xml_details_path="$2"
-    _series="$3"
+    _series="$2"
 
-    _series_details=$(xpath "$_file_xml_details_path" "/OME/Image[@ID='Image:"$_series"']/Pixels")
-    _width=$(echo $_series_details | xpath "string(/Pixels/@SizeX)" 2>/dev/null | grep -Eo "[0-9]+$")
-    _height=$(echo $_series_details | xpath "string(/Pixels/@SizeY)" 2>/dev/null | grep -Eo "[0-9]+$")
-
-    log_info "Converting series=$_series of '$_file_path' to JPEG [width=$_width | height=$_height]"
     _file_wo_extension=$(get_file_basename "$_file_path")
-    _crop_piece_width=$(( $BFTOOL_CONVERTER_LIMIT/$_height ))
-
-    if [[ $_crop_piece_width -ge $_width ]]; then
-        target_png="$_PARSER_TMP_DIR/$_file_wo_extension.png"
-        log_info "Conversion should not exceed memory limits, processing whole image"
-        bfconvert -series $_series -overwrite "$_file_path" "$target_png"
-        convert_to_jpeg "$target_png"
-        return $?
+    target_tiff="$_PARSER_LOCAL_TMP_DIR/$_file_wo_extension.tiff"
+    bfconvert -bigtiff -series $_series -overwrite "$_file_path" "$target_tiff"
+    if [ $? -ne 0 ]; then
+      log_warn "Errors during conversion to tiff, exiting..."
+      return 1
     fi
 
-    log_info "Conversion of the whole image at once could cause out-of-memory issues, processing in chunks"
-    _full_pieces=$(( $_width/$_crop_piece_width ))
-    _last_chunk_width=$(( $_width - _full_pieces*$_crop_piece_width ))
-
-    if [[ $_last_chunk_width -gt 0 ]]; then
-        log_info "Total chunks: $(( $_full_pieces + 1 ))"
-    else
-        log_info "Total chunks: $_full_pieces"
-    fi
-
-    log_info "Cropping params: [width=$_crop_piece_width | height=$_height]"
-
-    for (( i=0; i < $_full_pieces; i++ )); do
-        log_info "Converting peiece number=$i"
-        _width_padding=$(( $i*_crop_piece_width ))
-        _target_png="$_PARSER_TMP_DIR/$_file_wo_extension""_$i.png"
-        bfconvert -series $_series -crop $_width_padding,0,$_crop_piece_width,$_height -overwrite "$_file_path" "$_target_png"
-        convert_to_jpeg "$_target_png"
-    done
-
-    if [[ $_last_chunk_width -gt 0 ]]; then
-        log_info "Converting last chunk"
-        _last_chunk_width_padding=$(( $_full_pieces*$_crop_piece_width ))
-        _target_png="$_PARSER_TMP_DIR/$_file_wo_extension""_$(( $_full_pieces )).png"
-        bfconvert -series $_series -crop $_last_chunk_width_padding,0,$_last_chunk_width,$_height -overwrite "$_file_path" "$_target_png"
-        convert_to_jpeg "$_target_png"
-    else
-      _full_pieces=$(( $_full_pieces - 1 ))
-    fi
-
-    log_info "Merging chunks..."
-    _merged_file="$_PARSER_TMP_DIR/$_file_wo_extension.jpeg"
-    mv -f "$_PARSER_TMP_DIR/$_file_wo_extension""_0.jpeg" "$_merged_file"
-    for (( i=1; i <= $_full_pieces; i++ )); do
-        _chunk_to_append="$_PARSER_TMP_DIR/$_file_wo_extension""_$i.jpeg"
-        convert "$_merged_file" "$_chunk_to_append" +append "$_merged_file"
-        rm -f "$_chunk_to_append"
-    done
     return 0
 }
 
 log_info "Start processing '$_FILE_PATH'"
 
-build_jpeg "$_FILE_PATH" "$_XML_DETAILS_PATH" "$_SERIES_NUM"
+build_tiff "$_FILE_PATH" "$_SERIES_NUM"
 if [ $? -ne 0 ]; then
-    log_warn "Unable to retrieve .jpeg image to build deep zoom"
+    log_warn "Unable to retrieve .tiff image to build deep zoom"
     exit 1
 fi
 
 function generate_deepzoom() {
     _src_img="$1"
     _dz_location="$2"
-    vips dzsave "$_src_img" "$_dz_location" --background 0 --centre --layout google
+    vips dzsave "$_src_img" "$_dz_location" --background 0 --centre --layout google --suffix .jpg[Q=100] --tile-size=$_TILES_SIZE
     if [ $? -ne 0 ]; then
         log_warn "Errors during deep zoom generation, exiting..."
         exit 1
     fi
 }
 
-_FILE_BASENAME=$(get_file_basename "$_FILE_PATH")
-log_info "Generating deep zoom structure..."
-dz_tmp="$_PARSER_TMP_DIR/$_FILE_BASENAME.tiles.new"
-dz_final="$_TILES_PARENT_DIR/$_FILE_BASENAME.tiles"
-if [[ ! -d "$dz_final" ]]; then
-    log_info "The final location is empty, generating DZ there immediately"
-    generate_deepzoom "$_PARSER_TMP_DIR/$_FILE_BASENAME.jpeg" "$dz_final"
-else
-    log_info "Generating DZ in temporary location"
-    rm -rf "$dz_tmp"
-    generate_deepzoom "$_PARSER_TMP_DIR/$_FILE_BASENAME.jpeg" "$dz_tmp"
-    log_info "Moving DZ to the final location..."
-    dz_final_cloud="$(get_cloud_path $dz_final)"
-    pipe storage rm -r -y "$dz_final_cloud"
+function tmp_dir_cleanup() {
+  log_info "Cleaning local DZ temporary files [$_PARSER_LOCAL_TMP_DIR] ..."
+  rm -rf "$_PARSER_LOCAL_TMP_DIR"
+  if [ $? -ne 0 ]; then
+      log_warn "An error occurred during cleanup!"
+      exit 1
+  fi
+}
 
-    dz_tmp_cloud="$(get_cloud_path $dz_tmp)"
-    pipe storage mkdir "$dz_final_cloud" && \
-    pipe storage mv -r -f "$dz_tmp_cloud" "$dz_final_cloud"
-    if [ $? -ne 0 ]; then
-        log_warn "Errors during deep zoom finalization, exiting..."
-        exit 1
-    fi
+log_info "Generating deep zoom structure..."
+_FILE_BASENAME=$(get_file_basename "$_FILE_PATH")
+dz_tmp="$_PARSER_LOCAL_TMP_DIR/$_FILE_BASENAME.tiles"
+dz_final="$_TILES_PARENT_DIR/$_FILE_BASENAME.tiles"
+
+log_info "Generating DZ in [$dz_tmp]"
+rm -rf "$dz_tmp"
+generate_deepzoom "$_PARSER_LOCAL_TMP_DIR/$_FILE_BASENAME.tiff" "$dz_tmp"
+
+dz_final_cloud="$(get_cloud_path $dz_final)"
+log_info "Moving DZ to the final location [$dz_final_cloud]..."
+pipe storage rm -r -y "$dz_final_cloud"
+if [[ -z "$WSI_PARSER_AWS_CLI_FINALIZATION" ]]; then
+    pipe storage mkdir "$dz_final_cloud"
+    pipe storage mv -r -f "$dz_tmp" "$dz_final_cloud"
+else
+    aws s3 cp --recursive --quiet "$dz_tmp" "$(get_cloud_path $dz_final s3)"
 fi
-log_info "Deep zoom is generated successfully!"
+if [ $? -ne 0 ]; then
+    log_warn "Errors during deep zoom finalization, exiting..."
+    tmp_dir_cleanup
+    exit 1
+fi
+
+tmp_dir_cleanup
 exit 0
--- deploy/docker/cp-tools/research/wsi-parser/process_files.py
+++ deploy/docker/cp-tools/research/wsi-parser/process_files.py
@@ -12,22 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import errno
 import json
 import os
 import multiprocessing
 import datetime
+import shutil
+import tempfile
 import time
 import xml.etree.ElementTree as ET
 
 from pipeline.api import PipelineAPI, TaskStatus
 from pipeline.log import Logger
-
+from pipeline.common import get_path_with_trailing_delimiter
 
 WSI_PROCESSING_TASK_NAME = 'WSI processing'
 TAGS_MAPPING_RULE_DELIMITER = ','
 TAGS_MAPPING_KEYS_DELIMITER = '='
 SCHEMA_PREFIX = '{http://www.openmicroscopy.org/Schemas/OME/2016-06}'
-DZ_IMAGE_AREA_LIMIT = int(os.getenv('WSI_PARSING_DZ_IMAGE_LIMIT', 15000 * 25000))
 WSI_ACTIVE_PROCESSING_TIMEOUT_MIN = int(os.getenv('WSI_ACTIVE_PROCESSING_TIMEOUT_MIN', 360))
 DZ_TILES_SIZE = int(os.getenv('WSI_PARSING_DZ_TILES_SIZE', 256))
 
@@ -80,6 +82,11 @@ class WsiParsingUtils:
         return os.path.join(parent_dir, '.wsiparser', name_without_extension)
 
     @staticmethod
+    def generate_local_service_directory(file_path):
+        name_without_extension = WsiParsingUtils.get_basename_without_extension(file_path)
+        return tempfile.mkdtemp(prefix=name_without_extension + '.wsiparser.')
+
+    @staticmethod
     def create_service_dir_if_not_exist(file_path):
         directory = WsiParsingUtils.get_service_directory(file_path)
         if not os.path.exists(directory):
@@ -97,6 +104,13 @@ class WsiParsingUtils:
         processing_stat_file_modification_date = WsiParsingUtils.get_file_last_modification_time(active_stat_file)
         processing_deadline = datetime.datetime.now() - datetime.timedelta(minutes=WSI_ACTIVE_PROCESSING_TIMEOUT_MIN)
         return (processing_stat_file_modification_date - time.mktime(processing_deadline.timetuple())) < 0
+
+    @staticmethod
+    def extract_cloud_path(file_path):
+        path_chunks = file_path.split('/cloud-data/', 1)
+        if len(path_chunks) != 2:
+            raise RuntimeError('Unable to determine cloud path of [{}]'.format(file_path))
+        return 'cp://{}'.format(path_chunks[1])
 
 
 class WsiProcessingFileGenerator:
@@ -136,16 +150,14 @@ class WsiProcessingFileGenerator:
         return paths
 
     def is_processing_required(self, file_path):
+        if os.getenv('WSI_PARSING_SKIP_DATE_CHECK'):
+            return True
         active_stat_file = WsiParsingUtils.get_stat_active_file_name(file_path)
         if os.path.exists(active_stat_file):
             return WsiParsingUtils.active_processing_exceed_timeout(active_stat_file)
         stat_file = WsiParsingUtils.get_stat_file_name(file_path)
         if not os.path.isfile(stat_file):
             return True
-        with open(stat_file) as last_sync_stats:
-            json_stats = json.load(last_sync_stats)
-            if 'dz_pixel_limit' in json_stats and json_stats['dz_pixel_limit'] != DZ_IMAGE_AREA_LIMIT:
-                return True
         stat_file_modification_date = WsiParsingUtils.get_file_last_modification_time(stat_file)
         if self.is_modified_after(file_path, stat_file_modification_date):
             return True
@@ -168,15 +180,8 @@ class WsiFileTagProcessor:
         self.xml_info_tree = xml_info_tree
         self.api = PipelineAPI(os.environ['API'], 'logs')
         self.system_dictionaries_url = self.api.api_url + self.CATEGORICAL_ATTRIBUTE
-        self.cloud_path = self.extract_cloud_path(file_path)
+        self.cloud_path = WsiParsingUtils.extract_cloud_path(file_path)
         self.tags_mapping_rules = tags_mapping_rules
-
-    @staticmethod
-    def extract_cloud_path(file_path):
-        path_chunks = file_path.split('/cloud-data/', 1)
-        if len(path_chunks) != 2:
-            return None
-        return path_chunks[1]
 
     def log_processing_info(self, message, status=TaskStatus.RUNNING):
         Logger.log_task_event(WSI_PROCESSING_TASK_NAME, '[{}] {}'.format(self.file_path, message), status=status)
@@ -199,7 +204,7 @@ class WsiFileTagProcessor:
         pipe_tags = self.prepare_tags(existing_attributes_dictionary, tags_to_push)
         tags_to_push_str = ' '.join(pipe_tags)
         self.log_processing_info('Following tags will be assigned to the file: {}'.format(tags_to_push_str))
-        return os.system('pipe storage set-object-tags "cp://{}" {}'.format(self.cloud_path, tags_to_push_str))
+        return os.system('pipe storage set-object-tags "{}" {}'.format(self.cloud_path, tags_to_push_str))
 
     def prepare_tags(self, existing_attributes_dictionary, tags_to_push):
         attribute_updates = list()
@@ -295,6 +300,7 @@ class WsiFileParser:
         self.xml_info_tree = ET.parse(self.xml_info_file).getroot()
         self.stat_file_name = WsiParsingUtils.get_stat_file_name(self.file_path)
         self.tmp_stat_file_name = WsiParsingUtils.get_stat_active_file_name(self.file_path)
+        self.tmp_local_dir = WsiParsingUtils.generate_local_service_directory(self.file_path)
 
     def generate_xml_info_file(self):
         WsiParsingUtils.create_service_dir_if_not_exist(self.file_path)
@@ -303,21 +309,30 @@ class WsiFileParser:
     def log_processing_info(self, message, status=TaskStatus.RUNNING):
         Logger.log_task_event(WSI_PROCESSING_TASK_NAME, '[{}] {}'.format(self.file_path, message), status=status)
 
-    def create_tmp_stat_file(self):
+    def create_tmp_stat_file(self, image_details):
         WsiParsingUtils.create_service_dir_if_not_exist(self.file_path)
-        self._write_processing_stats_to_file(self.tmp_stat_file_name)
+        self._write_processing_stats_to_file(self.tmp_stat_file_name, image_details)
 
     def clear_tmp_stat_file(self):
         if os.path.exists(self.tmp_stat_file_name):
             os.remove(self.tmp_stat_file_name)
 
-    def update_stat_file(self):
-        WsiParsingUtils.create_service_dir_if_not_exist(self.file_path)
-        self._write_processing_stats_to_file(self.stat_file_name)
+    def clear_tmp_local_dir(self):
+        if os.path.exists(self.tmp_local_dir):
+            shutil.rmtree(self.tmp_local_dir)
 
-    def _write_processing_stats_to_file(self, file_path):
-        details = {'file': self.file_path, 'dz_pixel_limit': DZ_IMAGE_AREA_LIMIT}
-        with open(file_path, 'w') as output_file:
+    def update_stat_file(self, target_image_details):
+        WsiParsingUtils.create_service_dir_if_not_exist(self.file_path)
+        self._write_processing_stats_to_file(self.stat_file_name, target_image_details)
+
+    def _write_processing_stats_to_file(self, stat_file_path, target_image_details):
+        details = {
+            'file': self.file_path,
+            'target_series': target_image_details.id,
+            'width': target_image_details.width,
+            'height': target_image_details.height
+        }
+        with open(stat_file_path, 'w') as output_file:
             output_file.write(json.dumps(details, indent=4))
 
     def update_dz_info_file(self, original_width, original_height):
@@ -334,6 +349,13 @@ class WsiFileParser:
                                         original_width,
                                         original_height,
                                         max_zoom)
+
+    def _is_same_series_selected(self, stat_file, selected_series):
+        with open(WsiParsingUtils.get_stat_file_name(stat_file)) as last_sync_stats:
+            json_stats = json.load(last_sync_stats)
+            if 'target_series' in json_stats and json_stats['target_series'] == selected_series:
+                return True
+            return False
 
     def _max_zoom_level(self, tiles_dir):
         dz_layers_folders = 0
@@ -383,13 +405,7 @@ class WsiFileParser:
                 break
         self.log_processing_info('Target group is: {}'.format(target_group))
         if target_group:
-            for image_details in series_mapping[target_group]:
-                image_area_size = image_details.width * image_details.height
-                if image_area_size < DZ_IMAGE_AREA_LIMIT:
-                    target_image_details = image_details
-                    break
-            if not target_image_details:
-                target_image_details = series_mapping[target_group][-1]
+            target_image_details = series_mapping[target_group][0]
         return target_image_details
 
     def group_image_series(self, images):
@@ -414,13 +430,46 @@ class WsiFileParser:
             series_mapping[current_group_name] = current_group_details_list
         return series_mapping
 
+    def _mkdir(self, path):
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                return False
+        return True
+
+    def _localize_related_files(self):
+        image_cloud_path = WsiParsingUtils.extract_cloud_path(self.file_path)
+        local_tmp_dir_trailing = get_path_with_trailing_delimiter(self.tmp_local_dir)
+        main_file_localization_result = \
+            os.system('pipe storage cp -f "{}" "{}"'.format(image_cloud_path, local_tmp_dir_trailing))
+        if main_file_localization_result != 0:
+            return False
+        stacks_dir_name = '_{}_'.format(WsiParsingUtils.get_basename_without_extension(self.file_path))
+        stacks_mnt_path = os.path.join(os.path.dirname(self.file_path), stacks_dir_name)
+        if os.path.exists(stacks_mnt_path):
+            local_stacks_dir = get_path_with_trailing_delimiter(os.path.join(self.tmp_local_dir, stacks_dir_name))
+            if self._mkdir(local_stacks_dir):
+                stacks_cloud_path = get_path_with_trailing_delimiter(
+                    WsiParsingUtils.extract_cloud_path(stacks_mnt_path))
+                stacks_localization_result = os.system('pipe storage cp -r -f "{}" "{}"'
+                                                       .format(stacks_cloud_path, local_stacks_dir))
+                if stacks_localization_result != 0:
+                    self.log_processing_info('Some errors occurred during stacks localization')
+            else:
+                self.log_processing_info('Unable to create tmp folder for image stacks')
+        else:
+            self.log_processing_info('No stacks related files found')
+        return True
+
     def process_file(self):
         self.log_processing_info('Start processing')
         if os.path.exists(self.tmp_stat_file_name) \
                 and not WsiParsingUtils.active_processing_exceed_timeout(self.tmp_stat_file_name):
             log_info('This file is processed by another parser, skipping...')
             return 0
-        self.create_tmp_stat_file()
         if self.tags_mapping_rules:
             try:
                 if WsiFileTagProcessor(self.file_path, self.xml_info_tree, self.tags_mapping_rules).process_tags() != 0:
@@ -428,20 +477,30 @@ class WsiFileParser:
             except Exception as e:
                 log_info('An error occurred during tags processing: {}'.format(str(e)))
         target_image_details = self.calculate_target_series()
+        self.create_tmp_stat_file(target_image_details)
         target_series = target_image_details.id
         if target_series is None:
-            self.log_processing_info('Unable to determine target series, skipping DZ creation ')
+            self.log_processing_info('Unable to determine target series, skipping DZ creation... ')
             return 1
-        self.log_processing_info('Series #{} selected for DZ creation'.format(target_series))
-        conversion_result = os.system('bash "{}" "{}" "{}" "{}" "{}" "{}"'
+        elif self._is_same_series_selected(self.file_path, target_series):
+            self.log_processing_info('The same series [{}] is selected for image processing, skipping... '
+                                     .format(target_series))
+            return 0
+        self.log_processing_info('Series #{} selected for DZ creation [width={}; height={}]'
+                                 .format(target_series, target_image_details.width, target_image_details.height))
+        if not self._localize_related_files():
+            self.log_processing_info('Some errors occurred during copying files from the bucket, exiting...')
+            return 1
+        local_file_path = os.path.join(self.tmp_local_dir, os.path.basename(self.file_path))
+        conversion_result = os.system('bash "{}" {} {} "{}" "{}" "{}"'
                                       .format(self._DEEP_ZOOM_CREATION_SCRIPT,
-                                              self.file_path,
-                                              self.xml_info_file,
+                                              local_file_path,
                                               target_series,
+                                              DZ_TILES_SIZE,
                                               os.path.dirname(self.file_path),
-                                              WsiParsingUtils.get_service_directory(self.file_path)))
+                                              self.tmp_local_dir))
         if conversion_result == 0:
-            self.update_stat_file()
+            self.update_stat_file(target_image_details)
             self.update_dz_info_file(target_image_details.width, target_image_details.height)
             self.log_processing_info('File processing is finished')
         else:
@@ -468,6 +527,7 @@ def try_process_file(file_path):
     finally:
         if parser:
             parser.clear_tmp_stat_file()
+            parser.clear_tmp_local_dir()
 
 
 def process_wsi_files():
