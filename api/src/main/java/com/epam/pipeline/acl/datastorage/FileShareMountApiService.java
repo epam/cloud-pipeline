138338eadbcf57c85d376262f36b839d1884ee69
Andrey Georgitsa <andrey_georgitsa@epam.com>
2021-09-23 11:39:26 +0300

    Issue 2155 Expose share loading endpoint in corresponding controller

--- api/src/main/java/com/epam/pipeline/acl/datastorage/FileShareMountApiService.java
+++ api/src/main/java/com/epam/pipeline/acl/datastorage/FileShareMountApiService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,4 +39,8 @@ public class FileShareMountApiService {
         fileShareMountManager.delete(id);
     }
 
+    @PreAuthorize(AclExpressions.ADMIN_ONLY)
+    public FileShareMount load(final Long id) {
+        return fileShareMountManager.load(id);
+    }
 }
