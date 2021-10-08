6d0706d7bdb0a22fa5188ee30d37e8cf71e1fdaa
Andrey Tsibin <andrei_tcibin@epam.com>
2021-10-08 11:53:45 +0300

    Support manager node users and groups sharing within cluster

--- workflows/pipe-common/pipeline/log/logger.py
+++ workflows/pipe-common/pipeline/log/logger.py
@@ -12,8 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime
+import logging
 import os
+from abc import ABCMeta, abstractmethod
+
 from pipeline.api import LogEntry, TaskStatus, PipelineAPI
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -139,3 +144,157 @@ class Logger:
                              instance)
         pipe_api = PipelineAPI(_api_url, _log_dir)
         pipe_api.log_event(log_entry, omit_console=omit_console)
+
+
+class CloudPipelineLogger:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def info(self, message, task=None):
+        pass
+
+    @abstractmethod
+    def debug(self, message, task=None):
+        pass
+
+    @abstractmethod
+    def warning(self, message, task=None):
+        pass
+
+    @abstractmethod
+    def success(self, message, task=None):
+        pass
+
+    @abstractmethod
+    def error(self, message, task=None):
+        pass
+
+
+class LocalLogger(CloudPipelineLogger):
+
+    def __init__(self, inner=None):
+        self._inner = inner
+
+    def info(self, message, task=None):
+        logging.info(message)
+        if self._inner:
+            self._inner.info(message, task)
+
+    def debug(self, message, task=None):
+        logging.debug(message)
+        if self._inner:
+            self._inner.debug(message, task)
+
+    def warning(self, message, task=None):
+        logging.warning(message)
+        if self._inner:
+            self._inner.warning(message, task)
+
+    def success(self, message, task=None):
+        logging.info(message)
+        if self._inner:
+            self._inner.success(message, task)
+
+    def error(self, message, task=None):
+        logging.error(message)
+        if self._inner:
+            self._inner.error(message, task)
+
+
+class LevelLogger(CloudPipelineLogger):
+
+    _levels = ['ERROR', 'WARNING', 'INFO', 'DEBUG']
+    _error_idx = _levels.index('ERROR')
+    _warn_idx = _levels.index('WARNING')
+    _info_idx = _levels.index('INFO')
+    _debug_idx = _levels.index('DEBUG')
+
+    def __init__(self, level, inner):
+        self._level_idx = self._levels.index(level)
+        self._inner = inner
+
+    def info(self, message, task=None):
+        if self._applies(self._info_idx):
+            self._inner.info(message, task)
+
+    def debug(self, message, task=None):
+        if self._applies(self._debug_idx):
+            self._inner.debug(message, task)
+
+    def warning(self, message, task=None):
+        if self._applies(self._warn_idx):
+            self._inner.warning(message, task)
+
+    def success(self, message, task=None):
+        if self._applies(self._info_idx):
+            self._inner.success(message, task)
+
+    def error(self, message, task=None):
+        if self._applies(self._error_idx):
+            self._inner.error(message, task)
+
+    def _applies(self, level_idx):
+        return self._level_idx >= level_idx
+
+
+class TaskLogger(CloudPipelineLogger):
+
+    def __init__(self, task, inner):
+        self._task = task
+        self._inner = inner
+
+    def info(self, message, task=None):
+        self._inner.info(message, task or self._task)
+
+    def debug(self, message, task=None):
+        self._inner.debug(message, task or self._task)
+
+    def warning(self, message, task=None):
+        self._inner.warning(message, task or self._task)
+
+    def success(self, message, task=None):
+        self._inner.success(message, task or self._task)
+
+    def error(self, message, task=None):
+        self._inner.error(message, task or self._task)
+
+
+class RunLogger(CloudPipelineLogger):
+
+    _DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+    _DATE_WITH_MILLISECONDS = '%s.%03d'
+
+    def __init__(self, api, run_id, inner=None):
+        self._api = api
+        self._run_id = run_id
+        self._inner = inner
+
+    def info(self, message, task=None):
+        self._log(message=message, task=task, status='RUNNING')
+        if self._inner:
+            self._inner.info(message, task)
+
+    def debug(self, message, task=None):
+        self._log(message=message, task=task, status='RUNNING')
+        if self._inner:
+            self._inner.debug(message, task)
+
+    def warning(self, message, task=None):
+        self._log(message=message, task=task, status='RUNNING')
+        if self._inner:
+            self._inner.warning(message, task)
+
+    def success(self, message, task=None):
+        self._log(message=message, task=task, status='SUCCESS')
+        if self._inner:
+            self._inner.success(message, task)
+
+    def error(self, message, task=None):
+        self._log(message=message, task=task, status='FAILURE')
+        if self._inner:
+            self._inner.error(message, task)
+
+    def _log(self, message, task, status):
+        now_utc = datetime.datetime.utcnow()
+        formatted_dt = self._DATE_WITH_MILLISECONDS % (now_utc.strftime(self._DATE_FORMAT), now_utc.microsecond / 1000)
+        self._api.log_efficiently(run_id=self._run_id, message=message, task=task, status=status, date=formatted_dt)
---
+++ workflows/pipe-common/scripts/configure_shared_users.py
@@ -0,0 +1,74 @@
+# Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+import traceback
+
+from pipeline.api import PipelineAPI
+from pipeline.common.common import execute
+from pipeline.log.logger import LocalLogger, RunLogger, TaskLogger, LevelLogger
+from pipeline.utils.package import install_package
+from pipeline.utils.path import mkdir
+
+_MANAGER_CLUSTER_ROLE = 'master'
+_ETC_DIRECTORY = '/etc'
+_ETC_DIRECTORY_SHARED = '/etc-shared'
+_ETC_FILE_PATHS = ['passwd', 'shadow', 'group', 'gshadow']
+
+if __name__ == '__main__':
+    api_url = os.environ['API']
+    api_token = os.environ['API_TOKEN']
+    run_id = os.environ['RUN_ID']
+    cluster_role = os.getenv('cluster_role', _MANAGER_CLUSTER_ROLE)
+    logging_directory = os.getenv('CP_CAP_SHARE_USERS_LOGDIR', os.getenv('LOG_DIR', '/var/log'))
+    logging_level = os.getenv('CP_CAP_SHARE_USERS_LOGGING_LEVEL', 'ERROR')
+    logging_level_local = os.getenv('CP_CAP_SHARE_USERS_LOGGING_LEVEL_LOCAL', 'DEBUG')
+    logging_format = os.getenv('CP_CAP_SHARE_USERS_LOGGING_FORMAT', '%(asctime)s:%(levelname)s: %(message)s')
+
+    logging.basicConfig(level=logging_level_local, format=logging_format,
+                        filename=os.path.join(logging_directory, 'configure_shared_users.log'))
+
+    api = PipelineAPI(api_url=api_url, log_dir=logging_directory)
+    logger = RunLogger(api=api, run_id=run_id)
+    logger = TaskLogger(task='InitializeSharedUsers', inner=logger)
+    logger = LevelLogger(level=logging_level, inner=logger)
+    logger = LocalLogger(inner=logger)
+
+    try:
+        if cluster_role != _MANAGER_CLUSTER_ROLE:
+            logger.info('Configuring shared users and groups management...')
+
+            logger.info('Installing sshfs...')
+            install_package(rpm='fuse-sshfs', nonrpm='sshfs', logger=logger)
+
+            logger.info('Mounting etc directory from manager node...')
+            mkdir(_ETC_DIRECTORY_SHARED)
+            execute('sshfs -o ro,allow_other root@lizardfs-master:{etc_directory} {etc_directory_shared}'
+                    .format(etc_directory=_ETC_DIRECTORY, etc_directory_shared=_ETC_DIRECTORY_SHARED),
+                    logger=logger)
+
+            logger.info('Mounting users and groups from manager node etc directory...')
+            for etc_file_path in _ETC_FILE_PATHS:
+                file_path = os.path.join(_ETC_DIRECTORY, etc_file_path)
+                file_path_shared = os.path.join(_ETC_DIRECTORY_SHARED, etc_file_path)
+                execute('mount -o ro,bind,allow_other {file_path_shared} {file_path}'
+                        .format(file_path=file_path, file_path_shared=file_path_shared),
+                        logger=logger)
+
+            logger.success('Shared users and groups management was successfully configured.')
+    except BaseException as e:
+        traceback.print_exc()
+        stacktrace = traceback.format_stack()
+        logger.error('Shared users and groups management configuration has failed: {} {}'.format(e, stacktrace))
