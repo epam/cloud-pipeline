32f74b821b77666db8d30b885079d5d303400d85
pavel_silin <pavel_silin@epam.com>
2021-10-26 16:19:58 +0300

    issue 2242 initial structure for release notes agent

---
+++ release-notes-agent/src/main/java/com/epam/release/notes/agent/Application.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 EPAM Systems, Inc. (https://www.epam.com/)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.release.notes.agent;
+
+import com.epam.release.notes.agent.service.ReleaseNotificationService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ReleaseNotificationService notificationService() {
+        return null;
+    }
+
+    @Override
+    public void run(final String... args) {
+        Optional.ofNullable(notificationService()).ifPresent(ReleaseNotificationService::perform);
+    }
+}
