f1d182817499954f88343a2f4f6b5b8f3ef1e5a0
Mariia Zueva <mariia_zueva@epam.com>
2021-10-28 16:10:44 +0300

    Issue #2235 Assume AWS credentials - allow to specify `iamRole` for AWS region and use it for ec2 and lustre requests. (#2261)

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -117,6 +117,10 @@ public class SystemPreferences {
     private static final String STORAGE_FSBROWSER_BLACK_LIST_DEFAULT =
             "/bin,/var,/home,/root,/sbin,/sys,/usr,/boot,/dev,/lib,/proc,/etc";
     private static final String FACETED_FILTER_GROUP = "Faceted Filter";
+    public static final ObjectPreference<SharedStoragePermissions> DATA_SHARING_DEFAULT_PERMISSIONS =
+            new ObjectPreference<>("data.sharing.storage.folders.default.permissions", null,
+            new TypeReference<SharedStoragePermissions>() {}, DATA_SHARING_GROUP,
+            isNullOrValidJson(new TypeReference<SharedStoragePermissions>() {}));
 
     // COMMIT_GROUP
     public static final StringPreference COMMIT_DEPLOY_KEY = new StringPreference("commit.deploy.key", null,
@@ -612,10 +616,7 @@ public class SystemPreferences {
             DATA_SHARING_GROUP, pass);
     public static final StringPreference DATA_SHARING_FOLDERS_DIR =
             new StringPreference("data.sharing.storage.folders.directory", null, DATA_SHARING_GROUP, pass);
-    public static final ObjectPreference<SharedStoragePermissions> DATA_SHARING_DEFAULT_PERMISSIONS = new ObjectPreference<>(
-            "data.sharing.storage.folders.default.permissions", null,
-            new TypeReference<SharedStoragePermissions>() {}, DATA_SHARING_GROUP,
-            isNullOrValidJson(new TypeReference<SharedStoragePermissions>() {}));
+    private static final String CLOUD_REGION_GROUP = "Cloud region";
 
     // SYSTEM_GROUP
     /**
@@ -831,6 +832,11 @@ public class SystemPreferences {
             isValidEnum(LustreDeploymentType.class));
     public static final BooleanPreference LUSTRE_FS_MOUNT_IP = new BooleanPreference(
             "lustre.fs.mount.ip", false, LUSTRE_GROUP, pass);
+    // Cloud Region
+    public static final IntPreference CLOUD_REGION_TEMP_CREDENTIALS_DURATION = new IntPreference(
+            "cloud.temp.credentials.duration", 3600, CLOUD_REGION_GROUP, isGreaterThan(0));
+    public static final IntPreference CLOUD_REGION_TEMP_CREDENTIALS_EXPIRATION = new IntPreference(
+            "cloud.temp.credentials.expiration", 3600, CLOUD_REGION_GROUP, isGreaterThan(0));
 
     private static final Pattern GIT_VERSION_PATTERN = Pattern.compile("(\\d)\\.(\\d)");
 
