82c110d1e0f53082614227abd9747cd8bb2e64e8
Andrey Tsibin <andrei_tcibin@epam.com>
2021-10-28 12:42:02 +0300

    Add support for pipe fuse temporary resources cleanup

--- pipe-cli/src/utilities/clean_operations_manager.py
+++ pipe-cli/src/utilities/clean_operations_manager.py
@@ -32,10 +32,14 @@ class CleanOperationsManager:
         pass
 
     def clean(self, force=False, quiet=False):
-        logging.debug('Cleaning temporary directories...')
+        config_dir_path = os.path.dirname(Config.get_home_dir_config_path())
+        self._clean_pipe_tmp_dirs(config_dir_path, force, quiet)
+        self._clean_pipe_fuse_tmp_dirs(config_dir_path)
+
+    def _clean_pipe_tmp_dirs(self, config_dir_path, force, quiet):
+        logging.debug('Cleaning pipe temporary directories...')
         current_tmp_dir_path = sys._MEIPASS if is_frozen() else None
-        config_folder = os.path.dirname(Config.get_home_dir_config_path())
-        root_tmp_dir_path = os.path.join(config_folder, 'tmp')
+        root_tmp_dir_path = os.path.join(config_dir_path, 'tmp')
         if not os.path.isdir(root_tmp_dir_path):
             return
         any_tmp_dir_without_lock = False
@@ -44,13 +48,15 @@ class CleanOperationsManager:
             tmp_dir_lock_path = os.path.join(tmp_dir_path, self._LOCK_NAME)
             if not tmp_dir_name.startswith('_MEI') or tmp_dir_path == current_tmp_dir_path:
                 continue
+            if not os.path.isdir(tmp_dir_path):
+                continue
             if not os.path.exists(tmp_dir_lock_path) and not force:
-                logging.debug('Skipping temporary directory without lock deletion '
+                logging.debug('Skipping pipe temporary directory without lock deletion '
                               'because --force flag is not used %s...', tmp_dir_path)
                 any_tmp_dir_without_lock = True
                 continue
             if os.path.exists(tmp_dir_lock_path) and self._is_dir_locked(tmp_dir_path, tmp_dir_lock_path):
-                logging.debug('Skipping locked temporary directory deletion %s...', tmp_dir_path)
+                logging.debug('Skipping running pipe temporary directory deletion %s...', tmp_dir_path)
                 continue
             self._remove_dir(tmp_dir_path)
         if any_tmp_dir_without_lock and not quiet:
@@ -76,12 +82,51 @@ class CleanOperationsManager:
                 logging.debug('Unlocking temporary directory %s...', dir_path)
                 self._unlock(tmp_dir_lock_descriptor)
 
+    def _clean_pipe_fuse_tmp_dirs(self, config_dir_path):
+        logging.debug('Cleaning pipe fuse temporary directories...')
+        if not os.path.isdir(config_dir_path):
+            return
+        for tmp_item_name in os.listdir(config_dir_path):
+            tmp_item_path = os.path.join(config_dir_path, tmp_item_name)
+            if not tmp_item_name.startswith('pipe-fuse'):
+                continue
+            if os.path.isdir(tmp_item_path):
+                tmp_dir_executable_path = os.path.join(tmp_item_path, 'pipe-fuse')
+                if self._any_process_with_argument(tmp_dir_executable_path):
+                    logging.debug('Skipping running pipe fuse temporary directory deletion %s...', tmp_item_path)
+                    continue
+                self._remove_dir(tmp_item_path)
+            else:
+                if self._any_process_with_argument(tmp_item_path):
+                    logging.debug('Skipping running pipe fuse temporary file deletion %s...', tmp_item_path)
+                    continue
+                self._remove_file(tmp_item_path)
+
+    def _any_process_with_argument(self, argument):
+        logging.debug('Searching for any processes with argument %s...', argument)
+        import psutil
+        for proc in psutil.process_iter():
+            try:
+                if argument in proc.cmdline():
+                    logging.debug('Found process (%s) with argument %s', proc.pid, argument)
+                    return True
+            except psutil.AccessDenied:
+                pass
+        return False
+
     def _remove_dir(self, dir_path):
         try:
             logging.debug('Deleting temporary directory %s...', dir_path)
             shutil.rmtree(dir_path)
         except Exception:
             logging.warn('Temporary directory deletion has failed: %s', traceback.format_exc())
+
+    def _remove_file(self, file_path):
+        try:
+            logging.debug('Deleting temporary file %s...', file_path)
+            os.remove(file_path)
+        except Exception:
+            logging.warn('Temporary file deletion has failed: %s', traceback.format_exc())
 
     def lock(self, operation):
         tmp_dir_path = Config.get_base_source_dir()
