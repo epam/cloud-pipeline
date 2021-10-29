6b297e0e5855da0f181f52089787051b3660aa5c
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2021-10-29 11:49:51 +0300

    GUI Cloud regions form: 'Main Service Role' field for AWS regions

--- client/src/components/settings/AWSRegionsForm.js
+++ client/src/components/settings/AWSRegionsForm.js
@@ -725,6 +725,7 @@ class AWSRegionForm extends React.Component {
       'policy',
       'profile',
       'sshKeyName',
+      'iamRole',
       'tempCredentialsRole',
       {
         key: 'backupDuration',
@@ -900,6 +901,7 @@ class AWSRegionForm extends React.Component {
       check('profile', checkStringValue) ||
       check('sshKeyName', checkStringValue) ||
       check('azurePolicy', checkIPRangeValue) ||
+      check('iamRole', checkStringValue) ||
       check('tempCredentialsRole', checkStringValue) ||
       check('backupDuration', checkIntegerValue) ||
       check('versioningEnabled', checkBOOLValue) ||
@@ -1716,6 +1718,18 @@ class AWSRegionForm extends React.Component {
                   required: this.providerSupportsField('applicationName'),
                   message: 'Application name is required'
                 }]
+              })(
+                <Input
+                  size="small"
+                  disabled={this.props.pending} />
+              )}
+            </Form.Item>
+            <Form.Item
+              label="Main Service Role"
+              {...this.formItemLayout}
+              className={this.getFieldClassName('iamRole', 'edit-region-iam-role-container')}>
+              {getFieldDecorator('iamRole', {
+                initialValue: this.props.region.iamRole
               })(
                 <Input
                   size="small"
