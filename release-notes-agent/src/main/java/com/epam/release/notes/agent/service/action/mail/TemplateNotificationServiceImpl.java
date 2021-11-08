20c313fedd941e0e55874c400cdf51ebe7519305
Grigorii Kolychev <kolychevgv@mail.ru>
2021-11-08 17:10:16 +0300

    Issue #2246 added licenses

--- release-notes-agent/src/main/java/com/epam/release/notes/agent/service/action/mail/TemplateNotificationServiceImpl.java
+++ release-notes-agent/src/main/java/com/epam/release/notes/agent/service/action/mail/TemplateNotificationServiceImpl.java
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.epam.release.notes.agent.service.action.mail;
 
 import com.epam.release.notes.agent.entity.github.GitHubIssue;
