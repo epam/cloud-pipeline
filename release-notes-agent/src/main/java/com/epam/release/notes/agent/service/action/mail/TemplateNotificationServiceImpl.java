759c7cd41cc2f08fe988c0b9294e476bbbcd1453
Yulia Kamyshova <yulia_kamyshova@epam.com>
2021-11-08 13:25:30 +0300

    Issue 2277: Send notification for Release Notes Agent (#2282)
    * issue #2277 Sending Notification for Release Notes Agent
    
    * issue #2277 Sending Notification for Release Notes Agent: updates after review
    
    * issue #2282 recipients were added to EmailContent

--- release-notes-agent/src/main/java/com/epam/release/notes/agent/service/action/mail/TemplateNotificationServiceImpl.java
+++ release-notes-agent/src/main/java/com/epam/release/notes/agent/service/action/mail/TemplateNotificationServiceImpl.java
@@ -12,18 +12,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.epam.release.notes.agent.service.action.mail;
 
-package com.epam.release.notes.agent.entity.mail;
-
-import lombok.Builder;
-import lombok.Value;
+import com.epam.release.notes.agent.entity.github.GitHubIssue;
+import com.epam.release.notes.agent.entity.jira.JiraIssue;
+import com.epam.release.notes.agent.entity.mail.EmailContent;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Value
-@Builder
-public class EmailContent {
-    String title;
-    String body;
-    List<String> recipients;
+@Service
+public class TemplateNotificationServiceImpl implements TemplateNotificationService {
+
+    @Override
+    public EmailContent populate(String oldVersion, String newVersion, List<JiraIssue> jiraIssues, List<GitHubIssue> gitHubIssues) {
+        return null;
+    }
 }
