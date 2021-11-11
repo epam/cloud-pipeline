97d0f7078fb204dfa8ba795a3f6b13cd0fca72e0
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2021-11-11 16:25:17 +0300

    Issue #2233: Added storage.webdav.access.duration.seconds preference
    (cherry picked from commit 2abe6ecaa2b145f49d5febc01f55e0e504c26353)
    (cherry picked from commit 36fe9075855d610409758dc19fafbb15e10f02e1)

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -146,6 +146,8 @@ public class SystemPreferences {
                                isNullOrValidJson(new TypeReference<DataStorageTemplate>() {}));
     public static final IntPreference DATA_STORAGE_DAV_MOUNT_MAX_STORAGES = new IntPreference(
             "storage.dav.mount.max.storages", 32, DATA_STORAGE_GROUP, isGreaterThan(0));
+    public static final IntPreference DATA_STORAGE_DAV_ACCESS_DURATION_SECONDS = new IntPreference(
+            "storage.webdav.access.duration.seconds", 86400, DATA_STORAGE_GROUP, isGreaterThan(0));
 
     /**
      * Black list for mount points, accept notation like: '/dir/*', '/dir/**'
