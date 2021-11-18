42b64cbfdd4f2a5d247067b3e0fd70b3ff2fd015
Andrey Tsibin <andrei_tcibin@epam.com>
2021-11-18 16:39:24 +0300

    Resolve pmd and checkstyle issues

--- api/src/main/java/com/epam/pipeline/manager/billing/RunBillingExporter.java
+++ api/src/main/java/com/epam/pipeline/manager/billing/RunBillingExporter.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
