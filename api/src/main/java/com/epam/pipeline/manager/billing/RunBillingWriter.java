395f6e02ecf93b6891ba89b9f1fe6a5ea341cd9a
Andrey Tsibin <andrei_tcibin@epam.com>
2021-11-19 11:13:18 +0300

    Return scaled run durations and costs in export

--- api/src/main/java/com/epam/pipeline/manager/billing/RunBillingWriter.java
+++ api/src/main/java/com/epam/pipeline/manager/billing/RunBillingWriter.java
@@ -6,18 +6,27 @@ import com.epam.pipeline.manager.preference.SystemPreferences;
 import com.opencsv.CSVWriter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 public class RunBillingWriter implements Closeable {
 
     private static final char SEPARATOR = ',';
+    private static final int NUMERIC_SCALE = 2;
+    private static final long DURATION_DIVISOR = TimeUnit.MINUTES.convert(NumberUtils.LONG_ONE, TimeUnit.HOURS);
+    private static final long COST_DIVISOR = BigDecimal.ONE.setScale(4, RoundingMode.CEILING)
+            .unscaledValue()
+            .longValue();
     private static final String[] FALLBACK_HEADER = new String[]{
-        "Run", "Owner", "Pipeline", "Tool", "Instance", "Started", "Finished", "Duration", "Cost"};
+        "Run", "Owner", "Pipeline", "Tool", "Instance", "Started", "Finished", "Duration (hours)", "Cost ($)"};
 
     private final CSVWriter writer;
     private final BillingHelper billingHelper;
@@ -52,8 +61,15 @@ public class RunBillingWriter implements Closeable {
                 info.getInstanceType(),
                 billingHelper.asString(info.getStarted()),
                 billingHelper.asString(info.getFinished()),
-                billingHelper.asString(info.getDuration()),
-                billingHelper.asString(info.getCost())});
+                divided(info.getDuration(), DURATION_DIVISOR).toString(),
+                divided(info.getCost(), COST_DIVISOR).toString()});
+    }
+
+    private BigDecimal divided(final Long divider, final Long divisor) {
+        return Optional.ofNullable(divider)
+                .map(BigDecimal::valueOf)
+                .orElse(BigDecimal.ZERO)
+                .divide(BigDecimal.valueOf(divisor), NUMERIC_SCALE, RoundingMode.CEILING);
     }
 
     @Override
