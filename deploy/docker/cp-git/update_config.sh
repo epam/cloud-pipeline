7d2f2f3b3e33bacfc01c603a88dfd9180bc08fa6
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2021-12-07 21:06:26 +0300

    Automatically initialize a gitlab runner
    (cherry picked from commit de748f50f992d041b66bf48e28fbfe52c9043f48)
    (cherry picked from commit 99746895663635ab446f714a56fad2694baec246)

--- deploy/docker/cp-git/update_config.sh
+++ deploy/docker/cp-git/update_config.sh
@@ -65,7 +65,30 @@ GIT_PROXIES="gitlab_rails['env'] = {
   \"https_proxy\" => \"$CP_GITLAB_HTTPS_PROXY\"
 }"
 
-cat >> /etc/gitlab/gitlab.rb <<-EOF
+# If the smtp configuration is available - add it to gitlab as well
+if [ "$CP_NOTIFIER_SMTP_FROM" ] && \
+    [ "$CP_NOTIFIER_SMTP_PASS" ] && \
+    [ "$CP_NOTIFIER_SMTP_SERVER_HOST" ] && \
+    [ "$CP_NOTIFIER_SMTP_SERVER_PORT" ] && \
+    [ "$CP_NOTIFIER_SMTP_USER" ]; then
+    echo
+    echo "SMTP configuration available:"
+    echo "  Host: ${CP_NOTIFIER_SMTP_SERVER_HOST}:${CP_NOTIFIER_SMTP_SERVER_PORT}"
+    echo "  User: $CP_NOTIFIER_SMTP_USER"
+    echo "  From: $CP_NOTIFIER_SMTP_FROM"
+    echo
+    SMTP_SETTINGS="gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = \"$CP_NOTIFIER_SMTP_SERVER_HOST\"
+gitlab_rails['smtp_port'] = $CP_NOTIFIER_SMTP_SERVER_PORT
+gitlab_rails['smtp_user_name'] = \"$CP_NOTIFIER_SMTP_USER\"
+gitlab_rails['smtp_password'] = \"$CP_NOTIFIER_SMTP_PASS\"
+gitlab_rails['smtp_domain'] = \"$CP_NOTIFIER_SMTP_SERVER_HOST\"
+gitlab_rails['smtp_authentication'] = \"login\"
+gitlab_rails['smtp_enable_starttls_auto'] = ${CP_NOTIFIER_SMTP_START_TLS:-false}
+gitlab_rails['gitlab_email_from'] = \"$CP_NOTIFIER_SMTP_FROM\""
+fi
+
+cat > /etc/gitlab/gitlab.rb <<-EOF
 
 ${GIT_PROXIES}
 
@@ -113,4 +136,6 @@ gitlab_rails['omniauth_providers'] = [
   }
  }
 ]
+
+${SMTP_SETTINGS}
 EOF
