a11a936a9de3e8e354145e255a1b2b408f39c179
Andrey Tsibin <andrei_tcibin@epam.com>
2021-12-10 16:27:06 +0300

    Use common date time format for user and billing reports (#2391)

--- api/src/main/java/com/epam/pipeline/config/Constants.java
+++ api/src/main/java/com/epam/pipeline/config/Constants.java
@@ -19,6 +19,8 @@ package com.epam.pipeline.config;
 public final class Constants {
     public static final String FMT_ISO_LOCAL_DATE = "yyyy-MM-dd HH:mm:ss.SSS";
     public static final String TIME_FORMAT = "HH:mm:ss";
+    public static final String ELASTIC_DATE_TIME_FORMAT = FMT_ISO_LOCAL_DATE;
+    public static final String EXPORT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String SECURITY_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
     public static final String SIMPLE_DATE_FORMAT = "yyyyMMdd";
     public static final String SIMPLE_TIME_FORMAT = "HHmmss";
