a2a1063d45527e9107e8fe62d1b942343f5945af
Andrey Tsibin <andrei_tcibin@epam.com>
2022-01-25 18:00:41 +0300

    Do not use single index run billing loading optimization

--- api/src/main/java/com/epam/pipeline/manager/billing/RunBillingLoader.java
+++ api/src/main/java/com/epam/pipeline/manager/billing/RunBillingLoader.java
@@ -9,22 +9,16 @@ import com.epam.pipeline.manager.preference.SystemPreferences;
 import com.epam.pipeline.utils.StreamUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.sort.SortBuilders;
-import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -64,84 +58,8 @@ public class RunBillingLoader implements BillingLoader<RunBilling> {
                                         final BillingDiscount discount,
                                         final int pageSize) {
         final String[] indices = billingIndexHelper.yearlyRunIndicesBetween(from, to);
-        final LowLevelBillingLoader<RunBilling> loader = indices.length > 1
-                ? new MultiIndexRunBillingLoader(billingHelper)
-                : new SingleIndexRunBillingLoader(billingHelper);
+        final LowLevelBillingLoader<RunBilling> loader = new MultiIndexRunBillingLoader(billingHelper);
         return loader.billings(client, indices, from, to, filters, discount, pageSize);
-    }
-
-    @RequiredArgsConstructor
-    public static class SingleIndexRunBillingLoader implements LowLevelBillingLoader<RunBilling> {
-
-        private final BillingHelper billingHelper;
-
-        @Override
-        public Stream<RunBilling> billings(final RestHighLevelClient client,
-                                           final String[] indices,
-                                           final LocalDate from,
-                                           final LocalDate to,
-                                           final Map<String, List<String>> filters,
-                                           final BillingDiscount discount,
-                                           final int pageSize) {
-            return StreamUtils.from(iterator(client, indices, from, to, filters, pageSize))
-                    .flatMap(response -> billings(response, discount));
-        }
-
-        private Iterator<SearchResponse> iterator(final RestHighLevelClient client,
-                                                  final String[] indices,
-                                                  final LocalDate from,
-                                                  final LocalDate to,
-                                                  final Map<String, List<String>> filters,
-                                                  final int pageSize) {
-            return new ElasticDocumentsIterator(pageSize,
-                searchAfter -> getRequest(indices, from, to, filters, searchAfter, pageSize),
-                billingHelper.searchWith(client));
-        }
-
-        private SearchRequest getRequest(final String[] indices,
-                                         final LocalDate from,
-                                         final LocalDate to,
-                                         final Map<String, List<String>> filters,
-                                         final Object[] searchAfter,
-                                         final int pageSize) {
-            final SearchSourceBuilder source = new SearchSourceBuilder()
-                    .size(pageSize)
-                    .query(billingHelper.queryByDateAndFilters(from, to, filters))
-                    .sort(SortBuilders.fieldSort(BillingUtils.COST_FIELD).order(SortOrder.DESC))
-                    .sort(SortBuilders.fieldSort(BillingUtils.DOC_ID_FIELD).order(SortOrder.DESC));
-            Optional.ofNullable(searchAfter).ifPresent(source::searchAfter);
-            return new SearchRequest()
-                    .indicesOptions(IndicesOptions.lenientExpandOpen())
-                    .indices(indices)
-                    .source(source);
-        }
-
-        private Stream<RunBilling> billings(final SearchResponse response, final BillingDiscount discount) {
-            return Optional.ofNullable(response)
-                    .map(SearchResponse::getHits)
-                    .map(SearchHits::getHits)
-                    .map(Arrays::stream)
-                    .orElseGet(Stream::empty)
-                    .map(hit -> getBilling(hit, discount));
-        }
-
-        private RunBilling getBilling(final SearchHit hit, final BillingDiscount discount) {
-            final Map<String, Object> topHitFields = MapUtils.emptyIfNull(hit.getSourceAsMap());
-            return RunBilling.builder()
-                    .runId(NumberUtils.toLong(BillingUtils.asString(topHitFields.get(BillingUtils.RUN_ID_FIELD))))
-                    .owner(BillingUtils.asString(topHitFields.get(BillingUtils.OWNER_FIELD)))
-                    .billingCenter(BillingUtils.asString(topHitFields.get(BillingUtils.BILLING_CENTER_FIELD)))
-                    .pipeline(BillingUtils.asString(topHitFields.get(BillingUtils.PIPELINE_NAME_FIELD)))
-                    .tool(BillingUtils.asString(topHitFields.get(BillingUtils.TOOL_FIELD)))
-                    .computeType(BillingUtils.asString(topHitFields.get(BillingUtils.COMPUTE_TYPE_FIELD)))
-                    .instanceType(BillingUtils.asString(topHitFields.get(BillingUtils.INSTANCE_TYPE_FIELD)))
-                    .started(BillingUtils.asDateTime(topHitFields.get(BillingUtils.STARTED_FIELD)))
-                    .finished(BillingUtils.asDateTime(topHitFields.get(BillingUtils.FINISHED_FIELD)))
-                    .duration(NumberUtils.toLong(BillingUtils.asString(topHitFields.get(BillingUtils.RUN_USAGE_FIELD))))
-                    .cost(BillingUtils.withDiscount(discount.getComputes(), NumberUtils.toLong(BillingUtils.asString(
-                            topHitFields.get(BillingUtils.COST_FIELD)))))
-                    .build();
-        }
     }
 
     @RequiredArgsConstructor
@@ -158,7 +76,7 @@ public class RunBillingLoader implements BillingLoader<RunBilling> {
                                            final BillingDiscount discount,
                                            final int pageSize) {
             return StreamUtils.from(iterator(client, indices, from, to, filters, discount, pageSize))
-                    .flatMap(documents -> billings(documents));
+                    .flatMap(this::billings);
         }
 
         private Iterator<SearchResponse> iterator(final RestHighLevelClient client,
