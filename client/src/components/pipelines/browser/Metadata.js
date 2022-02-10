5cc4188acb800e5a54da64f79c187836a6fc1a90
Zhanna Kaymedenova <52781231+ZMaratovna@users.noreply.github.com>
2022-02-10 08:55:34 +0300

    GUI NGS project metadata folder: ignore SampleSheet cells selection (#2495)
    * ignore SampleSheet cells in spread selection actions
    
    * additional condition for samplesheet

--- client/src/components/pipelines/browser/Metadata.js
+++ client/src/components/pipelines/browser/Metadata.js
@@ -1402,7 +1402,7 @@ export default class Metadata extends React.Component {
         row: index + dataItemFrom
       }));
       const columns = tableColumns
-        .filter(column => column.selected)
+        .filter(column => column.selected && !this.isSampleSheetColumn(column.key))
         .map((column, index) => ({key: mapColumnName(column), column: index}));
       const actions = autoFillEntities.buildAutoFillActions(
         elements,
