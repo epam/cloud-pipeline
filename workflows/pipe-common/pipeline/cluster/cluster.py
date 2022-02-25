1ad0c2b3f9f72e25477d437a637ef66fe1e2186a
Yulia Kamyshova <yulia_kamyshova@epam.com>
2022-02-25 18:35:16 +0300

    pipe-common: add initial sge cluster support

---
+++ workflows/pipe-common/pipeline/cluster/cluster.py
@@ -0,0 +1,63 @@
+# Copyright 2022 EPAM Systems, Inc. (https://www.epam.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import subprocess
+from random import getrandbits
+from threading import RLock
+from . import utils
+
+lock = RLock()
+
+
+class AbstractCluster(object):
+    def submit_job(self, command, job_name, threads, common_logfile, work_directory, get_output=True):
+        pass
+
+    def build_job_command(self, command, job_name, threads, logfile, work_directory):
+        pass
+
+
+class SGECluster(AbstractCluster):
+    def __init__(self):
+        pass
+
+    def submit_job(self, command, job_name, threads, common_logfile, get_output=True, work_directory=os.getcwd()):
+        if not command:
+            raise ValueError("The job command is not set.")
+        if not job_name:
+            print("The job name is not set. The default one will be used.")
+        job_logfile = utils.get_log_filename(work_directory, job_name)
+        job_command = self.build_job_command(command, job_name, threads, job_logfile, work_directory)
+        job_result = utils.run(job_command,get_output)
+        if common_logfile and get_output:
+            utils.merge_log(common_logfile, job_logfile)
+        try:
+            os.remove(job_logfile)
+        except OSError as e:
+            print("Could not delete {file} file.\n".format(file=job_logfile) + e.__str__())
+            pass
+        return job_result
+
+    def build_job_command(self, command, job_name, threads, logfile, work_directory):
+        job_options = "-sync y {job_name} -V -j y -R y -o {standard_output_logfile} -pe local {threads} " \
+            .format(standard_output_logfile=logfile, job_name="-N " + job_name if job_name else "",
+                    threads=threads)
+        tmp_directory = utils.create_directory(work_directory, "tmp")
+        bash_script = "{tmp_directory}/{job_name}.sh".format(tmp_directory=tmp_directory,
+                                                             job_name=job_name + str(getrandbits(64)) if job_name
+                                                             else str(getrandbits(64)))
+        with open(bash_script, "w") as file:
+            file.write("#!/usr/bin/env bash\n")
+            file.write(command + "\n")
+        return "qsub {job_options} {bash_script}".format(job_options=job_options, bash_script=bash_script)
---
+++ workflows/pipe-common/pipeline/cluster/utils.py
@@ -0,0 +1,61 @@
+# Copyright 2022 EPAM Systems, Inc. (https://www.epam.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import subprocess
+from random import getrandbits
+from threading import RLock
+
+lock = RLock()
+
+
+def get_log_filename(work_directory, job_name):
+    logs_directory = create_directory(work_directory, "logs")
+    random_number = getrandbits(64)
+    return os.path.join(logs_directory, "{job_name}_{number}_out.log"
+                        .format(number=random_number, job_name=job_name if job_name else random_number))
+
+
+def merge_log(common_logfile, job_logfile):
+    piece_size = 4096
+    with open(common_logfile, 'ab+') as common_logfile, open(job_logfile, 'rb') as job_logfile:
+        while True:
+            piece = job_logfile.read(piece_size)
+            if piece == b'':
+                common_logfile.write("\n".encode())
+                common_logfile.seek(0)
+                job_logfile.seek(0)
+                break
+            common_logfile.write(piece)
+
+
+def create_directory(path, name):
+    directory = os.path.join(path, name)
+    with lock:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    return directory
+
+
+def run(job_command, get_output=True, env=None):
+    if get_output:
+        process = subprocess.Popen(job_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    else:
+        process = subprocess.Popen(job_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, env=env)
+    stdout, stderr = process.communicate()
+    exit_code = process.wait()
+    if exit_code != 0:
+        raise RuntimeError('Command "{}" exited with return code: {}, stdout: {}, stderr: {}'
+                           .format(job_command, exit_code, stdout, stderr))
+    return stdout
