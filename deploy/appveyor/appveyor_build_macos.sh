e2b4554da6fdd68c63984f0f34da7f1227ccbf38
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2022-03-12 22:58:28 +0300

    Update appveyor_build_macos.sh

--- deploy/appveyor/appveyor_build_macos.sh
+++ deploy/appveyor/appveyor_build_macos.sh
@@ -13,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+echo $http_proxy
+echo $https_proxy
+echo $HTTP_PROXY
+echo $HTTPS_PROXY
 
 set -e
 
