999a76cf4377a82ab999ef09e2291200c755de3d
Andrey Georgitsa <andrey_georgitsa@epam.com>
2022-03-24 11:56:20 +0300

    Issue 2568 Extend VM summary with instance type

--- vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/model/vm/VirtualMachine.java
+++ vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/model/vm/VirtualMachine.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ public class VirtualMachine {
     private String instanceId;
     private String instanceName;
     private String privateIp;
+    private String instanceType;
     private CloudProvider cloudProvider;
     private Map<String, String> tags;
     private LocalDateTime created;
--- vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/vm/GCPMonitorService.java
+++ vm-monitor/src/main/java/com/epam/pipeline/vmmonitor/service/vm/GCPMonitorService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -135,6 +135,7 @@ public class GCPMonitorService implements VMMonitorService<GCPRegion> {
                              .cloudProvider(provider())
                              .instanceId(instance.getId().toString())
                              .privateIp(getInternalIP(instance))
+                             .instanceType(instance.getMachineType())
                              .tags(instance.getLabels())
                              .created(Instant.parse(instance.getCreationTimestamp())
                                      .atZone(ZoneOffset.UTC)
