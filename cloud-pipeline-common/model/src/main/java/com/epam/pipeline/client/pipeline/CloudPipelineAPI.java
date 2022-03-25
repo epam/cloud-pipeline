da39d7690eda5624715b99c454dc4b093bce7f7e
Andrey Georgitsa <andrey_georgitsa@epam.com>
2022-03-25 12:24:36 +0300

    Issue #2568 Extend notification on missing node/label (#2569)
    * Issue 2568 Extend VM summary with instance type
    
    * Issue 2568 Extend missing node notification: specify instance type, creation date, matching runs and action recommendation
    
    * Issue 2568 Extend missing labels notification
    
    * Issue 2568 Make external URL configurable via `application.properties`
    
    (cherry picked from commit 27a3f99671968fab3d0ba3ac6409cfde9dc0f422)
    (cherry picked from commit 813e7f427f35ce2ac498930ce256d40b39ab443b)

--- cloud-pipeline-common/model/src/main/java/com/epam/pipeline/client/pipeline/CloudPipelineAPI.java
+++ cloud-pipeline-common/model/src/main/java/com/epam/pipeline/client/pipeline/CloudPipelineAPI.java
@@ -44,10 +44,12 @@ import com.epam.pipeline.entity.region.AbstractCloudRegion;
 import com.epam.pipeline.entity.region.AwsRegion;
 import com.epam.pipeline.entity.security.acl.AclClass;
 import com.epam.pipeline.entity.user.PipelineUser;
+import com.epam.pipeline.rest.PagedResult;
 import com.epam.pipeline.rest.Result;
 import com.epam.pipeline.vo.EntityPermissionVO;
 import com.epam.pipeline.vo.EntityVO;
 import com.epam.pipeline.vo.FilterNodesVO;
+import com.epam.pipeline.vo.PagingRunFilterExpressionVO;
 import com.epam.pipeline.vo.RunStatusVO;
 import com.epam.pipeline.vo.notification.NotificationMessageVO;
 import okhttp3.MultipartBody;
@@ -99,6 +101,9 @@ public interface CloudPipelineAPI {
 
     @GET("run/{runId}/logs")
     Call<Result<List<RunLog>>> loadLogs(@Path(RUN_ID) Long runId);
+
+    @POST("run/search")
+    Call<Result<PagedResult<List<PipelineRun>>>> searchPipelineRuns(@Body PagingRunFilterExpressionVO filterVO);
 
     @POST("metadata/load")
     Call<Result<List<MetadataEntry>>> loadFolderMetadata(@Body List<EntityVO> entities);
