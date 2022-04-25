1d137481230088674dc25d69792f00a4e1ed2d39
Mariia_Tolstaia <Mariia_Tolstaia@epam.com>
2022-04-25 14:29:55 +0300

    Update Copyright

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/SamplesMetadataTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/SamplesMetadataTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -402,7 +402,7 @@ public class SamplesMetadataTest
                 })
                 .deleteExtraBrackets(110)
                 .saveAndCommitWithMessage("test: sample metadata")
-                .click(DELETE)////
+                .click(DELETE)
                 .click(button("OK"))
                 .uploadFile(getFile(launchScript))
                 .ensure(byText(launchScript), visible);
