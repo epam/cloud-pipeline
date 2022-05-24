d59670072ac6785f195644ff3c01751e542e484b
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2022-05-24 14:05:54 +0300

    Quick launcher, string substitution rules ('before@')

--- quick-launcher/src/models/process-string.js
+++ quick-launcher/src/models/process-string.js
@@ -1,12 +1,45 @@
+const modifiers = {
+  'uppercased': (value) => (value || '').toUpperCase(),
+  'uppercase': (value) => (value || '').toUpperCase(),
+  'upper': (value) => (value || '').toUpperCase(),
+  'lowercased': (value) => (value || '').toLowerCase(),
+  'lowercase': (value) => (value || '').toLowerCase(),
+  'lower': (value) => (value || '').toLowerCase(),
+  'before@': (value) => (value || '').split('@')[0]
+};
+
 export default function processString(string, options) {
-  const keys = Object
-    .keys(options || {})
-    .filter(key => options[key] !== undefined);
-  let result = string || '';
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const replaceRegExp = new RegExp(`\\[${key}\\]`, 'ig');
-    result = result.replace(replaceRegExp, options[key] || '');
-  }
-  return result.replace(/\[host\]/ig, document.location.host);
+  let result = '';
+  const r = /(\[([^\]]+)\])/;
+  let e;
+  const placeholders = Object.keys(options || {})
+    .map(key => ({ key: key.toLowerCase(), value: options[key] }));
+  do {
+    e = r.exec(string);
+    if (e) {
+      result = result.concat(string.slice(0, e.index));
+      const [keyRaw, mods] = e[2].split(':');
+      const key = keyRaw.toLowerCase();
+      const placeholder = placeholders.find(p => p.key === key);
+      if (placeholder) {
+        const appliedModifiers = (mods || '')
+          .split(/[,;]/g)
+          .map(mod => mod.trim().toLowerCase())
+        let value = placeholder.value;
+        for (const modifier of appliedModifiers) {
+          const fn = modifiers[modifier];
+          if (typeof fn === 'function') {
+            value = fn(value);
+          }
+        }
+        result = result.concat(value);
+      } else {
+        result = result.concat(e[0]);
+      }
+      string = string.slice(e.index + e[0].length);
+    } else {
+      result = result.concat(string);
+    }
+  } while (e);
+  return result;
 }
