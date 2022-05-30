7fd44adb75ae4fa30b52467a5a1a6845ff699c34
Andrey Tsibin <andrei_tcibin@epam.com>
2022-05-30 13:54:32 +0300

    Add support for dts cron self registration

--- scripts/dts/install_dts.sh
+++ scripts/dts/install_dts.sh
@@ -12,39 +12,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ -z "$API" ] || [ -z "$API_TOKEN" ] || [ -z "$API_PUBLIC_KEY" ];
-then
+#export API="{PUT REST API URL HERE AND UNCOMMENT}"
+#export API_TOKEN="{PUT REST API TOKEN HERE AND UNCOMMENT}"
+#export API_PUBLIC_KEY="{PUT REST API PUBLIC KEY AND UNCOMMENT}"
+export API_PUBLIC_KEY="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnSd1kqvU3XcNjMC9Rm7uOxREoa0zV51LH0J+CTF5VmPBprQPu95zr+qQhp5MK8r9jIvFNGDwNgo9D2AG/jIuv9LDQJv4WV3g4czYJODxW16lASaXdbRx+95a3uGdqYwbL/JJIg+T2G1rJEk1aYAZ3KmRuOBWR57BAeywtb8A3WQIDAQAB"
+
+if [ -z "$API" ] || [ -z "$API_TOKEN" ] || [ -z "$API_PUBLIC_KEY" ]; then
   echo "Please set all the required environment variables and restart the installation: API, API_TOKEN, API_PUBLIC_KEY"
   exit 1
 fi
 
-export DISTRIBUTION_URL=${API%"/restapi/"}
-export DTS_DIR=/opt/DTS
-export DTS_NAME=$(hostname)
+if ! command -v crontab >/dev/null 2>&1; then
+  apt-get install -y cron
+  yum install -y cronie
+  if ! command -v crontab >/dev/null 2>&1; then
+    echo "Please install the required applications and restart the installation: crontab"
+    exit 1
+  fi
+  cron
+  crond
+fi
+
+if ! command -v wget >/dev/null 2>&1; then
+  apt-get install -y wget
+  yum install -y wget
+  if ! command -v wget >/dev/null 2>&1; then
+    echo "Please install the required applications and restart the installation: wget"
+    exit 1
+  fi
+fi
+
+if ! command -v unzip >/dev/null 2>&1; then
+  apt-get install -y unzip
+  yum install -y unzip
+  if ! command -v unzip >/dev/null 2>&1; then
+    echo "Please install the required applications and restart the installation: unzip"
+    exit 1
+  fi
+fi
+
+export DISTRIBUTION_URL="${API%"/restapi/"}"
+export DTS_DIR="/opt/CloudPipeline/DTS"
+export DTS_NAME="$(hostname)"
 
 mkdir -p "${DTS_DIR}"
 mkdir -p "${DTS_DIR}/logs"
+mkdir -p "${DTS_DIR}/locks"
 
-INSTALLER_LOG="${DTS_DIR}/logs/installer.log"
+exec > >(tee -a "${DTS_DIR}/logs/installer.log") 2>&1
 
-echo "Installing Data Transfer Service on $DTS_NAME host..." > "${INSTALLER_LOG}"
+echo "Installing Data Transfer Service on $DTS_NAME host..."
 wget "${DISTRIBUTION_URL}/deploy_dts.sh" -O "${DTS_DIR}/deploy_dts.sh" --no-check-certificate -q
 
-bash "${DTS_DIR}/deploy_dts.sh" install &
+bash "${DTS_DIR}/deploy_dts.sh" install
 
 echo "Waiting for Data Transfer Service to become ready on $DTS_NAME host..."
 
 duration=0
 
-while [ $duration -le 900 ];
-do
-  grep "$DTS_DIR/logs/launcher.log" "Launching data transfer service..." && break
+while [ $duration -le 900 ]; do
+  grep "Launching data transfer service..." "$DTS_DIR/logs/launcher.log" >/dev/null 2>&1 && break
   sleep 10
-  duration=$(( $duration + 10 ))
+  duration=$(( duration + 10 ))
 done
 
-if [ $duration -le 900 ];
-then
+if [ $duration -le 900 ]; then
   echo "Data Transfer Service $DTS_NAME is ready after $duration seconds"
 else
   echo "Data Transfer Service $DTS_NAME is not ready after $duration seconds"
