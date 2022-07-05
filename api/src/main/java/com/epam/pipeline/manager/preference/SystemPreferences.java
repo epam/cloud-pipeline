4bd4581e9443375061220c28699947f03cbbe3aa
Pavel Silin <pavel_silin@epam.com>
2022-07-05 17:47:26 +0300

    Issue 2713 sync blocked users with LDAP with new approach (#2717)
    * implement new approach to sync blocked users with LDAP
    
    * issue 2713 test added
    
    * issue 2713 comments
    
    (cherry picked from commit 439fef87e396893ee62a288d398c1216eced0e2c)

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -28,6 +28,7 @@ import com.epam.pipeline.entity.cluster.PriceType;
 import com.epam.pipeline.entity.cluster.container.ContainerMemoryResourcePolicy;
 import com.epam.pipeline.entity.datastorage.nfs.NFSMountPolicy;
 import com.epam.pipeline.entity.git.GitlabVersion;
+import com.epam.pipeline.entity.ldap.LdapBlockedUserSearchMethod;
 import com.epam.pipeline.entity.monitoring.IdleRunAction;
 import com.epam.pipeline.entity.monitoring.LongPausedRunAction;
 import com.epam.pipeline.entity.notification.filter.NotificationFilter;
@@ -834,6 +835,11 @@ public class SystemPreferences {
             "ldap.response.timeout", 60000, LDAP_GROUP, isGreaterThanOrEquals(0));
     public static final StringPreference LDAP_BLOCKED_USER_FILTER = new StringPreference(
             "ldap.blocked.user.filter", "", LDAP_GROUP, pass);
+
+    public static final StringPreference LDAP_BLOCKED_USER_SEARCH_METHOD = new StringPreference(
+            "ldap.blocked.user.search.method",
+            LdapBlockedUserSearchMethod.LOAD_BLOCKED.name(), LDAP_GROUP,
+            isValidEnum(LdapBlockedUserSearchMethod.class));
     public static final StringPreference LDAP_BLOCKED_USER_NAME_ATTRIBUTE = new StringPreference(
             "ldap.blocked.user.name.attribute", "sAMAccountName", LDAP_GROUP, pass);
     public static final IntPreference LDAP_BLOCKED_USERS_FILTER_PAGE_SIZE = new IntPreference(
