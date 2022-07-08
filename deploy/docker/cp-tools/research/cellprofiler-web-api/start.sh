f576c022839a5df213bd6a1a35d9026712d4cb2d
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2022-07-09 00:37:40 +0300

    Fixes to the Dockerfile

--- deploy/docker/cp-tools/research/cellprofiler-web-api/start.sh
+++ deploy/docker/cp-tools/research/cellprofiler-web-api/start.sh
@@ -2,7 +2,7 @@ if [ -z "$CELLPROFILER_API_LOGS_DIR" ] ; then
     CELLPROFILER_API_LOGS_DIR="$CELLPROFILER_API_HOME/logs"
 fi
 mkdir -p "$CELLPROFILER_API_LOGS_DIR"
-nohup python3.9 $CELLPROFILER_API_HOME/hcs.py --host=${CELLPROFILER_API_HOST:-0.0.0.0} \
+nohup python3.8 $CELLPROFILER_API_HOME/hcs.py --host=${CELLPROFILER_API_HOST:-0.0.0.0} \
                                             --port=${CELLPROFILER_API_PORT:-8080} \
                                             --process_count=${CELLPROFILER_API_PROCESSES:-2} > "$CELLPROFILER_API_LOGS_DIR/serve_cp_api.log" 2>&1 &
 tail -f "$CELLPROFILER_API_LOGS_DIR/serve_cp_api.log"
