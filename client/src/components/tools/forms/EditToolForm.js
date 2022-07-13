09922dbaf30d50d9dc549475bc1d198480879787
Aleksandr Gorodetskii <gorod2004@gmail.com>
2022-07-13 18:02:59 +0300

    GUI Tools kube labels (#2727) - add possibility to configure kubeLabels for tools, backported to stage/0.16/20220314

--- client/src/components/tools/forms/EditToolForm.js
+++ client/src/components/tools/forms/EditToolForm.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,6 +34,10 @@ import {
 import classNames from 'classnames';
 
 import ToolEndpointsFormItem from '../elements/ToolEndpointsFormItem';
+import KubeLabels, {
+  kubeLabelsHasChanges,
+  prepareKubeLabelsPayload
+} from '../elements/KubeLabels';
 import CodeEditor from '../../special/CodeEditor';
 import {getSpotTypeName} from '../../special/spot-instance-names';
 import EditToolFormParameters from './EditToolFormParameters';
@@ -146,6 +150,9 @@ export default class EditToolForm extends React.Component {
 
   state = {
     labels: [],
+    kubeLabels: [],
+    initialKubeLabels: [],
+    kubeLabelsHasErrors: false,
     labelInputVisible: false,
     labelInputValue: '',
     endpointInputVisible: false,
@@ -211,6 +218,13 @@ export default class EditToolForm extends React.Component {
 
   saveLabelInputRef = input => {
     this.input = input;
+  };
+
+  onKubeLabelsChange = (labels = [], errors = {}) => {
+    this.setState({
+      kubeLabels: labels,
+      kubeLabelsHasErrors: Object.keys(errors).length > 0
+    });
   };
 
   handleSubmit = (e) => {
@@ -334,7 +348,8 @@ export default class EditToolForm extends React.Component {
           cmd_template: this.defaultCommand,
           instance_disk: values.disk,
           instance_size: values.instanceType,
-          is_spot: `${values.is_spot}` === 'true'
+          is_spot: `${values.is_spot}` === 'true',
+          kubeLabels: prepareKubeLabelsPayload(this.state.kubeLabels)
         };
         this.setState({pending: true}, async () => {
           if (this.props.onSubmit) {
@@ -486,6 +501,11 @@ export default class EditToolForm extends React.Component {
         const gpuScalingParameters = state.gpuScalingConfiguration
           ? getGPUScalingSkippedParameters(this.props.preferences)
           : [];
+        const kubeLabels = Object
+          .entries((this.props.configuration || {}).kubeLabels || {})
+          .map(([key, value]) => ({key, value}));
+        state.kubeLabels = kubeLabels;
+        state.initialKubeLabels = kubeLabels;
         if (props.configuration && props.configuration.parameters) {
           for (let key in props.configuration.parameters) {
             if (!props.configuration.parameters.hasOwnProperty(key) ||
@@ -831,7 +851,11 @@ export default class EditToolForm extends React.Component {
       autoScaledPriceTypeValue !== this.state.autoScaledPriceType ||
       (this.state.launchCluster && nodesCount !== this.state.nodesCount) ||
       (this.state.launchCluster && this.state.autoScaledCluster && maxNodesCount !== this.state.maxNodesCount) ||
-      limitMountsFieldChanged() || cloudRegionFieldChanged() || additionalCapabilitiesChanged();
+      limitMountsFieldChanged() || cloudRegionFieldChanged() || additionalCapabilitiesChanged() ||
+      kubeLabelsHasChanges(
+        this.state.initialKubeLabels,
+        this.state.kubeLabels
+      );
   };
 
   initializeEndpointsControl = (control) => {
@@ -1281,6 +1305,27 @@ export default class EditToolForm extends React.Component {
                   </Row>
                 </Col>
               </Row>
+              {
+                <Row style={{marginBottom: 10, marginTop: 10}}>
+                  <Col
+                    xs={24}
+                    sm={6}
+                    style={{paddingRight: 10}}
+                    className={classNames(
+                      'cp-accent',
+                      styles.toolSettingsTitle
+                    )}
+                  >
+                    Kube labels:
+                  </Col>
+                  <Col xs={24} sm={12}>
+                    <KubeLabels
+                      labels={this.state.kubeLabels}
+                      onChange={this.onKubeLabelsChange}
+                    />
+                  </Col>
+                </Row>
+              }
               <Form.Item
                 {...this.formItemLayout}
                 label="Cloud Region"
@@ -1539,7 +1584,8 @@ export default class EditToolForm extends React.Component {
                   this.props.readOnly ||
                   !this.modified() ||
                   (this.toolFormSystemParameters && !this.toolFormSystemParameters.isValid) ||
-                  (this.toolFormParameters && !this.toolFormParameters.isValid)
+                  (this.toolFormParameters && !this.toolFormParameters.isValid) ||
+                  this.state.kubeLabelsHasErrors
                 }>
                 SAVE
               </Button>
