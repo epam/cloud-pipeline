2ace232b3621837ccd502619157e005a3333095c
Aleksandr Gorodetskii <41908792+AleksandrGorodetskii@users.noreply.github.com>
2022-08-03 12:46:53 +0300

    GUI Billing storages reports: Mark deleted storages on storages table and bar chart (#2761)
    * GUI Billing storages reports: Mark deleted storages on storages table
    
    * GUI Billing storages reports: Mark deleted storages on bar chart

--- client/src/components/billing/reports/charts/extensions/index.js
+++ client/src/components/billing/reports/charts/extensions/index.js
@@ -24,6 +24,7 @@ import * as GenerateImagePlugin from './generate-image-plugin';
 import * as PieChartDataLabelPlugin from './pie-chart-data-label-plugin';
 import * as PointDataLabelPlugin from './point-data-label-plugin';
 import * as VerticalLinePlugin from './vertical-line-plugin';
+import * as HighlightTicksPlugin from './highlight-ticks-plugin';
 
 export {
   BarchartDataLabelPlugin,
@@ -33,5 +34,6 @@ export {
   PieChartDataLabelPlugin,
   PointDataLabelPlugin,
   VerticalLinePlugin,
-  SummaryChart
+  SummaryChart,
+  HighlightTicksPlugin
 };
