cf28dbeb74cc889858c5ab4f945df6393b1a231c
Aleksandr Gorodetskii <gorod2004@gmail.com>
2022-08-11 13:26:27 +0300

    [Billing reports] Show main info in a widget (#2769) - GUI, backported to stage/0.16/20220314

--- client/src/components/main/home/layout/panel-icons.js
+++ client/src/components/main/home/layout/panel-icons.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,5 +25,6 @@ export default {
   [Panels.projects]: 'api',
   [Panels.notifications]: 'notification',
   [Panels.recentlyCompletedRuns]: 'clock-circle-o',
-  [Panels.services]: 'rocket'
+  [Panels.services]: 'rocket',
+  [Panels.userCosts]: 'area-chart'
 };
