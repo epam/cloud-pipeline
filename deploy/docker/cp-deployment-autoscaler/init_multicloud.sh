625c8d42e84119d7c01e85bee585eb66a7f01662
Andrey Tsibin <andrei_tcibin@epam.com>
2022-08-23 11:55:03 +0300

    Introduce proxies support in deployment autoscaler.
    Relates to #2639.

--- deploy/docker/cp-deployment-autoscaler/init_multicloud.sh
+++ deploy/docker/cp-deployment-autoscaler/init_multicloud.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+function update_nameserver {
+  local nameserver="$1"
+  local ping_times="$2"
+
+  local is_nameserver_reachable="0"
+  if [ "$nameserver" ] && [[ "$nameserver" != "@"*"@" ]]; then
+    if [ "$ping_times" ]; then
+      if [ "$ping_times" == "infinity" ]; then
+        ping_times=86400
+      fi
+      for i in $(seq 1 $ping_times); do
+        echo "Pinging nameserver $nameserver on port 53"
+        if nc -z -w 1 $nameserver 53 ; then
+          echo "nameserver $nameserver can be reached on port 53"
+          is_nameserver_reachable="1"
+          break
+        fi
+      done
+
+      if [ "$is_nameserver_reachable" != "1" ]; then
+        echo "Elapsed $ping_times retries, but $nameserver can NOT be reached on port 53"
+      fi
+    fi
+
+    cp /etc/resolv.conf /etc/resolv.conf.backup
+    chattr -i /etc/resolv.conf
+    sed -i '/nameserver/d' /etc/resolv.conf
+    echo "nameserver $nameserver" >> /etc/resolv.conf
+    chattr +i /etc/resolv.conf
+  fi
+}
+
 user_data_log="/var/log/user_data.log"
 exec > "$user_data_log" 2>&1
 
@@ -9,6 +41,9 @@ export KUBE_TOKEN="@KUBE_TOKEN@"
 export KUBE_DNS_IP="@KUBE_DNS_IP@"
 export KUBE_LABELS="@KUBE_LABELS@"
 export AWS_FS_URL="@AWS_FS_URL@"
+export http_proxy="@HTTP_PROXY@"
+export https_proxy="@HTTPS_PROXY@"
+export no_proxy="@NO_PROXY@"
 
 mkdir -p /etc/docker
 cat <<EOT > /etc/docker/daemon.json
@@ -17,6 +52,12 @@ cat <<EOT > /etc/docker/daemon.json
   "storage-driver": "overlay2"
 }
 EOT
+
+mkdir -p /etc/systemd/system/docker.service.d
+cat > /etc/systemd/system/docker.service.d/http-proxy.conf << EOF
+  [Service]
+  Environment="http_proxy=$http_proxy" "https_proxy=$https_proxy" "no_proxy=$no_proxy"
+EOF
 
 echo "KUBELET_EXTRA_ARGS=--node-labels $KUBE_LABELS" >> /etc/sysconfig/kubelet
 
@@ -41,22 +82,19 @@ fi
 kubeadm join --token "$KUBE_TOKEN" "$KUBE_IP:$KUBE_PORT" --discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors all --node-name "$_KUBE_NODE_NAME"
 systemctl start kubelet
 
-if ! grep "$KUBE_DNS_IP" /etc/resolv.conf -q; then
-    chattr -i /etc/resolv.conf
-    sed -i "1s/^/nameserver $KUBE_DNS_IP\n/" /etc/resolv.conf
-    chattr +i /etc/resolv.conf
-fi
+yum install -y nc
+update_nameserver "$KUBE_DNS_IP" "infinity"
 
 if [[ $cloud == *"EC2"* ]]; then
-  amazon-linux-extras install -y lustre2.10
-  yum install -y lustre-client --disablerepo=kubernetes
-  mkdir -p /opt
-  mount -t lustre -o noatime,flock "$AWS_FS_URL" /opt
-  echo "$AWS_FS_URL /opt lustre defaults,noatime,flock,_netdev 0 0" >> /etc/fstab
+    amazon-linux-extras install -y lustre2.10
+    yum install -y lustre-client --disablerepo=kubernetes
+    mkdir -p /opt
+    mount -t lustre -o noatime,flock "$AWS_FS_URL" /opt
+    echo "$AWS_FS_URL /opt lustre defaults,noatime,flock,_netdev 0 0" >> /etc/fstab
 elif [[ $cloud == *"Microsoft"* ]]; then
-  echo "WARNING: Azure shared file system mounting is not yet supported."
-  # todo: Implement
+    echo "WARNING: Azure shared file system mounting is not yet supported."
+    # todo: Implement
 elif [[ $gcloud_header == *"Google"* ]]; then
-  echo "WARNING: Google Cloud shared file system mounting is not yet supported."
-  # todo: Implement
+    echo "WARNING: Google Cloud shared file system mounting is not yet supported."
+    # todo: Implement
 fi
