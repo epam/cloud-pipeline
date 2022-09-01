cf1f5fa408e6bfa1feb59035bda4f8b46883513b
Oksana Kolesnikova <Oksana_Kolesnikova@epam.com>
2022-09-01 13:12:57 +0300

    Video generation from hcs Images by timepoints - time sequence changes

--- deploy/docker/cp-tools/research/cellprofiler-web-api/app/src/hcs_manager.py
+++ deploy/docker/cp-tools/research/cellprofiler-web-api/app/src/hcs_manager.py
@@ -7,6 +7,13 @@ import numpy as np
 from moviepy.editor import ImageSequenceClip
 import time
 from PIL import Image, ImageOps
+import xml.etree.ElementTree as ET
+import json
+import os
+import errno
+
+CELLPROFILER_API_COMMON_RESULTS_DIR = os.getenv('CELLPROFILER_API_COMMON_RESULTS_DIR')
+HCS_CLOUD_FILES_SCHEMA = os.getenv('HCS_PARSING_CLOUD_FILES_SCHEMA', 's3')
 
 
 class HCSManager:
@@ -115,39 +122,56 @@ class HCSManager:
         clip_format = params.get('format') if 'format' in params else '.webm'
         codec = params.get('codec') if 'codec' in params else None
         fps = int(params.get('fps')) if 'fps' in params else 1
-        ome_tiff_dir = self._get_required_field(params, 'path')
+        sequence_id = params.get('sequenceId') if 'sequenceId' in params else None
+
+        path = self._get_required_field(params, 'path')
+        path = self.extract_local_path(path)
         by_field = int(self._get_required_field(params, 'byField'))
-        chs = int(self._get_required_field(params, 'chs'))
-        tps = int(self._get_required_field(params, 'tps'))
         cell = int(self._get_required_field(params, 'cell'))
 
-        clip_dir = ome_tiff_dir
-        clip_file_name = ('field_' if by_field else 'well_') + str(cell)
-        ome_tiff = self.get_ome_tiff_path(ome_tiff_dir, by_field)
+        preview_dir, sequences = self.parse_hcs(path, sequence_id)
+        preview_dir = self.extract_local_path(preview_dir)
+        index_path = preview_dir + '/index.xml'
+        all_channels = self.get_all_channels(index_path)
+        channels_num = len(all_channels)
+        selected_channel_ids, selected_channels = self.get_selected_channels(params, all_channels)
 
-        channel_ids, channels = self.get_channels(chs, params)
-
-        images = imread(ome_tiff, key=self.get_pages(channel_ids, chs, tps, cell))
         clips = []
-        curr = 0
-        for time_point in range(tps):
-            channel_images = []
-            for channel in channel_ids:
-                channel_image = images[curr]
-                channel_params = next(filter(lambda c: c['id'] == channel, channels))
-                colored_image = self.color_image(channel_image, channel_params)
-                channel_images.append(colored_image)
-                curr = curr + 1
-            merged_image = self.merge_channels(channel_images)
-            clips.append(np.array(merged_image))
+        for seq in sequences.keys():
+            timepoints = len(sequences[seq])
+            ome_tiff_path = self.get_ome_tiff_path(preview_dir, seq, by_field)
+            images = imread(ome_tiff_path, key=self.get_pages(selected_channel_ids, channels_num, timepoints, cell))
+            curr = 0
+            for time_point in range(timepoints):
+                channel_images = []
+                for channel in selected_channels:
+                    channel_image = images[curr]
+                    colored_image = self.color_image(channel_image, channel)
+                    channel_images.append(colored_image)
+                    curr = curr + 1
+                merged_image = self.merge_channels(channel_images)
+                clips.append(np.array(merged_image))
+
+        clip_path = CELLPROFILER_API_COMMON_RESULTS_DIR + self.get_clip_file_dir(path)
+        self._mkdir(clip_path)
+        clip_name = self.get_clip_file_name(by_field, cell, clip_format, sequence_id)
+        clip_full_path = clip_path + clip_name
         slide = ImageSequenceClip(clips, fps)
-        slide.write_videofile(clip_dir + clip_file_name + clip_format, codec=codec)
+        slide.write_videofile(clip_full_path, codec=codec)
         t1 = time.time()
-        return round((t1 - t), 2)
+        return clip_full_path, round((t1 - t), 2)
 
     @staticmethod
-    def get_ome_tiff_path(ome_tiff_dir, by_field):
-        return ome_tiff_dir + ('data' if by_field else 'overview_data') + '.ome.tiff'
+    def get_clip_file_dir(path):
+        hcs_file_base_name = os.path.basename(path)
+        hcs_file_name = os.path.splitext(hcs_file_base_name)[0]
+        return '/video/{}/'.format(hcs_file_name)
+
+    @staticmethod
+    def get_clip_file_name(by_field, cell, clip_format, sequence_id):
+        cell_prefix = ('field{}' if by_field else '/well{}').format(str(cell))
+        sequence_prefix = ('' if sequence_id is None else 'seq{}').format(sequence_id)
+        return cell_prefix + sequence_prefix + clip_format
 
     @staticmethod
     def get_pages(channel_ids, channels, timepoints, cell):
@@ -161,27 +185,85 @@ class HCSManager:
         return pages
 
     @staticmethod
-    def get_channels(chs, params):
+    def extract_xml_schema(xml_info_root):
+        full_schema = xml_info_root.tag
+        return full_schema[:full_schema.rindex('}') + 1]
+
+    @staticmethod
+    def parse_hcs(path, sequence_id):
+        with open(path) as json_file:
+            data = json.load(json_file)
+            preview_dir = data['previewDir']
+            time_series_details = data['time_series_details']
+            sequences = dict()
+            if sequence_id is not None:
+                sequences[sequence_id] = time_series_details[sequence_id]
+            else:
+                sequences = time_series_details
+            return preview_dir, sequences
+
+    def get_all_channels(self, path):
+        tree = ET.parse(path)
+        hcs_xml_info_root = tree.getroot()
+        hcs_schema_prefix = self.extract_xml_schema(hcs_xml_info_root)
+        maps = hcs_xml_info_root.find(hcs_schema_prefix + 'Maps').findall(hcs_schema_prefix + 'Map')
         channels = []
-        channel_ids = []
-        for chl in range(chs):
-            channel_params = params.get('ch' + str(chl))
+        for m in maps:
+            entries = m.findall(hcs_schema_prefix + 'Entry')
+            for e in entries:
+                channel_name = e.find(hcs_schema_prefix + 'ChannelName')
+                if channel_name is not None:
+                    channel = dict()
+                    channel['id'] = int(e.attrib.get('ChannelID')) - 1
+                    channel['name'] = channel_name.text
+                    channels.append(channel)
+        return channels
+
+    @staticmethod
+    def extract_local_path(path, cloud_scheme=HCS_CLOUD_FILES_SCHEMA):
+        path_chunks = path.split(cloud_scheme + '://', 1)
+        if len(path_chunks) != 2:
+            raise RuntimeError('Unable to determine local path of [{}]'.format(path))
+        return '{}/{}'.format('/cloud-data', path_chunks[1])
+
+    @staticmethod
+    def _mkdir(path):
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                return False
+        return True
+
+    @staticmethod
+    def get_selected_channels(params, channels):
+        selected_channels = []
+        selected_channel_ids = []
+        for chl in channels:
+            channel_params = params.get(chl['name'])
             if channel_params is not None:
                 channel_params = channel_params.split(',')
-                if len(channel_params) != 6:
+                if len(channel_params) != 5:
                     raise RuntimeError("Channel should have 6 parameters")
                 channel = dict()
-                channel['id'] = int(channel_params[0])
-                channel['r'] = int(channel_params[1])
-                channel['g'] = int(channel_params[2])
-                channel['b'] = int(channel_params[3])
-                channel['min'] = int(channel_params[4])
-                channel['max'] = int(channel_params[5])
-                channel_ids.append(int(channel_params[0]))
-                channels.append(channel)
-        if len(channels) < 1:
+                channel['id'] = chl['id']
+                channel['name'] = chl['name']
+                channel['r'] = int(channel_params[0])
+                channel['g'] = int(channel_params[1])
+                channel['b'] = int(channel_params[2])
+                channel['min'] = int(channel_params[3])
+                channel['max'] = int(channel_params[4])
+                selected_channel_ids.append(chl['id'])
+                selected_channels.append(channel)
+        if len(selected_channels) < 1:
             raise RuntimeError("At least one channel is required")
-        return channel_ids, channels
+        return selected_channel_ids, selected_channels
+
+    @staticmethod
+    def get_ome_tiff_path(preview_dir, seq, by_field):
+        return preview_dir + '/{}/'.format(seq) + ('data.ome.tiff' if by_field else 'overview_data.ome.tiff')
 
     @staticmethod
     def get_channel_params(params, channel):
@@ -204,13 +286,13 @@ class HCSManager:
     def change_intensity(intensity, contrast_min, contrast_max):
         return (intensity - contrast_min) / max(0.0005, (contrast_max - contrast_min))
 
-    def color_image(self, image, params):
-        image = self.change_intensity(image, params['min'], params['max'])
+    def color_image(self, image, channel):
+        image = self.change_intensity(image, channel['min'], channel['max'])
         image = np.where(image <= 0, 0, 255 * image)
         image = np.where(image <= 255, image, 255)
         image = Image.fromarray(image)
         grayscale_image = ImageOps.grayscale(image)
-        rgb = (params['r'], params['g'], params['b'])
+        rgb = (channel['r'], channel['g'], channel['b'])
         colored_image = ImageOps.colorize(grayscale_image, black='black', white=rgb)
         return colored_image
 
