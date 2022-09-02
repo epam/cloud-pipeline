11d67b0381506505f7694475a7be162953e708e6
pavel_silin <pavel_silin@epam.com>
2022-09-02 13:09:25 +0300

    Issue 2721 change lifecycle notification object and process (#2799)

--- api/src/main/java/com/epam/pipeline/acl/user/RoleApiService.java
+++ api/src/main/java/com/epam/pipeline/acl/user/RoleApiService.java
@@ -20,7 +20,6 @@ import com.epam.pipeline.controller.vo.user.RoleVO;
 import com.epam.pipeline.entity.user.ExtendedRole;
 import com.epam.pipeline.entity.user.Role;
 import com.epam.pipeline.manager.user.RoleManager;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
