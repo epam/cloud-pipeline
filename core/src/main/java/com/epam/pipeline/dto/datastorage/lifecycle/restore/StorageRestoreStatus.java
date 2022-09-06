73891c116190094a89951eb80328648f889b589e
pavel_silin <pavel_silin@epam.com>
2022-09-06 17:14:27 +0300

    Issue 2721 restore datastorage folder from 'archived' storage class (#2795) - hf code style

--- core/src/main/java/com/epam/pipeline/dto/datastorage/lifecycle/restore/StorageRestoreStatus.java
+++ core/src/main/java/com/epam/pipeline/dto/datastorage/lifecycle/restore/StorageRestoreStatus.java
@@ -2,7 +2,6 @@ package com.epam.pipeline.dto.datastorage.lifecycle.restore;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
