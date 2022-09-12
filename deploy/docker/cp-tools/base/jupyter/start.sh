a1f5ca9ae9e1df39eb7688ebb04939acf332aee1
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2022-09-12 13:53:19 +0200

    Issue #2821: INACTIVE_USERS notification enhancements (#2826)
    (cherry picked from commit 2a2fb96752d8ded808e8e68833dc4be3e011fc93)

--- deploy/docker/cp-tools/base/jupyter/start.sh
+++ deploy/docker/cp-tools/base/jupyter/start.sh
@@ -7,13 +7,17 @@ export endpoint_number=0
 export notebook_dir=${NOTEBOOK_SERVE_DIR:-"/home/${OWNER}"}
 export base_url="/$pipeline_name-$jupyter_port-$endpoint_number"
 
-# "disable_check_xsrf" option was added, as "'_xsrf' argument missing from post"
-# might be thrown from time to time, see https://stackoverflow.com/questions/55014094/jupyter-notebook-not-saving-xsrf-argument-missing-from-post
-jupyter notebook --ip '0.0.0.0' \
-                 --port $jupyter_port \
-                 --no-browser \
-                 --NotebookApp.token='' \
-                 --NotebookApp.notebook_dir=$notebook_dir \
-                 --NotebookApp.base_url=$base_url \
-                 --NotebookApp.disable_check_xsrf=True \
-                 --allow-root
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip 0.0.0.0 --port $jupyter_port --no-browser --NotebookApp.token= --NotebookApp.notebook_dir=$notebook_dir --NotebookApp.base_url=$base_url --NotebookApp.disable_check_xsrf=True --allow-root"
+
+export CP_CAP_SPARK_HOST="${CP_CAP_SPARK_HOST:-$(hostname)}"
+export CP_CAP_SPARK_PORT="${CP_CAP_SPARK_PORT:-7077}"
+
+spark_master_url="spark://${CP_CAP_SPARK_HOST}:${CP_CAP_SPARK_PORT}"
+
+pyspark --packages io.projectglow:glow-spark3_2.12:1.2.1,io.delta:delta-core_2.12:2.1.0 \
+--conf spark.hadoop.io.compression.codecs=io.projectglow.sql.util.BGZFCodec \
+--conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+--conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
+--master "$spark_master_url" \
+
