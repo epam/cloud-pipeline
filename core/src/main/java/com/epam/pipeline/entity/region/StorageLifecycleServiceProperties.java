7578d7b92970e6eb35e675d50f395faab8c3eb5b
pavel_silin <pavel_silin@epam.com>
2022-09-12 18:27:45 +0300

    issue 2721 clean up

--- core/src/main/java/com/epam/pipeline/entity/region/StorageLifecycleServiceProperties.java
+++ core/src/main/java/com/epam/pipeline/entity/region/StorageLifecycleServiceProperties.java
@@ -2,10 +2,9 @@ package com.epam.pipeline.entity.region;
 
 import lombok.Data;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class StorageLifecycleServiceProperties {
-    private final Map<String, String> properties = new HashMap<>();
+    private final Map<String, String> properties;
 }
