9850d308f881839e574b10807e0e833c99914eb4
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2022-09-29 12:41:32 +0200

    Cloud Data auto-update: "Check for updates" button

--- cloud-pipeline-webdav-client/src/application/components/update-notification/index.js
+++ cloud-pipeline-webdav-client/src/application/components/update-notification/index.js
@@ -1,12 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import electron from 'electron';
 import {CloseOutlined} from '@ant-design/icons';
-import cloudPipelineAPI from '../../models/cloud-pipeline-api';
-import { log } from '../../models/log';
-import autoUpdateApplication, {autoUpdateAvailable} from '../../../auto-update';
+import checker from '../../../auto-update';
 import './update-notification.css';
-
-const UPDATE_INTERVAL_MS = 5 * 60 * 1000; // 5 minutes
 
 export default function UpdateNotification () {
   const config = (() => {
@@ -16,79 +12,32 @@ export default function UpdateNotification () {
     return (cfg || {}).config || {};
   })();
   const {
-    name: appName = 'Cloud Data',
-    componentVersion: currentVersion
+    name: appName = 'Cloud Data'
   } = config;
 
-  const [newVersion, setNewVersion] = useState(undefined);
-  const [available, setAvailable] = useState(undefined);
-  const [userClosedNotification, setUserClosedNotification] = useState(undefined);
   const [notificationVisible, setNotificationVisible] = useState(false);
   const [updating, setUpdating] = useState(false);
   const [error, setError] = useState(undefined);
 
-  useEffect(() => {
-    let handler;
-    let stopped = false;
-    const check = () => {
-      clearTimeout(handler);
-      Promise.resolve()
-        .then(() => cloudPipelineAPI.initialize())
-        .then(autoUpdateAvailable)
-        .then(autoUpdateAvailableForOS => {
-            setAvailable(autoUpdateAvailableForOS);
-            return cloudPipelineAPI.getAppInfo();
-          }
-        )
-        .then(version => {
-          const updateAvailable = version &&
-            // currentVersion &&
-            version !== currentVersion;
-          if (updateAvailable && newVersion !== version) {
-            log(`New version ${version} available. Current version: ${currentVersion || 'unknown'}`);
-          }
-          if (newVersion !== version) {
-            setNewVersion(version);
-          }
-        })
-        .catch(e => log(`Error fetching app version: ${e.message}`))
-        .then(() => {
-          if (!stopped) {
-            handler = setTimeout(check, UPDATE_INTERVAL_MS);
-          }
-        });
-    };
-    check();
-    return () => {
-      stopped = true;
-      clearTimeout(handler);
-    };
-  }, [
-    newVersion,
-    setNewVersion,
-    currentVersion,
-    setAvailable
-  ]);
+  const handle = useCallback((options = {}) => {
+    const {
+      available = false
+    } = options;
+    setNotificationVisible(available);
+  }, [setNotificationVisible]);
+
+  const closeNotification = useCallback(() => setNotificationVisible(false), [setNotificationVisible]);
 
   useEffect(() => {
-    setNotificationVisible(
-      available &&
-      !!newVersion &&
-      currentVersion !== newVersion &&
-      userClosedNotification !== newVersion
-    );
-  }, [
-    available,
-    userClosedNotification,
-    newVersion,
-    currentVersion,
-    setNotificationVisible
-  ]);
+    checker.addEventListener(handle);
+    return () => checker.removeEventListener(handle);
+  }, [checker, handle]);
+
   const onUpdateApplication = useCallback(async () => {
     setUpdating(true);
     setError(false);
     try {
-      await autoUpdateApplication();
+      await checker.update();
       // Normally, update script should kill current process.
       // If for some reason it is finished, but current process is alive,
       // we need to warn user that something went wrong.
@@ -99,10 +48,6 @@ export default function UpdateNotification () {
       setUpdating(false);
     }
   }, [setUpdating, setError, appName]);
-  const onCloseNotification = useCallback(
-    () => setUserClosedNotification(newVersion),
-    [setUserClosedNotification, newVersion]
-  );
   return (
     <div
       className="update-notification"
@@ -110,7 +55,7 @@ export default function UpdateNotification () {
     >
       <CloseOutlined
         className="close"
-        onClick={onCloseNotification}
+        onClick={closeNotification}
       />
       {
         updating && (
@@ -122,7 +67,7 @@ export default function UpdateNotification () {
       {
         !updating && (
           <div>
-            New version of <b>{appName}</b> is available. <a onClick={onUpdateApplication}>Install update</a>
+            New version of <b>{appName}</b> is available. <a onClick={onUpdateApplication}>Install updates</a>
           </div>
         )
       }
