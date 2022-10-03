695f138d58ce295b14f72fa4ce6a793cec287da6
Pavel Silin <pavel_silin@epam.com>
2022-10-03 18:16:16 +0300

    Issue 2721 Storage Lifecycle Service: Expand security log (#2833)
    * issue 2721 expand security logs with datastorage lifecycle events
    
    * issue 2721 expand security logs with datastorage lifecycle events WIP
    
    * issue 2721 Security logs: expand security logs for Storage Lifecycle events
    
    * issue 2721 Security logs: fix pmd
    
    * issue 2721 Security logs: change filebeat configuration and elastichsearch pipeline for api server to correctly set 'type' field
    
    * issue 2721 Security Logs: changes for configuration to reduce deploy actions for this feature

--- core/src/main/java/com/epam/pipeline/dto/datastorage/lifecycle/StorageLifecycleRule.java
+++ core/src/main/java/com/epam/pipeline/dto/datastorage/lifecycle/StorageLifecycleRule.java
@@ -25,6 +25,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -104,4 +105,17 @@ public class StorageLifecycleRule {
     private List<StorageLifecycleRuleProlongation> prolongations;
     private List<StorageLifecycleRuleTransition> transitions;
     private StorageLifecycleNotification notification;
+
+    public String toDescriptionString() {
+        return "id: " + id + ", datastorageId: " + datastorageId + ", pathGlob: " + pathGlob +
+                ", objectGlob: " + objectGlob +
+                ", transitionCriterion: " + transitionCriterion.toDescriptionString() +
+                ", transitionMethod: " + transitionMethod.name() +
+                ", transitions: " + transitions.stream().map(t ->
+                        "[to " + t.getStorageClass()
+                                + (t.getTransitionDate() != null
+                                        ? " on " + t.getTransitionDate()
+                                        : " after " + t.getTransitionAfterDays().toString() + " days") + "]"
+                ).collect(Collectors.joining(";"));
+    }
 }
