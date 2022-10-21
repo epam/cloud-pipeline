458cf8ae9db2b37ef1aa7a664d5bbb6e2ec5ae22
Andrey Tsibin <andrei_tcibin@epam.com>
2022-10-21 17:45:47 +0300

    Use no proxies for node metrics api requests.
    Relates to #2882 and improves changes introduced in #2884.

--- cloud-pipeline-common/model/src/main/java/com/epam/pipeline/client/RetrofitClientBuilder.java
+++ cloud-pipeline-common/model/src/main/java/com/epam/pipeline/client/RetrofitClientBuilder.java
@@ -2,20 +2,19 @@ package com.epam.pipeline.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.net.Proxy;
+
 public interface RetrofitClientBuilder {
 
     <T> T build(Class<T> type,
                 String schema, String host, int port);
-
-    <T> T build(Class<T> type,
-                String schema, String host, int port,
-                int connectTimeout, int readTimeout);
     
     <T> T build(Class<T> type,
                 String schema, String host, int port,
                 ObjectMapper mapper);
 
-    <T> T build(Class<T> type, String schema, String host, int port,
-                int connectTimeout, int readTimeout,
-                ObjectMapper mapper);
+    <T> T build(Class<T> type,
+                String schema, String host, int port,
+                ObjectMapper mapper, Proxy proxy);
+
 }
