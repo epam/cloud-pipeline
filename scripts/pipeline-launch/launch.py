70b1a8af904b484a8b75cc6a2d106ab5809009b5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 14:47:35 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/pipeline-launch/launch.py
+++ scripts/pipeline-launch/launch.py
@@ -49,7 +49,26 @@ def _download_file(source_url, target_path):
 def _extract_archive(source_path, target_path):
     if source_path.endswith('.tar.gz') or source_path.endswith('.tgz'):
         with tarfile.open(source_path, 'r:gz') as f:
-            f.extractall(path=target_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=target_path)
     elif source_path.endswith('.zip'):
         with zipfile.ZipFile(source_path, 'r') as f:
             f.extractall(path=target_path)
