70456172bfd8bc4e6dfdd99eecf6cfc9c27a834c
maryvictol <Mariia_Tolstaia@epam.com>
2022-10-31 12:40:28 +0300

    Remove STS/LTS duration from Create/Edit storage forms

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/DataStoragesTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/DataStoragesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2017-2022 EPAM Systems, Inc. (https://www.epam.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -380,7 +380,6 @@ public class DataStoragesTest extends AbstractBfxPipelineTest implements Navigat
         navigateToLibrary()
             .selectStorage(editableStorage)
             .clickEditStorageButton()
-            .setDurations("", "5")
             .clickSaveButton()
             .selectStorage(editableStorage)
             .validateLtsDuration("5");
@@ -393,7 +392,6 @@ public class DataStoragesTest extends AbstractBfxPipelineTest implements Navigat
         navigateToLibrary()
             .selectStorage(editableStorage)
             .clickEditStorageButton()
-            .setDurations("3","5")
             .clickSaveButton()
             .selectStorage(editableStorage)
             .validateStsDuration("3");
