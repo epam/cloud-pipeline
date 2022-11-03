022308b8a1291a0e3ab2b7ae6dabc883f940e33b
maryvictol <Mariia_Tolstaia@epam.com>
2022-11-03 15:57:22 +0300

    Update MetadataSamplesAO

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/MetadataSamplesAO.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/MetadataSamplesAO.java
@@ -221,7 +221,7 @@ public class MetadataSamplesAO implements AccessObject<MetadataSamplesAO> {
         }
 
         public void ensureCellContainsHyperlink() {
-            particularCell.shouldHave(hyperlink());
+            particularCell.find(tagName("a")).shouldHave(hyperlink());
         }
 
         public void ensureCellContains(String substring) {
