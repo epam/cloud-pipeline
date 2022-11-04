56bd32731bc0c842f261e1c20d19973c689a5638
Yulia Kamyshova <yulia_kamyshova@epam.com>
2022-11-04 18:29:24 +0300

    Quick-launcher: update start.sh
    Signed-off-by: Yulia Kamyshova <yulia_kamyshova@epam.com>

--- deploy/docker/cp-tools/quick-launcher/jupyter/start.sh
+++ deploy/docker/cp-tools/quick-launcher/jupyter/start.sh
@@ -17,135 +17,42 @@ if [ -z "$OWNER" ]; then
     echo "[ERROR] Owner of the job is not defined. Exiting"
     exit 1
 fi
-
-if [ -z "$IPYNB_NAME" ]; then
-    echo "[ERROR] IPython Notebook name (.ipynb) is not defined. Exiting"
-    exit 1
-fi
-
-if [ "$FOLDER_APPLICATION_STORAGE" ]; then
-    folder_app_storage_mountpoint=$(curl -s -k \
-                                -X GET \
-                                -H "Authorization: Bearer $API_TOKEN" \
-                                "$API/datastorage/$FOLDER_APPLICATION_STORAGE/load" \
-                            | jq -r '.payload.mountPoint')
-    if [ "$folder_app_storage_mountpoint" ] && [ "$folder_app_storage_mountpoint" != null ]; then
-        if [ ! -d "$folder_app_storage_mountpoint" ]; then
-            echo "[ERROR] FOLDER_APPLICATION_STORAGE ($FOLDER_APPLICATION_STORAGE) is not mounted to the instance at $folder_app_storage_mountpoint"
-        fi
-    else
-        echo "[ERROR] FOLDER_APPLICATION_STORAGE ($FOLDER_APPLICATION_STORAGE) can not be found"
-    fi
-else
-    echo "[ERROR] FOLDER_APPLICATION_STORAGE is not set, can't find path to ipynb"
-fi
-
-if [ "$FOLDER_APPLICATION_PATH" ]; then
-    FOLDER_APPLICATION_DIR=$(dirname $FOLDER_APPLICATION_PATH)
-    export FOLDER_APPLICATION_PATH=${folder_app_storage_mountpoint%/}/${FOLDER_APPLICATION_DIR#/}
-fi
-if [ ! -d "$FOLDER_APPLICATION_PATH" ]; then
-    echo "[ERROR] FOLDER_APPLICATION_PATH ($FOLDER_APPLICATION_PATH) is not found in the instance"
-    exit 1
-fi
-
-NOTEBOOK_DIR_NAME=$(basename $FOLDER_APPLICATION_PATH)
-export notebook_dir=$(dirname $FOLDER_APPLICATION_PATH)
-export ipynb_url="notebooks/$NOTEBOOK_DIR_NAME/$IPYNB_NAME"
 export jupyter_port=${NOTEBOOK_SERVE_PORT:-8888}
-export jupyter_server_inactivity_timeout_sec=${JUPYTER_SERVER_INACTIVITY_TIMEOUT_SEC:-120}
-export inactivity_timeout_sec=${PROCESS_ACTIVITY_TRACKING_INACTIVITY_TIMEOUT_SEC:-43200}
-export jupyter_server_cull_interval_sec=${JUPYTER_SERVER_CULL_INTERVAL_SEC:-60}
-base_pathname=$(echo $RUN_PRETTY_URL | cut -d';' -f 2)
-export base_url=${JUPYTER_BASE_URL:-"/$base_pathname/"}
+export ANACONDA_HOME="/opt/local/anaconda"
 
-# Get Anaconda System Lib path
-if [ "$py_sys_lib" ]; then
-    conda_sys_lib_mountpoint=$(curl -s -k \
-                                -X GET \
-                                -H "Authorization: Bearer $API_TOKEN" \
-                                "$API/datastorage/$py_sys_lib/load" \
-                            | jq -r '.payload.mountPoint')
-    if [ "$conda_sys_lib_mountpoint" ] && [ "$conda_sys_lib_mountpoint" != null ]; then
-        if [ ! -d "$conda_sys_lib_mountpoint" ]; then
-            echo "[ERROR] py_sys_lib ($py_sys_lib) is not mounted to the instance at $conda_sys_lib_mountpoint"
-        fi
-    else
-        echo "[ERROR] py_sys_lib ($py_sys_lib) can not be found"
-    fi
-else
-    echo "[ERROR] py_sys_lib is not set, no external library is mounted"
-fi
+function start_jupyter() {
+    # Construct notebook http URL, that will match EDGE's notation (e.g. /jupyter-gitlab-4800-8888-0 or /pipeline-4801-8888-0)
+    export pipeline_name=$(hostname | awk '{print tolower($0)}')
+    export endpoint_number=0
+    export notebook_dir=${NOTEBOOK_SERVE_DIR:-"/home/${OWNER}"}
+    export base_url="/$pipeline_name-$jupyter_port-$endpoint_number"
 
-export ANACONDA_HOME=${conda_sys_lib_mountpoint:-/opt/local/anaconda}
-if [ ! -f "$ANACONDA_HOME/etc/profile.d/conda.sh" ]; then
-    echo "[ERROR] Conda environment was not found in the instance's $ANACONDA_HOME path"
-    exit 1
-fi
-
-if [ -z "$APP_ENVIRONMENT" ]; then
-    if [ -f "$FOLDER_APPLICATION_PATH/gateway.spec" ]; then
-        export APP_ENVIRONMENT=$( cat "$FOLDER_APPLICATION_PATH/gateway.spec" | jq -r '.app_environment' )
-    fi
-    if [ -z "$APP_ENVIRONMENT" ]; then
-        echo "[WARN] Jupyter notebook environment (anaconda env) is not defined. Default 'base' env will be used."
-        export APP_ENVIRONMENT="base"
-    fi
-fi
-
-user_default_storage_id=$(curl -s -k \
-                                -X GET \
-                                -H "Authorization: Bearer $API_TOKEN" \
-                                "$API/whoami" \
-                            | jq -r '.payload.defaultStorageId')
-if [ "$user_default_storage_id" ] && [ "$user_default_storage_id" != null ]; then
-    user_default_storage_mountpoint=$(curl -s -k \
-                                    -X GET \
-                                    -H "Authorization: Bearer $API_TOKEN" \
-                                    "$API/datastorage/$user_default_storage_id/load" \
-                                | jq -r '.payload.mountPoint')
-    if [ "$user_default_storage_mountpoint" ] && [ "$user_default_storage_mountpoint" != null ]; then
-        if [ -d "$user_default_storage_mountpoint" ]; then
-            export LOG_DIR_PATH="${user_default_storage_mountpoint}/JupyterNotebooks/log"
-        else
-            echo "[ERROR] User default storage ($user_default_storage_id) is not mounted to the instance at $user_default_storage_mountpoint"
-        fi
-    else
-        echo "[ERROR] User default storage ($user_default_storage_id) mountpoint can not be found"
-    fi
-else
-    echo "[ERROR] User default storage was not found. '/home/${OWNER}/JupyterNotebooks/log' directory will be used for logs"
-    export LOG_DIR_PATH="/home/${OWNER}/JupyterNotebooks/log"
-fi
-mkdir -p $LOG_DIR_PATH
-
-# Execute delayed data storage mount
-function do_mount_delayed_storages() {
-    if [ "$CP_CAP_FORCE_DELAYED_MOUNT" == "true" ]; then
-        _limit_mounts_bkp="$CP_CAP_LIMIT_MOUNTS"
-        unset CP_CAP_LIMIT_MOUNTS
-
-        MOUNT_DATA_STORAGES_TASK_NAME="MountDataStoragesDelayed"
-        DATA_STORAGE_MOUNT_ROOT="/cloud-data"
-        mount_storages $DATA_STORAGE_MOUNT_ROOT $TMP_DIR $MOUNT_DATA_STORAGES_TASK_NAME
-
-        mkdir -p $DATA_STORAGE_MOUNT_ROOT
-        if [ -L $R_USER_HOME/cloud-data ]; then
-            unlink $R_USER_HOME/cloud-data
-        fi
-        [ -d $DATA_STORAGE_MOUNT_ROOT ] && ln -s -f $DATA_STORAGE_MOUNT_ROOT $R_USER_HOME/cloud-data || echo "$DATA_STORAGE_MOUNT_ROOT not found, no buckets will be available"
-
-        export CP_CAP_LIMIT_MOUNTS="$_limit_mounts_bkp"
-        unset _limit_mounts_bkp
-    fi
+    # "disable_check_xsrf" option was added, as "'_xsrf' argument missing from post"
+    # might be thrown from time to time, see https://stackoverflow.com/questions/55014094/jupyter-notebook-not-saving-xsrf-argument-missing-from-post
+    jupyter notebook --ip '0.0.0.0' \
+                     --port $jupyter_port \
+                     --no-browser \
+                     --NotebookApp.token='' \
+                     --NotebookApp.notebook_dir=$notebook_dir \
+                     --NotebookApp.base_url=$base_url \
+                     --NotebookApp.disable_check_xsrf=True \
+                     --allow-root
 }
 
-if [ "$cluster_role" == "worker" ]; then
-    # Mount "Delayed" storages in before the SGE is configured for the workers
-    # as the job can be submitted, which requires those storages
-    do_mount_delayed_storages
-else
-    # Start all the services for a worker
+function start_jupyter_app() {
+    if [ -z "$IPYNB_NAME" ]; then
+        echo "[ERROR] IPython Notebook name (.ipynb) is not defined. Exiting"
+        exit 1
+    fi
+
+    notebook_dir_name=$(basename $APP_PATH)
+    export notebook_dir=$(dirname $APP_PATH)
+    export ipynb_url="notebooks/$notebook_dir_name/$IPYNB_NAME"
+    export jupyter_server_inactivity_timeout_sec=${JUPYTER_SERVER_INACTIVITY_TIMEOUT_SEC:-120}
+    export inactivity_timeout_sec=${PROCESS_ACTIVITY_TRACKING_INACTIVITY_TIMEOUT_SEC:-43200}
+    export jupyter_server_cull_interval_sec=${JUPYTER_SERVER_CULL_INTERVAL_SEC:-60}
+    base_pathname=$(echo $RUN_PRETTY_URL | cut -d';' -f 2)
+    export base_url=${JUPYTER_BASE_URL:-"/$base_pathname/"}
 
     # Start activity tracker
     if [ "$PROCESS_ACTIVITY_TRACKING_ENABLED" == "true" ]; then
@@ -155,10 +62,6 @@ else
 
     # "disable_check_xsrf" option was added, as "'_xsrf' argument missing from post"
     # might be thrown from time to time, see https://stackoverflow.com/questions/55014094/jupyter-notebook-not-saving-xsrf-argument-missing-from-post
-
-    source $ANACONDA_HOME/etc/profile.d/conda.sh
-    conda activate ${APP_ENVIRONMENT}
-
     nohup jupyter-notebook --ip '0.0.0.0' \
                      --port $jupyter_port \
                      --no-browser \
@@ -174,38 +77,102 @@ else
                      --allow-root >> $LOG_DIR_PATH/jupyter_notebook_${RUN_ID}.log 2>&1 &
 
     pipe_log_success "Gateway Apps has been started" "InitializeApp"
+    echo "[INFO] Jupyter Notebook logs can be found at $LOG_DIR_PATH path"
+    sleep infinity
+}
+
+if [ "$FOLDER_APPLICATION_STORAGE" ]; then
+    folder_app_storage_mountpoint=$(curl -s -k \
+                                -X GET \
+                                -H "Authorization: Bearer $API_TOKEN" \
+                                "$API/datastorage/$FOLDER_APPLICATION_STORAGE/load" \
+                            | jq -r '.payload.mountPoint')
+    if [ "$folder_app_storage_mountpoint" ] && [ "$folder_app_storage_mountpoint" != null ]; then
+        if [ ! -d "$folder_app_storage_mountpoint" ]; then
+            echo "[ERROR] FOLDER_APPLICATION_STORAGE ($FOLDER_APPLICATION_STORAGE) is not mounted to the instance at $folder_app_storage_mountpoint"
+        fi
+    else
+        echo "[ERROR] FOLDER_APPLICATION_STORAGE ($FOLDER_APPLICATION_STORAGE) can not be found"
+    fi
+else
+    echo "[WARN] FOLDER_APPLICATION_STORAGE is not set, can't find path to ipynb"
 fi
-
-# Run the SGE installer
-if [ "$cluster_role" ]; then
-    _CAP_INIT_SCRIPT="$CP_CAP_SCRIPTS_DIR/$cluster_role"
-    if [ -f "$_CAP_INIT_SCRIPT" ]; then
-        echo "--> Executing $_CAP_INIT_SCRIPT"
-        chmod +x "$_CAP_INIT_SCRIPT"
-        # Run as a job and await - hack to workaround accident job stop due to PID1 issue
-        eval "$_CAP_INIT_SCRIPT &"
-        _CAP_INIT_PID=$!
-        wait $_CAP_INIT_PID
-
-        _CAP_INIT_SCRIPT_RESULT=$?
-        if [ $_CAP_INIT_SCRIPT_RESULT -ne 0 ];
-        then
-                echo "[ERROR] $_CAP_INIT_SCRIPT failed with $_CAP_INIT_SCRIPT_RESULT. Exiting"
-                exit "$_CAP_INIT_SCRIPT_RESULT"
-        else
-                # Reload env vars, in case they were updated within cap init scripts
-                source "$CP_ENV_FILE_TO_SOURCE"
-                echo "--> Done $_CAP_INIT_SCRIPT"
-        fi
-
-        if [ "$cluster_role" = "master" ] && [ "$CP_CAP_AUTOSCALE" = "true" ] &&  [ "$CP_CAP_SGE" = "true" ]; then
-                nohup $CP_PYTHON2_PATH $COMMON_REPO_DIR/scripts/autoscale_sge.py 1>/dev/null 2>$LOG_DIR/.nohup.sge.autoscale.log &
-        fi
+export FOLDER_STORAGE_MOUNT_POINT=${folder_app_storage_mountpoint%/}
+if [ "$FOLDER_APPLICATION_PATH" ]; then
+    export APP_PATH=$FOLDER_STORAGE_MOUNT_POINT/$(dirname $FOLDER_APPLICATION_PATH)
+    if [ ! -d "$APP_PATH" ]; then
+        echo "[ERROR] Jupyter notebook app folder was not found in $APP_PATH path. Check FOLDER_APPLICATION_PATH env ($FOLDER_APPLICATION_PATH)"
+        exit 1
     fi
 fi
 
-if [ "$cluster_role" == "master" ]; then
-    do_mount_delayed_storages
-fi
+user_default_storage_id=$(curl -s -k \
+                                -X GET \
+                                -H "Authorization: Bearer $API_TOKEN" \
+                                "$API/whoami" \
+                            | jq -r '.payload.defaultStorageId')
+if [ -z "$user_default_storage_id" ] || [ "$user_default_storage_id" == null ]; then
+    echo "[ERROR] User default storage was not found. '/home/${OWNER}' home directory will be used for work"
+    export USER_STORAGE_MOUNT_POINT="/home/${OWNER}"
+    export CONDA_ENVS_PATH="${ANACONDA_HOME}/envs"
+    export JUPYTER_PATH="${USER_STORAGE_MOUNT_POINT}/share/jupyter"
+    export LOG_DIR_PATH="${USER_STORAGE_MOUNT_POINT}/JupyterNotebooks/log"
+    mkdir -p $LOG_DIR_PATH
+else
+    user_default_storage_mountpoint=$(curl -s -k \
+                                    -X GET \
+                                    -H "Authorization: Bearer $API_TOKEN" \
+                                    "$API/datastorage/$user_default_storage_id/load" \
+                                    | jq -r '.payload.mountPoint')
+    if [ -z "$user_default_storage_mountpoint" ] || \
+       [ "$user_default_storage_mountpoint" == null ] || \
+       [ ! -d "$user_default_storage_mountpoint" ]; then
+        echo "[ERROR] User default storage ($user_default_storage_id) mountpoint can not be found"
+        export USER_STORAGE_MOUNT_POINT="/home/${OWNER}"
+    fi
+    export USER_STORAGE_MOUNT_POINT=$user_default_storage_mountpoint
 
-sleep infinity
+    if [ "$FOLDER_APPLICATION_PATH" ]; then
+        if [ -d "$APP_PATH/.conda/envs" ]; then
+            export CONDA_ENVS_PATH="$APP_PATH/.conda/envs"
+            export JUPYTER_PATH="$APP_PATH/.conda/share/jupyter"
+        else
+            STORAGE_MOUNT_POINT=$(echo "$USER_STORAGE_MOUNT_POINT" | cut -d "/" -f2)
+            export DEFAULT_STORAGE_USER=$(echo "$FOLDER_APPLICATION_PATH" | cut -d "/" -f1)
+            export CONDA_ENVS_PATH="/$STORAGE_MOUNT_POINT/$DEFAULT_STORAGE_USER/.conda/envs"
+            export JUPYTER_PATH="/$STORAGE_MOUNT_POINT/$DEFAULT_STORAGE_USER/.conda/share/jupyter"
+            if [ ! -d $CONDA_ENVS_PATH ] || [ ! -d $JUPYTER_PATH ]; then
+                echo "[ERROR] Jupyter notebook CONDA_ENVS_PATH ($CONDA_ENVS_PATH) env and/or JUPYTER_PATH ($JUPYTER_PATH) were not found. Check FOLDER_APPLICATION_PATH env ($FOLDER_APPLICATION_PATH)"
+                exit 1
+            fi
+        fi
+    else
+        export CONDA_ENVS_PATH="$user_default_storage_mountpoint/.conda/envs"
+        export JUPYTER_PATH="$user_default_storage_mountpoint/.conda/share/jupyter"
+    fi
+fi
+echo "[INFO] CONDA_ENVS_PATH=$CONDA_ENVS_PATH and JUPYTER_PATH=$JUPYTER_PATH were set for Jupyter Notebook"
+
+if [ -z "$APP_ENVIRONMENT" ]; then
+    if [ -f "$APP_PATH/gateway.spec" ]; then
+        export APP_ENVIRONMENT=$( cat "$APP_PATH/gateway.spec" | jq -r '.app_environment' )
+    elif [ -d "$APP_PATH/.conda/envs" ]; then
+        export ENV_PATH=$(find "$APP_PATH/.conda/envs" -maxdepth 1 -type d | tail -n1)
+        export APP_ENVIRONMENT=$(basename $ENV_PATH)
+    fi
+    if [ -z "$APP_ENVIRONMENT" ] || [ "$APP_ENVIRONMENT" == null ]; then
+        export APP_ENVIRONMENT="base"
+    fi
+fi
+source $ANACONDA_HOME/etc/profile.d/conda.sh
+conda activate ${APP_ENVIRONMENT}
+
+if [ "$FOLDER_APPLICATION_PATH" ]; then
+    if [ -z "$LOG_DIR_PATH" ]; then
+        export LOG_DIR_PATH="${USER_STORAGE_MOUNT_POINT}/JupyterNotebooks/log"
+        mkdir -p $LOG_DIR_PATH
+    fi
+    start_jupyter_app
+else
+    start_jupyter
+fi
