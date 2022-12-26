8a103dbda052653d3040d4263eb1b0550f0f5d30
mzueva <mariia_zueva@epam.com>
2022-12-26 16:31:27 +0100

    Issue #2975: Make new preference visible by default

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -310,13 +310,13 @@ public class SystemPreferences {
     public static final BooleanPreference GITLAB_HASHED_REPO_SUPPORT = new BooleanPreference(
             "git.gitlab.hashed.repo.support", false, GIT_GROUP, pass);
     public static final StringPreference GITLAB_DEFAULT_SRC_DIRECTORY = new StringPreference(
-            "gitlab.default.src.directory", "src/", GIT_GROUP, pass);
+            "gitlab.default.src.directory", "src/", GIT_GROUP, pass, true);
     public static final StringPreference GITLAB_DEFAULT_DOC_DIRECTORY = new StringPreference(
-            "gitlab.default.doc.directory", "docs/", GIT_GROUP, pass);
+            "gitlab.default.doc.directory", "docs/", GIT_GROUP, pass, true);
     public static final StringPreference BITBUCKET_DEFAULT_SRC_DIRECTORY = new StringPreference(
-            "bitbucket.default.src.directory", "/", GIT_GROUP, pass);
+            "bitbucket.default.src.directory", "/", GIT_GROUP, pass, true);
     public static final StringPreference BITBUCKET_DEFAULT_DOC_DIRECTORY = new StringPreference(
-            "bitbucket.default.doc.directory", null, GIT_GROUP, pass);
+            "bitbucket.default.doc.directory", null, GIT_GROUP, pass, true);
 
     // DOCKER_SECURITY_GROUP
     /**
