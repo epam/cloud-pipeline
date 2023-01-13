0ddf13b9aaf67f86c4d1c6933968e592f7dfd570
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2023-01-13 17:30:24 +0100

    GUI Run capabilities: "required" capabilities feature - applying custom capability's parameters (confirmation window)

--- client/src/components/runs/actions/run.js
+++ client/src/components/runs/actions/run.js
@@ -457,6 +457,10 @@ function runFn (
                 stores.preferences
               );
             }
+            payload.params = applyCustomCapabilitiesParameters(
+              payload.params,
+              stores.preferences
+            );
             if (
               checkRequiredCapabilitiesErrors(
                 getEnabledCapabilities(payload.params),
