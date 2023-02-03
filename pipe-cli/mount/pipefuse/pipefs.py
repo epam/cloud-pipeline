2f8928ef7e59e85c3903a93a659c9b098945ad82
Ekaterina_Kazachkova <ekaterina_kazachkova@epam.com>
2023-02-03 11:18:52 +0100

    Archived S3 files shall be excluded from pipe storage & pipe mount operations - cleanup

--- pipe-cli/mount/pipefuse/pipefs.py
+++ pipe-cli/mount/pipefuse/pipefs.py
@@ -370,6 +370,7 @@ class ResilientFS(ChainingService):
                 err_msg = str(e)
                 if isinstance(e, ClientError) \
                         and err_msg and 'InvalidObjectState' in err_msg and 'storage class' in err_msg:
+                    logging.exception('Failed to access archived file. This file shall be restored first.')
                     raise FuseOSError(errno.EACCES)
                 logging.exception('Uncaught exception from underlying file system.')
                 raise FuseOSError(errno.EINVAL)
