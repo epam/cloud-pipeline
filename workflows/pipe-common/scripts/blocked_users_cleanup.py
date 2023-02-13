5fa16c1439b4082046a882906532471f791f9d24
mzueva <mariia_zueva@epam.com>
2023-02-13 17:55:57 +0100

    Issue #3036 notifications implemented

--- workflows/pipe-common/scripts/blocked_users_cleanup.py
+++ workflows/pipe-common/scripts/blocked_users_cleanup.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import logging
+import mutex
 import os
 import subprocess
 from datetime import datetime, timedelta
@@ -29,6 +30,124 @@ PIPE_STORAGE_CP = "pipe storage cp"
 
 PAUSED = "PAUSED"
 RUNNING = "RUNNING"
+
+
+EMAIL_TEMPLATE = '''
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        table,
+        td {{
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }}
+    </style>
+</head>
+
+<body>
+<p>Dear user,</p>
+<p>*** This is a system generated email, do not reply to this email ***</p>
+<p>Please find the list of the assets associated with the blocked users along with asset status. </p>
+<p>
+<table>
+    <tr>
+        <td><b>Object</b></td>
+        <td><b>Owner</b></td>
+        <td><b>Status</b></td>
+        <td><b>Message</b></td>
+    </tr>
+    {events}
+</table>
+</p>
+<p>Best regards,</p>
+<p>{deploy_name} Platform</p>
+</body>
+
+</html>
+'''
+
+EVENT_PATTERN = '''
+ <tr>
+            <td>{object}</td>
+            <td>{owner}</td>
+            <td>{status}</td>
+            <td>{message}</td>
+ </tr>
+'''
+
+
+EMAIL_SUBJECT = '[%s]: Blocked users assets'
+RUN_TYPE = 'RUN'
+TOOL_TYPE = 'TOOL'
+STORAGE_TYPE = 'STORAGE'
+DELETED = 'DELETED'
+
+
+def get_api_link(url):
+    return url.rstrip('/').replace('/restapi', '')
+
+
+class Event(object):
+
+    def __init__(self, id, name, type, owner, status, message):
+        self.id = str(id)
+        self.name = name
+        self.type = type
+        self.owner = owner
+        self.status = status
+        self.message = message
+
+    def get_object_str(self, api_url):
+        if self.type == RUN_TYPE:
+            return '<a href="{}/#/run/{}/plain">Pipeline Run {}</a>'.format(api_url, self.id, self.id)
+        if self.type == TOOL_TYPE:
+            if self.status == DELETED:
+                return 'Tool {} ({})'.format(self.name, self.id)
+            return '<a href="{}/#/tool/{}/description">Tool {}</a>'.format(api_url, self.id, self.name)
+        if self.type == STORAGE_TYPE:
+            if self.status == DELETED:
+                return 'Storage {} ({})'.format(self.name, self.id)
+            return '<a href="{}/#/storage/{}">Storage {}</a>'.format(api_url, self.id, self.name)
+        return ''
+
+
+class Notifier(object):
+
+    def __init__(self, api, deploy_name, notify_users):
+        self.notifications = []
+        self.api = api
+        self.deploy_name = deploy_name
+        self.notify_users = notify_users
+
+    def add(self, notification):
+        self.notifications.append(notification)
+
+    def send_notifications(self):
+        if not self.notifications or not self.notify_users:
+            return
+        self.api.create_notification(EMAIL_SUBJECT % self.deploy_name,
+                                     self.build_text(),
+                                     self.notify_users[0],
+                                     copy_users=self.notify_users[1:] if len(
+                                         self.notify_users) > 0 else None,
+                                     )
+
+    def build_text(self):
+        event_str = ''
+        api_link = get_api_link(self.api.api_url)
+        for event in self.notifications:
+            object_str = event.get_object_str(api_link)
+            event_str += EVENT_PATTERN.format(**{'object': object_str,
+                                                 'owner': event.owner,
+                                                 'status': event.status,
+                                                 'message': event.message})
+
+        return EMAIL_TEMPLATE.format(**{'events': event_str,
+                                        'deploy_name': self.deploy_name})
 
 
 def cleanup_users():
@@ -52,6 +171,9 @@ def cleanup_users():
     logger = LevelLogger(level=logging_level, inner=logger)
     logger = LocalLogger(inner=logger)
 
+    notify_users = os.getenv('CP_CLEANUP_NOTIFY_USERS', '').split(',')
+    notifier = Notifier(api, os.getenv('CP_DEPLOY_NAME', 'Cloud Pipeline'), notify_users)
+
     # while True:
     try:
         # time.sleep(cleanup_timeout)
@@ -66,10 +188,11 @@ def cleanup_users():
         logger.info('Loaded {} blocked users: {}'.format(len(blocked_users),
                                                          ', '.join([user.get('userName') for user in blocked_users])))
         if len(blocked_users) > 0:
-            _cleanup_paused_instances(api, logger, blocked_users, dry_run)
-            _cleanup_running_instances(api, logger, blocked_users, dry_run)
-            _cleanup_tools(api, logger, blocked_users, dry_run)
-            _cleanup_storages(api, logger, users, blocked_users, dry_run)
+            _cleanup_paused_instances(api, logger, blocked_users, dry_run, notifier)
+            _cleanup_running_instances(api, logger, blocked_users, dry_run, notifier)
+            _cleanup_tools(api, logger, blocked_users, dry_run, notifier)
+            _cleanup_storages(api, logger, users, blocked_users, dry_run, notifier)
+        notifier.send_notifications()
     except KeyboardInterrupt:
         logger.warning('Interrupted.')
         # break
@@ -109,7 +232,7 @@ def _get_tools(registries):
     return _flatten(tools)
 
 
-def _cleanup_paused_instances(api, logger, users, dry_run):
+def _cleanup_paused_instances(api, logger, users, dry_run, notifier):
     exp_days = os.getenv('CP_PAUSED_EXP_DAYS')
     if exp_days is None:
         logger.info('CP_PAUSED_EXP_DAYS not defined. Paused instances cleanup skipped...')
@@ -129,16 +252,22 @@ def _cleanup_paused_instances(api, logger, users, dry_run):
             logger.debug('Processing paused instance {}.'.format(run_id))
             if not dry_run:
                 api.terminate_run(run_id)
+                notifier.add(Event(run_id, None, RUN_TYPE, run.get('owner'), 'TERMINATED', ''))
+            else:
+                notifier.add(Event(Event(run_id, None, RUN_TYPE, run.get('owner'),
+                                         'TO BE TERMINATED', 'Skipped due to dry run')))
             logger.debug('Paused instance {} terminated.'.format(run_id))
         except KeyboardInterrupt:
             logger.warning('Interrupted.')
             raise
-        except Exception:
+        except Exception as e:
             logger.warning('Paused instances cleanup has failed.')
+            notifier.add(Event(run.get('id'), None, RUN_TYPE, run.get('owner'), 'ERROR',
+                               'Failed to terminate paused run: ' + e.message))
     logger.info('Finishing paused instances cleanup...')
 
 
-def _cleanup_running_instances(api, logger, users, dry_run):
+def _cleanup_running_instances(api, logger, users, dry_run, notifier):
     exp_days = os.getenv('CP_RUNNING_EXP_DAYS')
     if exp_days is None:
         logger.info('CP_RUNNING_EXP_DAYS not defined. Running instances cleanup skipped...')
@@ -159,16 +288,22 @@ def _cleanup_running_instances(api, logger, users, dry_run):
             run_id = run.get('id')
             if not dry_run:
                 api.stop_run(run_id)
+                notifier.add(Event(run_id, None, RUN_TYPE, run.get('owner'), 'STOPPED', ''))
+            else:
+                notifier.add(Event(Event(run_id, None, RUN_TYPE, run.get('owner'),
+                                         'TO BE STOPPED', 'Skipped due to dry run')))
             logger.debug('Running instance {} stopped.'.format(run_id))
         except KeyboardInterrupt:
             logger.warning('Interrupted.', trace=True)
             raise
-        except Exception:
+        except Exception as e:
             logger.warning('Running instances cleanup has failed.')
+            notifier.add(Event(run.get('id'), None, RUN_TYPE, run.get('owner'), 'ERROR',
+                               'Failed to stop active run: ' + e.message))
     logger.info('Finishing running instances cleanup...')
 
 
-def _cleanup_tools(api, logger, users, dry_run):
+def _cleanup_tools(api, logger, users, dry_run, notifier):
     exp_days = os.getenv('CP_TOOL_EXP_DAYS')
     if exp_days is None:
         logger.info('CP_TOOL_EXP_DAYS not defined. Tools cleanup skipped...')
@@ -193,25 +328,35 @@ def _cleanup_tools(api, logger, users, dry_run):
             logger.debug('Checking permissions')
             permissions = api.get_permissions(tool_id, 'TOOL')
             if permissions is not None:
-                logger.debug("Tool  won't be deleted because it is shared with other users.")
+                logger.debug("Tool {} won't be deleted because it is shared with other users.".format(tool_id))
+                notifier.add(Event(tool_id, tool.get('image'), TOOL_TYPE, tool.get('owner'), 'CANNOT DELETE',
+                                   "Tool {} won't be deleted because it is shared with other users.".format(tool_id)))
                 continue
             logger.debug('Checking links')
             if _is_parent(tool_id, tools):
                 logger.debug("Tool {} won't be deleted because it has linked tool(s).".format(tool_id))
+                notifier.add(Event(tool_id, tool.get('image'), TOOL_TYPE, tool.get('owner'), 'CANNOT DELETE',
+                                   "Tool {} won't be deleted because it has linked tool(s).".format(tool_id)))
                 continue
             logger.debug('Removing tool {}'.format(tool_id))
             if not dry_run:
                 api.delete_tool(tool.get('image'))
+                notifier.add(Event(tool_id, tool.get('image'), TOOL_TYPE, tool.get('owner'), DELETED, ''))
+            else:
+                notifier.add(Event(Event(tool_id, tool.get('image'), TOOL_TYPE, tool.get('owner'),
+                                   'TO BE DELETED', 'Skipped due to dry run')))
             logger.debug('Tool {} deleted.'.format(tool_id))
         except KeyboardInterrupt:
             logger.warning('Interrupted.')
             raise
-        except Exception:
+        except Exception as e:
             logger.warning('Tool cleanup has failed.')
+            notifier.add(Event(tool.get('id'), tool.get('image'), TOOL_TYPE, tool.get('owner'), 'ERROR',
+                               'Failed to process tool: ' + e.message))
     logger.info('Finishing tools cleanup...')
 
 
-def _cleanup_storages(api, logger, users, blocked_users, dry_run):
+def _cleanup_storages(api, logger, users, blocked_users, dry_run, notifier):
     default_exp_days = os.getenv('CP_DATASTORAGE_DEFAULT_EXP_DAYS')
     general_exp_days = os.getenv('CP_DATASTORAGE_GENERAL_EXP_DAYS')
     delete_content = _extract_boolean_parameter('CP_DELETE_STORAGE_CONTENT', 'true')
@@ -231,9 +376,36 @@ def _cleanup_storages(api, logger, users, blocked_users, dry_run):
             storage_id = storage.get('id')
             logger.debug('Processing storage id {}, path {}'.format(storage_id, storage.get('path')))
             logger.debug('Checking permissions for storage {}'.format(storage_id))
+            default_storage_users = _get_default_storage_users(users, storage_id)
+            owner = [user for user in users if user.get('userName') == storage.get('owner')].pop(0)
+
+            if len(default_storage_users) > 0:
+                logger.debug('Storage {} is a default home storage for user(s) {}'
+                             .format(storage_id, ','.join([user.get('userName') for user in default_storage_users])))
+                if default_exp_days is None:
+                    logger.debug(
+                        'CP_DATASTORAGE_DEFAULT_EXP_DAYS not defined. Storage {} cleanup skipped...'.format(storage_id))
+                    continue
+                if not _clean_up(owner, int(default_exp_days)):
+                    logger.debug(
+                        'Storage cleanup timeout doesn\'t exceed yet. Storage {} cleanup skipped...'.format(storage_id))
+                    continue
+            else:
+                logger.debug('Storage {} is owned by {}'.format(storage_id, storage.get('owner')))
+                if general_exp_days is None:
+                    logger.debug(
+                        'CP_DATASTORAGE_GENERAL_EXP_DAYS not defined. Storage {} cleanup skipped...'.format(storage_id))
+                    continue
+                if not _clean_up(owner, int(general_exp_days)):
+                    logger.debug(
+                        'Storage cleanup timeout doesn\'t exceed yet. Storage {} cleanup skipped...'.format(storage_id))
+                    continue
+
             permissions = api.get_permissions(storage_id, 'DATA_STORAGE')
             if permissions is not None:
                 logger.debug("Storage {} won't be deleted because it is shared with other users.".format(storage_id))
+                notifier.add(Event(storage_id, storage.get('name'), STORAGE_TYPE, storage.get('owner'), 'CANNOT DELETE',
+                                   "Storage {} won't be deleted because it is shared with other users.".format(storage_id)))
                 continue
             logger.debug('Storage {} is not shared with any users'.format(storage_id))
             logger.debug('Checking child storages for storage {}'.format(storage_id))
@@ -241,23 +413,23 @@ def _cleanup_storages(api, logger, users, blocked_users, dry_run):
             if child_storages:
                 logger.debug("Storage {} won't be deleted because it has child storage(s): {}"
                              .format(storage_id, ', '.join([s.get('path') for s in child_storages])))
+                notifier.add(Event(storage_id, storage.get('name'), STORAGE_TYPE, storage.get('owner'), 'CANNOT DELETE',
+                                   "Storage {} won't be deleted because it has child storage(s): {}"
+                                   .format(storage_id, ', '.join([s.get('path') for s in child_storages]))
+                                   ))
                 continue
             logger.debug('Storage {} doesn\'t have any child storages'.format(storage_id))
-            owner = [user for user in users if user.get('userName') == storage.get('owner')].pop(0)
-            default_storage_users = _get_default_storage_users(users, storage_id)
             if len(default_storage_users) > 0:
-                logger.debug('Storage {} is a default home storage for user(s) {}'
-                             .format(storage_id, ','.join([user.get('userName') for user in default_storage_users])))
-                _cleanup_default_datastorage(api, logger, storage, owner, default_storage_users, dry_run,
-                                             default_exp_days, delete_content)
+                _cleanup_default_datastorage(api, logger, storage, default_storage_users, dry_run, delete_content, notifier)
             else:
-                logger.debug('Storage {} is owned by {}'.format(storage_id, storage.get('owner')))
-                _cleanup_general_datastorage(api, logger, storage, owner, dry_run, general_exp_days, delete_content)
+                _cleanup_general_datastorage(api, logger, storage, dry_run, delete_content, notifier)
         except KeyboardInterrupt:
             logger.warning('Interrupted.')
             raise
         except Exception as e:
             logger.error('Data storage cleanup has failed: {}'.format(e.message))
+            notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'), 'ERROR',
+                               'Failed to process storage: ' + e.message))
     logger.info('Finishing data storages cleanup...')
 
 
@@ -323,14 +495,8 @@ def _replace_datastorage_content(logger, storage, mount_point, destination):
     return True
 
 
-def _cleanup_default_datastorage(api, logger, storage, user, default_storage_users, dry_run, exp_days, delete_content):
+def _cleanup_default_datastorage(api, logger, storage, default_storage_users, dry_run, delete_content, notifier):
     storage_id = storage.get('id')
-    if exp_days is None:
-        logger.debug('CP_DATASTORAGE_DEFAULT_EXP_DAYS not defined. Storage {} cleanup skipped...'.format(storage_id))
-        return
-    if not _clean_up(user, int(exp_days)):
-        logger.debug('Storage cleanup timeout doesn\'t exceed yet. Storage {} cleanup skipped...'.format(storage_id))
-        return
     logger.debug('Storage {} is eligible for deletion. Checking storage content'.format(storage_id))
     storage_items = api.load_datastorage_items(storage_id)
     if storage_items is not None:
@@ -347,6 +513,8 @@ def _cleanup_default_datastorage(api, logger, storage, user, default_storage_use
             result = _replace_datastorage_content(logger, storage.get('path'), source, destination)
             if not result:
                 logger.warning("Data backup failed, storage {} can't be deleted.".format(storage_id))
+                notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'), 'ERROR',
+                                   "Data backup failed"))
                 return
     logger.debug('Cleaning user default storage attribute {} for users {} '.format(storage_id,
                                                                                    ','.join([user.get('userName') for user in default_storage_users])))
@@ -356,20 +524,22 @@ def _cleanup_default_datastorage(api, logger, storage, user, default_storage_use
     logger.debug('Deleting storage {}'.format(storage_id))
     if not dry_run:
         api.delete_datastorage(storage_id, delete_content)
+        notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'), DELETED, ''))
+    else:
+        notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'),
+                           'TO BE DELETED', 'Skipped due to dry run'))
     logger.debug('Datastorage {} deleted successfully'.format(storage_id))
 
 
-def _cleanup_general_datastorage(api, logger, storage, user, dry_run, exp_days, delete_content):
+def _cleanup_general_datastorage(api, logger, storage, dry_run, delete_content, notifier):
     storage_id = storage.get('id')
-    if exp_days is None:
-        logger.debug('CP_DATASTORAGE_GENERAL_EXP_DAYS not defined. Storage {} cleanup skipped...'.format(storage_id))
-        return
-    if not _clean_up(user, int(exp_days)):
-        logger.debug('Storage cleanup timeout doesn\'t exceed yet. Storage {} cleanup skipped...'.format(storage_id))
-        return
     logger.debug('Deleting storage {}'.format(storage_id))
     if not dry_run:
         api.delete_datastorage(storage.get('id'), delete_content)
+        notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'), DELETED, ''))
+    else:
+        notifier.add(Event(storage.get('id'), storage.get('name'), STORAGE_TYPE, storage.get('owner'),
+                           'TO BE DELETED', 'Skipped due to dry run'))
     logger.debug('Datastorage {} deleted.'.format(storage_id))
 
 
