8de9fe55f2425b3eeca2fade2a12a48913f6c6c3
mzueva <mariia_zueva@epam.com>
2023-02-17 18:27:10 +0100

    Allow storage update/delete by owner and admin

--- api/src/main/java/com/epam/pipeline/acl/datastorage/DataStorageApiService.java
+++ api/src/main/java/com/epam/pipeline/acl/datastorage/DataStorageApiService.java
@@ -230,7 +230,7 @@ public class DataStorageApiService {
         return dataStorageManager.create(dataStorageVO, proceedOnCloud, true, true, skipPolicy);
     }
 
-    @PreAuthorize("hasRole('ADMIN') OR @storagePermissionManager.storagePermissionById(#dataStorageVO.id, 'WRITE')")
+    @PreAuthorize("hasRole('ADMIN') OR @storagePermissionManager.storagePermissionById(#dataStorageVO.id, 'OWNER')")
     @AclMask
     public AbstractDataStorage update(DataStorageVO dataStorageVO) {
         return dataStorageManager.update(dataStorageVO);
@@ -242,7 +242,7 @@ public class DataStorageApiService {
     }
 
     @PreAuthorize("hasRole('ADMIN') OR (hasRole('STORAGE_MANAGER') AND "
-            + "@storagePermissionManager.storagePermissionById(#id, 'WRITE'))")
+            + "@storagePermissionManager.storagePermissionById(#id, 'OWNER'))")
     public AbstractDataStorage delete(Long id, boolean proceedOnCloud) {
         return dataStorageManager.delete(id, proceedOnCloud);
     }
