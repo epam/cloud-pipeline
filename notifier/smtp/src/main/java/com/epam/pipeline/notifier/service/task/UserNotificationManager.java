1a368974dfdb10314dea2356ab878f67ba81d0d0
Oksana_Kolesnikova <oksana_kolesnikova@epam.com>
2023-02-22 16:45:46 +0300

    Issue 3015 Show notifications in GUI - fix multiple user notifications

--- notifier/smtp/src/main/java/com/epam/pipeline/notifier/service/task/UserNotificationManager.java
+++ notifier/smtp/src/main/java/com/epam/pipeline/notifier/service/task/UserNotificationManager.java
@@ -23,13 +23,16 @@ import com.epam.pipeline.notifier.repository.UserNotificationRepository;
 import com.epam.pipeline.notifier.service.TemplateService;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.collections4.SetUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -52,11 +55,13 @@ public class UserNotificationManager implements NotificationManager {
 
     private List<UserNotification> toUserNotifications(final NotificationMessage message) {
         final MessageText messageText = templateService.buildMessageText(message);
+        final Set<Long> userIds = new HashSet<>();
+        if (message.getToUserId() != null) {
+            userIds.add(message.getToUserId());
+        }
+        userIds.addAll(ListUtils.emptyIfNull(message.getCopyUserIds()));
         final List<UserNotification> results = new ArrayList<>();
-        final UserNotification userNotification = buildNotification(message.getToUserId(), messageText);
-        results.add(userNotification);
-        ListUtils.emptyIfNull(message.getCopyUserIds())
-                .forEach(userId -> results.add(buildNotification(userId, messageText)));
+        SetUtils.emptyIfNull(userIds).forEach(userId -> results.add(buildNotification(userId, messageText)));
         return results;
     }
 
