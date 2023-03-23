d11aace95d8a7a06399da3a46c7899f4d1b9c33f
AlfiyaRF <47081183+AlfiyaRF@users.noreply.github.com>
2023-03-23 10:17:56 +0100

    Run jobs in other regions in case of insufficient capacity (#3122) - run log form: restarted runs info, 'run shift policy' setting for cloud region

--- client/src/components/runs/logs/Log.js
+++ client/src/components/runs/logs/Log.js
@@ -96,6 +96,7 @@ import RunTimelineInfo from './misc/run-timeline-info';
 import evaluateRunPrice from '../../../utils/evaluate-run-price';
 import DataStorageLink from '../../special/data-storage-link';
 import fetchRunInfo from './misc/fetch-run-info';
+import RestartedRunsInfo from './misc/restarted-runs-info';
 
 const FIRE_CLOUD_ENVIRONMENT = 'FIRECLOUD';
 const DTS_ENVIRONMENT = 'DTS';
@@ -1616,9 +1617,15 @@ class Logs extends localization.LocalizedReactComponent {
       const {runId} = this.props.params;
 
       const resumeFailureReason = getResumeFailureReason(run);
-      ResumeFailureReason = resumeFailureReason
-        ? (<Alert type="warning" message={resumeFailureReason} />)
-        : null;
+      if (resumeFailureReason) {
+        ResumeFailureReason = (
+          <Alert
+            type="warning"
+            style={{margin: '5px 0'}}
+            message={resumeFailureReason}
+          />
+        );
+      }
       let pipelineLink;
       if (pipeline) {
         if (pipeline.id) {
@@ -2115,15 +2122,17 @@ class Logs extends localization.LocalizedReactComponent {
             </Row>
             {
               stateReasonMessage && (
-                <Row type="flex">
-                  <Alert
-                    message={`Server failure reason: ${stateReasonMessage}`}
-                    type="error"
-                  />
-                </Row>
+                <Alert
+                  message={`Server failure reason: ${stateReasonMessage}`}
+                  type="error"
+                />
               )
             }
             {ResumeFailureReason}
+            <RestartedRunsInfo
+              style={{margin: '5px 0'}}
+              run={run}
+            />
             <Row>
               {Details}
             </Row>
