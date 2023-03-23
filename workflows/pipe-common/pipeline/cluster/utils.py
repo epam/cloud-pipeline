5ad6c63b216014d0b2876aa870d9ad0932ef0b89
Ekaterina_Kazachkova <ekaterina_kazachkova@epam.com>
2023-03-23 15:59:24 +0300

    CommonRepo: cluster error handling

--- workflows/pipe-common/pipeline/cluster/utils.py
+++ workflows/pipe-common/pipeline/cluster/utils.py
@@ -16,10 +16,9 @@ import os
 import subprocess
 
 
-def get_log_filename(work_directory, job_name, lock, job_identifier):
+def get_log_filename(work_directory, job_name, lock):
     logs_directory = create_directory(work_directory, "logs", lock=lock)
-    return os.path.join(logs_directory, "{job_name}_{number}_out.log"
-                        .format(number=job_identifier, job_name=job_name if job_name else job_identifier))
+    return os.path.join(logs_directory, "{job_name}_out.log".format(job_name=job_name))
 
 
 def merge_log(common_logfile, job_logfile, lock):
