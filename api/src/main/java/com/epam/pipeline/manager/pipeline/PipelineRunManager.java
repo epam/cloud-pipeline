f9fbee152b51454d11711d633a4af6577e1c4576
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2023-03-28 14:14:55 +0300

    Issue #3122: Run jobs in other regions in case of insufficient capacity - fixes (#3157)

--- api/src/main/java/com/epam/pipeline/manager/pipeline/PipelineRunManager.java
+++ api/src/main/java/com/epam/pipeline/manager/pipeline/PipelineRunManager.java
@@ -1007,7 +1007,7 @@ public class PipelineRunManager {
         restartRun.setRestartedRunId(restartedRun.getId());
         restartRun.setDate(DateUtils.now());
         restartRunManager.createRestartRun(restartRun);
-        return run;
+        return restartedRun;
     }
 
     /**
