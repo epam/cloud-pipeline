805d9bf44774d20cbe8e3775e5dc2f481a7ff9d1
maryvictol <Mariia_Tolstaia@epam.com>
2023-03-28 20:35:28 +0300

    Update runs table locators

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/RunsMenuAO.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/RunsMenuAO.java
@@ -336,8 +336,8 @@ public class RunsMenuAO implements AccessObject<RunsMenuAO> {
 
     public RunsMenuAO viewAvailableActiveRuns() {
         $(withText("Currently viewing")).waitUntil(visible, C.DEFAULT_TIMEOUT);
-        if ($(elementWithText(tagName("a"), "View other available active runs")).isDisplayed()) {
-            $(elementWithText(tagName("a"), "View other available active runs")).shouldBe(visible).click();
+        if ($(elementWithText(tagName("b"), "other available ")).isDisplayed()) {
+            $(elementWithText(tagName("b"), "other available ")).shouldBe(visible).click();
             sleep(2, SECONDS);
         }
         return this;
