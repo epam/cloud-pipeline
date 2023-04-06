adb7376a16ded558e501a05666d37cef4d29a4d8
Mariia Tolstaia <47791878+maryvictol@users.noreply.github.com>
2023-04-06 14:48:21 +0300

    Check pipe version test (#2876)
    * Check pipe version test

---
+++ docs/testcases/UI/Test_Environment/TC-TEST-ENVIRONMENT-3.md
@@ -0,0 +1,36 @@
+# Check update pipe version in the existing run
+
+Test verifies that in the existing active run launched before the application update 
+- the old version of pipe CLI continues to work correctly after updating the application
+- the new version of pipe CLI can be installed and works correctly
+
+**Prerequisites**:
+- Admin user
+
+**Preparations:**
+
+Using API methods:
+1. Create test tool 
+2. Specify an endpoint for the service launched in a Tool
+3. Launch test tool created at step 1
+4. Update application
+
+| Steps | Actions | Expected results |
+| :---: | --- | --- |
+| 1 | Login as the admin user from the prerequisites | |
+| 2 | Open the **Settings** page | |
+| 3 | Click the **CLI** tab | |
+| 4 | Click the **Pipe CLI** tab | |
+| 5 | Select **`Linux-Binary`** from drop-down for ***Operation system:*** | |
+| 6 | Store *Pipe CLI Installation Content* | |
+| 7 | Click **Generate access key** button | |
+| 8 | Store *CLI configure command* | |
+| 9 | Open the **Runs** page | |
+| 10 | Click the run launched at step 3 of the Preparations | |
+| 11 | Click the **SSH** hyperlink | |
+| 12 | In the opened tab, enter and perform the command: `pipe --version` | |
+| 13 | Enter and perform the command: `pipe ssh <runID>`, where `<runID>` is ID of run launched at step 3 of the Preparations | Interactive session over the SSH protocol is started |
+| 14 | Enter and perform the commands from *Pipe CLI Installation Content* stored at step 6 | |
+| 15 | Enter and perform the *CLI configure command* stored at step 8 | |
+| 16 | Enter and perform the command: `<pipe_new_version>/pipe --version`, where `<pipe_new_version>` is the path of the new version pipe CLI installed at step 14 | Version differs from the version in output of step 12 |
+| 17 | Enter and perform the command: `<pipe_new_version>/pipe ssh <runID>` | Interactive session over the SSH protocol is started |
---
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/EnvironmentTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017-2023 EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.pipeline.autotests;
+
+import com.epam.pipeline.autotests.ao.settings.CliAO;
+import com.epam.pipeline.autotests.mixins.Navigation;
+import com.epam.pipeline.autotests.utils.TestCase;
+import org.testng.annotations.Test;
+
+import static com.epam.pipeline.autotests.utils.C.AUTH_TOKEN;
+import static com.epam.pipeline.autotests.utils.C.TEST_RUN_TAG;
+import static java.lang.String.format;
+import static org.testng.Assert.assertNotEquals;
+
+/**
+ * Test should be performed after the application update.
+ * Prerequisites should be created before the application update.
+ * To create Prerequisites:
+ * 1. Launch tool (for ex. centos) and ssh to the created run
+ * 2. Create 2 scripts: pipe_version_update_prerequisites.sh and pipe_version_update_utils.sh
+ * (scripts text can be found in the resources)
+ * 3. Check that 'RUN_TAG' in the pipe_version_update_prerequisites.sh has the same value as 'e2e.ui.test.run.tag' parameter in the config file
+ * 4. Launch script: bash pipe_version_update_prerequisites.sh CP_REGISTRY_NAME API API_TOKEN HTTP_PROXY
+ * 5. Update application
+ */
+public class EnvironmentTest  extends AbstractBfxPipelineTest implements Navigation {
+
+    private final String COMMAND_VERSION = "%s --version";
+    private final String COMMAND_SSH = "%s ssh %s";
+    private final String rootHost = "root@pipeline";
+    private final String initPipePath = "pipe";
+    private String oldPipeVersion;
+    private String newPipeVersion;
+
+    @Test
+    @TestCase(value = "TC-TEST-ENVIRONMENT-3")
+    public void checkUpdatePipeVersionInExistingRun() {
+        if ("false".equals(AUTH_TOKEN)) {
+            return;
+        }
+        final CliAO cliAO = navigationMenu()
+                .settings()
+                .switchToCLI()
+                .switchPipeCLI()
+                .selectOperationSystem(CliAO.OperationSystem.LINUX_BINARY);
+        final String operationSystemInstallationContent = cliAO.getOperationSystemInstallationContent();
+        final String pipePath = cliAO.getPipePathFromConfigureCommand();
+        final String runID = runsMenu()
+                .activeRuns()
+                .viewAvailableActiveRuns()
+                .getRunIdByTag(TEST_RUN_TAG);
+        runsMenu()
+                .activeRuns()
+                .viewAvailableActiveRuns()
+                .showLog(runID)
+                .waitForSshLink()
+                .ssh(shell -> {
+                    final String sshFirstLine = shell.waitUntilTextAppears(runID).getFirstLine();
+                    oldPipeVersion = shell
+                            .waitUntilTextAppears(runID)
+                            .execute(format(COMMAND_VERSION, initPipePath))
+                            .assertNextStringIsVisible(format(COMMAND_VERSION, initPipePath), rootHost)
+                            .lastCommandResult(format(COMMAND_VERSION, initPipePath));
+                    shell.execute(format(COMMAND_SSH, initPipePath, runID))
+                            .assertNextStringIsVisible(format(COMMAND_SSH, initPipePath, runID), rootHost)
+                            .assertPageAfterCommandContainsStrings(sshFirstLine);
+                    newPipeVersion = shell.execute(operationSystemInstallationContent)
+                            .execute(format(COMMAND_VERSION, pipePath))
+                            .assertNextStringIsVisible(format(COMMAND_VERSION, pipePath), rootHost)
+                            .lastCommandResult(format(COMMAND_VERSION, pipePath));
+                    checkPipeVersion(oldPipeVersion, newPipeVersion);
+                    shell.execute(format(COMMAND_SSH, pipePath, runID))
+                            .assertNextStringIsVisible(format(COMMAND_SSH, pipePath, runID), rootHost)
+                            .assertPageAfterCommandContainsStrings(sshFirstLine)
+                            .close();
+                });
+    }
+
+    private void checkPipeVersion(final String old_version, final String new_version) {
+        assertNotEquals(new_version.substring(0, new_version.indexOf("Access")),
+                old_version.substring(0, old_version.indexOf("Access")));
+    }
+}
