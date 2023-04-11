f13c303aab6e6382870f86415eb930a83ca819ca
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2023-04-11 11:40:39 +0200

    Add storage id to storage audit logs (#3187), Data Sharing Service GUI

--- data-sharing-service/client/src/models/s3Storage/s3Storage.js
+++ data-sharing-service/client/src/models/s3Storage/s3Storage.js
@@ -202,7 +202,8 @@ class S3Storage {
       UploadId: uploadId
     };
     auditStorageAccessManager.reportWriteAccess({
-      fullPath: `s3://${this._storage.path}/${this.prefix + name}`
+      fullPath: `s3://${this._storage.path}/${this.prefix + name}`,
+      storageId: this._storage.id
     });
     const upload = this._s3.uploadPart(params);
     upload.on('httpUploadProgress', uploadProgress);
