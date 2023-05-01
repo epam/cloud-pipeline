751078778de8063ea75e102dc80d787c2731d729
maryvictol <Mariia_Tolstaia@epam.com>
2023-05-02 00:11:22 +0300

    Update AuditTest

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/AuditTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/AuditTest.java
@@ -309,6 +309,7 @@ public class AuditTest extends AbstractSeveralPipelineRunningTest
     }
 
     private void executeCommands(String [] commands) {
+        final int[] i = {1};
         runsMenu()
                 .showLog(getLastRunId())
                 .waitForSshLink()
@@ -317,7 +318,9 @@ public class AuditTest extends AbstractSeveralPipelineRunningTest
                     for (String comm : commands) {
                         System.out.println(comm);
                         shell.execute(comm)
+                                .screenshot("comm" + i[0])
                                 .waitUntilTextAfterCommandAppears(comm, getLastRunId());
+                        i[0]++;
                     }
                     shell.close();
                 });
