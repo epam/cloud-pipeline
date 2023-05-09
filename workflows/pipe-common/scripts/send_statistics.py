7f2f8dfcd3d8f10750885bee5f312ed47306f91b
Oksana_Kolesnikova <oksana_kolesnikova@epam.com>
2023-05-09 12:51:51 +0300

    Issue 3161 Send platform usage statistics to users - add templates

--- workflows/pipe-common/scripts/send_statistics.py
+++ workflows/pipe-common/scripts/send_statistics.py
@@ -20,7 +20,7 @@ import itertools
 import argparse
 from datetime import datetime
 import json
-
+from enum import Enum
 from pipeline.api import PipelineAPI
 from pipeline.log.logger import LocalLogger, RunLogger, TaskLogger, LevelLogger
 
@@ -29,6 +29,16 @@ DATE_FORMAT = "%Y-%m-%d"
 DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 CAPABILITY_TEMPLATE = "CP_CAP_CUSTOM_{}=true=boolean"
 
+STATISTICS_TEMPLATE = "statistics-template.html"
+TABLE_TEMPLATE = "group-3-table.html"
+TABLE_CENTER_TEMPLATE = "group-3-table-center.html"
+
+TYPE = 'Type'
+TYPE_GPU = 'GPU'
+TYPE_CPU = 'CPU'
+COST = 'Cost ($)'
+DURATION_HOURS = 'Duration (hours)'
+ROUND = 4
 
 EMAIL_TEMPLATE = '''
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -64,10 +74,18 @@ EMAIL_TEMPLATE = '''
 EMAIL_SUBJECT = '[%s]: Platform usage statistics'
 
 
+class Top3(Enum):
+    INSTANCES = ("Top-3 instances", "instance")
+    PIPELINES = ("Top-3 pipelines", "pipeline")
+    TOOLS = ("Top-3 tools", "tool")
+    BUCKETS = ("Top-3 used buckets", "bucket")
+    CAPABILITIES = ("Top-3 run capabilities", "capability")
+
+
 class Stat(object):
 
     def __init__(self, compute_hours, runs_count, storage_read, storage_write, usage_weight, login_time, cpu_hours,
-                 gpu_hours, clusters_compute_hours, worker_nodes_count,
+                 gpu_hours, clusters_compute_secs, worker_nodes_count,
                  top3_instance_types, top3_pipelines, top3_tools, top3_used_buckets, top3_run_capabilities):
         self.compute_hours = compute_hours
         self.runs_count = runs_count
@@ -77,7 +95,7 @@ class Stat(object):
         self.login_time = login_time
         self.cpu_hours = cpu_hours
         self.gpu_hours = gpu_hours
-        self.clusters_compute_hours = clusters_compute_hours
+        self.clusters_compute_secs = clusters_compute_secs
         self.worker_nodes_count = worker_nodes_count
         self.top3_instance_types = top3_instance_types
         self.top3_pipelines = top3_pipelines
@@ -86,32 +104,85 @@ class Stat(object):
         self.top3_run_capabilities = top3_run_capabilities
 
     @staticmethod
-    def format_top3(top):
-        top3_str = ''
-        for key, value in top:
-            top3_str += '<li>{}: {}</li>'.format(key, value)
-        return '<ul>{}</ul>'.format(top3_str)
+    def format_count(count):
+        return "<b>{}</b>".format(count)
 
-    def get_object_str(self, template):
-        return template.format(**{'compute_hours': self.compute_hours,
-                                  'runs_count': self.runs_count,
-                                  'storage_read': self.storage_read,
-                                  'storage_write': self.storage_write,
-                                  'usage_weight': self.usage_weight,
-                                  'login_time': self.login_time,
-                                  'cpu_hours': self.cpu_hours,
-                                  'gpu_hours': self.gpu_hours,
-                                  'clusters_compute_hours': self.clusters_compute_hours,
-                                  'worker_nodes_count': self.worker_nodes_count,
-                                  'top3_instance_types': self.format_top3(self.top3_instance_types),
-                                  'top3_pipelines': self.format_top3(self.top3_pipelines),
-                                  'top3_tools': self.format_top3(self.top3_tools),
-                                  'top3_used_buckets': self.format_top3(self.top3_used_buckets),
-                                  'top3_run_capabilities': self.format_top3(self.top3_run_capabilities)})
+    @staticmethod
+    def format_weight(weight):
+        return "<b>{}</b><span>%</span>".format(round(weight, ROUND))
+
+    @staticmethod
+    def format_hours(float_hours):
+        return Stat.format_seconds(int(float_hours * 3600))
+
+    @staticmethod
+    def format_seconds(seconds):
+        hours, seconds = divmod(seconds, 3600)
+        result = "<b>{}</b><span>&nbsp;h</span>".format(int(hours))
+        minutes, seconds = divmod(seconds, 60)
+        if minutes > 0:
+            return "{}&nbsp;<b>{}</b><span>&nbsp;m</span>".format(result, int(minutes))
+        return result
+
+    @staticmethod
+    def format_items(top):
+        items = ''
+        for key, value in top:
+            items += '<tr><td class="group3-left">{}</td><td class="group3-right">{}</td></tr>'.format(key, value)
+        return items
+
+    @staticmethod
+    def format_tables(tables, table_center_templ, table_templ):
+        table_html = ''
+        row_templ = '<tr>{}</tr>'
+        while len(tables) > 0:
+            if len(tables) == 1:
+                column_html = Stat.format_column(table_center_templ, tables)
+                table_html += row_templ.format(column_html)
+            else:
+                column_html = Stat.format_column(table_templ, tables)
+                column_html += Stat.format_column(table_templ, tables)
+                table_html += row_templ.format(column_html)
+        return table_html
+
+    @staticmethod
+    def format_column(table_center_templ, tables):
+        column = tables.popitem()
+        return table_center_templ.format(**{"TABLE_TITLE": column[0][0],
+                                            "TABLE_ICON": column[0][1],
+                                            "ITEMS": column[1]})
+
+    def get_tables(self):
+        tables = dict()
+        if len(self.top3_instance_types) > 0:
+            tables[Top3.INSTANCES.value] = Stat.format_items(self.top3_instance_types)
+        if len(self.top3_pipelines) > 0:
+            tables[Top3.PIPELINES.value] = Stat.format_items(self.top3_pipelines)
+        if len(self.top3_tools) > 0:
+            tables[Top3.TOOLS.value] = Stat.format_items(self.top3_tools)
+        if len(self.top3_used_buckets) > 0:
+            tables[Top3.BUCKETS.value] = Stat.format_items(self.top3_used_buckets)
+        if len(self.top3_run_capabilities) > 0:
+            tables[Top3.CAPABILITIES.value] = Stat.format_items(self.top3_run_capabilities)
+        return tables
+
+    def get_object_str(self, start, end, deploy_name, template, table_templ, table_center_templ):
+        return template.format(**{'PLATFORM_NAME': deploy_name,
+                                  'PERIOD': "{} - {}".format(start, end),
+                                  'COMPUTE_HOURS': Stat.format_hours(self.compute_hours),
+                                  'RUNS_COUNT': Stat.format_count(self.runs_count),
+                                  'LOGIN_TIME': Stat.format_hours(self.login_time),
+                                  'USAGE_WEIGHT': Stat.format_weight(self.usage_weight),
+                                  'READ_REQUESTS': self.storage_read,
+                                  'WRITE_REQUESTS': self.storage_write,
+                                  'CPU': Stat.format_hours(self.cpu_hours),
+                                  'GPU': Stat.format_hours(self.gpu_hours),
+                                  'COMPUTE': Stat.format_seconds(self.clusters_compute_secs),
+                                  'WORKER_NODES': self.worker_nodes_count,
+                                  'TABLE': Stat.format_tables(self.get_tables(), table_center_templ, table_templ)})
 
 
 class Notifier(object):
-
     def __init__(self, api, start, end, stat, deploy_name, notify_users):
         self.api = api
         self.start = start
@@ -120,17 +191,17 @@ class Notifier(object):
         self.deploy_name = deploy_name
         self.notify_users = notify_users
 
-    def send_notifications(self, template):
+    def send_notifications(self, template, table_templ, table_center_templ):
         if not self.notify_users:
             return
         self.api.create_notification(EMAIL_SUBJECT % self.deploy_name,
-                                     self.build_text(template),
+                                     self.build_text(template, table_templ, table_center_templ),
                                      self.notify_users[0],
-                                     copy_users=self.notify_users[1:] if len(self.notify_users) > 0 else None
-                                     )
+                                     copy_users=self.notify_users[1:] if len(self.notify_users) > 0 else None)
 
-    def build_text(self, template):
-        stat_str = self.stat.get_object_str(template)
+    def build_text(self, template, table_templ, table_center_templ):
+        stat_str = self.stat.get_object_str(self.start, self.end, self.deploy_name,
+                                            template, table_templ, table_center_templ)
         return EMAIL_TEMPLATE.format(**{'text': stat_str,
                                         'from': self.start,
                                         'to': self.end,
@@ -201,9 +272,7 @@ def send_statistics():
         if len(users) > 0:
             logger.info('{} User(s) collected({}).'.format(len(users), ','.join(users)))
             logger.info('Collecting and sending statistics...')
-            with open(template_path, 'r') as file:
-                template = file.read()
-                _send_users_stat(api, logger, from_date, to_date, users, template)
+            _send_users_stat(api, logger, from_date, to_date, users, template_path)
         else:
             logger.info('No users found to collect and send statistics.')
     except KeyboardInterrupt:
@@ -216,20 +285,21 @@ def send_statistics():
         raise
 
 
-def _send_users_stat(api, logger, from_date, to_date, users, template):
-
+def _send_users_stat(api, logger, from_date, to_date, users, template_path):
     capabilities = _get_capabilities(api)
     storages = api.data_storage_load_all()
 
     platform_usage_costs = _get_platform_usage_cost(api, from_date, to_date)
     logger.info('Platform Usage costs: {}.'.format(platform_usage_costs))
 
+    template, table_templ, table_center_templ = _get_templates(template_path)
+
     for user in users:
         logger.info('User: {}'.format(user))
         stat = _get_statistics(api, capabilities, logger, platform_usage_costs, from_date, to_date,
                                storages, user)
         notifier = Notifier(api, from_date, to_date, stat, os.getenv('CP_DEPLOY_NAME', 'Cloud Pipeline'), [user])
-        notifier.send_notifications(template)
+        notifier.send_notifications(template, table_templ, table_center_templ)
 
 
 def _get_statistics(api, capabilities, logger, platform_usage_costs, from_date, to_date, storages, user):
@@ -242,9 +312,9 @@ def _get_statistics(api, capabilities, logger, platform_usage_costs, from_date, 
     logger.info('Storage read requests count: {}.'.format(storage_read))
     compute_hours = _get_compute_hours(runs)
     logger.info('Compute hours: {}.'.format(compute_hours))
-    cpu_hours = _get_cpu_compute_hours(runs)
+    cpu_hours = _get_compute_hours_by_type(runs, TYPE_CPU)
     logger.info('CPU Compute hours: {}.'.format(cpu_hours))
-    gpu_hours = _get_gpu_compute_hours(runs)
+    gpu_hours = _get_compute_hours_by_type(runs, TYPE_GPU)
     logger.info('GPU Compute hours: {}.'.format(gpu_hours))
     runs_count = _get_runs_count(runs)
     logger.info('Runs count: {}.'.format(runs_count))
@@ -252,8 +322,8 @@ def _get_statistics(api, capabilities, logger, platform_usage_costs, from_date, 
     logger.info('Usage costs: {}.'.format(usage_costs))
     usage_weight = usage_costs / platform_usage_costs
     logger.info('Usage weight: {}.'.format(usage_weight))
-    clusters_compute_hours = _get_cluster_compute_hours(api, from_date_time, to_date_time, user)
-    logger.info('Clusters compute hours: {}.'.format(clusters_compute_hours))
+    clusters_compute_secs = _get_cluster_compute_secs(api, from_date_time, to_date_time, user)
+    logger.info('Clusters compute seconds: {}.'.format(clusters_compute_secs))
     worker_nodes_count = _get_worker_nodes_count(api, from_date_time, to_date_time, user)
     logger.info('Clusters worker nodes count: {}.'.format(worker_nodes_count))
     login_time = _get_login_time(api, from_date_time, to_date_time, user)
@@ -269,7 +339,7 @@ def _get_statistics(api, capabilities, logger, platform_usage_costs, from_date, 
     top3_run_capabilities = _get_top3_run_capabilities(api, from_date_time, to_date_time, user, capabilities)
     logger.info('Top 3 Capabilities: {}.'.format(top3_run_capabilities))
     return Stat(compute_hours, runs_count, storage_read, storage_write, usage_weight, login_time, cpu_hours,
-                gpu_hours, clusters_compute_hours, worker_nodes_count,
+                gpu_hours, clusters_compute_secs, worker_nodes_count,
                 top3_instance_types, top3_pipelines, top3_tools, top3_used_buckets, top3_run_capabilities)
 
 
@@ -291,8 +361,11 @@ def _get_runs(api, from_date, to_date, user):
 
 
 def _get_storage_usage(api, from_date_time, to_date_time, user, usage):
-    filter = {'messageTimestampFrom': from_date_time, 'messageTimestampTo': to_date_time, 'types': ['audit'],
-              'users': [user], 'message': usage}
+    filter = {'messageTimestampFrom': from_date_time,
+              'messageTimestampTo': to_date_time,
+              'types': ['audit'],
+              'users': [user],
+              'message': usage}
     storage_usage = api.log_group(filter, "user")
     if storage_usage is None or storage_usage.get(user) is None:
         return 0
@@ -309,18 +382,18 @@ def _get_top3_run_capabilities(api, from_date, to_date, user, capabilities):
                 start_date = datetime.strptime(r.get('startDate'), DATE_TIME_FORMAT)
                 end_date = datetime.strptime(r.get('endDate'), DATE_TIME_FORMAT)
                 total_time += (end_date - start_date).total_seconds()
-            capabilities_dict[c] = total_time / 3600
+            capabilities_dict[c] = total_time
     return sorted(capabilities_dict.items(), key=lambda item: float(item[1]), reverse=True)[:3]
 
 
-def _get_cluster_compute_hours(api, from_date, to_date, user):
+def _get_cluster_compute_secs(api, from_date, to_date, user):
     runs = api.filter_runs_all(from_date, to_date, user, {'master': True})
     total_time = 0
     for r in runs:
         start_date = datetime.strptime(r.get('startDate'), DATE_TIME_FORMAT)
         end_date = datetime.strptime(r.get('endDate'), DATE_TIME_FORMAT)
         total_time += (end_date - start_date).total_seconds()
-    return total_time / 3600
+    return total_time
 
 
 def _get_worker_nodes_count(api, from_date, to_date, user):
@@ -342,7 +415,7 @@ def _read_content(content, skip_first_row=True):
 
 
 def _get_usage_costs(runs):
-    return sum([float(r.get('Cost ($)')) for r in runs])
+    return sum([float(r.get(COST)) for r in runs])
 
 
 def _get_runs_count(runs):
@@ -350,17 +423,12 @@ def _get_runs_count(runs):
 
 
 def _get_compute_hours(runs):
-    durations = [float(r.get('Duration (hours)')) for r in runs]
+    durations = [float(r.get(DURATION_HOURS)) for r in runs]
     return sum(durations)
 
 
-def _get_cpu_compute_hours(runs):
-    durations = [float(r.get('Duration (hours)')) for r in runs if r.get('Type') == 'CPU']
-    return sum(durations)
-
-
-def _get_gpu_compute_hours(runs):
-    durations = [float(r.get('Duration (hours)')) for r in runs if r.get('Type') == 'GPU']
+def _get_compute_hours_by_type(runs, type):
+    durations = [float(r.get(DURATION_HOURS)) for r in runs if r.get(TYPE) == type]
     return sum(durations)
 
 
@@ -376,7 +444,9 @@ def _get_top3(runs, entity):
 
 
 def _get_used_buckets(api, from_date_time, to_date_time, user, storages):
-    filter = {'messageTimestampFrom': from_date_time, 'messageTimestampTo': to_date_time, 'types': ['audit'],
+    filter = {'messageTimestampFrom': from_date_time,
+              'messageTimestampTo': to_date_time,
+              'types': ['audit'],
               'users': [user]}
     logs_by_storage = api.log_group(filter, "storageId")
     top3_storages = list()
@@ -387,6 +457,16 @@ def _get_used_buckets(api, from_date_time, to_date_time, user, storages):
             if len(storage_name) > 0:
                 top3_storages.append((storage_name, value))
     return top3_storages
+
+
+def _get_templates(templ_path):
+    with open(os.path.join(templ_path, STATISTICS_TEMPLATE), 'r') as file:
+        template = file.read()
+    with open(os.path.join(templ_path, TABLE_TEMPLATE), 'r') as file:
+        table_templ = file.read()
+    with open(os.path.join(templ_path, TABLE_CENTER_TEMPLATE), 'r') as file:
+        table_center_templ = file.read()
+    return template, table_templ, table_center_templ
 
 
 if __name__ == '__main__':
