0f8927b20f8c2f459c4975f00d16d6bcb46226ee
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2023-05-22 23:41:11 +0300

    Issue #3235: Return additional information for docker tree (#3241)
    * Issue #3235: Return additional information for docker tree (WIP: tests required)
    
    * Issue #3235: Return additional information for docker tree - tests fixed
    
    * Issue #3235: Return additional information for docker tree - tests

--- api/src/test/java/com/epam/pipeline/dao/tool/ToolVersionDaoTest.java
+++ api/src/test/java/com/epam/pipeline/dao/tool/ToolVersionDaoTest.java
@@ -79,6 +79,7 @@ public class ToolVersionDaoTest extends AbstractJdbcTest {
     private Tool symlink;
     private ToolVersion toolVersion1;
     private ToolVersion toolVersion2;
+    private ConfigurationEntry configurationEntry;
 
     @Before
     public void setUp() {
@@ -123,7 +124,7 @@ public class ToolVersionDaoTest extends AbstractJdbcTest {
         symlink.setLink(tool.getId());
         toolDao.createTool(symlink);
 
-        ConfigurationEntry configurationEntry = new ConfigurationEntry();
+        configurationEntry = new ConfigurationEntry();
         configurationEntry.setName(ConfigurationEntry.DEFAULT);
         toolVersion1 = ToolVersion
                 .builder()
@@ -310,6 +311,30 @@ public class ToolVersionDaoTest extends AbstractJdbcTest {
         final List<ToolVersion> actualVersions = toolVersionDao.loadToolWithSettings(symlink.getId());
 
         assertVersionsWithSettings(actualVersions, expectedVersions);
+    }
+
+    @Test
+    @Transactional
+    public void testLoadAllToolWithSettings() {
+        final List<ToolVersion> versions = Arrays.asList(toolVersion1, toolVersion2);
+        versions.forEach(toolVersionDao::createToolVersionWithSettings);
+
+        final List<ToolVersion> actualVersions = toolVersionDao.loadAllLatestToolVersions();
+
+        final ToolVersion symlinkVersion = ToolVersion
+                .builder()
+                .id(toolVersion1.getId())
+                .digest(TEST_DIGEST)
+                .size(TEST_SIZE)
+                .version(TEST_VERSION)
+                .modificationDate(TEST_LAST_MODIFIED_DATE)
+                .platform(TEST_PLATFORM)
+                .toolId(symlink.getId())
+                .settings(Collections.singletonList(configurationEntry))
+                .build();
+        final List<ToolVersion> expected = Arrays.asList(toolVersion1, symlinkVersion);
+
+        assertVersionsWithSettings(actualVersions, expected);
     }
 
     private void assertVersions(final List<ToolVersion> actualVersions, final List<ToolVersion> expectedVersions) {
--- api/src/test/java/com/epam/pipeline/test/aspect/AspectTestBeans.java
+++ api/src/test/java/com/epam/pipeline/test/aspect/AspectTestBeans.java
@@ -70,6 +70,7 @@ import com.epam.pipeline.dao.user.RoleDao;
 import com.epam.pipeline.dao.user.UserDao;
 import com.epam.pipeline.manager.billing.BillingManager;
 import com.epam.pipeline.manager.billing.detail.EntityBillingDetailsLoader;
+import com.epam.pipeline.manager.cloud.CloudFacade;
 import com.epam.pipeline.manager.cluster.InstanceOfferScheduler;
 import com.epam.pipeline.manager.cluster.PodMonitor;
 import com.epam.pipeline.manager.contextual.handler.ContextualPreferenceHandler;
@@ -485,4 +486,7 @@ public class AspectTestBeans {
 
     @MockBean
     protected StorageEventCollector events;
+
+    @MockBean
+    protected CloudFacade cloudFacade;
 }
