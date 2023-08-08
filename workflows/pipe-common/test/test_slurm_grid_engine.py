dc41d0fe3b2e7ecdec130853f5a599330a014c0c
Andrei Tcibin <andrei_tcibin@epam.com>
2023-08-08 11:44:52 +0200

    Improve slurm support in grid engine autoscaler (#3331)
    * Use separate log names for sge and slurm
    
    * Specify explicit slurm worker resources
    
    * Consider only running jobs during scale down checks
    
    * Support complex user name parsing in slurm jobs
    
    * Add grid engine specific active host tags
    
    * Launch grid engine autoscaler for static slurm clusters
    
    * Process slurm queue as default and static queue

--- workflows/pipe-common/test/test_slurm_grid_engine.py
+++ workflows/pipe-common/test/test_slurm_grid_engine.py
@@ -38,14 +38,15 @@ def setup_function():
 def test_qstat_parsing():
     stdout = """JobId=8 JobName=wrap1 UserId=root(0) GroupId=root(0) MCS_label=N/A Priority=4294901752 Nice=0 Account=(null) QOS=(null) JobState=RUNNING Reason=None Dependency=(null) Requeue=1 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0 RunTime=00:00:15 TimeLimit=365-00:00:00 TimeMin=N/A SubmitTime=2023-07-31T13:55:30 EligibleTime=2023-07-31T13:55:30 AccrueTime=2023-07-31T13:55:30 StartTime=2023-07-31T13:55:30 EndTime=2024-07-30T13:55:30 Deadline=N/A SuspendTime=None SecsPreSuspend=0 LastSchedEval=2023-07-31T13:55:30 Scheduler=Backfill Partition=main.q AllocNode:Sid=pipeline-47608:6013 ReqNodeList=(null) ExcNodeList=(null) NodeList=pipeline-47608 BatchHost=pipeline-47608 NumNodes=1 NumCPUs=1 NumTasks=1 CPUs/Task=1 ReqB:S:C:T=0:0:*:* TRES=cpu=1,node=1,billing=1 Socks/Node=* NtasksPerN:B:S:C=0:0:*:* CoreSpec=* MinCPUsNode=1 MinMemoryNode=0 MinTmpDiskNode=0 Features=(null) DelayBoot=00:00:00 OverSubscribe=OK Contiguous=0 Licenses=(null) Network=(null) Command=(null) WorkDir=/root StdErr=/root/slurm-8.out StdIn=/dev/null StdOut=/root/slurm-8.out Power=
 JobId=9 JobName=wrap2 UserId=root(0) GroupId=root(0) MCS_label=N/A Priority=4294901752 Nice=0 Account=(null) QOS=(null) JobState=PENDING Reason=None Dependency=(null) Requeue=1 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0 RunTime=00:00:15 TimeLimit=365-00:00:00 TimeMin=N/A SubmitTime=2023-07-31T13:55:35 EligibleTime=2023-07-31T13:55:35 AccrueTime=Unknown StartTime=Unknown EndTime=2024-07-30T13:55:30 Deadline=N/A SuspendTime=None SecsPreSuspend=0 LastSchedEval=2023-07-31T13:55:30 Scheduler=Backfill Partition=main.q AllocNode:Sid=pipeline-47608:6013 ReqNodeList=(null) ExcNodeList=(null) NodeList= BatchHost=pipeline-47608 NumNodes=1 NumCPUs=2 NumTasks=1 CPUs/Task=2 ReqB:S:C:T=0:0:*:* TRES=cpu=2,node=1,billing=1 Socks/Node=* NtasksPerN:B:S:C=0:0:*:* CoreSpec=* MinCPUsNode=1 MinMemoryNode=500M MinTmpDiskNode=0 Features=(null) DelayBoot=00:00:00 OverSubscribe=OK Contiguous=0 Licenses=(null) Network=(null) Command=(null) WorkDir=/root StdErr=/root/slurm-8.out StdIn=/dev/null StdOut=/root/slurm-8.out Power=
+JobId=10 JobName=wrap3 UserId=user-name(123) GroupId=root(0) MCS_label=N/A Priority=4294901752 Nice=0 Account=(null) QOS=(null) JobState=PENDING Reason=None Dependency=(null) Requeue=1 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0 RunTime=00:00:15 TimeLimit=365-00:00:00 TimeMin=N/A SubmitTime=2023-07-31T13:55:35 EligibleTime=2023-07-31T13:55:35 AccrueTime=Unknown StartTime=Unknown EndTime=2024-07-30T13:55:30 Deadline=N/A SuspendTime=None SecsPreSuspend=0 LastSchedEval=2023-07-31T13:55:30 Scheduler=Backfill Partition=main.q AllocNode:Sid=pipeline-47608:6013 ReqNodeList=(null) ExcNodeList=(null) NodeList= BatchHost=pipeline-47608 NumNodes=1 NumCPUs=2 NumTasks=1 CPUs/Task=2 ReqB:S:C:T=0:0:*:* TRES=cpu=2,node=1,billing=1 Socks/Node=* NtasksPerN:B:S:C=0:0:*:* CoreSpec=* MinCPUsNode=1 MinMemoryNode=500M MinTmpDiskNode=0 Features=(null) DelayBoot=00:00:00 OverSubscribe=OK Contiguous=0 Licenses=(null) Network=(null) Command=(null) WorkDir=/root StdErr=/root/slurm-8.out StdIn=/dev/null StdOut=/root/slurm-8.out Power=
     """
 
     executor.execute = MagicMock(return_value=stdout)
     jobs = grid_engine.get_jobs()
 
-    assert len(jobs) == 2
+    assert len(jobs) == 3
 
-    job_1, job_2 = jobs
+    job_1, job_2, job_3 = jobs
 
     assert job_1.name == 'wrap1'
     assert job_1.user == 'root'
@@ -63,6 +64,15 @@ JobId=9 JobName=wrap2 UserId=root(0) GroupId=root(0) MCS_label=N/A Priority=4294
     assert job_2.gpu == 0
     assert job_2.mem == 1
     assert job_2.datetime == datetime(2023, 7, 31,
+                                      13, 55, 35)
+
+    assert job_3.name == 'wrap3'
+    assert job_3.user == 'user-name'
+    assert job_3.state == GridEngineJobState.PENDING
+    assert job_3.cpu == 2
+    assert job_3.gpu == 0
+    assert job_3.mem == 1
+    assert job_3.datetime == datetime(2023, 7, 31,
                                       13, 55, 35)
 
 
