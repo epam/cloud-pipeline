0e2961ad586009282d39f94f1ae100b12e1b0839
Pavel Silin <pavel_silin@epam.com>
2024-02-01 12:59:33 +0100

    Increase docker version to 20.10

--- deploy/contents/install/app/install-master.sh
+++ deploy/contents/install/app/install-master.sh
@@ -63,17 +63,17 @@ yum install -y yum-utils \
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo && \
-yum install -y  docker-ce-18.03* \
-                docker-ce-cli-18.03* \
+yum install -y  docker-ce-20.10* \
+                docker-ce-cli-20.10* \
                 containerd.io
 if [ $? -ne 0 ]; then
-  echo "Unable to install docker from the official repository, trying to use default docker-18.03*"
+  echo "Unable to install docker from the official repository, trying to use default docker-20.10*"
   
   # Otherwise try to install default docker (e.g. if it's amazon linux)
   rm -f /etc/yum.repos.d/docker-ce.repo
-  yum install -y docker-18.03*
+  yum install -y docker-20.10*
   if [ $? -ne 0 ]; then
-    echo "Unable to install default docker-18.03* too, exiting"
+    echo "Unable to install default docker-20.10* too, exiting"
     exit 1
   fi
 fi
