f79474cd4e6eab819c878a47277d9ecc5fcef22a
Pavel Silin <pavel_silin@epam.com>
2024-04-24 18:33:45 +0200

    AWS omics: workflow pipeline template WIP

--- deploy/docker/cp-tools/base/aws-omics/omics-utils/container_syncronizer.sh
+++ deploy/docker/cp-tools/base/aws-omics/omics-utils/container_syncronizer.sh
@@ -30,24 +30,25 @@ function parse_options {
 
 parse_options "$@"
 
-export GET_PRIVATE_IMAGE_NAME_SCRIPT=${GET_PRIVATE_IMAGE_NAME_SCRIPT:-/opt/omics-utils/get_private_image_name.py}
-export PRIVATE_REGISTRY_NAMING_PROPERTIES="${_PRIVATE_REGISTRY_NAMING_PROPERTIES:-/opt/omics-utils/public_registry_properties.json}"
+export _RESYNC_IMAGES=${RESYNC_IMAGES:-true}
+export GET_PRIVATE_IMAGE_NAME_SCRIPT=${GET_PRIVATE_IMAGE_NAME_SCRIPT:-/opt/omics/utils/get_private_image_name.py}
+export PRIVATE_REGISTRY_NAMING_PROPERTIES="${_PRIVATE_REGISTRY_NAMING_PROPERTIES:-/opt/omics/utils/public_registry_properties.json}"
 if [ ! -f "$PRIVATE_REGISTRY_NAMING_PROPERTIES" ]; then
     echo "Can't find a file: ${PRIVATE_REGISTRY_NAMING_PROPERTIES}. Please --public_registry_properties with an existing config or left unchanged to use default value: /opt/omics-utils/public_registry_properties.json"
     exit 1
 fi
 
-_sync_image_log=$(tempfile)
+_sync_image_log=$(mktemp)
 if [ -z "${PRIVATE_ECR}" ]; then
     echo "AWS private ECR is not set! Please specify it with --ecr option. Exiting."
     exit 1
 else
     echo "AWS private ECR is set to: ${PRIVATE_ECR}. Logging in ..."
      aws_region=$(echo $PRIVATE_ECR | grep -oP ".+.dkr.ecr.\K.+(?=.amazonaws.com)")
-    docker login -u AWS -p "$(aws ecr get-login-password --region $aws_region)" "${PRIVATE_ECR}" &> $_sync_image_log
+    docker login -u AWS -p "$(aws ecr get-login-password --region $aws_region)" "${PRIVATE_ECR}" &> "$_sync_image_log"
     if [ $? -ne 0 ]; then
         echo "There was a problem with login to ECR $PRIVATE_ECR ..."
-        cat $_sync_image_log
+        cat "$_sync_image_log"
         exit 1
     else
         echo "Successfully logged in ECR $PRIVATE_ECR ..."
@@ -56,12 +57,12 @@ fi
 
 if [ -n "$IMAGE_PULL_CONFIG" ]; then
     echo "Image pull config was set as: ${IMAGE_PULL_CONFIG}. Will pull -> push images..."
-    _pull_image_log=$(tempfile)
-    readarray -t images < <(jq -r --compact-output '.manifest[].source_uri,.manifest[].target_image' "${IMAGE_PULL_CONFIG}")
+    _pull_image_log=$(mktemp)
+    readarray -t images < <(jq -r --compact-output '.manifest[]' "${IMAGE_PULL_CONFIG}")
     for image in "${images[@]}"; do
         echo "Image $image will pulled -> pushed..."
 
-        private_image=$(python3 "$GET_PRIVATE_IMAGE_NAME_SCRIPT" --image "$image" --ecr "$PRIVATE_ECR" --images-config "${PRIVATE_REGISTRY_NAMING_PROPERTIES}" 2> "$_pull_image_log")
+        private_image=$(python "$GET_PRIVATE_IMAGE_NAME_SCRIPT" --image "$image" --ecr "$PRIVATE_ECR" --images-config "${PRIVATE_REGISTRY_NAMING_PROPERTIES}" 2> "$_pull_image_log")
         if [ $? -ne 0 ]; then
             echo "There was a problem with configuring private image name for $image ..."
             cat "$_pull_image_log"
@@ -70,26 +71,34 @@ if [ -n "$IMAGE_PULL_CONFIG" ]; then
             echo "Private ECR image path is set to: $private_image"
         fi
 
-        docker pull "$image" &> "$_pull_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with pulling image $image ..."
-            cat "$_pull_image_log"
-            exit 1
-        fi
-
-        docker tag "$image" "$private_image"
         _ECR_REPO_NAME=$(echo "${private_image#"${PRIVATE_ECR}/"}" | cut -d: -f1)
-        aws ecr create-repository --region "$aws_region" --repository-name "$_ECR_REPO_NAME" &> "$_pull_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with creating ECR repo for ${_ECR_REPO_NAME} ..."
-            cat "$_pull_image_log"
+        _ECR_REPO_TAG=$(echo "${_ECR_REPO_NAME}" | grep ":"  &> /dev/null && echo "${_ECR_REPO_NAME}" | cut -d: -f2 || echo latest)
+        if ! aws ecr describe-images --repository-name="$_ECR_REPO_NAME" --region "$aws_region" &> /dev/null; then
+            if aws ecr create-repository --region "$aws_region" --repository-name "$_ECR_REPO_NAME" &> "$_pull_image_log"; then
+                echo "There was a problem with creating ECR repo for ${_ECR_REPO_NAME} ..."
+                cat "$_pull_image_log"
+            else
+                echo "Private ECR repo: $_ECR_REPO_NAME was created."
+            fi
         fi
 
-        docker push "$private_image" &> "$_pull_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with pushing image $image to $private_image ..."
-            cat "$_pull_image_log"
-            exit 1
+        if ! aws ecr describe-images --repository-name="$_ECR_REPO_NAME" --image-ids=imageTag="$_ECR_REPO_TAG" --region "$aws_region" &> /dev/null || [ "$_RESYNC_IMAGES" == "true" ]; then
+            docker pull "$image" &> "$_pull_image_log"
+            if [ $? -ne 0 ]; then
+                echo "There was a problem with pulling image $image ..."
+                cat "$_pull_image_log"
+                exit 1
+            fi
+
+            docker tag "$image" "$private_image"
+            docker push "$private_image" &> "$_pull_image_log"
+            if [ $? -ne 0 ]; then
+                echo "There was a problem with pushing image $image to $private_image ..."
+                cat "$_pull_image_log"
+                exit 1
+            else
+                echo "Image $private_image pushed to the Private ECR repo."
+            fi
         fi
     done
 else
@@ -100,35 +109,42 @@ fi
 if [ -n "$IMAGE_BUILD_CONFIG" ]; then
     echo "Image build config was set as:${IMAGE_BUILD_CONFIG}. Will build -> push images..."
 
-    _build_image_log=$(tempfile)
+    _build_image_log=$(mktemp)
     readarray -t images < <(jq -r --compact-output '.manifest[] | to_entries | map(.value) | join(",")' "${IMAGE_BUILD_CONFIG}")
     for build_dir_and_image_name in "${images[@]}"; do
         _IMAGE_BUILD_DIR=$(echo "$build_dir_and_image_name" | cut -d, -f1)
         _IMAGE_NAME="$PRIVATE_ECR/$(echo "$build_dir_and_image_name" | cut -d, -f2)"
         echo "Image will be built from : ${_IMAGE_BUILD_DIR}, and pushed to ${_IMAGE_NAME}."
-        docker build -t "$_IMAGE_NAME" "$_IMAGE_BUILD_DIR" &> "$_build_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with build process for image $_IMAGE_NAME ..."
-            cat "$_build_image_log"
-            exit 1
-        else
-           echo "Successfully built image $_IMAGE_NAME ..."
-        fi
 
         _ECR_REPO_NAME=$(echo "${_IMAGE_NAME#"${PRIVATE_ECR}/"}" | cut -d: -f1)
-        aws ecr create-repository --region "$aws_region" --repository-name "$_ECR_REPO_NAME" &> "$_build_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with creating ECR repo for ${_ECR_REPO_NAME}. Continue with pushing ..."
-            cat "$_build_image_log"
+        _ECR_REPO_TAG=$(echo "${_IMAGE_NAME}" | grep ":"  &> /dev/null && echo "${_IMAGE_NAME}" | cut -d: -f2 || echo latest)
+        if ! aws ecr describe-images --repository-name="$_ECR_REPO_NAME" --region "$aws_region" &> /dev/null; then
+            if aws ecr create-repository --region "$aws_region" --repository-name "$_ECR_REPO_NAME" &> "$_pull_image_log"; then
+                echo "There was a problem with creating ECR repo for ${_ECR_REPO_NAME} ..."
+                cat "$_pull_image_log"
+            else
+                echo "Private ECR repo: $_ECR_REPO_NAME was created."
+            fi
         fi
 
-        docker push "$_IMAGE_NAME"  &> "$_build_image_log"
-        if [ $? -ne 0 ]; then
-            echo "There was a problem with pushing image to $_IMAGE_NAME ..."
-            cat "$_build_image_log"
-            exit 1
-        else
-           echo "Successfully pushed image $_IMAGE_NAME ..."
+        if ! aws ecr describe-images --repository-name="$_ECR_REPO_NAME" --image-ids=imageTag="$_ECR_REPO_TAG" --region "$aws_region" &> /dev/null || [ "$_RESYNC_IMAGES" == "true" ]; then
+            docker build -t "$_IMAGE_NAME" "$(echo "$_IMAGE_BUILD_DIR" | envsubst)" &> "$_build_image_log"
+            if [ $? -ne 0 ]; then
+                echo "There was a problem with build process for image $_IMAGE_NAME ..."
+                cat "$_build_image_log"
+                exit 1
+            else
+               echo "Successfully built image $_IMAGE_NAME ..."
+            fi
+
+            docker push "$_IMAGE_NAME"  &> "$_build_image_log"
+            if [ $? -ne 0 ]; then
+                echo "There was a problem with pushing image to $_IMAGE_NAME ..."
+                cat "$_build_image_log"
+                exit 1
+            else
+               echo "Successfully pushed image $_IMAGE_NAME ..."
+            fi
         fi
     done
 else
--- workflows/pipe-templates/__AWS_OMICS/omics-workflow/src/run-omics-workflow.sh
+++ workflows/pipe-templates/__AWS_OMICS/omics-workflow/src/run-omics-workflow.sh
@@ -3,13 +3,13 @@
 _TASK_NAME="AWSOmicsWorkflow"
 
 if [ "${engine}" != "NEXTFLOW" ] && [ "${engine}" != "WDL" ] && [ "${engine}" != "CWL" ]; then
-    pipe_log ERROR "Parameter engine is set to: '${engine}', allowed options are: 'NEXTFLOW', 'CWL', 'WDL'" $_TASK_NAME
+    pipe_log_fail "Parameter engine is set to: '${engine}', allowed options are: 'NEXTFLOW', 'CWL', 'WDL'" $_TASK_NAME
     exit 1
 fi
 
 which aws &> /dev/null
 if [ $? -ne 0 ]; then
-    pipe_log ERROR "Can't find aws utility. Are you using library/aws-omics docker image?" $_TASK_NAME
+    pipe_log_fail "Can't find aws utility. Are you using library/aws-omics docker image?" $_TASK_NAME
     exit 1
 fi
 
@@ -23,10 +23,10 @@ CP_OMICS_SERICE_ROLE=$(curl -X GET \
                       | jq -r '.payload.omicsServiceRole // empty')
 
 if [ "${CP_OMICS_SERICE_ROLE}" == "empty" ]; then
-    pipe_log ERROR "Couldn't get Omics Service Role information from Cloud Region with id ${CLOUD_REGION_ID}" $_TASK_NAME
+    pipe_log_fail "Couldn't get Omics Service Role information from Cloud Region with id ${CLOUD_REGION_ID}" $_TASK_NAME
     exit 1
 else
-  pipe_log INFO "Omics Service Role is configured by region setting as: $CP_OMICS_SERICE_ROLE, Assuming..." $_TASK_NAME
+  pipe_log_info "Omics Service Role is configured by region setting as: $CP_OMICS_SERICE_ROLE, Assuming..." $_TASK_NAME
   aws sts assume-role --role-arn "$CP_OMICS_SERICE_ROLE" \
       --role-session-name "CP_AWS_OMICS_WORKFLOW_${RUN_ID}" \
       --duration-seconds "${CP_OMICS_ROLE_ASSUME_SESSION_DURATION:-3600}"
@@ -42,18 +42,18 @@ CP_PRIVATE_ECR=$(curl -X GET \
                   | jq -r '.payload.omicsEcrUrl // empty')
 
 if [ "${CP_PRIVATE_ECR}" == "empty" ]; then
-    pipe_log ERROR "Couldn't get AWS Private ECR information from Cloud Region with id ${CLOUD_REGION_ID}" $_TASK_NAME
-    exit 1
+  pipe_log_fail "Couldn't get AWS Private ECR information from Cloud Region with id ${CLOUD_REGION_ID}" $_TASK_NAME
+  exit 1
 else
-  pipe_log INFO "AWS Private ECR for Omics workflow is configured by region setting as: $CP_PRIVATE_ECR" $_TASK_NAME
+  pipe_log_info "AWS Private ECR for Omics workflow is configured by region setting as: $CP_PRIVATE_ECR" $_TASK_NAME
 fi
 
-pipe_log INFO "Synchronizing docker images in private ECR repo." $_TASK_NAME
-if [ ! -f /opt/omcis-utils/container_syncronizer.sh ]; then
-    pipe_log ERROR "Script /opt/omcis-utils/container_syncronizer.sh wasn't found. Are you using library/aws-omics docker image?" $_TASK_NAME
+pipe_log_info "Synchronizing docker images in private ECR repo." $_TASK_NAME
+if [ ! -f /opt/omics/utils/container_syncronizer.sh ]; then
+    pipe_log_fail "Script /opt/omics/utils/container_syncronizer.sh wasn't found. Are you using library/aws-omics docker image?" $_TASK_NAME
     exit 1
 fi
-ECR_SYNC_CMD="bash ./omics-utils/container_syncronizer.sh --public_registry_properties /opt/omics-utils/public_registry_properties.json --ecr ${CP_PRIVATE_ECR}"
+ECR_SYNC_CMD="bash /opt/omics/utils/container_syncronizer.sh --public_registry_properties /opt/omics/utils/public_registry_properties.json --ecr ${CP_PRIVATE_ECR}"
 if [ -f $SCRIPTS_DIR/src/image-build-manifest.json ]; then
     ECR_SYNC_CMD="$ECR_SYNC_CMD --image_build_config $SCRIPTS_DIR/src/image-build-manifest.json"
 fi
@@ -62,5 +62,58 @@ if [ -f $SCRIPTS_DIR/src/image-pull-manifest.json ]; then
     ECR_SYNC_CMD="$ECR_SYNC_CMD --image_pull_config $SCRIPTS_DIR/src/image-pull-manifest.json"
 fi
 
-pipe_exec "$ECR_SYNC_CMD" $_TASK_NAME
+_SYNC_IMAGES_TASK_NAME="AWSOmicsECRSyncImages"
+pipe_exec "$ECR_SYNC_CMD" $_SYNC_IMAGES_TASK_NAME
+pipe_log_info "Successfully synchronized docker images in private ECR repo." $_TASK_NAME
 
+pipe_log_info "Preparing parameters-template.json and parameters.json for a workflow, based on pipeline run parameters." $_TASK_NAME
+PARAM_TYPE_SUFFIX="_PARAM_TYPE"
+while IFS= read -r line; do
+    PARAM_TYPE_NAME=$(awk -F "=" '{print $1}' <<< $line)
+    PARAM_NAME=${PARAM_TYPE_NAME%$PARAM_TYPE_SUFFIX}
+    if [ -z "$_PARAMETERS_TEMPLATE" ]; then
+        _PARAMETERS_TEMPLATE="\"$PARAM_NAME\": { \"description\": \"value of $PARAM_NAME\" }"
+    else
+        _PARAMETERS_TEMPLATE="${_PARAMETERS_TEMPLATE}, \"$PARAM_NAME\": { \"description\": \"$PARAM_NAME\" }"
+    fi
+
+    if [ -z "$_PARAMETERS" ]; then
+        _PARAMETERS="\"$PARAM_NAME\": \"${!PARAM_NAME}\""
+    else
+        _PARAMETERS="${_PARAMETERS}, \"$PARAM_NAME\": \"${!PARAM_NAME}\""
+    fi
+done <<< "$(env | grep ${PARAM_TYPE_SUFFIX})"
+
+echo "{ ${_PARAMETERS_TEMPLATE} }" | jq > $SCRIPTS_DIR/src/parameters_template.json
+echo "{ ${_PARAMETERS} }" | jq > $SCRIPTS_DIR/src/parameters.json
+
+pipe_log_info "Packaging workflow in a zip distribution." $_TASK_NAME
+_WORKFLOW_DEFINITION_ZIP="/tmp/cp_omics_workflow_${RUN_ID}.zip"
+if [ -d $SCRIPTS_DIR/src/workflow ]; then
+    zip -9 -r "$_WORKFLOW_DEFINITION_ZIP" $SCRIPTS_DIR/src/workflow
+else
+    pipe_log_fail "Couldn't find workflow directory in $SCRIPTS_DIR/src"
+fi
+
+pipe_log_info "Registering workflow." $_TASK_NAME
+workflow_name="cp_omics_workflow_${RUN_ID}"
+workflow_id=$(aws omics create-workflow \
+    --engine $engine \
+    --definition-zip $_WORKFLOW_DEFINITION_ZIP \
+    --name "$workflow_name" \
+    --parameter-template file://$SCRIPTS_DIR/src/parameters_template.json \
+    --query 'id' \
+    --output text
+)
+pipe_log_info "Waiting for the workflow to be available." $_TASK_NAME
+aws omics wait workflow-active --id "${workflow_id}"
+
+pipe_log_info "Receiving workflow metadata." $_TASK_NAME
+aws omics get-workflow --id "${workflow_id}" > "/tmp/${workflow_name}.json"
+
+aws omics start-run \
+    --role-arn "${CP_OMICS_SERICE_ROLE}" \
+    --workflow-id "$(jq -r '.id' ${workflow_name}.json)" \
+    --name "$workflow_name" \
+    --output-uri ${output_path} \
+    --parameters file://$SCRIPTS_DIR/src/parameters.json
