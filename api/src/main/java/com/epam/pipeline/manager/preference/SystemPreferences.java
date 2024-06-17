cd05a6f1930633437bf7dcaa74c78f2bfff25aa7
Ekaterina_Kazachkova <ekaterina_kazachkova@epam.com>
2024-06-17 19:34:26 +0400

    API: Allow to specify node unavailable grace period directly for run

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -459,6 +459,9 @@ public class SystemPreferences {
         "cluster.kill.not.matching.nodes", true, CLUSTER_GROUP, pass);
     public static final BooleanPreference CLUSTER_ENABLE_AUTOSCALING = new BooleanPreference(
         "cluster.enable.autoscaling", true, CLUSTER_GROUP, pass);
+    /**
+     * This preference can be specified for run directly viy NODE_UNAVAILABLE_GRACE_PERIOD_MINUTES system parameter.
+     */
     public static final IntPreference CLUSTER_NODE_UNAVAILABLE_GRACE_PERIOD_MINUTES = new IntPreference(
         "cluster.node.unavailable.grace.period.minutes", 30, CLUSTER_GROUP, isGreaterThanOrEquals(0));
 
