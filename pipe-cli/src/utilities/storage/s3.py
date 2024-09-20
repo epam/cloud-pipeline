67b72aa0536fdf1ea6e6454c9626d8ba552ccd51
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2024-09-20 17:47:52 +0300

    PipeCLI: pipe storage cp/mv - retryable AWS API call (#3714)
    (cherry picked from commit 5afeaa333aea6fbe39a4cd822a81b7b6b6d5d1a4)

--- pipe-cli/src/utilities/storage/s3.py
+++ pipe-cli/src/utilities/storage/s3.py
@@ -55,7 +55,7 @@ class StorageItemManager(object):
         self.events = events
         self.region_name = region_name
         self.endpoint = endpoint
-        _boto_config = S3BucketOperations.get_proxy_config(cross_region=cross_region)
+        _boto_config = S3BucketOperations.get_boto_config(cross_region=cross_region)
         self.s3 = session.resource('s3', config=_boto_config,
                                    region_name=self.region_name,
                                    endpoint_url=endpoint,
@@ -78,7 +78,7 @@ class StorageItemManager(object):
         return '&'.join(['%s=%s' % (key, value) for key, value in tags.items()])
 
     def _get_client(self):
-        _boto_config = S3BucketOperations.get_proxy_config()
+        _boto_config = S3BucketOperations.get_boto_config()
         client = self.session.client('s3', config=_boto_config,
                                      region_name=self.region_name,
                                      endpoint_url=self.endpoint,
@@ -321,7 +321,7 @@ class TransferBetweenBucketsManager(StorageItemManager, AbstractTransferManager)
             source_wrapper.delete_item(path)
 
     def build_source_client(self, source_region, source_endpoint):
-        _boto_config = S3BucketOperations.get_proxy_config(cross_region=self.cross_region)
+        _boto_config = S3BucketOperations.get_boto_config(cross_region=self.cross_region)
         source_s3 = self.session.resource('s3',
                                           config=_boto_config,
                                           region_name=source_region,
@@ -867,19 +867,22 @@ class S3BucketOperations(object):
     __config__ = None
 
     @classmethod
-    def get_proxy_config(cls, cross_region=False):
+    def get_boto_config(cls, cross_region=False):
+        max_attempts = os.getenv('CP_AWS_MAX_ATTEMPTS')
+        retries = {'max_attempts': int(max_attempts)} if max_attempts else None
         if cls.__config__ is None:
             cls.__config__ = Config.instance()
         if cls.__config__.proxy is None:
             if cross_region:
                 os.environ['no_proxy'] = ''
-            return None
+            return AwsConfig(retries=retries)
         else:
-            return AwsConfig(proxies=cls.__config__.resolve_proxy(target_url=cls.S3_ENDPOINT_URL))
+            return AwsConfig(proxies=cls.__config__.resolve_proxy(target_url=cls.S3_ENDPOINT_URL),
+                             retries=retries)
 
     @classmethod
     def _get_client(cls, session, region_name=None, endpoint=None):
-        _boto_config = S3BucketOperations.get_proxy_config()
+        _boto_config = S3BucketOperations.get_boto_config()
         client = session.client('s3', config=_boto_config, region_name=region_name,
                                 endpoint_url=endpoint, verify=False if endpoint else None)
         client._endpoint.http_session.adapters['https://'] = BotocoreHTTPSession(
