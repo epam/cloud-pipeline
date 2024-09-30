058b17dc08591a8433a00cce45c4aee41e709d1b
AleksandrGorodetskii <gorod2004@gmail.com>
2024-09-30 14:12:20 +0300

    GUI Runs table: export runs - remove unnecessary code

--- client/src/components/pipelines/browser/Pipeline.js
+++ client/src/components/pipelines/browser/Pipeline.js
@@ -456,7 +456,6 @@ export default class Pipeline extends localization.LocalizedReactComponent {
       docsPath
     } = values || {};
     const hide = message.loading(`Updating ${this.localizedString('pipeline')} ${name}...`, 0);
-    console.log('1')
     await this.updatePipelineRequest.send({
       id: this.props.pipeline.value.id,
       name: name,
