5496a828aace9895482c89d68db51c50b623f218
AleksandrGorodetskii <gorod2004@gmail.com>
2024-10-01 11:38:14 +0300

    GUI LaunchForm: enum parameters style adjustments

--- client/src/components/pipelines/launch/form/LaunchPipelineForm.js
+++ client/src/components/pipelines/launch/form/LaunchPipelineForm.js
@@ -3679,9 +3679,9 @@ class LaunchPipelineForm extends localization.LocalizedReactComponent {
                   <div className={classNames(styles.parameterValue, {
                     [styles.hiddenItem]: systemParameterValueIsBlocked
                   })}>
+                    {icon && renderParameterIcon()}
                     {formItem}
                     {renderRemoveButton()}
-                    {icon && renderParameterIcon()}
                   </div>
                   {parameterHint ? (
                     <div
