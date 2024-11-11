07caa8d25b98ec631f70f21c3468c8e4bed0744c
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2024-11-11 20:34:50 +0300

    Multi-modal gpustat
    (cherry picked from commit 7b5441b7eb127a91348cf03e43f5eeb714e93a1a)

--- workflows/gpustat/build-linux.sh
+++ workflows/gpustat/build-linux.sh
@@ -37,7 +37,7 @@ python3 ./waf all
 cd \$_BUILD_TMP
 git clone https://github.com/sidoruka/gpustat-web
 cd gpustat-web
-git checkout 4eaeeab78bb531eb38d71c3081430ac4af121c64
+git checkout 087eeca44f0678cfb05abfd69864f03567f834e9
 cat > requirements.txt <<EOF
 six==1.7
 termcolor
