286e6b7b28d8ee759a0eb23ceb661129056037bd
bklimov-web <Bogdan_Klimov@epam.com>
2024-12-08 23:09:21 +0300

    fix functionality

--- portals-ui/sites/ngs-portal/src/pages/projects/hooks/use-project-tags.ts
+++ portals-ui/sites/ngs-portal/src/pages/projects/hooks/use-project-tags.ts
@@ -1,43 +1,116 @@
 import type { Project } from '@cloud-pipeline/core';
 import { useMemo } from 'react';
-import type { Tag } from '../types';
+import type { Tag, TagFilters } from '../types';
+import { ProjectFilter } from '../constants';
 
-export const useProjectTags = (projects?: Project[]) => {
-  return useMemo(() => {
-    if (!projects?.length) {
-      return {};
+type Props = {
+  project: Project;
+  tagsToFilter: TagFilters;
+  tagToExclude?: string;
+  tagToReplace?: TagFilters;
+};
+
+export const doesProjectMatchFilters = ({
+  project,
+  tagsToFilter,
+  tagToReplace = {},
+}: Props) => {
+  if (!Object.keys(tagsToFilter).length) {
+    return true;
+  }
+
+  const effectiveFilters = Object.entries({
+    ...tagsToFilter,
+    ...tagToReplace,
+  });
+
+  return effectiveFilters.every(([filterName, values]) => {
+    if (filterName === (ProjectFilter.OWNER as string)) {
+      return values.includes(project.owner);
     }
 
-    // Map for faster lookup
-    const tagsMap: Record<string, Map<string, Tag>> = {};
+    const projectTagValue = project.data?.[filterName]?.value;
+    return projectTagValue && values.includes(projectTagValue);
+  });
+};
 
-    for (const { data } of projects) {
-      if (!data) {
-        continue;
+const buildTagsMap = (projects?: Project[]) => {
+  if (!projects?.length) {
+    return {};
+  }
+
+  // Map for faster lookup
+  const tagsMap: Record<string, Map<string, Tag>> = {};
+
+  for (const project of projects) {
+    if (!project.data) {
+      continue;
+    }
+
+    for (const [key, { value }] of Object.entries(project.data)) {
+      if (!tagsMap[key]) {
+        tagsMap[key] = new Map();
       }
 
-      for (const [key, { value }] of Object.entries(data)) {
-        if (!tagsMap[key]) {
-          tagsMap[key] = new Map();
-        }
+      const currentTagMap = tagsMap[key];
+      const tag = currentTagMap.get(value);
 
-        const currentTagMap = tagsMap[key];
-        const tag = currentTagMap.get(value);
-
-        if (tag) {
-          tag.count++;
-        } else {
-          currentTagMap.set(value, { id: value, count: 1 });
-        }
+      if (tag) {
+        tag.count++;
+      } else {
+        currentTagMap.set(value, { id: value, count: 1 });
       }
     }
+  }
 
-    // Convert maps to arrays
-    const tags: Record<string, Tag[]> = {};
-    for (const [key, tagMap] of Object.entries(tagsMap)) {
-      tags[key] = Array.from(tagMap.values());
-    }
+  // Convert maps to arrays
+  const tags: Record<string, Tag[]> = {};
+  for (const [key, tagMap] of Object.entries(tagsMap)) {
+    tags[key] = Array.from(tagMap.values());
+  }
 
-    return tags;
+  return tags;
+};
+
+export const useProjectTags = (
+  tagsToFilter: TagFilters,
+  projects?: Project[],
+) => {
+  const projectTags = useMemo(() => {
+    return buildTagsMap(projects);
   }, [projects]);
+  console.log('🚀 ~ projectTags ~ projectTags:', projectTags);
+
+  const dynamicTags = useMemo(() => {
+    if (!Object.keys(tagsToFilter).length) {
+      return projectTags;
+    }
+
+    const updatedTags: Record<string, Tag[]> = {};
+
+    Object.entries(projectTags).forEach(([tagName, tagValues]) => {
+      updatedTags[tagName] = tagValues.map((tag) => {
+        const matchingProjects = projects?.filter((project) =>
+          doesProjectMatchFilters({
+            project,
+            tagsToFilter,
+            tagToReplace: { [tagName]: [tag.id] },
+          }),
+        );
+
+        const testProjects = matchingProjects?.filter(
+          (project) => project?.data?.[tagName]?.value === tag.id,
+        );
+
+        return {
+          ...tag,
+          count: testProjects?.length ?? 0,
+        };
+      });
+    });
+
+    return updatedTags;
+  }, [projectTags, projects, tagsToFilter]);
+
+  return dynamicTags;
 };
