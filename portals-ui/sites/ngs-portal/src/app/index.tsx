f38f24515519ce2341931c74c6021a868c358eb0
Aleksandr Gorodetskii <41908792+AleksandrGorodetskii@users.noreply.github.com>
2024-12-13 14:31:09 +0300

    NEW GUI customized antd theme (#3827)
    * NEW GUI: Customized antd theme
    
    * NEW GUI: Customized antd theme - remove unneccessary type

--- portals-ui/sites/ngs-portal/src/app/index.tsx
+++ portals-ui/sites/ngs-portal/src/app/index.tsx
@@ -7,14 +7,18 @@ import {
 import '@epam/uui/styles.css';
 import '@epam/loveship/styles.css';
 import { appRouter } from './config';
+import { ConfigProvider } from 'antd';
+import theme from './theme';
 
 const adapterRouter = new Router6AdaptedRouter(appRouter);
 
 export default function Application() {
   const { services } = useUuiServices({ router: adapterRouter });
   return (
-    <UuiContext.Provider value={services}>
-      <RouterProvider router={appRouter} />
-    </UuiContext.Provider>
+    <ConfigProvider theme={theme}>
+      <UuiContext.Provider value={services}>
+        <RouterProvider router={appRouter} />
+      </UuiContext.Provider>
+    </ConfigProvider>
   );
 }
---
+++ portals-ui/sites/ngs-portal/src/app/theme/index.ts
@@ -0,0 +1,18 @@
+const primary = '#009ecc';
+const bg = '#F5F6FA';
+
+const theme = {
+  token: {
+    colorPrimary: primary,
+    colorLink: primary,
+    colorInfo: primary,
+    colorInfoText: primary,
+    colorPrimaryText: primary,
+    borderRadius: 3,
+    colorBgElevated: bg,
+    colorBorderBg: bg,
+    colorBgContainer: bg,
+  },
+};
+
+export default theme;
