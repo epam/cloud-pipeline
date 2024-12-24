9cd716529ed7a23bc6e9da884d898c786d476a7a
Aleksandr Gorodetskii <41908792+AleksandrGorodetskii@users.noreply.github.com>
2024-12-24 10:51:33 +0300

    NEW GUI - set link colors same as colorPrimary from antd (#3843)
    * NEW GUI - set link colors same as colorPrimary from antd
    
    * NEW GUI - set link colors same as colorPrimary from antd

---
+++ portals-ui/sites/ngs-portal/src/app/theme/colors.ts
@@ -0,0 +1,11 @@
+// Tailwind colors map
+const colors = {
+  sky500: '#0ea5e9',
+  sky400: '#38bdf8',
+  sky300: '#7dd3fc',
+  green500: '#22c55e',
+  red500: '#e00444',
+  orange500: '#f97316',
+};
+
+export default colors;
