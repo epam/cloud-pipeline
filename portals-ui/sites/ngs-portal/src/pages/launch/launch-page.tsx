c48fd89361436fca873f6a8cf0089054bc91932e
AleksandrGorodetskii <gorod2004@gmail.com>
2025-01-09 14:54:53 +0300

    NEW GUI Launch Form - remove version and configuration selectors

--- portals-ui/sites/ngs-portal/src/pages/launch/launch-page.tsx
+++ portals-ui/sites/ngs-portal/src/pages/launch/launch-page.tsx
@@ -8,17 +8,8 @@ import './style.css';
 
 export function LaunchPage() {
   const { pipelineId } = useParams();
-  const {
-    pipelineInfo,
-    version,
-    versions,
-    configuration,
-    configurations,
-    errors,
-    pending,
-    setVersion,
-    setConfiguration,
-  } = useLaunchInfo(Number(pipelineId));
+  const { pipelineInfo, version, versions, configuration, errors, pending } =
+    useLaunchInfo(Number(pipelineId));
   const launchInfoLoaded = useMemo(
     () => version && versions?.length && configuration && pipelineInfo,
     [configuration, pipelineInfo, version, versions?.length],
@@ -32,16 +23,7 @@ export function LaunchPage() {
   return (
     <div className="overflow-auto list-container p-4 h-full w-full flex flex-col">
       <Spin wrapperClassName="spin-container" spinning={pending}>
-        <LaunchHeader
-          pipelineInfo={pipelineInfo}
-          version={version}
-          versions={versions}
-          configuration={configuration}
-          configurations={configurations}
-          setVersion={setVersion}
-          setConfiguration={setConfiguration}
-          disabled={pending || errors.length > 0}
-        />
+        <LaunchHeader pipelineInfo={pipelineInfo} />
         <LaunchForm
           version={version}
           configuration={configuration}
