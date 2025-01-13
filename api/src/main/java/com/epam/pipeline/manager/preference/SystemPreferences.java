4dce815d3d9084b0c2330fadc02966283afaf52b
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2025-01-13 16:21:35 +0300

    ui.standalone.nodes.allow.terminate preference

--- api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
+++ api/src/main/java/com/epam/pipeline/manager/preference/SystemPreferences.java
@@ -883,6 +883,8 @@ public class SystemPreferences {
             "", UI_GROUP, pass, true);
     public static final BooleanPreference UI_QUICK_SEARCH_DISABLED = new BooleanPreference(
             "ui.quick.search.disabled", false, UI_GROUP, pass, true);
+    public static final BooleanPreference UI_STANDALONE_NODES_ALLOW_TERMINATE = new BooleanPreference(
+            "ui.standalone.nodes.allow.terminate", false, UI_GROUP, pass, true);
 
 
     // BASE_URLS_GROUP
