9649bf70625b4fb60f652c71a670a725be654e41
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2025-01-30 12:43:33 +0100

    ngs-portal: start.sh script update

--- portals-ui/sites/ngs-portal/start.sh
+++ portals-ui/sites/ngs-portal/start.sh
@@ -3,12 +3,26 @@
 export CP_API_HOST=$(echo $API | awk -F[/] '{print $3}')
 export NGS_PORTAL_ROOT="${NGS_PORTAL_ROOT:-/opt/ngs-portal}"
 
+if [ "$NGS_PORTAL_SETTINGS" ]; then
+  pipe storage cp "${NGS_PORTAL_SETTINGS}" "${NGS_PORTAL_ROOT}/settings.json"
+fi
+
 NGINX_CONF="$(envsubst '${CP_API_HOST} ${API_TOKEN} ${NGS_PORTAL_ROOT}' < /etc/nginx/nginx.conf)"
 echo "$NGINX_CONF" > /etc/nginx/nginx.conf
 
-# define the content of the settings.json file
-settings_content="{\"api\": \"$API\"}"
-echo "$settings_content" > "$NGS_PORTAL_ROOT/settings.json"
+SETTINGS_FILE="$NGS_PORTAL_ROOT/settings.json"
+
+# Check if settings.json exists and read its content
+if [ -f "$SETTINGS_FILE" ]; then
+  existing_content=$(cat "$SETTINGS_FILE")
+else
+  existing_content='{}'  # Default to empty JSON object if file does not exist
+fi
+
+# Update the "api" property while keeping other properties intact
+updated_content=$(echo "$existing_content" | jq 'if has("api") | not then .api = $api else . end' --arg api "$API")
+
+echo "$updated_content" > "$SETTINGS_FILE"
 
 nginx
 
