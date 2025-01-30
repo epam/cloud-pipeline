29ff892f31c07abe2a2af324beb0c3d841d710b6
Aleksandr Gorodetskii <41908792+AleksandrGorodetskii@users.noreply.github.com>
2025-01-30 12:46:31 +0300

    NEW GUI Run - stop run confirmation (#3883)
    * NEW GUI Run - stop run confirmation
    
    * NEW GUI Run - stop run confirmation styling

--- portals-ui/sites/ngs-portal/src/pages/run-logs/run-logs-page.tsx
+++ portals-ui/sites/ngs-portal/src/pages/run-logs/run-logs-page.tsx
@@ -4,6 +4,7 @@ import RunLogsHeader from './run-logs-header.tsx';
 import { useRunLogsTabs } from './hooks/use-run-logs-tabs.tsx';
 import { ItemLayout } from '../../shared/ui/index.ts';
 import useRunWithLogsInfo from './hooks/use-run-with-logs-info.tsx';
+import './style.css';
 
 export function RunLogsPage() {
   const { runId } = useParams();
@@ -11,30 +12,29 @@ export function RunLogsPage() {
     Number(runId),
   );
   const { activeTab, tabs, handleChangeTab } = useRunLogsTabs(run, logs);
-  if (!run || pending) {
-    return <Spin spinning />;
-  }
   if (error) {
     return <Alert message={error} type="error" />;
   }
   return (
     <div className="overflow-hidden gap-4 h-full w-full flex flex-col">
-      <ItemLayout
-        classes={{
-          content: 'overflow-hidden flex grow',
-          layoutCard: 'overflow-hidden',
-        }}
-        header={
-          <RunLogsHeader
-            activeKey={activeTab.key}
-            tabs={tabs}
-            onChangeTab={handleChangeTab}
-            run={run}
-            refresh={refreshRun}
-          />
-        }
-        main={activeTab.content}
-      />
+      <Spin wrapperClassName="spin-container" spinning={pending || !run}>
+        <ItemLayout
+          classes={{
+            content: 'overflow-hidden flex grow',
+            layoutCard: 'overflow-hidden',
+          }}
+          header={
+            <RunLogsHeader
+              activeKey={activeTab.key}
+              tabs={tabs}
+              onChangeTab={handleChangeTab}
+              run={run}
+              refresh={refreshRun}
+            />
+          }
+          main={activeTab.content}
+        />
+      </Spin>
     </div>
   );
 }
