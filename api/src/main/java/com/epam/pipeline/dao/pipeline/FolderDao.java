f508063b465f0f18506517fbae6773da343d659d
Oksana Kolesnikova <83080415+okolesn@users.noreply.github.com>
2025-01-31 12:48:59 +0100

    Issue 3866 [NGS Portal] Add "project id" optional property for runs /run filters - fixes (#3886)

--- api/src/main/java/com/epam/pipeline/dao/pipeline/FolderDao.java
+++ api/src/main/java/com/epam/pipeline/dao/pipeline/FolderDao.java
@@ -250,6 +250,7 @@ public class FolderDao extends NamedParameterJdbcDaoSupport {
                         Long parentId = rs.getLong(PARENT_ID.name());
                         if (!rs.wasNull()) {
                             folder.setParentId(parentId);
+                            folder.setParent(new Folder(parentId));
                             folderToChildren.putIfAbsent(parentId, new HashSet<>());
                             folderToChildren.get(parentId).add(folderId);
                         }
@@ -269,6 +270,7 @@ public class FolderDao extends NamedParameterJdbcDaoSupport {
                                 new Date(rs.getTimestamp(PIPELINE_CREATED_DATE.name()).getTime()));
                         pipeline.setLocked(rs.getBoolean(PIPELINE_LOCKED.name()));
                         pipeline.setParentFolderId(folderId);
+                        pipeline.setParent(new Folder(folderId));
                         pipeline.setOwner(rs.getString(OWNER.name()));
                         String rawVisibility = rs.getString(PIPELINE_VISIBILITY.name());
                         pipeline.setVisibility(
@@ -319,6 +321,7 @@ public class FolderDao extends NamedParameterJdbcDaoSupport {
                         StoragePolicy policy = DataStorageDao.DataStorageParameters.getStoragePolicy(rs);
                         dataStorage.setStoragePolicy(policy);
                         dataStorage.setParentFolderId(folderId);
+                        dataStorage.setParent(new Folder(folderId));
                         dataStorage.setLocked(rs.getBoolean(DATASTORAGE_LOCKED.name()));
                         dataStorage.setShared(rs.getBoolean(DATASTORAGE_SHARED.name()));
                         dataStorage.setSensitive(rs.getBoolean(DATASTORAGE_SENSITIVE.name()));
@@ -353,6 +356,7 @@ public class FolderDao extends NamedParameterJdbcDaoSupport {
             long parentId = rs.getLong(PARENT_ID.name());
             if (!rs.wasNull()) {
                 folder.setParentId(parentId);
+                folder.setParent(new Folder(parentId));
             }
             folder.setCreatedDate(new Date(rs.getTimestamp(CREATED_DATE.name()).getTime()));
             folder.setOwner(rs.getString(OWNER.name()));
