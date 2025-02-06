95a302187bbce99574ddabb0e9c122422b2d8cdc
Pavel Silin <pavel_silin@epam.com>
2025-02-06 14:39:56 +0100

    issue 3895 DAO test

--- api/src/main/resources/db/migration/v2025.02.05_17.00__issue_3895_pipeline_run_results.sql
+++ api/src/main/resources/db/migration/v2025.02.05_17.00__issue_3895_pipeline_run_results.sql
@@ -1,9 +1,9 @@
 CREATE TABLE pipeline.pipeline_run_result (
     run_id INTEGER REFERENCES pipeline.pipeline_run(run_id) NOT NULL,
     name VARCHAR NOT NULL,
-    pattern VARCHAR NOT NULL,
+    file_mask VARCHAR NOT NULL,
     path VARCHAR NOT NULL,
-    CONSTRAINT pipeline_run_result_constrain UNIQUE(run_id, name, pattern, path)
+    CONSTRAINT pipeline_run_result_constrain UNIQUE(run_id, name, file_mask, path)
 );
 
 ALTER TABLE PIPELINE.DATASTORAGE_RULE ADD COLUMN IS_RESULT BOOLEAN DEFAULT FALSE NOT NULL;
