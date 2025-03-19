8c2fbfcf95761621681cd7d939a6537c10300689
maryvictol <Mariia_Tolstaia@epam.com>
2025-03-19 17:18:51 +0300

    Update e2e tests 3229/3751

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/RBACPermissionTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/RBACPermissionTest.java
@@ -30,10 +30,8 @@ import static com.epam.pipeline.autotests.utils.Privilege.WRITE;
 import static com.epam.pipeline.autotests.utils.PrivilegeValue.ALLOW;
 import static com.epam.pipeline.autotests.utils.PrivilegeValue.INHERIT;
 import static java.lang.String.format;
-import com.epam.pipeline.autotests.ao.UserManagementAO.UsersTabAO;
 import com.epam.pipeline.autotests.ao.UserManagementAO.UsersTabAO.UserEntry.EditUserPopup;
 import com.epam.pipeline.autotests.mixins.Authorization;
-import com.epam.pipeline.autotests.utils.C;
 import com.epam.pipeline.autotests.utils.TestCase;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -46,8 +44,7 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
     private String[][] attr = {{"key1", "value1"}, {"key2", "value2"},
                                {"key3", "value3"}, {"key4", "value4"}};
     private final String toolInstanceTypesMask = "Allowed tool instance types mask";
-    private final String mask = "m.*";
-    private final String testGroup = C.ROLE_USER;
+    private final String mask = "*";
     private String[] initialMiscMetadataSensitiveKeys = {""};
     private String miscMetadataSensitiveKeys = "misc.metadata.sensitive.keys";
     private String miscMetadataSensitiveKeysTestValue = format("[\"%s\"]", attr[0][0]);
@@ -57,15 +54,27 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
 
     @BeforeClass
     public void prepareTestGroups() {
-        Stream.of(testGroup1, testGroup2)
-                .forEach(group -> createTestGroup(group));
+        createTestGroup(testGroup1, admin);
+        createTestGroup(testGroup2, user);
+        initialMiscMetadataSensitiveKeys = navigationMenu()
+                .settings()
+                .switchToPreferences()
+                .getPreference(miscMetadataSensitiveKeys);
     }
 
     @AfterClass
     public void removeTestGroups() {
         loginAsUser(admin);
+        setMiscMetadataSensitiveKeys(initialMiscMetadataSensitiveKeys[0]);
         Stream.of(testGroup1, testGroup2)
-                .forEach(group -> removeTestGroup(group));
+                .forEach(group -> {
+                     navigationMenu()
+                         .settings()
+                         .switchToUserManagement()
+                         .switchToGroups()
+                         .searchGroupBySubstring(group)
+                         .deleteGroupIfPresent(group);
+                });
     }
 
     @Test(priority = 1)
@@ -93,63 +102,53 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
     @Test(priority = 1, dependsOnMethods = "readGrantPermissionsToUserAccount")
     @TestCase(value = "3229_2")
     public void wtiteGrantPermissionsToUserAccount() {
-            loginAsUser(admin);
-            UsersTabAO testUser = navigationMenu()
-                    .settings()
-                    .switchToUserManagement()
-                    .switchToUsers();
-            testUser
-                    .searchUserEntry(user.login)
-                    .edit()
-                    .addRoleOrGroupIfNonExist(testGroup)
-                    .ok();
-            testUser
-                    .searchUserEntry(admin.login)
-                    .edit()
-                    .getUserPermissionsTab()
-                    .addNewGroup(testGroup)
-                    .selectByName(testGroup)
-                    .showPermissions()
-                    .set(WRITE, ALLOW)
-                    .savePermissions()
-                    .closeAll();
-            initialMiscMetadataSensitiveKeys = navigationMenu()
-                    .settings()
-                    .switchToPreferences()
-                    .getPreference(miscMetadataSensitiveKeys);
-            setMiscMetadataSensitiveKeys(miscMetadataSensitiveKeysTestValue);
-            loginAsUser(user);
-            navigationMenu()
-                    .settings()
-                    .ensure(USER_MANAGEMENT_TAB, visible)
-                    .switchToUserManagement()
-                    .switchToUsers()
-                    .searchUserEntry(admin.login)
-                    .openEditUserPopUp()
-                    .ensureNotVisible(IMPERSONATE)
-                    .ensureDisable(DELETE, BLOCK)
-                    .isListOfRolesBlocked()
-                    .isAllowedLaunchOptionsDisable(toolInstanceTypesMask)
-                    .showMetadata()
-                    .assertKeyNotPresent(attr[0][0])
-                    .assertKeysArePresent(attr[1][0], attr[2][0])
-                    .deleteKeys(attr[1][0])
-                    .selectKey(attr[2][0])
-                    .changeValue(attr[2][1] = format("%s_new", attr[2][1]))
-                    .changeKey(attr[2][0] = format("%s_new", attr[2][0]))
-                    .close()
-                    .addKeyWithValue(attr[3][0], attr[3][1])
-                    .ok();
-            navigationMenu()
-                    .settings()
-                    .switchToUserManagement()
-                    .switchToUsers()
-                    .searchUserEntry(admin.login)
-                    .openEditUserPopUp()
-                    .showMetadata()
-                    .assertKeysAreNotPresent(attr[0][0], attr[1][0])
-                    .assertKeysArePresent(attr[3][0], attr[2][0])
-                    .ok();
+        loginAsUser(admin);
+        navigationMenu()
+                .settings()
+                .switchToUserManagement()
+                .switchToUsers()
+                .searchUserEntry(admin.login)
+                .edit()
+                .getUserPermissionsTab()
+                .addNewGroup(testGroup2)
+                .selectByName(testGroup2)
+                .showPermissions()
+                .set(WRITE, ALLOW)
+                .savePermissions()
+                .closeAll();
+        setMiscMetadataSensitiveKeys(miscMetadataSensitiveKeysTestValue);
+        loginAsUser(user);
+        navigationMenu()
+                .settings()
+                .ensure(USER_MANAGEMENT_TAB, visible)
+                .switchToUserManagement()
+                .switchToUsers()
+                .searchUserEntry(admin.login)
+                .openEditUserPopUp()
+                .ensureNotVisible(IMPERSONATE)
+                .ensureDisable(DELETE, BLOCK)
+                .isListOfRolesBlocked()
+                .isAllowedLaunchOptionsDisable(toolInstanceTypesMask)
+                .showMetadata()
+                .assertKeyNotPresent(attr[0][0])
+                .assertKeysArePresent(attr[1][0], attr[2][0])
+                .deleteKeys(attr[1][0])
+                .selectKey(attr[2][0])
+                .changeValue(attr[2][1] = format("%s_new", attr[2][1]))
+                .changeKey(attr[2][0] = format("%s_new", attr[2][0]))
+                .close()
+                .addKeyWithValue(attr[3][0], attr[3][1])
+                .ok();
+        navigationMenu()
+                .settings()
+                .switchToUserManagement()
+                .switchToUsers()
+                .searchUserEntry(admin.login)
+                .openEditUserPopUp()
+                .showMetadata()
+                .assertKeysAreNotPresent(attr[0][0], attr[1][0])
+                .assertKeysArePresent(attr[3][0], attr[2][0])
+                .ok();
     }
 
     @Test(priority = 1, dependsOnMethods = "wtiteGrantPermissionsToUserAccount")
@@ -163,7 +162,7 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
                     .switchToUsers().searchUserEntry(admin.login)
                     .edit()
                     .getUserPermissionsTab()
-                    .selectByName(testGroup)
+                    .selectByName(testGroup2)
                     .showPermissions()
                     .set(EXECUTE, ALLOW)
                     .set(READ, INHERIT)
@@ -201,6 +200,7 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
     @TestCase(value = "3751_1")
     public void readGrantPermissionsToUserGroup() {
         groupPermissionsPreparations();
+        setMiscMetadataSensitiveKeys(initialMiscMetadataSensitiveKeys[0]);
         loginAsUser(user);
         EditGroupPopup editGroupPopup = navigationMenu()
                 .settings()
@@ -221,7 +221,21 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
     @Test(priority = 2, dependsOnMethods = "readGrantPermissionsToUserGroup")
     @TestCase(value = "3751_2")
     public void writeGrantPermissionsToUserGroup() {
-        groupPermissionsPreparations();
+        loginAsUser(admin);
+        navigationMenu()
+                .settings()
+                .switchToUserManagement()
+                .switchToGroups()
+                .searchGroupBySubstring(testGroup1)
+                .editGroup(testGroup1)
+                .getGroupPermissionsTab()
+                .addNewGroup(testGroup2)
+                .selectByName(testGroup2)
+                .showPermissions()
+                .set(WRITE, ALLOW)
+                .savePermissions()
+                .closeAll();
+        setMiscMetadataSensitiveKeys(miscMetadataSensitiveKeysTestValue);
         loginAsUser(user);
         EditGroupPopup editGroupPopup = navigationMenu()
                 .settings()
@@ -233,8 +247,23 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
                 .isListOfUsersBlocked()
                 .isAllowedLaunchOptionsDisable(toolInstanceTypesMask)
                 .showMetadata()
-                .assertKeysArePresent(attr[0][0], attr[1][0], attr[2][0])
-                .assertKeysAreDisabled(attr[0][0], attr[1][0], attr[2][0])
+                .assertKeyNotPresent(attr[0][0])
+                .assertKeysArePresent(attr[1][0], attr[2][0])
+                .deleteKeys(attr[1][0])
+                .selectKey(attr[2][0])
+                .changeValue(attr[2][1] = format("%s_new", attr[2][1]))
+                .changeKey(attr[2][0] = format("%s_new", attr[2][0]))
+                .close()
+                .addKeyWithValue(attr[3][0], attr[3][1])
+                .ok();
+        navigationMenu()
+                .settings()
+                .switchToUserManagement()
+                .switchToGroups()
+                .openEditGroupPopUp(testGroup1)
+                .showMetadata()
+                .assertKeysAreNotPresent(attr[0][0], attr[1][0])
+                .assertKeysArePresent(attr[3][0], attr[2][0])
                 .ok();
         editGroupPopup.ok();
     }
@@ -273,29 +302,24 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
     }
 
     private void cleanUpUserPermissions() {
-        try {
-            loginAsUser(admin);
-            UserEntry testUser = navigationMenu()
-                    .settings()
-                    .switchToUserManagement()
-                    .switchToUsers()
-                    .searchUserEntry(admin.login);
-            testUser
-                    .edit()
-                    .getUserPermissionsTab()
-                    .deleteIfPresent(user.login)
-                    .deleteIfPresent(testGroup)
-                    .closeAll();
-            testUser
-                    .edit()
-                    .addAllowedLaunchOptions(toolInstanceTypesMask, "")
-                    .showMetadata()
-                    .deleteKeys(attr[0][0], attr[1][0], attr[2][0], attr[3][0])
-                    .ok();
-        } finally {
-            loginAsUser(admin);
-            setMiscMetadataSensitiveKeys(initialMiscMetadataSensitiveKeys[0]);
-        }
+        loginAsUser(admin);
+        UserEntry testUser = navigationMenu()
+                .settings()
+                .switchToUserManagement()
+                .switchToUsers()
+                .searchUserEntry(admin.login);
+        testUser
+                .edit()
+                .getUserPermissionsTab()
+                .deleteIfPresent(user.login)
+                .deleteIfPresent(testGroup2)
+                .closeAll();
+        testUser
+                .edit()
+                .addAllowedLaunchOptions(toolInstanceTypesMask, "")
+                .showMetadata()
+                .deleteKeys(attr[0][0], attr[1][0], attr[2][0], attr[3][0])
+                .ok();
     }
 
     private void groupPermissionsPreparations() {
@@ -323,7 +347,7 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
                 .ok();
     }
 
-    private void createTestGroup(String groupName) {
+    private void createTestGroup(String groupName, Account account) {
         navigationMenu()
                 .settings()
                 .switchToUserManagement()
@@ -333,17 +357,8 @@ public class RBACPermissionTest extends AbstractBfxPipelineTest implements Autho
                 .create()
                 .searchGroupBySubstring(groupName)
                 .editGroup(groupName)
-                .addUserIfNonExist(admin.login)
+                .addUserIfNonExist(account.login)
                 .ok();
-    }
-
-    private void removeTestGroup(String groupName) {
-        navigationMenu()
-                .settings()
-                .switchToUserManagement()
-                .switchToGroups()
-                .searchGroupBySubstring(groupName)
-                .deleteGroupIfPresent(groupName);
     }
 
     private void setMiscMetadataSensitiveKeys(String value) {
