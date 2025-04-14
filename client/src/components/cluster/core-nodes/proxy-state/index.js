00f5f07add8bb9747760003e8ac8e012a7e680a7
AleksandrGorodetskii <gorod2004@gmail.com>
2025-04-14 13:39:16 +0300

    GUI Issue #3954 TinyProxy charts - lint fixes

--- client/src/components/cluster/core-nodes/proxy-state/index.js
+++ client/src/components/cluster/core-nodes/proxy-state/index.js
@@ -28,7 +28,7 @@ import ThemedReport from '../../../billing/reports/themed-report';
 import Filters from './components/filters';
 import {getDatasetOptions, getDatasetStyles, formatLabel} from './utils';
 import copyTextToClipboard from '../../../special/copy-text-to-clipboard';
-import { toJS } from 'mobx';
+import {toJS} from 'mobx';
 
 const FETCH_DELAY = 500;
 
