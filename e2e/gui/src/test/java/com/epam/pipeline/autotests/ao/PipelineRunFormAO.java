3b153a61bcc8ab92c5d7a730a8ef09e64520c7eb
maryvictol <Mariia_Tolstaia@epam.com>
2025-04-16 18:07:58 +0300

    Update after review

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/PipelineRunFormAO.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/ao/PipelineRunFormAO.java
@@ -55,9 +55,9 @@ public class PipelineRunFormAO implements AccessObject<PipelineRunFormAO> {
     public static final String DEFAULT_TYPE = C.DEFAULT_INSTANCE;
 
     private final Map<Primitive, SelenideElement> elements = initialiseElements(
-            entry(LAUNCH, context().find(byClassName("launch-pipeline-form__layout-header")).find(byText("Launch"))),
-            entry(PIPELINE, context().find(byClassName("launch-pipeline-form__layout-header")).find(byId("launch-form-pipeline-name"))),
-            entry(VERSION, context().find(byClassName("launch-pipeline-form__layout-header"))
+            entry(LAUNCH, context().find(byId("launch-pipeline-form-header")).find(byText("Launch"))),
+            entry(PIPELINE, context().find(byId("launch-form-pipeline-name"))),
+            entry(VERSION, context().find(byId("launch-pipeline-form-header"))
                     .find(byText("Version:")).parent().find(byClassName("ant-select-selection"))),
             entry(ESTIMATED_PRICE, $(byClassName("launch-pipeline-form__layout-header")).find(byText("Estimated price per hour:"))),
             entry(INFORMATION_ICON, $(byClassName("launch-pipeline-form__layout-header")).find(byClassName("launch-pipeline-form__hint"))),
@@ -450,10 +450,7 @@ public class PipelineRunFormAO implements AccessObject<PipelineRunFormAO> {
     }
 
     public PipelineRunFormAO checkPipelineVersion(final String expectedVersion) {
-        String actualVersion = get(VERSION).$("input").getAttribute("value");
-        assertTrue(actualVersion.equals(expectedVersion),
-                format("Pipeline version corresponds expected version '%s'",
-                        expectedVersion));
+        get(VERSION).$("input").should(attribute("value", expectedVersion));
         return this;
     }
 
