da1be21957d624967472f3d14aaf124a3166ac55
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2025-04-29 16:24:28 +0200

    Storage permissions restrictions (#3970)

--- client/src/components/pipelines/browser/forms/DataStorageEditDialog.js
+++ client/src/components/pipelines/browser/forms/DataStorageEditDialog.js
@@ -123,6 +123,34 @@ export class DataStorageEditDialog extends React.Component {
     return false;
   }
 
+  @computed
+  get permissionsRestrictions () {
+    const {
+      preferences,
+      authenticatedUserInfo
+    } = this.props;
+    const isAdmin = authenticatedUserInfo.loaded && authenticatedUserInfo.value && authenticatedUserInfo.value.admin;
+    if (preferences.loaded && !isAdmin) {
+      const restrictions = preferences.uiStoragesPermissionsRestrictions;
+      const defaultMask = restrictions.map((rule) => ({
+        role: rule.role,
+        mask: rule.defaultMask
+      }));
+      const enabledMask = restrictions.map((rule) => ({
+        role: rule.role,
+        mask: rule.enabledMask
+      }));
+      return {
+        defaultMask,
+        enabledMask
+      };
+    }
+    return {
+      defaultMask: [],
+      enabledMask: []
+    };
+  }
+
   openDeleteDialog = () => {
     this.setState({deleteDialogVisible: true});
   };
@@ -457,6 +485,9 @@ export class DataStorageEditDialog extends React.Component {
       this.setState({activeTab: 'info'});
     };
     const skipPolicyFlagVisible = !this.props.dataStorage;
+
+    const {defaultMask, enabledMask} = this.permissionsRestrictions;
+
     return (
       <Modal
         maskClosable={!this.props.pending && !this.state.restrictedAccessCheckInProgress}
@@ -790,7 +821,10 @@ export class DataStorageEditDialog extends React.Component {
                 <PermissionsForm
                   readonly={isReadOnly}
                   objectIdentifier={this.props.dataStorage.id}
-                  objectType="DATA_STORAGE" />
+                  objectType="DATA_STORAGE"
+                  defaultMask={defaultMask}
+                  enabledMask={enabledMask}
+                />
               </Tabs.TabPane>
             }
             {this.transitionRulesAvailable && (
