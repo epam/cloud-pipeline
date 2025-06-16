f064df722b68be5388f3a6c3036ab65091cc0eef
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2025-06-16 03:05:34 +0300

    no message

--- deploy/github_actions/gha_pack_dist.sh
+++ deploy/github_actions/gha_pack_dist.sh
@@ -81,6 +81,7 @@ mv pipe-cli/dist/dist-folder/pipe.tar.gz ${API_STATIC_PATH}/pipe-el6.tar.gz
 ./gradlew distTar   -PbuildNumber=${CLOUD_PIPELINE_BUILD_NUMBER}.${GITHUB_SHA} \
                     -Pprofile=release \
                     -x test \
+                    -x :pipe-cli:buildLinux \
                     -Pfast \
                     --no-daemon
 
