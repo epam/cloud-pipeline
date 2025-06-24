e9253c6faed8e90b32961d9e0d0a88a6727ea429
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2025-06-24 11:42:31 +0200

    Cloud Pipeline chatbot - prompts fix

--- deploy/docker/cp-ai/cp_ai/agents/launch/tools.py
+++ deploy/docker/cp-ai/cp_ai/agents/launch/tools.py
@@ -154,9 +154,20 @@ def _fulfill_parameters_based_on_user_query(
 
     # sometimes model responds with {"parameters": {...}} format
     parsed = reformat('parameters', parsed)
-    result = parsed
-    if params is not None:
-        result.update(params)
+    result = {}
+    if parsed is not None and isinstance(parsed, dict):
+        for p, v in parsed.items():
+            if v is not None and isinstance(v, dict):
+                v = v.get('value', None)
+            if v is not None:
+                result.update({p: v})
+    if params is not None and isinstance(params, dict):
+        for p, v in params.items():
+            if v is not None and isinstance(v, dict):
+                v = v.get('value', None)
+            if v is not None:
+                result.update({p: v})
+    result = {v: p for v, p in result.items() if p not in {'instance_size', 'instance_disk', 'disk', 'instance_type'}}
     agents_logger.info(f'fulfilled parameters: {repr(result)}')
     return result
 
