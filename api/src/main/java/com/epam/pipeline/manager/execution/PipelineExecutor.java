276fb944d85bc96d076e72fd2f52863a939132e4
Mariia_Zueva <mariia_zueva@epam.com>
2025-06-30 11:02:17 +0200

    Allow to configure container resources and limits for runs

--- api/src/main/java/com/epam/pipeline/manager/execution/PipelineExecutor.java
+++ api/src/main/java/com/epam/pipeline/manager/execution/PipelineExecutor.java
@@ -29,6 +29,7 @@ import com.epam.pipeline.manager.cluster.KubernetesConstants;
 import com.epam.pipeline.manager.cluster.KubernetesManager;
 import com.epam.pipeline.manager.cluster.container.ContainerMemoryResourceService;
 import com.epam.pipeline.manager.cluster.container.ContainerResources;
+import com.epam.pipeline.manager.cluster.container.ResourcesParameter;
 import com.epam.pipeline.manager.contextual.ContextualPreferenceManager;
 import com.epam.pipeline.manager.preference.PreferenceManager;
 import com.epam.pipeline.manager.preference.SystemPreferences;
@@ -267,12 +268,22 @@ public class PipelineExecutor {
         spec.setDnsConfig(podDNSConfig);
     }
 
-    private boolean isParameterEnabled(List<EnvVar> envVars, String parameter) {
-        return ListUtils.emptyIfNull(envVars)
-                .stream()
-                .anyMatch(env -> parameter.equals(env.getName()) && TRUE.equals(env.getValue()));
+    private boolean isParameterEnabled(final List<EnvVar> envVars, final String name) {
+        return isParameterEqual(envVars, name, TRUE);
     }
 
+    private boolean isParameterEqual(final List<EnvVar> envVars, final String name, final String value) {
+        return getParameter(envVars, name)
+                .map(value::equals)
+                .orElse(false);
+    }
+
+    private Optional<String> getParameter(final List<EnvVar> envVars, final String name) {
+        return ListUtils.emptyIfNull(envVars).stream()
+                .filter(var -> name.equals(var.getName()))
+                .map(EnvVar::getValue)
+                .findFirst();
+    }
 
     private Container getContainer(final PipelineRun run,
                                    final List<EnvVar> envVars,
@@ -319,9 +330,24 @@ public class PipelineExecutor {
     }
 
     private ContainerResources buildResources(final PipelineRun run, final List<EnvVar> envVars) {
-        return ContainerResources.merge(
+        final ContainerResources defaultResources = ContainerResources.merge(
                 buildCpuResources(run, envVars),
                 buildMemoryResources(run, envVars));
+        final Map<String, ResourcesParameter> mapping =
+                preferenceManager.getPreference(SystemPreferences.LAUNCH_CONTAINER_REQUESTS_MAPPING);
+        MapUtils.emptyIfNull(mapping).forEach((parameter, resource) -> {
+            getParameter(envVars, parameter)
+                    .filter(StringUtils::isNotBlank)
+                    .ifPresent(value -> {
+                        if (resource.isLimits()) {
+                            defaultResources.getLimits().put(resource.getName(), new Quantity(value));
+                        }
+                        if (resource.isRequests()) {
+                            defaultResources.getRequests().put(resource.getName(), new Quantity(value));
+                        }
+                    });
+        });
+        return defaultResources;
     }
 
     private ContainerResources buildMemoryResources(final PipelineRun run, final List<EnvVar> envVars) {
