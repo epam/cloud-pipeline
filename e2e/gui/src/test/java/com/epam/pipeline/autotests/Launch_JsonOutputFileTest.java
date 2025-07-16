18d3ffc8cdc6610b5902ca656ebfec0b6125adbc
maryvictol <Mariia_Tolstaia@epam.com>
2025-07-16 22:38:04 +0300

    Update Launch_JsonOutputFileTest

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/Launch_JsonOutputFileTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/Launch_JsonOutputFileTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,8 @@ package com.epam.pipeline.autotests;
 import com.epam.pipeline.autotests.ao.ParameterType;
 import com.epam.pipeline.autotests.ao.Template;
 import com.epam.pipeline.autotests.utils.*;
+import static com.epam.pipeline.autotests.utils.C.DEFAULT_INSTANCE;
+import static com.epam.pipeline.autotests.utils.C.DEFAULT_INSTANCE_PRICE_TYPE;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -28,65 +30,67 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Launch_JsonOutputFileTest extends AbstractAutoRemovingPipelineRunningTest {
 
-    private static final String LAUNCH_SCRIPT = "/fileKeeper.sh";
-    private static final String CONFIG_JSON = "config.json";
-    private static final String STORAGE_RULES_FOLDER = "storage_rules_folder";
-    private static final String FILE_TO_STORE_NAME = "storage_rules_test.test";
+  private static final String LAUNCH_SCRIPT = "/fileKeeper.sh";
+  private static final String CONFIG_JSON = "config.json";
+  private static final String STORAGE_RULES_FOLDER = "storage_rules_folder";
+  private static final String FILE_TO_STORE_NAME = "storage_rules_test.test";
 
-    private final String storage = "epmcmbi-test-storage-360-" + Utils.randomSuffix();
+  private final String storage = "epmcmbi-test-storage-360-" + Utils.randomSuffix();
 
-    @BeforeClass
-    public void createPipeline() {
-        navigationMenu()
-            .library()
-            .createStorage(storage)
-            .createPipeline(Template.SHELL, getPipelineName());
-    }
+  @BeforeClass
+  public void createPipeline() {
+    navigationMenu()
+      .library()
+      .createStorage(storage)
+      .createPipeline(Template.SHELL, getPipelineName());
+  }
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() {
-        navigationMenu()
-            .library()
-            .removeStorage(storage);
-    }
+  @AfterClass(alwaysRun = true)
+  public void tearDown() {
+    navigationMenu()
+      .library()
+      .removeStorage(storage);
+  }
 
-    @Test
-    @TestCase("EPMCMBIBPC-360")
-    public void launchPipelineWithJSONParameter() {
-        final String pathToFile = String.format("%s://%s/%s/%s/", C.STORAGE_PREFIX, storage, STORAGE_RULES_FOLDER,
-                getPipelineName());
-        final String pipelineScript = Utils.getFileNameFromPipelineName(getPipelineName(), "sh");
-        final String parameterName = "result";
-        navigationMenu()
-            .library()
-            .clickOnPipeline(getPipelineName())
-            .firstVersion()
-            .codeTab()
-            .clickOnFile(pipelineScript)
-            .editFile(code -> Utils.readResourceFully(LAUNCH_SCRIPT))
-            .saveAndCommitWithMessage("test: Replace script with custom one")
-            .sleep(3, SECONDS)
-            .clickOnFile(CONFIG_JSON)
-            .sleep(1, SECONDS)
-            .editFile(transferringJsonToObject(profiles -> {
-                final ConfigurationProfile profile = selectProfileWithName("default", profiles);
-                profile.configuration.parameters.put(parameterName, Parameter.required("output", pathToFile));
-                return profiles;
-            }))
-            .saveAndCommitWithMessage("test: Add required output parameter named result")
-            .runPipeline()
-            .validateThereIsParameterOfType(parameterName, pathToFile, ParameterType.OUTPUT, true)
-            .waitUntilLaunchButtonAppear()
-            .launchAndWaitUntilFinished(this);
-    }
+  @Test
+  @TestCase("EPMCMBIBPC-360")
+  public void launchPipelineWithJSONParameter() {
+    final String pathToFile = String.format("%s://%s/%s/%s/", C.STORAGE_PREFIX, storage, STORAGE_RULES_FOLDER,
+        getPipelineName());
+    final String pipelineScript = Utils.getFileNameFromPipelineName(getPipelineName(), "sh");
+    final String parameterName = "result";
+    navigationMenu()
+      .library()
+      .clickOnPipeline(getPipelineName())
+      .firstVersion()
+      .codeTab()
+      .clickOnFile(pipelineScript)
+      .editFile(code -> Utils.readResourceFully(LAUNCH_SCRIPT))
+      .saveAndCommitWithMessage("test: Replace script with custom one")
+      .sleep(3, SECONDS)
+      .clickOnFile(CONFIG_JSON)
+      .sleep(1, SECONDS)
+      .editFile(transferringJsonToObject(profiles -> {
+        final ConfigurationProfile profile = selectProfileWithName("default", profiles);
+        profile.configuration.parameters.put(parameterName, Parameter.required("output", pathToFile));
+        return profiles;
+      }))
+      .saveAndCommitWithMessage("test: Add required output parameter named result")
+      .runPipeline()
+      .setTypeValue(DEFAULT_INSTANCE)
+      .setPriceType(DEFAULT_INSTANCE_PRICE_TYPE)
+      .validateThereIsParameterOfType(parameterName, pathToFile, ParameterType.OUTPUT, true)
+      .waitUntilLaunchButtonAppear()
+      .launchAndWaitUntilFinished(this);
+  }
 
-    @Test(dependsOnMethods = {"launchPipelineWithJSONParameter"})
-    @TestCase("EPMCMBIBPC-360")
-    public void validateOutputRun() {
-        navigationMenu()
-            .library()
-            .selectStorage(storage)
-            .navigateUsingAddressBar(String.format("%s/%s", STORAGE_RULES_FOLDER, getPipelineName()))
-            .validateElementIsPresent(FILE_TO_STORE_NAME);
-    }
+  @Test(dependsOnMethods = {"launchPipelineWithJSONParameter"})
+  @TestCase("EPMCMBIBPC-360")
+  public void validateOutputRun() {
+    navigationMenu()
+      .library()
+      .selectStorage(storage)
+      .navigateUsingAddressBar(String.format("%s/%s", STORAGE_RULES_FOLDER, getPipelineName()))
+      .validateElementIsPresent(FILE_TO_STORE_NAME);
+  }
 }
