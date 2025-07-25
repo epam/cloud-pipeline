bd456b68793432c4f5b23881d4fd3279aa390e1f
maryvictol <Mariia_Tolstaia@epam.com>
2025-07-25 12:15:07 +0300

    Change scaling_coeff

--- e2e/gui/src/test/java/com/epam/pipeline/autotests/LaunchParametersTest.java
+++ e2e/gui/src/test/java/com/epam/pipeline/autotests/LaunchParametersTest.java
@@ -155,6 +155,10 @@ public class LaunchParametersTest extends AbstractSeveralPipelineRunningTest
                 .searchUserEntry(userWithoutCompletedRuns.login)
                 .edit()
                 .deleteRoleOrGroupIfExist(USER_ROLE)
+                .ok()
+                .searchUserEntry(user.login)
+                .edit()
+                .addRoleOrGroupIfNonExist(USER_ROLE)
                 .ok();
     }
 
@@ -691,7 +695,7 @@ public class LaunchParametersTest extends AbstractSeveralPipelineRunningTest
         for(int i = 0; i < 3; i++) {
             sizeDisk[i+1] = (int) Math.floor(sizeDisk[i] * SCALING_COEFF);
         }
-        String bigFileCommand = format("fallocate -l %sG test.big", scaling[2] - 1.5);
+        String bigFileCommand = format("fallocate -l %sG test.big", scaling[2] - 2);
         shell.execute(bigFileCommand)
                 .assertNextStringIsVisible(bigFileCommand, rootHost);
     }
