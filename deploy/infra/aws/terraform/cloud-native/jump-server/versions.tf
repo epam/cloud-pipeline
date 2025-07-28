44970bb4387221d50c30522c241efffabcbbd917
Mariia Zueva <mariia_zueva@epam.com>
2025-07-28 20:30:19 +0200

    Update versions.tf

--- deploy/infra/aws/terraform/cloud-native/jump-server/versions.tf
+++ deploy/infra/aws/terraform/cloud-native/jump-server/versions.tf
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.30.0"
+      version = "5.38.0"
     }
     external = {
       source  = "hashicorp/external"
