352c991a4d4cba994a508b30ff647d6640b8fe7c
Pavel Silin <pavel_silin@epam.com>
2025-10-07 10:47:07 +0200

    issue #4102 stop serverless api run by runId + ignore unknown fields in ServiceUrlVO

--- api/src/main/java/com/epam/pipeline/controller/vo/ServiceUrlVO.java
+++ api/src/main/java/com/epam/pipeline/controller/vo/ServiceUrlVO.java
@@ -16,9 +16,11 @@
 
 package com.epam.pipeline.controller.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceUrlVO {
 
     private final String name;
