c9934efe4160291840f2573e9b87a3bc109dcff9
AleksandrGorodetskii <gorod2004@gmail.com>
2025-11-07 13:09:59 +0300

    GUI Issue #4120: Azure storages file download is not available - fix

--- client/src/components/pipelines/browser/data-storage/index.js
+++ client/src/components/pipelines/browser/data-storage/index.js
@@ -592,6 +592,10 @@ export default class DataStorage extends React.Component {
     if (!info) {
       return [];
     }
+    const isAzure = /^az$/i.test(
+      this.storage.info.storageType ||
+      this.storage.info.type
+    );
     const {sensitive} = info;
     const items = [];
     const documentPreviewAvailable = (item) => {
@@ -665,7 +669,9 @@ export default class DataStorage extends React.Component {
     };
     items.push(...elements.map(i => {
       const restored = (this.getRestoredStatus(i) || {}).status === STATUS.SUCCEEDED;
-      const archived = i.labels && !isStandardClass(i.labels['StorageClass']);
+      const archived = isAzure
+        ? false
+        : i.labels && !isStandardClass(i.labels['StorageClass']);
       const active = i.labels && isActiveClass(i.labels['StorageClass']);
       const type = i.type.toLowerCase();
       const isDownloadable = !this.isOmicsStore
