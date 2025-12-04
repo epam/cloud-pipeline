735bedb925c40feb54ddf1fdf3df1b0675851301
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2025-12-04 15:04:13 +0400

    cleanups

--- elasticsearch-agent/src/main/java/com/epam/pipeline/elasticsearchagent/service/EventToRequestConverter.java
+++ elasticsearch-agent/src/main/java/com/epam/pipeline/elasticsearchagent/service/EventToRequestConverter.java
@@ -18,23 +18,17 @@ package com.epam.pipeline.elasticsearchagent.service;
 import com.epam.pipeline.elasticsearch.ElasticStackVersion;
 import com.epam.pipeline.elasticsearch.model.DeleteRequest;
 import com.epam.pipeline.elasticsearch.model.DocWriteRequest;
-import com.epam.pipeline.elasticsearch.model.XContentBuilder;
 import com.epam.pipeline.elasticsearchagent.model.PipelineEvent;
-import com.epam.pipeline.entity.user.PipelineUser;
-import org.springframework.util.CollectionUtils;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public interface EventToRequestConverter {
 
     String INDEX_TYPE = "_doc";
     String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
     SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_PATTERN);
-    String ATTRIBUTE_NAME = "Name";
 
     String getIndexPrefix();
     String getIndexName();
@@ -56,20 +50,6 @@ public interface EventToRequestConverter {
             return null;
         }
         return SIMPLE_DATE_FORMAT.format(date);
-    }
-
-    default XContentBuilder buildUserContent(final PipelineUser user,
-                                             final XContentBuilder jsonBuilder) throws IOException {
-        if (user != null) {
-            Map<String, String> attributes = user.getAttributes();
-            jsonBuilder
-                    .field("ownerUserId", user.getId())
-                    .field("ownerUserName", user.getUserName())
-                    .field("ownerFriendlyName",
-                            CollectionUtils.isEmpty(attributes) ? null : attributes.get(ATTRIBUTE_NAME))
-                    .field("ownerGroups", user.getGroups());
-        }
-        return jsonBuilder;
     }
 
 }
