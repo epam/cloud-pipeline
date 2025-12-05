30f7ed01b416d4a10a7842a8c42ab43c79d70d27
Pavel Silin <pavel_silin@epam.com>
2025-12-05 11:55:23 +0100

    issue #4201 Fix problem with deleting user or role if cloud profile is assigned (#4203)
    (cherry picked from commit 8518bb60c8c2f8740e5497c7611ca4ce93a03510)

--- api/src/main/java/com/epam/pipeline/manager/cloud/credentials/CloudProfileCredentialsManagerProvider.java
+++ api/src/main/java/com/epam/pipeline/manager/cloud/credentials/CloudProfileCredentialsManagerProvider.java
@@ -119,10 +119,9 @@ public class CloudProfileCredentialsManagerProvider {
         final List<CloudProfileCredentialsEntity> profiles = SetUtils.emptyIfNull(profileIds).stream()
                 .map(this::findEntity)
                 .collect(Collectors.toList());
-        if (principal) {
-            return toDtos(assignProfilesToUser(profiles, sidId, defaultProfileId).getCloudProfiles());
-        }
-        return toDtos(assignProfilesToRole(sidId, defaultProfileId, profiles).getCloudProfiles());
+        return principal
+                ? toDtos(assignProfilesToUser(profiles, sidId, defaultProfileId).getCloudProfiles())
+                : toDtos(assignProfilesToRole(sidId, defaultProfileId, profiles).getCloudProfiles());
     }
 
     public TemporaryCredentials generateProfileCredentials(final Long profileId, final Long regionId) {
