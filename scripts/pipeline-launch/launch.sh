92a3f60387e178ebde26698ca1d2fa9ee0ca8598
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2025-12-16 13:29:03 +0300

    Change deb packages verfification procedure to support 20.04+

--- scripts/pipeline-launch/launch.sh
+++ scripts/pipeline-launch/launch.sh
@@ -348,7 +348,7 @@ function check_package_installed {
       fi
 
       if check_installed "dpkg"; then
-            dpkg -q "$_PACKAGE_TO_CHECK" &> /dev/null
+            dpkg -S "$_PACKAGE_TO_CHECK" &> /dev/null
             return $?
       elif check_installed "rpm"; then
             rpm -q "$_PACKAGE_TO_CHECK"  &> /dev/null
