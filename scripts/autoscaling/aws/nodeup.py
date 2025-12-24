e398293d94943ba6e7020e14bc8ded94f5eeb443
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2025-12-24 22:46:57 +0300

    Do not rely on condition index when checking if kube node is ready
    (cherry picked from commit ada82e15bb3ef634b04817a718a809d8de048033)

--- scripts/autoscaling/aws/nodeup.py
+++ scripts/autoscaling/aws/nodeup.py
@@ -953,6 +953,11 @@ def verify_run_id(ec2, run_id):
         pipe_log('No existing instance found for RunID {}\n-'.format(run_id))
     return ins_id, ins_ip
 
+def is_node_ready(node_conditions):
+    for condition in node_conditions:
+        if condition.get("type") == u"Ready":
+            return condition.get("status") == u"True"
+    return False
 
 def verify_regnode(ec2, ins_id, num_rep, time_rep, run_id, api):
     nodenames = get_possible_kube_node_names(ec2, ins_id)
@@ -976,8 +981,8 @@ def verify_regnode(ec2, ins_id, num_rep, time_rep, run_id, api):
         rep = 0
         while rep <= num_rep:
             node = pykube.Node.objects(api).filter(field_selector={'metadata.name': ret_namenode})
-            status = node.response['items'][0]['status']['conditions'][3]['status']
-            if status == u'True':
+            node_conditions = node.response['items'][0]['status']['conditions']
+            if is_node_ready(node_conditions):
                 pipe_log('- Node ({}) status is READY'.format(ret_namenode))
                 break
             rep = increment_or_fail(num_rep, rep,
