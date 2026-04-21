9928ea840b970ae1c91eb5b23359cf2efa0d82bb
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2026-04-21 17:43:06 +0400

    Support file system autoscaling for capacity blocks - support --instance-type for aws provider only

--- scripts/autoscaling/init_multicloud.sh
+++ scripts/autoscaling/init_multicloud.sh
@@ -351,7 +351,7 @@ RestartSec=10
 Environment="API_ARGS=--api-url $_API_URL --api-token $_API_TOKEN"
 Environment="NODE_ARGS=--node-name $_KUBE_NODE_NAME"
 Environment="MOUNT_POINT_ARGS=--mount-point $_MOUNT_POINT"
-Environment="INSTANCE_TYPE_ARGS=--instance-type $_CLOUD_INSTANCE_TYPE"
+Environment="INSTANCE_TYPE_ARGS=$( [[ $cloud == *"EC2"* ]] && echo "--instance-type $_CLOUD_INSTANCE_TYPE" )"
 ExecStart=/usr/bin/fsautoscale \$API_ARGS \$NODE_ARGS \$MOUNT_POINT_ARGS \$INSTANCE_TYPE_ARGS
 
 [Install]
