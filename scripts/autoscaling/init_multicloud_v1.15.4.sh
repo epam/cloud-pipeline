23f921b86a8c47b2722614b59617b22014da8fc7
Ekaterina Kazachkova <ekaterina_kazachkova@epam.com>
2026-04-21 14:26:07 +0400

    Support file system autoscaling for capacity blocks - init cloud scripts

--- scripts/autoscaling/init_multicloud_v1.15.4.sh
+++ scripts/autoscaling/init_multicloud_v1.15.4.sh
@@ -534,7 +534,8 @@ RestartSec=10
 Environment="API_ARGS=--api-url $_API_URL --api-token $_API_TOKEN"
 Environment="NODE_ARGS=--node-name $_KUBE_NODE_NAME"
 Environment="MOUNT_POINT_ARGS=--mount-point $_MOUNT_POINT"
-ExecStart=/usr/bin/fsautoscale \$API_ARGS \$NODE_ARGS \$MOUNT_POINT_ARGS
+Environment="INSTANCE_TYPE_ARGS=--instance-type $_CLOUD_INSTANCE_TYPE"
+ExecStart=/usr/bin/fsautoscale \$API_ARGS \$NODE_ARGS \$MOUNT_POINT_ARGS \$INSTANCE_TYPE_ARGS
 
 [Install]
 WantedBy=multi-user.target
